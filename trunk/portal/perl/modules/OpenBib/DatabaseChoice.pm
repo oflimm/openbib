1105a083a1e5d2013fdf32a46acd1a3d7b39312a
flimm <flimm@e7c75b2f-8ac8-f892-83e0-76010f6c4c37>
2005-09-04 06:58:44 +0000

    - Ueberarbeitung gemaess PBP-Coding-Standards   * Indentation   * Subroutine argument passing   * Reference Variables   * Uncuddled elses   * Vertical alignment   * Assignments   * Fat Commas

--- trunk/portal/perl/modules/OpenBib/DatabaseChoice.pm
+++ trunk/portal/perl/modules/OpenBib/DatabaseChoice.pm
@@ -29,22 +29,17 @@
 
 package OpenBib::DatabaseChoice;
 
-use Apache::Constants qw(:common);
-
 use strict;
 use warnings;
 no warnings 'redefine';
 
-use Apache::Request();      # CGI-Handling (or require)
-
-use Log::Log4perl qw(get_logger :levels);
-
+use Apache::Constants qw(:common);
+use Apache::Request ();
 use DBI;
-
+use Log::Log4perl qw(get_logger :levels);
 use Template;
 
 use OpenBib::Common::Util;
-
 use OpenBib::Config;
 
 # Importieren der Konfigurationsdaten als Globale Variablen
@@ -55,233 +50,210 @@ use vars qw(%config);
 *config=\%OpenBib::Config::config;
 
 sub handler {
-  my $r=shift;
+    my $r=shift;
   
-  # Log4perl logger erzeugen
+    # Log4perl logger erzeugen
+    my $logger = get_logger();
 
-  my $logger = get_logger();
+    my $query=Apache::Request->new($r);
 
-  my $query=Apache::Request->new($r);
+    my $status=$query->parse;
 
-  my $status=$query->parse;
+    if ($status) {
+        $logger->error("Cannot parse Arguments - ".$query->notes("error-notes"));
+    }
 
-  if ($status){
-    $logger->error("Cannot parse Arguments - ".$query->notes("error-notes"));
-  }
-
-  my $stylesheet=OpenBib::Common::Util::get_css_by_browsertype($r);
+    my $stylesheet=OpenBib::Common::Util::get_css_by_browsertype($r);
   
-  my $sessionID=($query->param('sessionID'))?$query->param('sessionID'):'';
-  my @databases=($query->param('database'))?$query->param('database'):();
-  my $singleidn=$query->param('singleidn') || '';
-
-  my $action=($query->param('action'))?$query->param('action'):'';
+    # CGI-Uebergabe
+    my $sessionID = ($query->param('sessionID'))?$query->param('sessionID'):'';
+    my @databases = ($query->param('database'))?$query->param('database'):();
+    my $singleidn = $query->param('singleidn') || '';
+    my $action    = ($query->param('action'))?$query->param('action'):'';
+    my $verf      = $query->param('verf')      || '';
+    my $hst       = $query->param('hst')       || '';
+    my $swt       = $query->param('swt')       || '';
+    my $kor       = $query->param('kor')       || '';
+    my $sign      = $query->param('sign')      || '';
+    my $isbn      = $query->param('isbn')      || '';
+    my $issn      = $query->param('issn')      || '';
+    my $notation  = $query->param('notation')  || '';
+    my $ejahr     = $query->param('ejahr')     || '';
+    my $queryid   = $query->param('queryid')   || '';
   
-  # CGI-Uebergabe
+    my %checkeddb;
   
-  my $verf=$query->param('verf') || '';
-  my $hst=$query->param('hst') || '';
-  my $swt=$query->param('swt') || '';
-  my $kor=$query->param('kor') || '';
-  my $sign=$query->param('sign') || '';
-  my $isbn=$query->param('isbn') || '';
-  my $issn=$query->param('issn') || '';
-  my $notation=$query->param('notation') || '';
-  my $ejahr=$query->param('ejahr') || '';
-  my $bool1=$query->param('bool1') || '';
-  my $bool2=$query->param('bool2') || '';
-  my $bool3=$query->param('bool3') || '';
-  my $bool4=$query->param('bool4') || '';
-  my $bool5=$query->param('bool5') || '';
-  my $bool6=$query->param('bool6') || '';
-  my $bool7=$query->param('bool7') || '';
-  my $queryid=$query->param('queryid') || '';
+    #####################################################################
+    # Verbindung zur SQL-Datenbank herstellen
   
-  my %checkeddb;
+    my $sessiondbh
+        = DBI->connect("DBI:$config{dbimodule}:dbname=$config{sessiondbname};host=$config{sessiondbhost};port=$config{sessiondbport}", $config{sessiondbuser}, $config{sessiondbpasswd})
+            or $logger->error_die($DBI::errstr);
   
-  #####################################################################
-  # Verbindung zur SQL-Datenbank herstellen
+    my $userdbh
+        = DBI->connect("DBI:$config{dbimodule}:dbname=$config{userdbname};host=$config{userdbhost};port=$config{userdbport}", $config{userdbuser}, $config{userdbpasswd})
+            or $logger->error_die($DBI::errstr);
   
-  my $sessiondbh=DBI->connect("DBI:$config{dbimodule}:dbname=$config{sessiondbname};host=$config{sessiondbhost};port=$config{sessiondbport}", $config{sessiondbuser}, $config{sessiondbpasswd}) or $logger->error_die($DBI::errstr);
-  
-  my $userdbh=DBI->connect("DBI:$config{dbimodule}:dbname=$config{userdbname};host=$config{userdbhost};port=$config{userdbport}", $config{userdbuser}, $config{userdbpasswd}) or $logger->error_die($DBI::errstr);
-  
-  unless (OpenBib::Common::Util::session_is_valid($sessiondbh,$sessionID)){
-    OpenBib::Common::Util::print_warning("Ung&uuml;ltige Session",$r);
+    unless (OpenBib::Common::Util::session_is_valid($sessiondbh,$sessionID)){
+        OpenBib::Common::Util::print_warning("Ung&uuml;ltige Session",$r);
       
-    $sessiondbh->disconnect();
-    $userdbh->disconnect();
+        $sessiondbh->disconnect();
+        $userdbh->disconnect();
       
-    return OK;
-  }
+        return OK;
+    }
 
-  my $view="";
+    my $view="";
 
-  if ($query->param('view')){
-      $view=$query->param('view');
-  }
-  else {
-    $view=OpenBib::Common::Util::get_viewname_of_session($sessiondbh,$sessionID);
-  }
-    
-  my $userid=OpenBib::Common::Util::get_userid_of_session($userdbh,$sessionID);
-
-  my $idnresult="";
-  
-  if ($sessionID ne ""){
-    
-    # Wenn Kataloge ausgewaehlt wurden
-    if ($action eq "Kataloge auswählen"){
-      
-      # Zuerst die bestehende Auswahl loeschen
-      
-      $idnresult=$sessiondbh->prepare("delete from dbchoice where sessionid = ?") or $logger->error($DBI::errstr);
-      $idnresult->execute($sessionID) or $logger->error($DBI::errstr);
-      
-      # Wenn es eine neue Auswahl gibt, dann wird diese eingetragen
-      my $database;
-      foreach $database (@databases){
-	$idnresult=$sessiondbh->prepare("insert into dbchoice (sessionid,dbname) values (?,?)") or $logger->error($DBI::errstr);
-	$idnresult->execute($sessionID,$database) or $logger->error($DBI::errstr);
-      }
-
-      # Neue Datenbankauswahl ist voreingestellt
-
-      $idnresult=$sessiondbh->prepare("delete from sessionprofile where sessionid = ? ") or $logger->error($DBI::errstr);
-      $idnresult->execute($sessionID) or $logger->error($DBI::errstr);
-
-      $idnresult=$sessiondbh->prepare("insert into sessionprofile values (?,'dbauswahl') ") or $logger->error($DBI::errstr);
-      $idnresult->execute($sessionID) or $logger->error($DBI::errstr);
-    
-      $idnresult->finish();
-      
-      $r->internal_redirect("http://$config{servername}$config{searchframe_loc}?sessionID=$sessionID&view=$view");
-      
+    if ($query->param('view')) {
+        $view=$query->param('view');
+    }
+    else {
+        $view=OpenBib::Common::Util::get_viewname_of_session($sessiondbh,$sessionID);
     }
     
-    # ... sonst anzeigen
-    else {
-      $idnresult=$sessiondbh->prepare("select dbname from dbchoice where sessionid = ?") or $logger->error($DBI::errstr);
-      $idnresult->execute($sessionID) or $logger->error($DBI::errstr);
-      while (my $result=$idnresult->fetchrow_hashref()){
-	my $dbname=$result->{'dbname'};
-	$checkeddb{$dbname}="checked=\"checked\"";
-      }
-      $idnresult->finish();
+    my $userid=OpenBib::Common::Util::get_userid_of_session($userdbh,$sessionID);
 
-      my $lastcategory="";
-      my $count=0;
-
-      my $maxcolumn=$config{databasechoice_maxcolumn};
+    my $idnresult="";
+  
+    # Wenn Kataloge ausgewaehlt wurden
+    if ($action eq "Kataloge auswählen") {
+        # Zuerst die bestehende Auswahl loeschen
       
-      my %stype;
-	    
-      $idnresult=$sessiondbh->prepare("select * from dbinfo where active=1 order by faculty ASC, description ASC") or $logger->error($DBI::errstr);
-      $idnresult->execute() or $logger->error($DBI::errstr);
-
-      my @catdb=();
-
-      while (my $result=$idnresult->fetchrow_hashref){
-	my $category=$result->{'faculty'};
-	my $name=$result->{'description'};
-	my $systemtype=$result->{'system'};
-	my $pool=$result->{'dbname'};
-	my $url=$result->{'url'};
-	my $sigel=$result->{'sigel'};
-	
-	my $rcolumn;
-
-	if ($systemtype eq "a"){
-	  $stype{$pool}="yellow";
-	}
-	elsif ($systemtype eq "b"){
-	  $stype{$pool}="red";
-	}
-	elsif ($systemtype eq "l"){
-	  $stype{$pool}="green";
-	}
-	elsif ($systemtype eq "s"){
-	  $stype{$pool}="blue";
-	}
-
-	if ($category ne $lastcategory){
-	  while ($count % $maxcolumn != 0){
-
-	    $rcolumn=($count % $maxcolumn)+1;
-	    # 'Leereintrag erzeugen'
-	    push @catdb, { 
-			  column => $rcolumn, 
-			  category => $lastcategory,
-			  db => '',
-			  name => '',
-			  systemtype => '',
-			  sigel => '',
-			  url => '',
-			 };
-	    
-	    $count++;
-	  }
-
-	  $count=0;
-	}
-
-	$lastcategory=$category;
-
-	$rcolumn=($count % $maxcolumn)+1;
-
-	my $checked="";
-	if (defined $checkeddb{$pool}){
-          $checked="checked=\"checked\"";
+        $idnresult=$sessiondbh->prepare("delete from dbchoice where sessionid = ?") or $logger->error($DBI::errstr);
+        $idnresult->execute($sessionID) or $logger->error($DBI::errstr);
+      
+        # Wenn es eine neue Auswahl gibt, dann wird diese eingetragen
+        foreach my $database (@databases) {
+            $idnresult=$sessiondbh->prepare("insert into dbchoice (sessionid,dbname) values (?,?)") or $logger->error($DBI::errstr);
+            $idnresult->execute($sessionID,$database) or $logger->error($DBI::errstr);
         }
 
-	push @catdb, { 
-		      column => $rcolumn,
-		      category => $category,
-		      db => $pool,
-		      name => $name,
-		      systemtype => $stype{$pool},
-		      sigel => $sigel,
-		      url => $url,
-		      checked => $checked,
-		     };
+        # Neue Datenbankauswahl ist voreingestellt
+        $idnresult=$sessiondbh->prepare("delete from sessionprofile where sessionid = ? ") or $logger->error($DBI::errstr);
+        $idnresult->execute($sessionID) or $logger->error($DBI::errstr);
 
-
-	$count++;
-      }
-      
-      # TT-Data erzeugen
-
-      my $colspan=$maxcolumn*3;
-
-      my $ttdata={
-		  view       => $view,
-		  stylesheet => $stylesheet,
-		  sessionID  => $sessionID,
-		  show_corporate_banner => 0,
-		  show_foot_banner      => 1,
-		  show_testsystem_info  => 0,
-		  maxcolumn  => $maxcolumn,
-		  colspan    => $colspan,
-		  catdb      => \@catdb,
-		  config     => \%config,
-		 };
+        $idnresult=$sessiondbh->prepare("insert into sessionprofile values (?,'dbauswahl') ") or $logger->error($DBI::errstr);
+        $idnresult->execute($sessionID) or $logger->error($DBI::errstr);
     
-      OpenBib::Common::Util::print_page($config{tt_databasechoice_tname},$ttdata,$r);
-
-
-      $idnresult->finish();
-      $sessiondbh->disconnect();
+        $idnresult->finish();
       
+        $r->internal_redirect("http://$config{servername}$config{searchframe_loc}?sessionID=$sessionID&view=$view");
     }
-  }
-  else {
-    OpenBib::Common::Util::print_warning("Sie haben keine gueltige Session.",$r);
-  }
+    # ... sonst anzeigen
+    else {
+        $idnresult=$sessiondbh->prepare("select dbname from dbchoice where sessionid = ?") or $logger->error($DBI::errstr);
+        $idnresult->execute($sessionID) or $logger->error($DBI::errstr);
+        while (my $result=$idnresult->fetchrow_hashref()) {
+            my $dbname=$result->{'dbname'};
+            $checkeddb{$dbname}="checked=\"checked\"";
+        }
+        $idnresult->finish();
+
+        my $lastcategory="";
+        my $count=0;
+
+        my $maxcolumn=$config{databasechoice_maxcolumn};
+      
+        my %stype;
+	    
+        $idnresult=$sessiondbh->prepare("select * from dbinfo where active=1 order by faculty ASC, description ASC") or $logger->error($DBI::errstr);
+        $idnresult->execute() or $logger->error($DBI::errstr);
+
+        my @catdb=();
+
+        while (my $result=$idnresult->fetchrow_hashref) {
+            my $category   = $result->{'faculty'};
+            my $name       = $result->{'description'};
+            my $systemtype = $result->{'system'};
+            my $pool       = $result->{'dbname'};
+            my $url        = $result->{'url'};
+            my $sigel      = $result->{'sigel'};
+	
+            my $rcolumn;
+
+            if ($systemtype eq "a") {
+                $stype{$pool}="yellow";
+            }
+            elsif ($systemtype eq "b") {
+                $stype{$pool}="red";
+            }
+            elsif ($systemtype eq "l") {
+                $stype{$pool}="green";
+            }
+            elsif ($systemtype eq "s") {
+                $stype{$pool}="blue";
+            }
+
+            if ($category ne $lastcategory) {
+                while ($count % $maxcolumn != 0) {
+                    $rcolumn=($count % $maxcolumn)+1;
+
+                    # 'Leereintrag erzeugen'
+                    push @catdb, { 
+                        column     => $rcolumn, 
+                        category   => $lastcategory,
+                        db         => '',
+                        name       => '',
+                        systemtype => '',
+                        sigel      => '',
+                        url        => '',
+                    };
+                    $count++;
+                }
+                $count=0;
+            }
+
+            $lastcategory=$category;
+
+            $rcolumn=($count % $maxcolumn)+1;
+
+            my $checked="";
+            if (defined $checkeddb{$pool}) {
+                $checked="checked=\"checked\"";
+            }
+
+            push @catdb, { 
+                column     => $rcolumn,
+                category   => $category,
+                db         => $pool,
+                name       => $name,
+                systemtype => $stype{$pool},
+                sigel      => $sigel,
+                url        => $url,
+                checked    => $checked,
+            };
+
+
+            $count++;
+        }
+      
+        # TT-Data erzeugen
+        my $colspan=$maxcolumn*3;
+
+        my $ttdata={
+            view       => $view,
+            stylesheet => $stylesheet,
+            sessionID  => $sessionID,
+            maxcolumn  => $maxcolumn,
+            colspan    => $colspan,
+            catdb      => \@catdb,
+            show_corporate_banner => 0,
+            show_foot_banner      => 1,
+            show_testsystem_info  => 0,
+            config     => \%config,
+        };
+    
+        OpenBib::Common::Util::print_page($config{tt_databasechoice_tname},$ttdata,$r);
+        $idnresult->finish();
+        $sessiondbh->disconnect();
+        return OK;
+    }
   
-  $sessiondbh->disconnect();
-  $userdbh->disconnect();
+    $sessiondbh->disconnect();
+    $userdbh->disconnect();
   
-  return OK;
+    return OK;
 }
 
 1;

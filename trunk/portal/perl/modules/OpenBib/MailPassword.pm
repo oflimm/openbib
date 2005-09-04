6cf7e7cf84e6c1b598865163f66492d7b06c50da
flimm <flimm@e7c75b2f-8ac8-f892-83e0-76010f6c4c37>
2005-09-04 07:52:56 +0000

    - Ueberarbeitung gemaess PBP-Coding-Standards   * Indentation   * Subroutine argument passing   * Reference Variables   * Uncuddled elses   * Vertical alignment   * Assignments   * Fat Commas

--- trunk/portal/perl/modules/OpenBib/MailPassword.pm
+++ trunk/portal/perl/modules/OpenBib/MailPassword.pm
@@ -22,135 +22,127 @@
 #  an die Free Software Foundation, Inc., 675 Mass Ave, Cambridge,
 #  MA 02139, USA.
 #
-#####################################################################   
+#####################################################################
 
 #####################################################################
-# Einladen der benoetigten Perl-Module 
+# Einladen der benoetigten Perl-Module
 #####################################################################
 
 package OpenBib::MailPassword;
-
-use Apache::Constants qw(:common);
 
 use strict;
 use warnings;
 no warnings 'redefine';
 
-use Apache::Request();      # CGI-Handling (or require)
-
-use Log::Log4perl qw(get_logger :levels);
-
-use POSIX;
-use Socket;
-
-use Digest::MD5;
+use Apache::Constants qw(:common);
+use Apache::Request ();
 use DBI;
+use Log::Log4perl qw(get_logger :levels);
+use POSIX;
 
 use OpenBib::Common::Util;
-
 use OpenBib::Config;
 
 # Importieren der Konfigurationsdaten als Globale Variablen
 # in diesem Namespace
-
 use vars qw(%config);
 
 *config=\%OpenBib::Config::config;
 
 sub handler {
+    my $r=shift;
 
-  my $r=shift;
+    # Log4perl logger erzeugen
+    my $logger = get_logger();
 
-  # Log4perl logger erzeugen
+    my $query=Apache::Request->new($r);
 
-  my $logger = get_logger();
+    my $status=$query->parse;
 
-  my $query=Apache::Request->new($r);
-
-  my $status=$query->parse;
-
-  if ($status){
-    $logger->error("Cannot parse Arguments - ".$query->notes("error-notes"));
-  }
-
-  my $stylesheet=OpenBib::Common::Util::get_css_by_browsertype($r);
-
-  my $action=($query->param('action'))?$query->param('action'):'none';
-  my $code=($query->param('code'))?$query->param('code'):'1';
-  my $targetid=($query->param('targetid'))?$query->param('targetid'):'none';
-  my $loginname=($query->param('loginname'))?$query->param('loginname'):'';
-  my $password=($query->param('password'))?$query->param('password'):'';
-  my $sessionID=$query->param('sessionID')||'';
-  
-  my $sessiondbh=DBI->connect("DBI:$config{dbimodule}:dbname=$config{sessiondbname};host=$config{sessiondbhost};port=$config{sessiondbport}", $config{sessiondbuser}, $config{sessiondbpasswd}) or $logger->error_die($DBI::errstr);
-  
-  my $userdbh=DBI->connect("DBI:$config{dbimodule}:dbname=$config{userdbname};host=$config{userdbhost};port=$config{userdbport}", $config{userdbuser}, $config{userdbpasswd}) or $logger->error_die($DBI::errstr);
-  
-  unless (OpenBib::Common::Util::session_is_valid($sessiondbh,$sessionID)){
-    OpenBib::Common::Util::print_warning("Ung&uuml;ltige Session",$r);
-    
-    $sessiondbh->disconnect();
-    $userdbh->disconnect();
-    
-    return OK;
-  }
-
-  my $view="";
-
-  if ($query->param('view')){
-    $view=$query->param('view');
-  }
-  else {
-    $view=OpenBib::Common::Util::get_viewname_of_session($sessiondbh,$sessionID);
-  }
-  
-  if ($action eq "show"){
-
-    # TT-Data erzeugen
-
-    my $ttdata={
-		view       => $view,
-		stylesheet => $stylesheet,
-
-
-		sessionID  => $sessionID,
-		loginname => $loginname,
-
-		show_corporate_banner => 0,
-		show_foot_banner => 1,
-		config     => \%config,
-	       };
-
-    OpenBib::Common::Util::print_page($config{tt_mailpassword_tname},$ttdata,$r);
-    
-  }
-  elsif ($action eq "sendpw"){
-    my $loginfailed=0;
-    
-    if ($loginname eq ""){
-      OpenBib::Common::Util::print_warning("Sie haben keine E-Mail Adresse eingegeben",$r);
-      $sessiondbh->disconnect();
-      $userdbh->disconnect();
-      return OK;
+    if ($status) {
+        $logger->error("Cannot parse Arguments - ".$query->notes("error-notes"));
     }
+
+    my $stylesheet=OpenBib::Common::Util::get_css_by_browsertype($r);
+
+    my $action    = ($query->param('action'))?$query->param('action'):'none';
+    my $code      = ($query->param('code'))?$query->param('code'):'1';
+    my $targetid  = ($query->param('targetid'))?$query->param('targetid'):'none';
+    my $loginname = ($query->param('loginname'))?$query->param('loginname'):'';
+    my $password  = ($query->param('password'))?$query->param('password'):'';
+    my $sessionID = $query->param('sessionID')||'';
+  
+    my $sessiondbh
+        = DBI->connect("DBI:$config{dbimodule}:dbname=$config{sessiondbname};host=$config{sessiondbhost};port=$config{sessiondbport}", $config{sessiondbuser}, $config{sessiondbpasswd})
+            or $logger->error_die($DBI::errstr);
+  
+    my $userdbh
+        = DBI->connect("DBI:$config{dbimodule}:dbname=$config{userdbname};host=$config{userdbhost};port=$config{userdbport}", $config{userdbuser}, $config{userdbpasswd})
+            or $logger->error_die($DBI::errstr);
+  
+    unless (OpenBib::Common::Util::session_is_valid($sessiondbh,$sessionID)){
+        OpenBib::Common::Util::print_warning("Ung&uuml;ltige Session",$r);
     
-    my $targetresult=$userdbh->prepare("select pin from user where loginname = ?") or $logger->error($DBI::errstr);
+        $sessiondbh->disconnect();
+        $userdbh->disconnect();
     
-    $targetresult->execute($loginname) or $logger->error($DBI::errstr);
-    
-    my $result=$targetresult->fetchrow_hashref();
-    my $password=$result->{'pin'};
-    $targetresult->finish();
-    
-    if (!$password){
-      OpenBib::Common::Util::print_warning("Es existiert kein Passwort f&uuml;r die Kennung $loginname",$r);
-      $sessiondbh->disconnect();
-      $userdbh->disconnect();
-      return OK;
+        return OK;
     }
+
+    my $view="";
+
+    if ($query->param('view')) {
+        $view=$query->param('view');
+    }
+    else {
+        $view=OpenBib::Common::Util::get_viewname_of_session($sessiondbh,$sessionID);
+    }
+  
+    if ($action eq "show") {
+
+        # TT-Data erzeugen
+
+        my $ttdata={
+            view       => $view,
+            stylesheet => $stylesheet,
+
+
+            sessionID  => $sessionID,
+            loginname  => $loginname,
+
+            show_corporate_banner => 0,
+            show_foot_banner      => 1,
+            config     => \%config,
+        };
+
+        OpenBib::Common::Util::print_page($config{tt_mailpassword_tname},$ttdata,$r);
+    }
+    elsif ($action eq "sendpw") {
+        my $loginfailed=0;
     
-    open(MAIL,"| /usr/lib/sendmail -t -f$config{contact_email}");
-    print MAIL << "MAILSEND";
+        if ($loginname eq "") {
+            OpenBib::Common::Util::print_warning("Sie haben keine E-Mail Adresse eingegeben",$r);
+            $sessiondbh->disconnect();
+            $userdbh->disconnect();
+            return OK;
+        }
+    
+        my $targetresult=$userdbh->prepare("select pin from user where loginname = ?") or $logger->error($DBI::errstr);
+        $targetresult->execute($loginname) or $logger->error($DBI::errstr);
+    
+        my $result=$targetresult->fetchrow_hashref();
+        my $password=$result->{'pin'};
+        $targetresult->finish();
+    
+        if (!$password) {
+            OpenBib::Common::Util::print_warning("Es existiert kein Passwort f&uuml;r die Kennung $loginname",$r);
+            $sessiondbh->disconnect();
+            $userdbh->disconnect();
+            return OK;
+        }
+    
+        open(MAIL,"| /usr/lib/sendmail -t -f$config{contact_email}");
+        print MAIL << "MAILSEND";
 From: $config{contact_email}
 To: $loginname
 Subject: Ihr vergessenes KUG-Passwort
@@ -170,22 +162,21 @@ Mit freundlichen Gruessen
 Ihr KUG-Team
 MAILSEND
 
-    my $ttdata={
-		view       => $view,
-		stylesheet => $stylesheet,
+        my $ttdata={
+            view       => $view,
+            stylesheet => $stylesheet,
 
-		show_corporate_banner => 0,
-		show_foot_banner => 1,
-		config     => \%config,
-	       };
+            show_corporate_banner => 0,
+            show_foot_banner      => 1,
+            config     => \%config,
+        };
 
-    OpenBib::Common::Util::print_page($config{tt_mailpassword_success_tname},$ttdata,$r);
-  
-  }
-  
-  $sessiondbh->disconnect();
-  $userdbh->disconnect();
-  return OK;
+        OpenBib::Common::Util::print_page($config{tt_mailpassword_success_tname},$ttdata,$r);
+    }
+
+    $sessiondbh->disconnect();
+    $userdbh->disconnect();
+    return OK;
 }
 
 1;

838777da4052be7b59337c069f37b1480800e49a
flimm <flimm@e7c75b2f-8ac8-f892-83e0-76010f6c4c37>
2006-04-10 20:36:17 +0000

    - L10N via maketext/gettext

---
+++ portal/perl/modules/OpenBib/L10N.pm
@@ -0,0 +1,52 @@
+#####################################################################
+#
+#  OpenBib::L10N
+#
+#  Dieses File ist (C) 2006 Oliver Flimm <flimm@openbib.org>
+#
+#  Dieses Programm ist freie Software. Sie koennen es unter
+#  den Bedingungen der GNU General Public License, wie von der
+#  Free Software Foundation herausgegeben, weitergeben und/oder
+#  modifizieren, entweder unter Version 2 der Lizenz oder (wenn
+#  Sie es wuenschen) jeder spaeteren Version.
+#
+#  Die Veroeffentlichung dieses Programms erfolgt in der
+#  Hoffnung, dass es Ihnen von Nutzen sein wird, aber OHNE JEDE
+#  GEWAEHRLEISTUNG - sogar ohne die implizite Gewaehrleistung
+#  der MARKTREIFE oder der EIGNUNG FUER EINEN BESTIMMTEN ZWECK.
+#  Details finden Sie in der GNU General Public License.
+#
+#  Sie sollten eine Kopie der GNU General Public License zusammen
+#  mit diesem Programm erhalten haben. Falls nicht, schreiben Sie
+#  an die Free Software Foundation, Inc., 675 Mass Ave, Cambridge,
+#  MA 02139, USA.
+#
+#####################################################################
+
+#####################################################################
+# Einladen der benoetigten Perl-Module
+#####################################################################
+
+package OpenBib::L10N;
+
+use strict;
+use warnings;
+no warnings 'redefine';
+use utf8;
+
+use OpenBib::Config;
+
+# Importieren der Konfigurationsdaten als Globale Variablen
+# in diesem Namespace
+use vars qw(%config);
+
+*config = \%OpenBib::Config::config;
+
+use base 'Locale::Maketext';
+use Locale::Maketext::Lexicon {
+    '*'    => [Gettext => "$OpenBib::Config::config{locale_base_path}/*/LC_MESSAGES/openbib.po"],
+    _auto  => 1,
+    _style => 'gettext',
+};
+
+1;

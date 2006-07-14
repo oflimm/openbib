46b7c6dcd2fabe795375b2d7e38f77335915f520
flimm <flimm@e7c75b2f-8ac8-f892-83e0-76010f6c4c37>
2006-07-14 18:36:17 +0000

    - OpenBib::Search::Local::Xapian wurde in ein Objekt umgewandelt

--- trunk/portal/perl/modules/OpenBib/Search/Local/Xapian.pm
+++ trunk/portal/perl/modules/OpenBib/Search/Local/Xapian.pm
@@ -34,6 +34,7 @@ use Apache::Reload;
 use Apache::Request ();
 use Encode 'decode_utf8';
 use Log::Log4perl qw(get_logger :levels);
+use Search::Xapian;
 use Storable;
 use String::Tokenizer;
 use YAML ();
@@ -49,6 +50,16 @@ use vars qw(%config);
 
 if ($OpenBib::Config::config{benchmark}){
     use Benchmark ':hireswallclock';
+}
+
+sub new {
+    my $class = shift;
+
+    my $self = { };
+
+    bless ($self, $class);
+
+    return $self;
 }
 
 sub get_relevant_terms {
@@ -116,6 +127,98 @@ sub get_relevant_terms {
     return $fullterm_ref;
 }
 
+sub initial_search {
+    my ($self,$arg_ref) = @_;
+
+    # Set defaults
+    my $searchquery_ref   = exists $arg_ref->{searchquery_ref}
+        ? $arg_ref->{searchquery_ref} : undef;
+    my $serien            = exists $arg_ref->{serien}
+        ? $arg_ref->{serien}        : undef;
+    my $enrich            = exists $arg_ref->{enrich}
+        ? $arg_ref->{enrich}        : undef;
+    my $enrichkeys_ref    = exists $arg_ref->{enrichkeys_ref}
+        ? $arg_ref->{enrichkeys_ref}: undef;
+    my $dbh               = exists $arg_ref->{dbh}
+        ? $arg_ref->{dbh}           : undef;
+    my $database          = exists $arg_ref->{database}
+        ? $arg_ref->{database}           : undef;
+    my $maxhits           = exists $arg_ref->{maxhits}
+        ? $arg_ref->{maxhits}       : 50;
+
+    # Log4perl logger erzeugen
+    my $logger = get_logger();
+
+    my ($atime,$btime,$timeall);
+  
+    if ($config{benchmark}) {
+        $atime=new Benchmark;
+    }
+
+    my $qp = new Search::Xapian::QueryParser() || $logger->fatal("Couldn't open/create Xapian DB $!\n");
+    
+    my $querystring = lc($searchquery_ref->{fs}{norm});
+    
+    $querystring    = OpenBib::Common::Util::grundform({
+        content  => $querystring,
+    });
+        
+    $qp->set_default_op(Search::Xapian::OP_AND);
+    $qp->add_prefix('inauth'   ,'X1');
+    $qp->add_prefix('intitle'  ,'X2');
+    $qp->add_prefix('incorp'   ,'X3');
+    $qp->add_prefix('insubj'   ,'X4');
+    $qp->add_prefix('insys'    ,'X5');
+    $qp->add_prefix('inyear'   ,'X7');
+    $qp->add_prefix('inisbn'   ,'X8');
+    $qp->add_prefix('inissn'   ,'X9');
+    
+    my $enq       = $dbh->enquire($qp->parse_query($querystring));
+    my $thisquery = $enq->get_query()->get_description();
+    my @matches   = $enq->matches(0,99999);
+
+    $logger->debug("DB: $database");
+    
+    $logger->debug("Matches: ".YAML::Dump(\@matches));
+    
+    $self->{_querystring} = $querystring;
+    $self->{_enq}         = $enq;
+    $self->{_matches}     = \@matches;
+
+    $logger->info("Running query ".$self->{_querystring});
+
+    $logger->info("Found ".scalar(@matches)." matches");
+    return;
+}
+
+sub matches {
+    my $self=shift;
+
+    # Log4perl logger erzeugen
+    my $logger = get_logger();
+
+    $logger->debug(YAML::Dump($self->{_matches}));
+    return @{$self->{_matches}};
+}
+
+sub querystring {
+    my $self=shift;
+    return $self->{_querystring};
+}
+
+sub enq {
+    my $self=shift;
+    return $self->{_enq};
+}
+
+sub DESTROY {
+    my $self=shift;
+
+    # Log4perl logger erzeugen
+    my $logger = get_logger();
+    
+    $logger->debug("dying");
+}
 
 1;
 

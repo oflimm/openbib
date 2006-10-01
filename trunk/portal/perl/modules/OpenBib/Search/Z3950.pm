2e856a670b3debcfdf9e60c614cd2b8a4b55590f
flimm <flimm@e7c75b2f-8ac8-f892-83e0-76010f6c4c37>
2006-10-01 10:48:05 +0000

    - Erweiterung des Objektes um weitere Methoden, u.a.: DESTROY, mab2<->openbib

--- trunk/portal/perl/modules/OpenBib/Search/Z3950.pm
+++ trunk/portal/perl/modules/OpenBib/Search/Z3950.pm
@@ -50,5 +50,289 @@ sub new {
     return $subclass ;
 }
 
+sub to_openbib_list {
+    my ($self,$resultstring)=@_;
+
+    # Log4perl logger erzeugen
+    my $logger = get_logger();
+
+    return $self->mab2openbib_list($resultstring);
+}
+
+sub to_openbib_full {
+    my ($self,$resultstring)=@_;
+
+    # Log4perl logger erzeugen
+    my $logger = get_logger();
+
+    return $self->mab2openbib_full($resultstring);
+}
+
+sub mab2openbib_list {
+    my ($self,$resultstring)=@_;
+
+    # Log4perl logger erzeugen
+    my $logger = get_logger();
+
+    my $listitem_ref = {};
+    
+    my $config = new OpenBib::Config();
+
+    my $convtab_ref = (exists $config->{convtab}{singlepool})?
+        $config->{convtab}{singlepool}:$config->{convtab}{default};
+
+    my @record=split("",$resultstring);
+
+    my @autkor = ();
+
+    # id des Satzes bestimmen. Die id ist die letzte Zahl im
+    # Header des MAB-Satzes
+    ($listitem_ref->{id}) = $record[0] =~m/\s+(\d+)$/;
+
+    $logger->debug("Header-Line: $record[0]");
+    
+    # Bei der Auswertung der einzelnen Kategorien wird die
+    # Header-Zeile uebersprungen
+  CATLINE:
+    for (my $i=1;$i<$#record;$i++){
+        my $line = $record[$i];
+
+        $logger->debug("Line: $line");
+
+        if (length($line) > 5){
+            next CATLINE unless ($line=~/^\d\d\d/);
+            my $category  = sprintf "%04d", substr($line,0,3);
+            my $indicator = substr($line,3,1);
+            my $content   = Encode::decode('MAB2',substr($line,4,length($line)-4));
+
+            # Content filtern
+            $content=~s/^\s*\|//;
+            $content=~s/>/&gt;/g;
+            $content=~s/</&lt;/g;
+
+            $logger->debug("Category: $category\nContent: $content\n\n");
+            if ($category && $content){
+                
+                next CATLINE if (exists $convtab_ref->{blacklist_ref}->{$category});
+                
+                if (exists $convtab_ref->{listitemcat}{$category}){
+                    push @{$listitem_ref->{"T".$category}}, {
+                        indicator => $indicator,
+                        content   => $content,
+                    };    
+                };
+
+                $listitem_ref->{database}     = "USBK";
+
+                if ($category=~m/^0100/){
+                    push @{$listitem_ref->{P0100}}, {
+                        type    => 'aut',
+                        content => $content,
+                    };
+
+                    push @autkor, $content;
+                }
+                elsif ($category=~m/^0101/){
+                    push @{$listitem_ref->{P0101}}, {
+                        type       => 'aut',
+                        content    => $content,
+                    };
+
+                    push @autkor, $content;
+                }
+                elsif ($category=~m/^0103/){
+                    push @{$listitem_ref->{P0103}}, {
+                        type       => 'aut',
+                        content    => $content,
+                    };
+                    push @autkor, $content;
+                }
+                elsif ($category=~m/^0200/){
+                    push @{$listitem_ref->{C0200}}, {
+                        type       => 'kor',
+                        content    => $content,
+                    };
+                    
+                    push @autkor, $content;
+                }
+                elsif ($category=~m/^0201/){
+                    push @{$listitem_ref->{C0201}}, {
+                        type       => 'kor',
+                        content    => $content,
+                    };
+                    push @autkor, $content;
+                }
+                elsif ($category=~m/^0544/){
+                    push @{$listitem_ref->{X0014}}, {
+                        content    => $content,
+                    };
+                }
+            }
+        }
+    }
+    
+    # Kombinierte Verfasser/Koerperschaft hinzufuegen fuer Sortierung
+
+    if (@autkor){
+        push @{$listitem_ref->{'PC0001'}}, {
+            content   => join(" ; ",@autkor),
+        };
+    }
+    
+    $logger->debug("Item ".YAML::Dump($listitem_ref));
+    return $listitem_ref;
+}
+
+sub mab2openbib_full {
+    my ($self,$resultstring)=@_;
+
+    # Log4perl logger erzeugen
+    my $logger = get_logger();
+
+    my $config = new OpenBib::Config();
+    
+    my $listitem_ref    = {};
+    my $mexlistitem_ref = [];
+    
+    my $targetdbinfo_ref =  $config->get_targetdbinfo();
+    
+    my $convtab_ref = (exists $config->{convtab}{singlepool})?
+        $config->{convtab}{singlepool}:$config->{convtab}{default};
+
+    my @record=split("",$resultstring);
+
+    my @autkor = ();
+
+    # id des Satzes bestimmen. Die id ist die letzte Zahl im
+    # Header des MAB-Satzes
+    ($listitem_ref->{id}) = $record[0] =~m/\s+(\d+)$/;
+
+    $logger->debug("Header-Line: $record[0]");
+    # Bei der Auswertung der einzelnen Kategorien wird die
+    # Header-Zeile uebersprungen
+  CATLINE:
+    for (my $i=1;$i<$#record;$i++){
+        my $line = $record[$i];
+
+        $logger->debug("Line: $line");
+
+        if (length($line) > 5){
+            next CATLINE unless ($line=~/^\d\d\d/);
+            my $category  = sprintf "%04d", substr($line,0,3);
+            my $indicator = substr($line,3,1);
+            my $content   = Encode::decode('MAB2',substr($line,4,length($line)-4));
+
+            # Content filtern
+            $content=~s/^\s*\|//;
+            $content=~s/>/&gt;/g;
+            $content=~s/</&lt;/g;
+            
+            $logger->debug("Category: $category\nContent: $content\n\n");
+            if ($category && $content){
+                
+                next CATLINE if (exists $convtab_ref->{blacklist_ref}->{$category});
+                
+                push @{$listitem_ref->{"T".$category}}, {
+                    indicator => $indicator,
+                    content   => $content,
+                };
+
+                $listitem_ref->{database}     = "USBK";
+
+                if ($category=~m/^0100/){
+                    push @{$listitem_ref->{P0100}}, {
+                        type    => 'aut',
+                        content => $content,
+                    };
+
+                    push @autkor, $content;
+                }
+                elsif ($category=~m/^0101/){
+                    push @{$listitem_ref->{P0101}}, {
+                        type       => 'aut',
+                        content    => $content,
+                    };
+
+                    push @autkor, $content;
+                }
+                elsif ($category=~m/^0103/){
+                    push @{$listitem_ref->{P0103}}, {
+                        type       => 'aut',
+                        content    => $content,
+                    };
+                    push @autkor, $content;
+                }
+                elsif ($category=~m/^0200/){
+                    push @{$listitem_ref->{C0200}}, {
+                        type       => 'kor',
+                        content    => $content,
+                    };
+                    
+                    push @autkor, $content;
+                }
+                elsif ($category=~m/^0201/){
+                    push @{$listitem_ref->{C0201}}, {
+                        type       => 'kor',
+                        content    => $content,
+                    };
+                    push @autkor, $content;
+                }
+                elsif ($category=~m/^0544/){
+                    my $thismexlistitem_ref = {};
+
+                    # Defaultwerte setzen
+                    $thismexlistitem_ref->{X0005}{content}="-";
+                    $thismexlistitem_ref->{X0014}{content}="-";
+                    $thismexlistitem_ref->{X0016}{content}="-";
+                    $thismexlistitem_ref->{X1204}{content}="-";
+                    $thismexlistitem_ref->{X4000}{content}="-";
+                    $thismexlistitem_ref->{X4001}{content}="";
+
+                    # Katalogname ist USBK (hardcoded)
+                    $thismexlistitem_ref->{X0014} = {
+                        content => $content
+                    }; 
+
+                    # Es wird der Datenbankname zur Findung des Sigels herangezogen
+                    my $sigel=$targetdbinfo_ref->{dbases}{"USBK"};
+                    if (exists $targetdbinfo_ref->{sigel}{$sigel}) {
+                        $thismexlistitem_ref->{X4000}{content}=$targetdbinfo_ref->{sigel}{$sigel};
+                    }
+                    
+                    my $bibinfourl="";
+                    
+                    # Bestimmung der Bibinfo-Url
+                    if (exists $targetdbinfo_ref->{bibinfo}{$sigel}) {
+                        $thismexlistitem_ref->{X4001}{content}=$targetdbinfo_ref->{bibinfo}{$sigel};
+                    }
+
+                    push @{$mexlistitem_ref}, $thismexlistitem_ref;
+                }
+            }
+        }
+    }
+    
+    # Kombinierte Verfasser/Koerperschaft hinzufuegen fuer Sortierung
+
+    if (@autkor){
+        push @{$listitem_ref->{'PC0001'}}, {
+            content   => join(" ; ",@autkor),
+        };
+    }
+    
+    $logger->debug("Item ".YAML::Dump($listitem_ref));
+    return ($listitem_ref,$mexlistitem_ref);
+}
+
+sub DESTROY {
+    my $self = shift;
+
+    if (defined $self->{conn}){
+        $self->{conn}->destroy();
+    }
+    
+    return;
+}
+
 1;
 

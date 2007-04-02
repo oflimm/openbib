faab610790fe663cfb9328cbab10486f51d29570
flimm <flimm@e7c75b2f-8ac8-f892-83e0-76010f6c4c37>
2007-04-02 21:30:09 +0000

    - Erweiterung um Funktion insert_tag, um Tags aus einer Auswahl-Liste   in ein Eingabefeld zu uebertragen. Nochmaliges Auswaehlen eines Tags   loescht diesen wieder im Eingabefeld.

--- portal/htdocs/js/openbib.js
+++ portal/htdocs/js/openbib.js
@@ -21,3 +21,54 @@ function expandContent(content,img){
     img.src='/images/openbib/collapse.png';
   }
 }
+
+function insert_tag(event) {
+
+  var this_element = "";
+  
+  if (!event)
+    event = window.event;
+
+  if (event.srcElement) {
+    // Der Internet Explorer verwendet srcElement
+    this_element = event.srcElement;
+  }
+  else if (event.target) {
+    // Mozilla und Abkoemmlinge verwenden target
+    this_element = event.target;
+  }
+  
+  var tag  = this_element.childNodes[0].nodeValue;
+
+  tag = tag.replace(/ /,"");
+  
+  var this_input = document.getElementById('inputfield');
+  
+  var tags = this_input.value.split(" ");
+  
+  if (tags[0] == "") {
+    tags.splice(0,1);
+  }
+  
+  var done = 0;
+  var new_tags = new Array();
+  
+  for (var i = 0; i < tags.length; i++) {
+    var this_tag = tags[i];
+    if (tag == this_tag) {
+      done = 1;
+    }
+    else {
+      new_tags.push(this_tag);
+    }
+  }
+      
+  if (!done) {
+    new_tags.push(tag) ;
+  }
+  
+  var new_input = new_tags.join(" ");
+  this_input.value = new_input;
+  
+  this_input.focus();
+}

1c78e0e4dad243668629dda7c841f12107ec112c
flimm <flimm@e7c75b2f-8ac8-f892-83e0-76010f6c4c37>
2007-05-30 12:17:03 +0000

    - Korrektur: Fehler file error - recursion into 'common/subtemplate'   behoben durch Option RECURSION => 1 bei der Objekterzeugung von Template

--- branches/stable-2_0-feature_freeze-branch/portal/perl/modules/OpenBib/MailPassword.pm
+++ branches/stable-2_0-feature_freeze-branch/portal/perl/modules/OpenBib/MailPassword.pm
@@ -157,6 +157,7 @@ sub handler {
 #         INCLUDE_PATH  => $config->{tt_include_path},
           # Es ist wesentlich, dass OUTPUT* hier und nicht im
           # Template::Provider definiert wird
+          RECURSION      => 1,
           OUTPUT_PATH   => '/tmp',
           OUTPUT        => $afile,
         });

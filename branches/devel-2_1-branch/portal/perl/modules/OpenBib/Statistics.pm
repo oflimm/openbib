1f9790bac17ead51be2d1ed0c249d532793c630f
flimm <flimm@e7c75b2f-8ac8-f892-83e0-76010f6c4c37>
2007-06-30 21:40:46 +0000

    - Erweiterung um Methoden get_number_of_event, get_ranking_of_event sowie   log_query. Die ersten beiden werden fuer statistische Auswertungen aus dem   eventlog verwendet, log_query legt die Suchbegriffe sowie die Suchkategorien-   Nutzung in den neuen Tabellen der Statistik-DB ab.

--- branches/devel-2_1-branch/portal/perl/modules/OpenBib/Statistics.pm
+++ branches/devel-2_1-branch/portal/perl/modules/OpenBib/Statistics.pm
@@ -30,7 +30,7 @@ use warnings;
 no warnings 'redefine';
 use utf8;
 
-use Encode 'decode_utf8';
+use Encode qw(decode_utf8 encode_utf8);
 use Log::Log4perl qw(get_logger :levels);
 use Storable ();
 
@@ -185,6 +185,224 @@ sub log_event {
     my $request=$self->{dbh}->prepare("insert into eventlog values (?,?,?,?)") or $logger->error($DBI::errstr);
     $request->execute($sessionID,$tstamp,$type,$content) or $logger->error($DBI::errstr);
     $request->finish;
+
+    return;
+}
+
+sub get_number_of_event {
+    my ($self,$arg_ref)=@_;
+
+    # Set defaults
+    my $tstamp       = exists $arg_ref->{tstamp}
+        ? $arg_ref->{tstamp}             : undef;
+
+    my $type         = exists $arg_ref->{type}
+        ? $arg_ref->{type}               : undef;
+    
+    my $content      = exists $arg_ref->{content}
+        ? $arg_ref->{content}            : undef;
+
+    # Log4perl logger erzeugen
+    my $logger = get_logger();
+
+    my $sqlstring="select count(tstamp) as rowcount, min(tstamp) as mintstamp from eventlog";
+
+    my @sqlwhere = ();
+    my @sqlargs  = ();
+
+    if ($type){
+        push @sqlwhere, " type = ?";
+	push @sqlargs,  $type;
+    } 
+
+    if ($content){
+        push @sqlwhere, " content = ?";
+	push @sqlargs,  $content;
+    } 
+
+    my $sqlwherestring  = join(" and ",@sqlwhere);
+
+    if ($sqlwherestring){
+      $sqlstring.=" where $sqlwherestring";
+    }
+
+    $logger->debug($sqlstring." ".join(" - ",@sqlargs));
+    my $request=$self->{dbh}->prepare($sqlstring) or $logger->error($DBI::errstr);
+    $request->execute(@sqlargs) or $logger->error($DBI::errstr);
+    
+    my $res        = $request->fetchrow_hashref;
+    my $count      = $res->{rowcount};
+    my $mintstamp  = $res->{mintstamp};
+
+
+    $request->finish;
+
+    return {
+	    number => $count,
+	    since  => $mintstamp,
+	    }
+}
+
+sub get_ranking_of_event {
+    my ($self,$arg_ref)=@_;
+
+    # Set defaults
+    my $tstamp       = exists $arg_ref->{tstamp}
+        ? $arg_ref->{tstamp}             : undef;
+
+    my $type         = exists $arg_ref->{type}
+        ? $arg_ref->{type}               : undef;
+    
+    # Log4perl logger erzeugen
+    my $logger = get_logger();
+
+    my $sqlstring="select count(content) as rowcount, content, min(tstamp) as mintstamp from eventlog";
+
+    my @sqlwhere = ();
+    my @sqlargs  = ();
+
+    if ($type){
+        push @sqlwhere, " type = ?";
+	push @sqlargs,  $type;
+    } 
+
+    my $sqlwherestring  = join(" and ",@sqlwhere);
+
+    if ($sqlwherestring){
+      $sqlstring.=" where $sqlwherestring";
+    }
+
+    $sqlstring.=" group by content order by rowcount DESC";
+
+    $logger->debug($sqlstring." ".join(" - ",@sqlargs));
+    my $request=$self->{dbh}->prepare($sqlstring) or $logger->error($DBI::errstr);
+    $request->execute(@sqlargs) or $logger->error($DBI::errstr);
+
+    my @ranking=();
+
+    while (my $res = $request->fetchrow_hashref){
+        my $count      = $res->{rowcount};
+	my $content    = $res->{content};
+	my $mintstamp  = $res->{mintstamp};
+
+	push @ranking, {
+			content   => $content,
+			number    => $count,
+			since     => $mintstamp,
+		       };
+    }
+    $request->finish;
+
+    $logger->debug(YAML::Dump(\@ranking));
+
+    return @ranking;
+}
+
+sub log_query {
+    my ($self,$arg_ref)=@_;
+
+    my $tstamp       = exists $arg_ref->{tstamp}
+        ? $arg_ref->{tstamp}             : undef;
+
+    # Set defaults
+    my $view            = exists $arg_ref->{view}
+        ? $arg_ref->{view}               : undef;
+
+    my $searchquery_ref = exists $arg_ref->{searchquery_ref}
+        ? $arg_ref->{searchquery_ref}    : undef;
+
+    # Log4perl logger erzeugen
+    my $logger = get_logger();
+
+    # Moegliche Queryterm-Typen
+    #
+    # 01 => fs
+    # 02 => hst 
+    # 03 => verf
+    # 04 => kor
+    # 05 => swt
+    # 06 => notation
+    # 07 => isbn
+    # 08 => issn
+    # 09 => sign
+    # 10 => mart
+    # 11 => hststring
+    # 12 => gtquelle
+    # 13 => ejahr
+
+    my $cat2type_ref = {
+			fs        => 1,
+			hst       => 2,
+			verf      => 3,
+			kor       => 4,
+			swt       => 5,
+			notation  => 6,
+			isbn      => 7,
+			issn      => 8,
+			sign      => 9,
+			mart      => 10,
+			hststring => 11,
+			gtquelle  => 12,
+			ejahr     => 13,
+		       };
+
+    my $used_category_ref = {
+			fs        => 0,
+			hst       => 0,
+			verf      => 0,
+			kor       => 0,
+			swt       => 0,
+			notation  => 0,
+			isbn      => 0,
+			issn      => 0,
+			sign      => 0,
+			mart      => 0,
+			hststring => 0,
+			gtquelle  => 0,
+			ejahr     => 0,
+			    };
+
+    my $termrequest     = $self->{dbh}->prepare("insert into queryterm values (?,?,?,?)") or $logger->error($DBI::errstr);
+    my $categoryrequest = $self->{dbh}->prepare("insert into querycategory values (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)") or $logger->error($DBI::errstr);
+
+    foreach my $cat (keys %$cat2type_ref){
+        my $thiscategory_terms = $searchquery_ref->{$cat}->{val};
+	
+	$thiscategory_terms    =~s/[^\p{Alphabetic}0-9 ]//g;
+
+	# Genutzte Kategorie merken
+	if ($thiscategory_terms){
+	  $used_category_ref->{$cat} = 1;
+	}
+
+	my $tokenizer = String::Tokenizer->new();
+	$tokenizer->tokenize($thiscategory_terms);
+	
+	my $i = $tokenizer->iterator();
+	
+	while ($i->hasNextToken()) {
+	  my $next = $i->nextToken();
+	  next if (!$next);
+	  
+	  $termrequest->execute($tstamp,$view,$cat2type_ref->{$cat},encode_utf8($next)) or $logger->error($DBI::errstr);
+	}
+    }
+    
+    $categoryrequest->execute($tstamp,$view,
+			      $used_category_ref->{fs},
+			      $used_category_ref->{hst},
+			      $used_category_ref->{verf},
+			      $used_category_ref->{kor},
+			      $used_category_ref->{swt},
+			      $used_category_ref->{notation},
+			      $used_category_ref->{isbn},
+			      $used_category_ref->{issn},
+			      $used_category_ref->{sign},
+			      $used_category_ref->{mart},
+			      $used_category_ref->{hststring},
+			      $used_category_ref->{gtquelle},
+			      $used_category_ref->{ejahr}
+			     ) or $logger->error($DBI::errstr);
 
     return;
 }

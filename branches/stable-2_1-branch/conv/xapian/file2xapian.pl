f108f9e8856e278e94a2e84c0fdedd01155b6bfe
flimm <flimm@e7c75b2f-8ac8-f892-83e0-76010f6c4c37>
2007-11-27 17:14:59 +0000

    - Begrenzung der Maximalen Laenge der Drilldown-Terme auf 100 ueber   entsprechende Konfigurationsvariable $DRILLDOWN_MAX_LEY_LEN

--- branches/stable-2_1-branch/conv/xapian/file2xapian.pl
+++ branches/stable-2_1-branch/conv/xapian/file2xapian.pl
@@ -87,6 +87,7 @@ if (!$database){
 }
 
 my $FLINT_BTREE_MAX_KEY_LEN = 245;
+my $DRILLDOWN_MAX_KEY_LEN   = 100;
 
 my %normdata                = ();
 
@@ -310,7 +311,7 @@ my $count = 1;
 		  $field="$tokinfo_ref->{prefix}$field";
 
                   # Begrenzung der keys auf FLINT_BTREE_MAX_KEY_LEN Zeichen
-		  $field=(length($field) > $FLINT_BTREE_MAX_KEY_LEN)?substr($field,0,$FLINT_BTREE_MAX_KEY_LEN):$field;
+		  next if (length($field) > $DRILLDOWN_MAX_KEY_LEN);
 
 		  $doc->add_term($field);
 	        }

62f96597229aa8ceb6f61d496977ab22bbd16b12
flimm <flimm@e7c75b2f-8ac8-f892-83e0-76010f6c4c37>
2007-12-12 21:14:30 +0000

    - Umstellung auf jquery in openbib.js fuer die Merkliste und das togglen   der Sichtbarkeit von Inhalten. Damit die Auslagerung in openbib.js   fuer dynamische Inhalte funktioniert, wird in einem Meta-Tag die   sessionID plaziert, die dann vom jquery-Code ausgewertet und fuer die   dynamische URL-Erzeugung (AJAX) herangezogen wird. Ebenso muss der URI   /portal/merkliste (managecollection_loc) in openbib.js hart   hereincodiert werden - bei einer Aenderung von managecollection_loc in   OpenBib::Config muss daher auch openbib.js entsprechend angepasst   werden.

--- portal/htdocs/js/openbib.js
+++ portal/htdocs/js/openbib.js
@@ -1,26 +1,97 @@
-function expandContentVis(content,img){
-  var data = eval('document.getElementById("' + content +'")');
-  if (data.style.visibility=="visible" || data.style.visibility=="" ){
-    data.style.visibility="hidden";
-    img.src='/images/openbib/expand.png';
-  }
-  else{
-    data.style.visibility="";
-    img.src='/images/openbib/collapse.png';
-  }
-}
+$(document).ready(function(){
 
-function expandContent(content,img){
-  var data = eval('document.getElementById("' + content +'")');
-  if (data.style.display=="block" || data.style.display=="" ){
-    data.style.display="none";
-    img.src='/images/openbib/expand.png';
-  }
-  else{
-    data.style.display="";
-    img.src='/images/openbib/collapse.png';
-  }
+var sessionID = $("meta[@name='sessionID']").attr("content");
+
+// Begin Merkliste
+// Merklistenfuellstand aktualisieren
+// Achtung!!! Wert von managecollection_loc aus OpenBib::Config ist hier
+// fest eingetragen und muss gegebenenfalls angepasst werden
+$.get("/portal/merkliste?sessionID="+sessionID+";action=show;do_collection_showcount=1",
+function (txt){
+ $("#collectioncount").html("["+txt+"]"); 
 }
+);
+
+$(".rlcollect a").click(function(){
+
+   // Insert-Funktion aufrufen
+   $.get(this.href);
+
+   // Merklistenfuellstand aktualisieren
+   $.get("/portal/merkliste?sessionID="+sessionID+";action=show;do_collection_showcount=1",
+function (txt){ $("#collectioncount").html("["+txt+"]"); });
+
+   return false;
+ });
+// Ende Merkliste
+
+// --------------------------------------------------------------------------
+
+// Begin Togglen / Suchhilfe
+// Bild setzen
+$("#searchinfo_toggle").html("<img src=\"/images/openbib/expand.png\" alt=\"Suchhilfe anzeigen\">")
+// Zuerst verstecken
+$("#searchinfo").hide();
+// und bei Klick Sichtbarkeit togglen
+$("#searchinfo_toggle").click(function(){
+ $("#searchinfo").toggle();
+});
+// Ende Togglen / Suchhilfe
+
+// --------------------------------------------------------------------------
+
+// Begin Togglen / Suchoptionen
+// Bild setzen
+$("#searchoptions_toggle").html("<img src=\"/images/openbib/expand.png\" alt=\"Optionen anzeigen\">")
+// Zuerst verstecken
+$("#searchoptions").hide();
+// und bei Klick Sichtbarkeit togglen
+$("#searchoptions_toggle").click(function(){
+ $("#searchoptions").toggle();
+});
+// Ende Togglen / Suchoptionen
+
+// --------------------------------------------------------------------------
+
+// Begin Togglen / Eigene Tags
+// Bild setzen
+$("#newtags_toggle").html("<img src=\"/images/openbib/expand.png\" alt=\"Tag-Eingabe anzeigen\">")
+// Zuerst verstecken
+$("#newtags").hide();
+// und bei Klick Sichtbarkeit togglen
+$("#newtags_toggle").click(function(){
+ $("#newtags").toggle();
+});
+// Ende Togglen / Eigene Tags
+
+// --------------------------------------------------------------------------
+
+// Begin Togglen / Eigene Reviews
+// Bild setzen
+$("#newreview_toggle").html("<img src=\"/images/openbib/expand.png\" alt=\"Bewertungs/Rezensions-Eingabe anzeigen\">")
+// Zuerst verstecken
+$("#newreview").hide();
+// und bei Klick Sichtbarkeit togglen
+$("#newreview_toggle").click(function(){
+ $("#newreview").toggle();
+});
+// Ende Togglen / Eigene Reviews
+
+// --------------------------------------------------------------------------
+
+// Begin Togglen / Alle Reviews
+// Bild setzen
+$("#allreviews_toggle").html("<img src=\"/images/openbib/expand.png\" alt=\"Alle Bewertungen anzeigen\">")
+// Zuerst verstecken
+$("#allreviews").hide();
+// und bei Klick Sichtbarkeit togglen
+$("#allreviews_toggle").click(function(){
+ $("#allreviews").toggle();
+});
+// Ende Togglen / alle Reviews
+ 
+});
+
 
 function insert_tag(event) {
 

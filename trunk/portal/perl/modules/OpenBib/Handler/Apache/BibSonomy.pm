2f620a213118168b0aa82019937354f3e2babe37
flimm <flimm@e7c75b2f-8ac8-f892-83e0-76010f6c4c37>
2008-02-02 16:58:58 +0000

    - Apache-Handler fuer den Zugriff auf erweiterten Mashup mit BibSonomy

---
+++ trunk/portal/perl/modules/OpenBib/Handler/Apache/BibSonomy.pm
@@ -0,0 +1,149 @@
+#####################################################################
+#
+#  OpenBib::Handler::Apache::BibSonomy.pm
+#
+#  Copyright 2008 Oliver Flimm <flimm@openbib.org>
+#
+#  Dieses Programm ist freie Software. Sie koennen es unter
+#  den Bedingungen der GNU General Public License, wie von der
+#  Free Software Foundation herausgegeben, weitergeben und/oder
+#  modifizieren, entweder unter Version 2 der Lizenz oder (wenn
+#  Sie es wuenschen) jeder spaeteren Version.
+#
+#  Die Veroeffentlichung dieses Programms erfolgt in der
+#  Hoffnung, dass es Ihnen von Nutzen sein wird, aber OHNE JEDE
+#  GEWAEHRLEISTUNG - sogar ohne die implizite Gewaehrleistung
+#  der MARKTREIFE oder der EIGNUNG FUER EINEN BESTIMMTEN ZWECK.
+#  Details finden Sie in der GNU General Public License.
+#
+#  Sie sollten eine Kopie der GNU General Public License zusammen
+#  mit diesem Programm erhalten haben. Falls nicht, schreiben Sie
+#  an die Free Software Foundation, Inc., 675 Mass Ave, Cambridge,
+#  MA 02139, USA.
+#
+#####################################################################
+
+#####################################################################
+# Einladen der benoetigten Perl-Module
+#####################################################################
+
+package OpenBib::Handler::Apache::BibSonomy;
+
+use strict;
+use warnings;
+no warnings 'redefine';
+use utf8;
+
+use Apache::Constants qw(:common);
+use Apache::Reload;
+use Apache::Request ();
+use Encode 'decode_utf8';
+use DBI;
+use Log::Log4perl qw(get_logger :levels);
+use POSIX;
+use Template;
+
+use OpenBib::Config;
+use OpenBib::L10N;
+use OpenBib::Session;
+use OpenBib::User;
+
+my $benchmark;
+
+if ($OpenBib::Config::config{benchmark}) {
+    use Benchmark ':hireswallclock';
+}
+
+sub handler {
+    my $r=shift;
+
+    # Log4perl logger erzeugen
+    my $logger = get_logger();
+
+    my $config = new OpenBib::Config();
+    
+    my $query=Apache::Request->instance($r);
+
+    my $status=$query->parse;
+
+    if ($status) {
+        $logger->error("Cannot parse Arguments - ".$query->notes("error-notes"));
+    }
+
+    my $session   = new OpenBib::Session({
+        sessionID => $query->param('sessionID'),
+    });
+
+    my $stylesheet=OpenBib::Common::Util::get_css_by_browsertype($r);
+  
+    #####################################################################
+    # Konfigurationsoptionen bei <FORM> mit Defaulteinstellungen
+    #####################################################################
+
+    my $offset         = $query->param('offset')           || 0;
+    my $hitrange       = $query->param('hitrange')         || 50;
+    my $sorttype       = $query->param('sorttype')         || "author";
+    my $sortorder      = $query->param('sortorder')        || "up";
+    my $titisbn        = $query->param('titisbn')          || '';
+    my $tag            = decode_utf8($query->param('tag')) || '';
+
+
+    my $action         = decode_utf8($query->param('action')) || '';
+    
+    #####                                                          ######
+    ####### E N D E  V A R I A B L E N D E K L A R A T I O N E N ########
+    #####                                                          ######
+  
+    ###########                                               ###########
+    ############## B E G I N N  P R O G R A M M F L U S S ###############
+    ###########                                               ###########
+  
+    my $queryoptions_ref
+        = $session->get_queryoptions($query);
+
+    # Message Katalog laden
+    my $msg = OpenBib::L10N->get_handle($queryoptions_ref->{l}) || $logger->error("L10N-Fehler");
+    $msg->fail_with( \&OpenBib::L10N::failure_handler );
+    
+    my $targetdbinfo_ref
+        = $config->get_targetdbinfo();
+
+    my $targetcircinfo_ref
+        = $config->get_targetcircinfo();
+
+    if (!$session->is_valid()){
+        OpenBib::Common::Util::print_warning($msg->maketext("Ungültige Session"),$r,$msg);
+
+        return OK;
+    }
+    
+    my $view="";
+
+    if ($query->param('view')) {
+        $view=$query->param('view');
+    }
+    else {
+        $view=$session->get_viewname();
+    }
+
+    if ($action eq "get_tags"){
+        if ($titisbn){
+            my @tags = OpenBib::BibSonomy->new()->get_posts({ isbn => $titisbn })->get_tags_of_posts();
+
+            # Start der Ausgabe mit korrektem Header
+            print $r->send_http_header("text/plain");
+
+            print "<bibsonomy_tags>\n";
+            foreach my $tag (@tags){
+                print "   <bibsonomy_tag>$tag</bibsonomy_tag>\n";
+            }
+            print "</bibsonomy_tags>\n";
+        }
+
+        return OK;
+    }
+
+    return OK;
+}
+
+1;

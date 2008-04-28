da0f1abd974f9c8fe7af4b575ae9c948de02038a
flimm <flimm@e7c75b2f-8ac8-f892-83e0-76010f6c4c37>
2008-04-28 17:01:00 +0000

    - Erweiterung um zusaetzliche Kategorien

--- trunk/conv/mab/mab2meta.pl
+++ trunk/conv/mab/mab2meta.pl
@@ -31,6 +31,8 @@
 # Einladen der benoetigten Perl-Module
 #####################################################################
 
+use utf8;
+
 use Getopt::Long;
 use Encode::MAB2;
 use MAB2::Record::Base;
@@ -71,7 +73,6 @@ $kordublastidx=1;
 $notdublastidx=1;
 $swtdublastidx=1;
 
-
 ######################################################################
 # Titel-Daten
 
@@ -88,66 +89,12 @@ my $titdefs_ref = {
         newcat => '0004', # Uebergeordn. Satz
         mut    => 1,
     },
+    '037'  => {           # Sprachencode
+        newcat => '0015', # Sprachencode
+        mut    => 1,
+    },
     '089' => {            # bandangaben in vorlageform
         newcat => '0089', # bandangaben in vorlageform
-        mult   => 1,
-    },
-    '331' => {            # hauptsachtitel in vorlageform oder mischform
-        newcat => '0331', # hauptsachtitel in vorlageform oder mischform
-        mult   => 1,
-    },
-    '403' => {            # ausgabebezeichnung in vorlageform
-        newcat => '0403', # ausgabebezeichnung in vorlageform
-        mult   => 1,
-    },
-    '425' => {            # erscheinungsjahr(e)
-        newcat => '0425', # erscheinungsjahr(e)
-        mult   => 1,
-    },
-    '433' => {            # umfangsangabe
-        newcat => '0433', # umfangsangabe
-        mult   => 1,
-    },
-    '540' => {            # internationale standardbuchnummer (isbn)
-        newcat => '0540', # internationale standardbuchnummer (isbn)
-        mult   => 1,
-    },
-    '335' => {            # zusaetze zum hauptsachtitel
-        newcat => '0335', # zusaetze zum hauptsachtitel
-        mult   => 1,
-    },
-    '359' => {            # verfasserangabe
-        newcat => '0359', # verfasserangabe
-        mult   => 1,
-    },
-    '410' => {            # ort(e) des 1. verlegers, druckers usw.
-        newcat => '0410', # ort(e) des 1. verlegers, druckers usw.
-        mult   => 1,
-    },
-    '412' => {            # name des 1. verlegers, druckers usw.
-        newcat => '0412', # name des 1. verlegers, druckers usw.
-        mult   => 1,
-    },
-    '655' => {            # E-Book URL
-        newcat => '0662', # E-Book URL
-        mult   => 1,
-    },
-    '659' => {            # E-Book Info
-        newcat => '0663', # E-Book Info
-        mult   => 1,
-    },
-    '700' => {            # Notationen/Systematik
-        newcat => '0700', # Notationen/Systematik
-        mult   => 1,
-        ref    => 'not',
-    },
-    '710' => {            # schlagwoerter und schlagwortketten
-        newcat => '0710', # schlagwoerter und schlagwortketten
-        mult   => 1,
-        ref    => 'swt',
-    },
-    '451' => {            # 1. gesamttitel in vorlageform
-        newcat => '0451', # 1. gesamttitel in vorlageform
         mult   => 1,
     },
     '100' => {            # name der 1. person in ansetzungsform
@@ -180,6 +127,236 @@ my $titdefs_ref = {
         mult   => 1,
         ref    => 'kor',
     },
+    '304' => {            # EST
+        newcat => '0304', # EST
+        mult   => 1,
+    },
+    '310' => {            # AST
+        newcat => '0310', # AST
+        mult   => 1,
+    },
+    '331' => {            # hauptsachtitel in vorlageform oder mischform
+        newcat => '0331', # hauptsachtitel in vorlageform oder mischform
+        mult   => 1,
+    },
+    '333' => {            # Zu erg. Urheber/HST Urheber
+        newcat => '0333', # Zu erg. Urheber/HST Urheber
+        mult   => 1,
+    },
+    '334' => {            # Material
+        newcat => '0334', # Material
+        mult   => 1,
+    },
+    '335' => {            # zusaetze zum hauptsachtitel
+        newcat => '0335', # zusaetze zum hauptsachtitel
+        mult   => 1,
+    },
+    '341' => {            # 
+        newcat => '0341', # 
+        mult   => 1,
+    },
+    '343' => {            # 
+        newcat => '0343', # 
+        mult   => 1,
+    },
+    '345' => {            # 
+        newcat => '0345', # 
+        mult   => 1,
+    },
+    '347' => {            # 
+        newcat => '0347', # 
+        mult   => 1,
+    },
+    '349' => {            # 
+        newcat => '0349', # 
+        mult   => 1,
+    },
+    '351' => {            # 
+        newcat => '0351', # 
+        mult   => 1,
+    },
+    '353' => {            # 
+        newcat => '0353', # 
+        mult   => 1,
+    },
+    '355' => {            # 
+        newcat => '0355', # 
+        mult   => 1,
+    },
+    '359' => {            # verfasserangabe
+        newcat => '0359', # verfasserangabe
+        mult   => 1,
+    },
+    '370' => {            # 
+        newcat => '0370', # 
+        mult   => 1,
+    },
+    '403' => {            # ausgabebezeichnung in vorlageform
+        newcat => '0403', # ausgabebezeichnung in vorlageform
+        mult   => 1,
+    },
+    '405' => {            # 
+        newcat => '0405', # 
+        mult   => 1,
+    },
+    '407' => {            # 
+        newcat => '0407', # 
+        mult   => 1,
+    },
+    '410' => {            # ort(e) des 1. verlegers, druckers usw.
+        newcat => '0410', # ort(e) des 1. verlegers, druckers usw.
+        mult   => 1,
+    },
+    '412' => {            # name des 1. verlegers, druckers usw.
+        newcat => '0412', # name des 1. verlegers, druckers usw.
+        mult   => 1,
+    },
+    '417' => {            # 
+        newcat => '0417', # 
+        mult   => 1,
+    },
+    '425' => {            # erscheinungsjahr(e)
+        newcat => '0425', # erscheinungsjahr(e)
+        mult   => 1,
+    },
+    '433' => {            # umfangsangabe
+        newcat => '0433', # umfangsangabe
+        mult   => 1,
+    },
+    '434' => {            # 
+        newcat => '0434', # 
+        mult   => 1,
+    },
+    '435' => {            # 
+        newcat => '0435', # 
+        mult   => 1,
+    },
+    '437' => {            # 
+        newcat => '0437', # 
+        mult   => 1,
+    },
+    '451' => {            # 1. gesamttitel in vorlageform
+        newcat => '0451', # 1. gesamttitel in vorlageform
+        mult   => 1,
+    },
+    '461' => {            # 2. gesamttitel in vorlageform
+        newcat => '0461', # 2. gesamttitel in vorlageform
+        mult   => 1,
+    },
+    '471' => {            # 3. gesamttitel in vorlageform
+        newcat => '0471', # 3. gesamttitel in vorlageform
+        mult   => 1,
+    },
+    '481' => {            # 4. gesamttitel in vorlageform
+        newcat => '0481', # 4. gesamttitel in vorlageform
+        mult   => 1,
+    },
+    '491' => {            # 5. gesamttitel in vorlageform
+        newcat => '0491', # 5. gesamttitel in vorlageform
+        mult   => 1,
+    },
+    '501' => {            # Fussnoten 
+        newcat => '0501', # Fussnoten
+        mult   => 1,
+    },
+    '509' => {            # 
+        newcat => '0509', # 
+        mult   => 1,
+    },
+    '511' => {            # 
+        newcat => '0511', # 
+        mult   => 1,
+    },
+    '517' => {            # 
+        newcat => '0517', # 
+        mult   => 1,
+    },
+    '519' => {            # 
+        newcat => '0519', # 
+        mult   => 1,
+    },
+    '519' => {            # 
+        newcat => '0519', # 
+        mult   => 1,
+    },
+    '524' => {            # Darin:
+        newcat => '0524', # 
+        mult   => 1,
+    },
+    '527' => {            # 
+        newcat => '0527', # 
+        mult   => 1,
+    },
+    '530' => {            # 
+        newcat => '0530', # 
+        mult   => 1,
+    },
+    '538' => {            # 
+        newcat => '0538', # 
+        mult   => 1,
+    },
+    '540' => {            # internationale standardbuchnummer (isbn)
+        newcat => '0540', # internationale standardbuchnummer (isbn)
+        mult   => 1,
+    },
+    '590' => {            # 
+        newcat => '0590', # 
+        mult   => 1,
+    },
+    '611' => {            # Herstellungsort
+        newcat => '0611', # 
+        mult   => 1,
+    },
+    '613' => {            # Hersteller
+        newcat => '0613', # 
+        mult   => 1,
+    },
+    '617' => {            # 
+        newcat => '0617', # 
+        mult   => 1,
+    },
+    '619' => {            # Herstellungsjahr
+        newcat => '0619', # 
+        mult   => 1,
+    },
+    '621' => {            # Quelle
+        newcat => '0508', # Quelle
+        mult   => 1,
+    },
+    '622' => {            # 
+        newcat => '0622', #   
+        mult   => 1,
+    },
+    '645' => {            # 
+        newcat => '0645', #   
+        mult   => 1,
+    },
+    '652' => {            # EDV Material
+        newcat => '0652', # EDV Material
+        mult   => 1,
+    },
+    '655' => {            # E-Book URL
+        newcat => '0662', # E-Book URL
+        mult   => 1,
+    },
+    '659' => {            # E-Book Info
+        newcat => '0663', # E-Book Info
+        mult   => 1,
+    },
+    '700' => {            # Notationen/Systematik
+        newcat => '0700', # Notationen/Systematik
+        mult   => 1,
+        ref    => 'not',
+    },
+    '740' => {            # schlagwoerter und schlagwortketten
+        newcat => '0710', # schlagwoerter und schlagwortketten
+        mult   => 1,
+        ref    => 'swt',
+    },
+    '750' => {            # Abstrakt 
+        newcat => '0750',  # Abstrakt
+        mult   => 1,
+    },
 };
 
 
@@ -193,6 +370,8 @@ print "Bearbeite Titel\n";
 
 tie @mab2titdata, 'Tie::MAB2::Recno', file => $filename;
 
+my $used_categories_ref = {};
+
 foreach my $rawrec (@mab2titdata){
     my $rec = MAB2::Record::Base->new($rawrec);
     #print $rec->readable."\n----------------------\n";    
@@ -203,6 +382,8 @@ foreach my $rawrec (@mab2titdata){
         my $indicator = $category_ref->[1];
         my $content   = konv($category_ref->[2]);
 
+        $used_categories_ref->{$category} = 1;
+        
         my $newcategory = "";
         
         if (!exists $titdefs_ref->{$category}){
@@ -215,9 +396,14 @@ foreach my $rawrec (@mab2titdata){
         if ($category =~ /^001$/ || $category =~ /^010$/){
             $content=~s/\D//g;
         }
-
+        
+        
         if ($category =~ /^002$/){
             $content=~s/(\d\d\d\d)(\d\d)(\d\d)/$3.$2.$1/;
+        }
+
+        if ($category =~ /^540$/){
+            $content=~s/^ISBN //;
         }
 
         # Standard-Konvertierung mit perkonv
@@ -226,6 +412,7 @@ foreach my $rawrec (@mab2titdata){
             $indicator="";
         }
 
+        # Spezialfaelle
         if (exists $titdefs_ref->{$category}{newcat}){
             $newcategory = $titdefs_ref->{$category}{newcat};
         }
@@ -233,7 +420,7 @@ foreach my $rawrec (@mab2titdata){
         if (exists $titdefs_ref->{$category}{ref}){
             if    ($titdefs_ref->{$category}{ref} eq 'aut'){
                 my $supplement="";
-                if ($content =~/^(.+?)( ; \[.*?$)/){
+                if ($content =~/^(.+?)( \[.*?$)/){
                     $content    = $1;
                     $supplement = $2;
                 }
@@ -242,21 +429,24 @@ foreach my $rawrec (@mab2titdata){
                 
                 if ($autidn > 0){
                     print PEROUT "0000:$autidn\n";
-                    print PEROUT "0001:$content\n;
+                    print PEROUT "0001:$content\n";
                     print PEROUT "9999:\n";
                 }
                 else {
                     $autidn=(-1)*$autidn;
                 }
-
-                $content="IDN: $autidn".$supplement;
+                    
+                    $content="IDN: $autidn";
+                    if ($supplement){
+                        $content.=" ;$supplement";
+                    }
             }
             elsif ($titdefs_ref->{$category}{ref} eq 'kor'){
                 $koridn=get_koridn($content);
                 
                 if ($koridn > 0){
                     print KOEOUT "0000:$koridn\n";
-                    print KOEOUT "0001:$content\n;
+                    print KOEOUT "0001:$content\n";
                     print KOEOUT "9999:\n";
                 }
                 else {
@@ -266,11 +456,14 @@ foreach my $rawrec (@mab2titdata){
                 $content="IDN: $koridn";
             }
             elsif ($titdefs_ref->{$category}{ref} eq 'swt'){
+                $content=~s{^‡[a-z]}{}; # Anfang weg
+                $content=~s{‡[a-z]}{ / }g;
+
                 $swtidn=get_swtidn($content);
                 
                 if ($swtidn > 0){
                     print SWDOUT "0000:$swtidn\n";
-                    print SWDOUT "0001:$content\n;
+                    print SWDOUT "0001:$content\n";
                     print SWDOUT "9999:\n";
                 }
                 else {
@@ -284,7 +477,7 @@ foreach my $rawrec (@mab2titdata){
                 
                 if ($notidn > 0){
                     print SYSOUT "0000:$notidn\n";
-                    print SYSOUT "0001:$content\n;
+                    print SYSOUT "0001:$content\n";
                     print SYSOUT "9999:\n";
                 }
                 else {
@@ -312,6 +505,11 @@ close(KOEOUT);
 close(SWDOUT);
 close(SYSOUT);
 
+print "Genutzte Kategorien in den Quelldaten:\n";
+foreach my $category (keys %$used_categories_ref){
+    print "$category\n";
+}
+
 sub konv {
   my ($line)=@_;
 
@@ -320,7 +518,8 @@ sub konv {
   $line=~s/</&lt;/g;
   $line=~s/\x{0088}//g;
   $line=~s/\x{0089}//g;
-
+  $line=~s/u//;
+  $line=~s/a//;
   return $line;
 }
 

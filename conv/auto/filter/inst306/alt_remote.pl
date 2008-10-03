b6f404b000916b9f0b289203ff1cb979b949b652
flimm <flimm@e7c75b2f-8ac8-f892-83e0-76010f6c4c37>
2008-10-03 19:31:57 +0000

    - Korrektur: Log-Text

--- conv/auto/filter/inst306/alt_remote.pl
+++ conv/auto/filter/inst306/alt_remote.pl
@@ -39,6 +39,6 @@ my $konvdir       = $config->{'conv_dir'};
 
 my $pool          = $ARGV[0];
 
-print "### $pool: Extrahiere der Daten der Mikrofilme aus den USB-Daten\n";
+print "### $pool: Extrahiere der Instituts-Daten aus den EWA-Daten\n";
 
 system("$rootdir/filter/$pool/gen-subset-mex.pl $pool");

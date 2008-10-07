05afb49845f19abb004edfb37461352b3bee8760
flimm <flimm@e7c75b2f-8ac8-f892-83e0-76010f6c4c37>
2008-10-07 11:07:05 +0000

    - Ueberarbeitung (Titel anderer Ausgaben) und Anpassung an neues XML-Format

--- portal/htdocs/gm/kugamazon.user.js
+++ portal/htdocs/gm/kugamazon.user.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name          Anreicherung des KUG in Amazon
+// @name          Amazon mit KUG-Verfügbarkeit
 // @namespace     http:/kug.ub.uni-koeln.de/gm/
 // @description	  Verfügbarkeit im KUG in der Amazon Buchanzeige, basiert auf Skript von from http://www.mundell.org
 // @include       http://*.amazon.*/*
@@ -14,16 +14,12 @@ function()
 
     var libraryUrlPattern = 'http://kug.ub.uni-koeln.de/portal/lastverteilung?view=kug&fs='
     var libraryName = 'KUG';
-    var libraryAvailability = /\<status\>available\<\/status\>/;
-    var libraryOtherEditions = /other editions available/;
-    var notFound = /ISBN nicht gefunden/;
 
     var libraryLookup = 
     {
         insertLink: function(isbn, hrefTitle, aLabel, color )
         {
             var div = origTitle.parentNode;
-//            var title = origTitle.firstChild.nodeValue;
     	    var title = '';
 
             var newTitle = document.createElement('b');
@@ -46,7 +42,43 @@ function()
             div.insertBefore(newTitle, origTitle);
             div.insertBefore(link, origTitle);
             div.insertBefore(br, origTitle);
-//            div.removeChild(origTitle);
+        },
+
+       insertSimilar: function(isbn, aSimilarTitles, aLabel, color )
+        {                     
+            var div = origTitle.parentNode;
+    	    var title = '';
+
+            var newTitle = document.createElement('b');
+            newTitle.setAttribute('class','sans');
+
+            var titleText = document.createTextNode(title);
+            newTitle.appendChild(titleText);
+        
+            var br = document.createElement('br');
+
+            var span1 = document.createElement('span');
+            span1.setAttribute('style','font-size:small; font-weight:bold; color:' + color);
+            
+            var label = document.createTextNode( aLabel );
+
+            span1.appendChild(label);
+
+            var span2 = document.createElement('span');
+            span2.setAttribute('style','font-size:small; font-weight:bold; color:' + color);
+
+            var urlstring = " ( ";
+            for (var i=0; i<aSimilarTitles.snapshotLength; i++) {
+                var permalink = aSimilarTitles.snapshotItem(i).firstChild.nodeValue;
+                urlstring = urlstring+"<a href=\""+permalink+"\" target=\"_blank\">"+(i+1)+"</a> ";
+            }
+            urlstring = urlstring + ")";
+            span2.innerHTML = urlstring;
+            
+            div.insertBefore(newTitle, origTitle);
+            div.insertBefore(span1, origTitle);
+            div.insertBefore(span2, origTitle);
+            div.insertBefore(br, origTitle);
         },
 
         doLookup: function ( isbn )
@@ -54,46 +86,46 @@ function()
             GM_xmlhttpRequest
             ({
                 method:'GET',
-                url: 'http://kug5.ub.uni-koeln.de/portal/connector/availability/' + isbn,
+                url: 'http://kug.ub.uni-koeln.de/portal/connector/availability/' + isbn,
                 onload:function(results)
                 {
                     page = results.responseText;
+                    var pagexml = (new DOMParser()).parseFromString(page, "text/xml");
+
+                    var availability = pagexml.evaluate("//availability/size", pagexml, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null ).singleNodeValue.firstChild.nodeValue;
+                    //GM_log('Availability:'+availability);
+
+                    var similar_availability = pagexml.evaluate("//similar_record_availability/size", pagexml, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null ).singleNodeValue.firstChild.nodeValue;
+                    //GM_log('Similar Availability:'+similar_availability);
+
                     //alert(page);
-                    if ( notFound.test(page) )
-                    {
-                        var due = page.match(notFound)[1]
-                        libraryLookup.insertLink (
-                            origTitle.firstChild.nodeValue,
-                            "ISBN nicht gefunden",
-                            "ISBN nicht gefunden im " + libraryName + " - Klicken Sie hier f&uuml;r eine Recherche",
-                            "red"
-                        );
-                    }
-                    else if ( libraryAvailability.test(page) )
+                    if ( availability > 0 )
                     {
                         libraryLookup.insertLink (
                             isbn,
-                            "mindestens in einem Katalog verfügbar",
-                            "Verfügbar im KUG",
+                            "in mindestens einem Katalog vorhanden",
+                            availability+" Titel im KUG vorhanden",
                             "green"
                         );
                     }
-                    else if ( libraryOtherEditions.test(page) )
+                    else if ( similar_availability > 0 )
                     {
-                        libraryLookup.insertLink (
+                        var similar_titles = pagexml.evaluate("//similar_record_availability/catalogue/permalink", pagexml, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null );
+
+                        libraryLookup.insertSimilar (
                             isbn,
-                            "Andere Ausgaben verfügbar",
-                            "Im KUG sind andere Ausgaben dieses Titels verfügbar",
-                            "green"
+                            similar_titles,
+                            similar_availability+" Titel im KUG in anderen Ausgaben vorhanden ",
+                            "orange"
                         );
                     }
                     else
                     {
                         libraryLookup.insertLink (
                             isbn,
-                            "ISBN nicht vorhanden",
-                            "ISBN konnte nicht im KUG gefunden werden",
-                            "orange"
+                            "nicht im KUG vorhanden",
+                            "Titel nicht im KUG vorhanden",
+                            "red"
                         );
                     }
                 }
@@ -103,20 +135,23 @@ function()
 
     }
 
-    try 
-    { var isbn = window.content.location.href.match(/\/(\d{7,9}[\d|X|x])\//)[1]; }
-     
+    //GM_log('running on GBS Skript');
 
-    catch (e)
-    { return; }
-    
+    try { 
+      var isbn = window.content.location.href.match(/\/(\d{7,9}[\d|X|x])\//)[1];
+    }
+    catch (e) { 
+       //GM_log('Fehler im isbn try');
+       return; 
+    }
+
     var origTitle = document.evaluate("//span[@id='btAsinTitle']", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null ).singleNodeValue;
 
+//    GM_log('Titel:'+origTitle.firstChild.nodeValue );
     if ( ! origTitle )
     { return; }
 
-//     alert( isbn );
-//     alert( origTitle.firstChild.nodeValue );
+//     GM_log('ISBN:'+ isbn );
 
     libraryLookup.doLookup(isbn);
 

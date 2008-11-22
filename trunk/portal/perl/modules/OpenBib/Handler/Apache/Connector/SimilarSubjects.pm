b20397f8859b6ef597d55f4666dc5b157be90399
flimm <flimm@e7c75b2f-8ac8-f892-83e0-76010f6c4c37>
2008-11-22 18:13:33 +0000

    - Aenderung: Massgabe ist sinnvollerweise die Anzahl ueberlappender Titel fuer die Wolke und nicht die Gesamtzahl der Titel, die das betreffende Schlagwort besitzen.

--- trunk/portal/perl/modules/OpenBib/Handler/Apache/Connector/SimilarSubjects.pm
+++ trunk/portal/perl/modules/OpenBib/Handler/Apache/Connector/SimilarSubjects.pm
@@ -90,7 +90,8 @@ sub handler {
     my $dbh   = DBI->connect("DBI:$config->{dbimodule}:dbname=$database;host=$config->{dbhost};port=$config->{dbport}", $config->{dbuser}, $config->{dbpasswd}) or $logger->error_die($DBI::errstr);
 
     my $similar_subjects_ref = [];
-
+    my $tit_swt_count_ref = {};
+   
     my $maxcount=0;
 	my $mincount=999999999;
 
@@ -136,18 +137,34 @@ sub handler {
         $request->execute($id);
 
         my @swtids=();
+        my $swtid_lookup_ref = {}; 
         while (my $result=$request->fetchrow_hashref){
+            $swtid_lookup_ref->{$result->{id}}=1;
             push @swtids, $result->{id};
         }
 
         my %similar_done = ();
         foreach my $swtid (@swtids){
-            $request = $dbh->prepare("select distinct c2.targetid as id from conn as c1 left join conn as c2 on c1.sourceid=c2.sourceid where c1.sourcetype=1 and c2.sourcetype=1 and c1.targettype=4 and c2.targettype=4 and c1.targetid=? and c2.targetid != ?");
+            $request = $dbh->prepare("select distinct c2.targetid as id, count(c2.sourceid) as titcount from conn as c1 left join conn as c2 on c1.sourceid=c2.sourceid where c1.sourcetype=1 and c2.sourcetype=1 and c1.targettype=4 and c2.targettype=4 and c1.targetid=? and c2.targetid != ? group by c2.sourceid");
             $request->execute($swtid,$swtid);
             
             while (my $result=$request->fetchrow_hashref){
-                my $similarid = $result->{id};
+                my $similarid       = $result->{id};
+                my $similartitcount = $result->{titcount};
 
+                # Wenn das zu einem Schlagwort benachbarte Schlagwort schon im
+                # aktuellen Titel enthalten ist, dann ignorieren
+                next if (exists $swtid_lookup_ref->{$similarid});
+
+                # Counts bzgl verschiedener Schlagworte des aktuellen Titels werden alle gezaehlt
+                if (!exists $tit_swt_count_ref->{$similarid}){
+                    $tit_swt_count_ref->{$similarid}=$similartitcount;
+                }
+                else {
+                    $tit_swt_count_ref->{$similarid}+=$similartitcount;
+                }
+
+                # Jetzt wurde gezaehlt, aber ein Eintrag muss nicht angelegt werden.
                 if (exists $similar_done{$similarid}){
                     next;
                 }
@@ -158,27 +175,26 @@ sub handler {
                 my $record=OpenBib::Record::Subject->new({database=>$database});
                 $record->load_name({dbh => $dbh, id=>$similarid});
                 my $content=$record->name_as_string;
-
-                # Ausgabe der Anzahl verk"upfter Titel
-                my $sqlrequest="select count(distinct sourceid) as conncount from conn where targetid=? and sourcetype=1 and targettype=4";
-                my $request2=$dbh->prepare($sqlrequest) or $logger->error($DBI::errstr);
-                $request2->execute($similarid);
-                my $result2=$request2->fetchrow_hashref;
-                my $count = $result2->{conncount};
-                
-                if ($maxcount < $count){
-                    $maxcount = $count;
-                }
-                
-                if ($mincount > $count){
-                    $mincount = $count;
-                }
                 
                 push @{$similar_subjects_ref}, {
                     id      => $similarid,
                     item    => $content,
-                    count   => $count,
+#                    count   => $count,
                 };            
+            }
+        }
+
+        foreach my $single_subject_ref (@{$similar_subjects_ref}){
+            my $count=$tit_swt_count_ref->{$single_subject_ref->{id}}/2;
+
+            $single_subject_ref->{count} = $count;
+            
+            if ($maxcount < $count){
+                $maxcount = $count;
+            }
+            
+            if ($mincount > $count){
+                $mincount = $count;
             }
         }
     }

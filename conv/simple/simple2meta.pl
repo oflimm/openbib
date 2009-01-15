41ed2c8a8febfe3e356717310370f04032387f3b
flimm <flimm@e7c75b2f-8ac8-f892-83e0-76010f6c4c37>
2009-01-15 22:13:57 +0000

    - Grundsaetzliche Ueberarbeitung entsprechend cdm2meta.pl. Insbesondere Integration einer YAML-Konfigurationsdatei fuer die jeweilige Konvertierung

--- conv/simple/simple2meta.pl
+++ conv/simple/simple2meta.pl
@@ -31,421 +31,309 @@
 # Einladen der benoetigten Perl-Module
 #####################################################################
 
+use Encode 'decode';
 use Getopt::Long;
+use DBI;
+use YAML::Syck;
 
 use OpenBib::Config;
 
-# Importieren der Konfigurationsdaten als Globale Variablen
-# in diesem Namespace
+our (@autdubbuf,@kordubbuf,@swtdubbuf,@notdubbuf);
+
+@autdubbuf = ();
+@kordubbuf = ();
+@swtdubbuf = ();
+@notdubbuf = ();
+$mexidn  =  1;
+
+my $config = OpenBib::Config->instance;
+
+my ($inputfile,$configfile);
 
 &GetOptions(
-	    "filename=s"       => \$filename,
+	    "inputfile=s"          => \$inputfile,
+            "configfile=s"         => \$configfile,
 	    );
 
-if (!$filename){
+if (!$inputfile && !$configfile){
     print << "HELP";
 simple2meta.pl - Aufrufsyntax
 
-    simple2meta.pl --filename=xxx
+    simple2meta.pl --inputfile=xxx --configfile=yyy.yml
 HELP
 exit;
 }
 
-$bufferidx=0;
-
-# Kategorieflags
-
-%autkonv=(
-    'AutorPerson'   => '0101:', # Person
-);
-
-%korkonv=(
-    'lok. Bezug'   => 1, # Topographisches Schlagwort: Kopenhagen
-);
-
-%notkonv=(
-    'prim. Ikon.'   => 1, # Primaeres Ikonographices Schlagwort: 25 I 12  prospect of city, town panorama, silhouette of city
-);
-
-%swtkonv=(
-    'sek. Ikon.'   => 1,  # Sekundaeres Ikonographisches Schlagwort: Kopenhagen
-);
-
-# Kategoriemappings
-
-%titelkonv=(
-    'OBJ-Dok-Nr.'  => '0000:',
-    'Bez-Verwalt.' => '', # Verwaltung
-    'Ort'          => '', # Köln
-    'Art'          => '', # (öffentliche) Sammlung
-    'Sozietät'     => '', # Universitäts- und Stadtbibliothek Köln
-    'Abteilung'    => '', # Altes Buch
-    'Invent-Nr.'   => '', # GG4/925-4/5 (v. S. 13)
-# Bez-Künstler:Herstellung
-# Name:Manesson-Mallet, Alain
-# ber. Funkt.:Autor
-# Bez-Künstler:Herstellung
-# Name:Zunner, Johann David
-# ber. Funkt.:Verleger
-    'Dat-Art'      => '', # Datierung
-    'num. Dat.'    => '0425:', # 1686
-    'Entst-Ort'    => '0591:', # Frankfurt a. M.
-    'Entst-Lands.' => '0594:', # Hessen
-    'Entst-Land'   => '0595:', # Deutschland
-    'Obj-Titel'    => '0331:', # Kopenhagen
-    'Gattung'      => '', # Druckgraphik
-    'U-Gattung'    => '', # Buchillustration
-    'Sachbegriff'  => '', # Bild
-    'Farbmater.'   => '', # Druckfarbe (schwarz)
-    '(Träger-)Mat' => '0334:', # Papier
-    'Technik'      => '', # Radierung
-    'HöhexBreite'  => '0407:', # 19,9 x 15,4 (Blatt)
-    'Höhe'         => '', # 17,4 (Platte)
-    'Breite'       => '', # 15,4 (Blatt)
-    # Block Anfang
-    'Beschr-Art'   => '', # Titel
-    'Transkr.'     => '0750:', # COPENHAGVE
-    'Anbr-Ort'     => '', # Blatt 
-    'Farbmat.'     => '', # Druckfarbe (schwarz)
-    'Technik'      => '', # Typendruck
-    'Sprache'      => '', # Französisch
-    'Übersetz.'    => '', # x
-    # Block Ende
-    'Foto'         => '', # Reproduktion
-    'Aufnahmenr.'  => '', # CD9/09222359/00000004
-    'Aufn-Art'     => '', # Microfilm
-    'Repro-Nr.'    => '', # <a href="http://sweethardt.ub.uni-koeln.de/retro/archiv/09222359/00000004.jpg"><img src="http://sweethardt.ub.uni-koeln.de/retro/archiv/09222359/00000004.gif"></a>
-    'Erhaltung'    => '', # gut
-    'Datum'        => '', # 2001.11.12
-    'Urh-Instit.'  => '', # Universitäts- und Stadtbibliothek Köln
-    'Urh-Autor'    => '', # Beatrix Herling
-);
+# Ininitalisierung mit Config-Parametern
+my $convconfig = YAML::Syck::LoadFile($configfile);
 
 # Einlesen und Reorganisieren
 
-open(DAT,"$filename");
+open(DAT,"$inputfile");
 
-while (<DAT>){
-    $buffer[$bufferidx++]=$_;
-}
+open (TIT,     ">:utf8","unload.TIT");
+open (AUT,     ">:utf8","unload.PER");
+open (KOR,     ">:utf8","unload.KOE");
+open (NOTATION,">:utf8","unload.SYS");
+open (SWT,     ">:utf8","unload.SWD");
+open (MEX,     ">:utf8","unload.MEX");
 
-$buffer[$bufferidx++]="ENDE\n";
+my $titid = 1;
 
-#######################################################################
-# Umwandeln
+my @buffer = ();
+while (my $line=<DAT>){
+    
+    # Ende erreicht
+    if ($line=~/^$convconfig->{file}{rec_sep}/){
 
-$titidn=1;
-$titidx=0;
-
-$autidn=1;
-$autidx=0;
-
-$swtidn=1;
-$swtidx=0;
-
-$mexidn=1;
-$mexidx=0;
-
-$koridn=1;
-$koridx=0;
-
-$notidn=1;
-$notidx=0;
-
-$tempidx=0;
-
-$i=0;
-$ti=0;
-
-$autdublastidx=1;
-$kordublastidx=1;
-$notdublastidx=1;
-$swtdublastidx=1;
-
-while ($i < $#buffer){
-    if ($buffer[$i]=~/^ENDE/){
-        
-        #    $titbuffer[$titidx++]="0000:".$titidn;
-        
-        while ($ti < $tempidx){
-            ($kateg,$content)=$tempbuffer[$ti]=~/^(.+?):(.*?)$/;
+        printf TIT "0000:%d\n", $titid;
             
-            if ($titelkonv{$kateg}){
-                if ($titelkonv{$kateg} eq "0000:"){
-                    $content=sprintf "%d", $content;
-                }
-                $titbuffer[$titidx++]=$titelkonv{$kateg}.$content;
+        foreach my $thisline (@buffer){
+            my ($kateg,$content)=$thisline=~/^(.+?)$convconfig->{file}{sep_char}(.*?)$/;
+            my $content = decode($convconfig->{encoding},$content);
+            
+            if (exists $convconfig->{title}{$kateg}){
                 
+                if ($content){
+                    print TIT $convconfig->{title}{$kateg}.$content."\n";
+                }
             }
             
             # Autoren abarbeiten Anfang
-            
-            elsif (exists $autkonv{$kateg}){
-                my $supplement="";
-                if ($content =~/^(.+?)( ; \[.*?$)/){
-                   $content    = $1;
-                   $supplement = $2;
-                }
-                $autidn=get_autidn($content);
-                
-                if ($autidn > 0){
-                    $autbuffer[$autidx++]="0000:".$autidn;
-                    $autbuffer[$autidx++]="0001:".$content;
-                    $autbuffer[$autidx++]="9999:";
-                    
+            elsif (exists $convconfig->{pers}{$kateg} && $content){
+                my @parts = ();
+                if (exists $convconfig->{category_split_chars}{$kateg} && $content=~/$convconfig->{category_split_chars}{$kateg}/){
+                    @parts = split($convconfig->{category_split_chars}{$kateg},$content);
                 }
                 else {
-                    $autidn=(-1)*$autidn;
+                    push @parts, $content;
                 }
                 
-                $titbuffer[$titidx++]="0101:IDN: ".$autidn.$supplement;
-            }
+                foreach my $part (@parts){
+                    my $autidn=get_autidn($part);
+                    
+                    if ($autidn > 0){
+                        print AUT "0000:$autidn\n";
+                        print AUT "0001:$part\n";
+                        print AUT "9999:\n";
+                        
+                    }
+                    else {
+                        $autidn=(-1)*$autidn;
+                    }
+                    
+                    print TIT $convconfig->{pers}{$kateg}."IDN: $autidn\n";
+                }
+            }       
             # Autoren abarbeiten Ende
             
             # Koerperschaften abarbeiten Anfang
-            
-            elsif (exists $korkonv{$kateg}){
-                
-                $koridn=get_koridn($content);
-                
-                if ($koridn > 0){
-                    $korbuffer[$koridx++]="0000:".$koridn;
-                    $korbuffer[$koridx++]="0001:".$content;
-                    $korbuffer[$koridx++]="9999:";
-                    
+            elsif (exists $convconfig->{corp}{$kateg} && $content){
+                my @parts = ();
+                if (exists $convconfig->{category_split_chars}{$kateg} && $content=~/$convconfig->{category_split_chars}{$kateg}/){
+                    @parts = split($convconfig->{category_split_chars}{$kateg},$content);
                 }
                 else {
-                    $koridn=(-1)*$koridn;
+                    push @parts, $content;
                 }
                 
-                $titbuffer[$titidx++]="0200:IDN: ".$koridn;
+                foreach my $part (@parts){
+
+                    my $koridn=get_koridn($part);
+                
+                    if ($koridn > 0){
+                        print KOR "0000:$koridn\n";
+                        print KOR "0001:$part\n";
+                        print KOR "9999:\n";
+                        
+                    }
+                    else {
+                        $koridn=(-1)*$koridn;
+                    }
+                
+                    print TIT $convconfig->{corp}{$kateg}."IDN: $koridn\n";
+                }
             }
             # Koerperschaften abarbeiten Ende
 
             # Notationen abarbeiten Anfang
-            
-            elsif (exists $notkonv{$kateg}){
+            elsif (exists $convconfig->{sys}{$kateg} && $content){
+                my @parts = ();
+                if (exists $convconfig->{category_split_chars}{$kateg} && $content=~/$convconfig->{category_split_chars}{$kateg}/){
+                    @parts = split($convconfig->{category_split_chars}{$kateg},$content);
+                }
+                else {
+                    push @parts, $content;
+                }
                 
-                $notidn=get_notidn($content);
-                
-                if ($notidn > 0){
-                    $notbuffer[$notidx++]="0000:".$notidn;
-                    $notbuffer[$notidx++]="0001:".$content;
-                    $notbuffer[$notidx++]="9999:";
+                foreach my $part (@parts){
+                    my $notidn=get_notidn($part);
                     
+                    if ($notidn > 0){	  
+                        print NOTATION "0000:$notidn\n";
+                        print NOTATION "0001:$part\n";
+                        print NOTATION "9999:\n";
+                    }
+                    else {
+                        $notidn=(-1)*$notidn;
+                    }
+                    print TIT $convconfig->{sys}{$kateg}."IDN: $swtidn\n";
                 }
-                else {
-                    $notidn=(-1)*$notidn;
-                }
-                
-                $titbuffer[$titidx++]="0700:IDN: ".$notidn;
             }
-            # Notationen abarbeiten Ende
+            # Schlagworte abarbeiten Ende
 
-            # Schlagworte abarbeiten Anfang
-
-            elsif (exists $swtkonv{$kateg}){
-                $swtidn=get_swtidn($content);
-                
-                if ($swtidn > 0){	  
-                    $swtbuffer[$swtidx++]="0000:".$swtidn;
-                    $tempbuffer[$ti]=~s/\*/\//g;
-                    $swtbuffer[$swtidx++]="0001:".$content;
-                    $swtbuffer[$swtidx++]="9999:";
-                }
-                else {
-                    $swtidn=(-1)*$swtidn;
-                }
-                
-                $titbuffer[$titidx++]="0710:IDN: ".$swtidn;
-                # Schlagworte abarbeiten Ende
-                
-            }
             
-            $ti++;
+            # Schlagworte abarbeiten Anfang
+            elsif (exists $convconfig->{subj}{$kateg} && $content){
+                my @parts = ();
+                if (exists $convconfig->{category_split_chars}{$kateg} && $content=~/$convconfig->{category_split_chars}{$kateg}/){
+                    @parts = split($convconfig->{category_split_chars}{$kateg},$content);
+                }
+                else {
+                    push @parts, $content;
+                }
+                
+                foreach my $part (@parts){
+                    my $swtidn=get_swtidn($part);
+                    
+                    if ($swtidn > 0){	  
+                        print SWT "0000:$swtidn\n";
+                        print SWT "0001:$part\n";
+                        print SWT "9999:\n";
+                    }
+                    else {
+                        $swtidn=(-1)*$swtidn;
+                    }
+                    print TIT $convconfig->{subj}{$kateg}."IDN: $swtidn\n";
+                }
+            }
+            # Schlagworte abarbeiten Ende
+
+            elsif (exists $convconfig->{exempl}{$kateg} && $content){
+                my @parts = ();
+                if (exists $convconfig->{category_split_chars}{$kateg} && $content=~/$convconfig->{category_split_chars}{$kateg}/){
+                    @parts = split($convconfig->{category_split_chars}{$kateg},$content);
+                }
+                else {
+                    push @parts, $content;
+                }
+                
+                foreach my $part (@parts){
+                    print MEX "0000:$mexidn\n";
+                    print MEX "0004:$titid\n";
+                    print MEX $convconfig->{exempl}{$kateg}.$part."\n";
+                    print MEX "9999:\n";
+                    $mexidn++;
+                }
+            }
         }
-        
-        $titbuffer[$titidx++]="9999:";
-        
-        # Serien einordnen
-        
-        #$titidn++;
-        $tempidx=0;
-        $ti=0;
-        
-        undef @tempbuffer;
+        print TIT "9999:\n";
+        $titid++;
+        @buffer=();
     }
     else {
-        $tempbuffer[$tempidx++]=$buffer[$i];
+        push @buffer, $line;
     }
-    
-    $i++;
 }
-  
-$lasttitidx=$titidx;
-$lastautidx=$autidx;
-$lastnotidx=$notidx;
-$lastmexidx=$mexidx;
-$lastkoridx=$koridx;
-$lastswtidx=$swtidx;
 
-# Ausgabe der EXP-Dateien
-
-ausgabetitfile();
-ausgabeautfile();
-ausgabekorfile();
-ausgabenotfile();
-ausgabeswtfile();
+close(TIT);
+close(AUT);
+close(KOR);
+close(NOTATION);
+close(SWT);
+close(MEX);
 
 close(DAT);
 
-sub ausgabetitfile {
-  open (TIT,">:utf8","unload.TIT");
-  $i=0;
-  while ($i < $lasttitidx){
-    print TIT $titbuffer[$i],"\n";
-    $i++;
-  }
-  close(TIT);
-}
-
-sub ausgabeautfile {
-  open(AUT,">:utf8","unload.PER");
-  $i=0;
-  while ($i < $lastautidx){
-    print AUT $autbuffer[$i],"\n";
-    $i++;
-  }
-  close(AUT);
-}
-
-sub ausgabekorfile {
-  open(KOR,">:utf8","unload.KOE");
-  $i=0;
-  while ($i < $lastkoridx){
-    print KOR $korbuffer[$i],"\n";
-    $i++;
-  }
-  close(KOR);
-}
-
-sub ausgabenotfile {
-  open(NOTATION,">:utf8","unload.SYS");
-  $i=0;
-  while ($i < $lastnotidx){
-    print NOTATION $notbuffer[$i],"\n";
-    $i++;
-  }
-  close(NOTATION);
-}
-
-sub ausgabeswtfile {
-  open(SWT,">:utf8","unload.SWD");
-  $i=0;
-  while ($i < $lastswtidx) {
-    print SWT $swtbuffer[$i],"\n";
-    $i++;
-  }
-  close(SWT);
-}
-
 sub get_autidn {
-  ($autans)=@_;
-  
-  $autdubidx=$startautidn;
-  $autdubidn=0;
-  #  print "Autans: $autans\n";
-  
-  while ($autdubidx < $autdublastidx){
-    if ($autans eq $autdubbuf[$autdubidx]){
-      $autdubidn=(-1)*$autdubidx;      
-      
-      #      print "AutIDN schon vorhanden: $autdubidn\n";
+    my ($autans)=@_;
+
+#    print "AUT $autans\n";
+
+    my $autdubidx=1;
+    my $autdubidn=0;
+
+#    print "AUT",YAML::Dump(\@autdubbuf),"\n";
+
+    while ($autdubidx <= $#autdubbuf){
+        if ($autans eq $autdubbuf[$autdubidx]){
+            $autdubidn=(-1)*$autdubidx;      
+        }
+        $autdubidx++;
     }
-    $autdubidx++;
-  }
-  if (!$autdubidn){
-    $autdubbuf[$autdublastidx]=$autans;
-    $autdubidn=$autdublastidx;
-    #    print "AutIDN noch nicht vorhanden: $autdubidn\n";
-    $autdublastidx++;
+    if (!$autdubidn){
+        $autdubbuf[$autdubidx]=$autans;
+        $autdubidn=$autdubidx;
+    }
+
+#    print "AUT",YAML::Dump(\@autdubbuf),"\n";
     
-  }
-  return $autdubidn;
+#    print $autdubidn,"\n";
+    return $autdubidn;
 }
 
 sub get_swtidn {
-  ($swtans)=@_;
-  
-  $swtdubidx=$startswtidn;
-  $swtdubidn=0;
-  #  print "Swtans: $swtans\n";
-  
-  while ($swtdubidx < $swtdublastidx){
-    if ($swtans eq $swtdubbuf[$swtdubidx]){
-      $swtdubidn=(-1)*$swtdubidx;      
-      
-#            print "SwtIDN schon vorhanden: $swtdubidn, $swtdublastidx\n";
-    }
-    $swtdubidx++;
-  }
-  if (!$swtdubidn){
-    $swtdubbuf[$swtdublastidx]=$swtans;
-    $swtdubidn=$swtdublastidx;
-#        print "SwtIDN noch nicht vorhanden: $swtdubidn, $swtdubidx, $swtdublastidx\n";
-    $swtdublastidx++;
+    my ($swtans)=@_;
+
+#    print "SWT $swtans\n";
     
-  }
-  return $swtdubidn;
+    my $swtdubidx=1;
+    my $swtdubidn=0;
+
+#    print "SWT", YAML::Dump(\@swtdubbuf),"\n";
+    
+    while ($swtdubidx <= $#swtdubbuf){
+        if ($swtans eq $swtdubbuf[$swtdubidx]){
+            $swtdubidn=(-1)*$swtdubidx;      
+        }
+        $swtdubidx++;
+    }
+    if (!$swtdubidn){
+        $swtdubbuf[$swtdubidx]=$swtans;
+        $swtdubidn=$swtdubidx;
+    }
+#    print $swtdubidn,"\n";
+
+#    print "SWT", YAML::Dump(\@swtdubbuf),"\n";
+#    print "-----\n";
+    return $swtdubidn;
 }
 
 sub get_koridn {
-  ($korans)=@_;
-  
-  $kordubidx=$startkoridn;
-  $kordubidn=0;
-  #  print "Korans: $korans\n";
-  
-  while ($kordubidx < $kordublastidx){
-    if ($korans eq $kordubbuf[$kordubidx]){
-      $kordubidn=(-1)*$kordubidx;      
-      
-      #      print "KorIDN schon vorhanden: $kordubidn\n";
-    }
-    $kordubidx++;
-  }
-  if (!$kordubidn){
-    $kordubbuf[$kordublastidx]=$korans;
-    $kordubidn=$kordublastidx;
-    #    print "KorIDN noch nicht vorhanden: $kordubidn\n";
-    $kordublastidx++;
+    my ($korans)=@_;
     
-  }
-  return $kordubidn;
+    my $kordubidx=1;
+    my $kordubidn=0;
+    
+    while ($kordubidx <= $#kordubbuf){
+        if ($korans eq $kordubbuf[$kordubidx]){
+            $kordubidn=(-1)*$kordubidx;      
+        }
+        $kordubidx++;
+    }
+    if (!$kordubidn){
+        $kordubbuf[$kordubidx]=$korans;
+        $kordubidn=$kordubidx;
+    }
+    return $kordubidn;
 }
 
 sub get_notidn {
-  ($notans)=@_;
-  
-  $notdubidx=$startnotidn;
-  $notdubidn=0;
-  #  print "Notans: $notans\n";
-  
-  while ($notdubidx < $notdublastidx){
-    if ($notans eq $notdubbuf[$notdubidx]){
-      $notdubidn=(-1)*$notdubidx;      
-      
-      #      print "NotIDN schon vorhanden: $notdubidn\n";
-    }
-    $notdubidx++;
-  }
-  if (!$notdubidn){
-    $notdubbuf[$notdublastidx]=$notans;
-    $notdubidn=$notdublastidx;
-    #    print "NotIDN noch nicht vorhanden: $notdubidn\n";
-    $notdublastidx++;
+    my ($notans)=@_;
     
-  }
-  return $notdubidn;
+    my $notdubidx=1;
+    my $notdubidn=0;
+    
+    while ($notdubidx <= $#notdubbuf){
+        if ($notans eq $notdubbuf[$notdubidx]){
+            $notdubidn=(-1)*$notdubidx;      
+        }
+        $notdubidx++;
+    }
+    if (!$notdubidn){
+        $notdubbuf[$notdubidx]=$notans;
+        $notdubidn=$notdubidx;
+    }
+    return $notdubidn;
 }
 

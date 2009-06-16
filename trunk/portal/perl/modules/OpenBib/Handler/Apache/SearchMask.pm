b2a357dd080d5136c40b3e758445aa9d8404e27f
flimm <flimm@e7c75b2f-8ac8-f892-83e0-76010f6c4c37>
2009-06-16 05:49:41 +0000

    - Anpassung an die neue Aufrufsyntax von config->get_number_of_titles

--- trunk/portal/perl/modules/OpenBib/Handler/Apache/SearchMask.pm
+++ trunk/portal/perl/modules/OpenBib/Handler/Apache/SearchMask.pm
@@ -222,7 +222,7 @@ sub handler {
     }
 
     my $alldbs     = $config->get_number_of_dbs($config->get_viewinfo($view)->{profilename});
-    my $alldbcount = $config->get_number_of_titles($config->get_viewinfo($view)->{profilename});
+    my $alldbcount = $config->get_number_of_titles({ profile => $config->get_viewinfo($view)->{profilename}});
 
     my @queries    = $session->get_all_searchqueries();
 

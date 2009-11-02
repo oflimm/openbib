4fda0fece470c680bc127a03f3f20412e938d412
flimm <flimm@e7c75b2f-8ac8-f892-83e0-76010f6c4c37>
2009-11-02 18:48:16 +0000

    - Korrektur: Standardloglevel auf INFO geaendert

--- trunk/conv/meta/meta2sql.pl
+++ trunk/conv/meta/meta2sql.pl
@@ -69,7 +69,7 @@ my $conv_config = new OpenBib::Conv::Config({dbname => $database});
 $logfile=($logfile)?$logfile:"/var/log/openbib/meta2sql-$database.log";
 
 my $log4Perl_config = << "L4PCONF";
-log4perl.rootLogger=DEBUG, LOGFILE, Screen
+log4perl.rootLogger=INFO, LOGFILE, Screen
 log4perl.appender.LOGFILE=Log::Log4perl::Appender::File
 log4perl.appender.LOGFILE.filename=$logfile
 log4perl.appender.LOGFILE.mode=append

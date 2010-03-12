93618cf4143a7d1946361ccd9518bf46853050cd
flimm <flimm@e7c75b2f-8ac8-f892-83e0-76010f6c4c37>
2010-03-12 12:16:43 +0000

    - Erweiterte Dokumentation von Events

--- branches/devel-2_4-branch/portal/perl/modules/OpenBib/Session.pm
+++ branches/devel-2_4-branch/portal/perl/modules/OpenBib/Session.pm
@@ -1051,8 +1051,10 @@ sub log_event {
     # Redirects 
     # 500 => TOC / hbz-Server
     # 501 => TOC / ImageWaere-Server
-    # 502 => USB E-Books / Vollzugriff
-    # 503 => Nationallizenzen / Vollzugriff
+    # 502 => USB E-Books aus E-Books-Katalog / Vollzugriff
+    # 503 => USB E-Books ueber Anreicherung / Vollzugriff
+    # 504 => Projekt Gutenberg / Vollzugriff
+    # 505 => Nationallizenzen / Vollzugriff
     # 510 => BibSonomy
     # 520 => Wikipedia / Personen
     # 521 => Wikipedia / ISBN

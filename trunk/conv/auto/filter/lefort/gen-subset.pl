c1aa47e6da2343e537e17972fd8294fc88d261fb
flimm <flimm@e7c75b2f-8ac8-f892-83e0-76010f6c4c37>
2010-07-09 07:29:23 +0000

    - Hueh, hott. Erst ist das Fehlen von Sekundaerliteratur ein absolutes Manko und wir selektieren daher zusaetzlich nach dem Schlagwort Gerturd von Le Fort, dann darf sie auf gar keinen Fall zusammen mit dem Sammlungsbestand erscheinen. Daher wurde die zusaetzliche Selektion per Schlagwort nun wieder deaktiviert...

--- trunk/conv/auto/filter/lefort/gen-subset.pl
+++ trunk/conv/auto/filter/lefort/gen-subset.pl
@@ -75,23 +75,15 @@ while (my $result=$request->fetchrow_hashref()){
   $titidns{$result->{'titid'}}=1;
 }
 
-my $count=0;
+# print "### $pool: Bestimme Titel-ID's anhand des Schlagworts 'Le Fort, Gertrud%'\n";
 
-foreach my $key (keys %titidns){
-    $count++;
-}
+# $request=$dbh->prepare("select distinct conn.sourceid as titid from conn,swt where swt.category=1 and swt.content like 'Le Fort, Gertrud%' and conn.targetid=swt.id and conn.sourcetype=1 and conn.targettype=4") or $logger->error($DBI::errstr);
 
-print "### $pool: Gefundene Titel-ID's $count\n";
+# $request->execute() or $logger->error($DBI::errstr);;
 
-print "### $pool: Bestimme Titel-ID's anhand des Schlagworts 'Le Fort, Gertrud%'\n";
-
-$request=$dbh->prepare("select distinct conn.sourceid as titid from conn,swt where swt.category=1 and swt.content like 'Le Fort, Gertrud%' and conn.targetid=swt.id and conn.sourcetype=1 and conn.targettype=4") or $logger->error($DBI::errstr);
-
-$request->execute() or $logger->error($DBI::errstr);;
-
-while (my $result=$request->fetchrow_hashref()){
-  $titidns{$result->{'titid'}}=1;
-}
+# while (my $result=$request->fetchrow_hashref()){
+#   $titidns{$result->{'titid'}}=1;
+# }
 
 my $count=0;
 

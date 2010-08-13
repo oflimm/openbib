896ce0ccc594097f6bcbd19b07dcfbd91a89f592
flimm <flimm@e7c75b2f-8ac8-f892-83e0-76010f6c4c37>
2010-08-13 20:29:27 +0000

    - Basis-Klasse fuer alle cgiapp-Apache-Klassen

---
+++ branches/devel-2_4-branch/portal/perl/modules/OpenBib/Handler/Apache.pm
@@ -0,0 +1,44 @@
+#####################################################################
+#
+#  OpenBib::Handler::Apache
+#
+#  Dieses File ist (C) 2010 Oliver Flimm <flimm@openbib.org>
+#
+#  Dieses Programm ist freie Software. Sie koennen es unter
+#  den Bedingungen der GNU General Public License, wie von der
+#  Free Software Foundation herausgegeben, weitergeben und/oder
+#  modifizieren, entweder unter Version 2 der Lizenz oder (wenn
+#  Sie es wuenschen) jeder spaeteren Version.
+#
+#  Die Veroeffentlichung dieses Programms erfolgt in der
+#  Hoffnung, dass es Ihnen von Nutzen sein wird, aber OHNE JEDE
+#  GEWAEHRLEISTUNG - sogar ohne die implizite Gewaehrleistung
+#  der MARKTREIFE oder der EIGNUNG FUER EINEN BESTIMMTEN ZWECK.
+#  Details finden Sie in der GNU General Public License.
+#
+#  Sie sollten eine Kopie der GNU General Public License zusammen
+#  mit diesem Programm erhalten haben. Falls nicht, schreiben Sie
+#  an die Free Software Foundation, Inc., 675 Mass Ave, Cambridge,
+#  MA 02139, USA.
+#
+#####################################################################
+
+#####################################################################
+# Einladen der benoetigten Perl-Module
+#####################################################################
+
+package OpenBib::Handler::Apache;
+
+use strict;
+use warnings;
+no warnings 'redefine';
+use utf8;
+
+use base 'CGI::Application';
+
+sub cgiapp_init() {       # overrides
+   my $self = shift;
+   $self->query->charset('UTF-8');  # cause CGI.pm to send a UTF-8 Content-Type header
+}
+
+1;

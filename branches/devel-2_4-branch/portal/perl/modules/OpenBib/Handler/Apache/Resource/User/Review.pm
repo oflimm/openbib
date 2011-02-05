3da58d95325f18b7fbd08ed4d13394b7605f2a33
flimm <flimm@e7c75b2f-8ac8-f892-83e0-76010f6c4c37>
2011-02-05 15:23:27 +0000

    - Erweiterungen

--- branches/devel-2_4-branch/portal/perl/modules/OpenBib/Handler/Apache/Resource/User/Review.pm
+++ branches/devel-2_4-branch/portal/perl/modules/OpenBib/Handler/Apache/Resource/User/Review.pm
@@ -260,7 +260,10 @@ sub update_record {
     my $r              = $self->param('r');
 
     my $view           = $self->param('view')           || '';
-
+    my $userid         = $self->param('userid')         || '';
+    my $reviewid       = $self->param('reviewid')         || '';
+    
+    
     my $config = OpenBib::Config->instance;
     
     my $query  = Apache2::Request->new($r);
@@ -340,11 +343,61 @@ sub update_record {
         rating    => $rating,
     });
 
-    $self->return_basurl;
+    $self->return_baseurl;
 
     return;
 }
 
+sub delete_record {
+    my $self = shift;
+
+    # Log4perl logger erzeugen
+    my $logger = get_logger();
+    
+    my $r              = $self->param('r');
+
+    my $view           = $self->param('view')           || '';
+    my $userid         = $self->param('userid')         || '';
+    my $reviewid       = $self->param('reviewid')         || '';
+
+    my $config = OpenBib::Config->instance;
+    
+    my $query  = Apache2::Request->new($r);
+
+    my $session = OpenBib::Session->instance({ apreq => $r });
+
+    my $queryoptions = OpenBib::QueryOptions->instance($query);
+
+    # Message Katalog laden
+    my $msg = OpenBib::L10N->get_handle($queryoptions->get_option('l')) || $logger->error("L10N-Fehler");
+    $msg->fail_with( \&OpenBib::L10N::failure_handler );
+
+    if (!$session->is_valid()){
+        OpenBib::Common::Util::print_warning($msg->maketext("Ungültige Session"),$r,$msg);
+
+        return Apache2::Const::OK;
+    }
+
+    my $user = OpenBib::User->instance({sessionID => $session->{ID}});
+
+    my $loginname  = $user->get_username();
+
+    if (!$user->{ID}){
+        OpenBib::Common::Util::print_warning("Sie müssen sich authentifizieren, um taggen zu können",$r,$msg);
+        return Apache2::Const::OK;
+    }
+    
+    $logger->debug("Loeschung des Reviews");
+
+    $user->del_review_of_user({
+        id        => $reviewid,
+        loginname => $loginname,
+    });
+
+    $self->return_baseurl;
+
+    return;
+}
 
 sub show_record_form {
     my $self = shift;
@@ -465,7 +518,7 @@ sub show_record_form {
     return Apache2::Const::OK;
 }
 
-sub show_collectionxxx {
+sub show_record_negotiate {
     my $self = shift;
 
     # Log4perl logger erzeugen
@@ -474,6 +527,8 @@ sub show_collectionxxx {
     my $r              = $self->param('r');
 
     my $view           = $self->param('view')           || '';
+    my $userid         = $self->param('userid')           || '';
+    my $reviewid       = $self->param('reviewid')           || '';
 
     my $config = OpenBib::Config->instance;
     
@@ -493,7 +548,6 @@ sub show_collectionxxx {
     my $database       = $query->param('db')    || '';
     my $sorttype       = $query->param('srt')    || "author";
     my $sortorder      = $query->param('srto')   || "up";
-    my $reviewid       = $query->param('reviewid')    || '';
     my $titid          = $query->param('titid')       || '';
     my $titdb          = $query->param('titdb')       || '';
     my $titisbn        = $query->param('titisbn')     || '';
@@ -502,7 +556,7 @@ sub show_collectionxxx {
     my $nickname       = decode_utf8($query->param('nickname')) || '';
     my $rating         = $query->param('rating')      || 0;
 
-    my $do_show        = $query->param('do_show')     || '';
+    my $method         = $query->param('_method')     || '';
     my $do_add         = $query->param('do_add')      || '';
     my $do_change      = $query->param('do_change')   || '';
     my $do_edit        = $query->param('do_edit')     || '';
@@ -533,171 +587,80 @@ sub show_collectionxxx {
 
     my $user = OpenBib::User->instance({sessionID => $session->{ID}});
 
+    if (! $user->{ID}){
+        # Aufruf-URL
+        my $return_url = $r->parsed_uri->unparse;
+        
+        # Return-URL in der Session abspeichern
+        
+        $session->set_returnurl($return_url);
+        
+        $r->internal_redirect("http://$config->{servername}$config->{base_loc}/$view/$config->{handler}{login_loc}{name}");
+        
+        return Apache2::Const::OK;
+    }
+    
+    unless($user->{ID} eq $userid){
+        OpenBib::Common::Util::print_warning("Der Zugriff ist nicht authorisiert. Melden Sie sich als zugeh&ouml;riger Nutzer an. User:$user->{ID}",$r,$msg);
+        return Apache2::Const::OK;
+    }
+
+    if ($method eq "DELETE"){
+        $self->delete_record;
+        return;
+    }
+
     my $loginname  = $user->get_username();
     my $targettype = $user->get_targettype_of_session($session->{ID});
 
-
-    if ($do_show){
-
-        if (!$user->{ID}){
-            OpenBib::Common::Util::print_warning("Sie müssen sich authentifizieren, um taggen zu können",$r,$msg);
-            return Apache2::Const::OK;
-        }
-
-        my $reviewlist_ref = $user->get_reviews({loginname => $loginname});
-
-        foreach my $review_ref (@$reviewlist_ref){
-            my $titelidn = $review_ref->{titid};
-            my $database = $review_ref->{titdb};
-
-            $review_ref->{titnormset} = OpenBib::Record::Title->new({database=>$database})->load_brief_record({id=>$titelidn})->to_rawdata;
-        }
+    my $review_ref = $user->get_review_of_user({id => $reviewid, loginname => $loginname});
+    
+    {
+        my $titelidn = $review_ref->{titid};
+        my $database = $review_ref->{titdb};
         
-        # TT-Data erzeugen
-        my $ttdata={
-            view             => $view,
-            stylesheet       => $stylesheet,
-            queryoptions_ref => $queryoptions->get_options,
-            sessionID        => $session->{ID},
-            targettype       => $targettype,
-            dbinfo           => $dbinfotable,
-            reviews          => $reviewlist_ref,
-
-            config           => $config,
-            user             => $user,
-            msg              => $msg,
-        };
-
-        OpenBib::Common::Util::print_page($config->{tt_userreviews_show_tname},$ttdata,$r);
-
-        return Apache2::Const::OK;
+        $review_ref->{titnormset} = OpenBib::Record::Title->new({database=>$database})->load_brief_record({id=>$titelidn})->to_rawdata;
     }
-
-    if ($do_add){
-
-        if (!$user->{ID}){
-            OpenBib::Common::Util::print_warning("Sie müssen sich authentifizieren, um taggen zu können",$r,$msg);
-            return Apache2::Const::OK;
-        }
-
-        $logger->debug("Aufnehmen/Aendern des Reviews");
+    
+    # TT-Data erzeugen
+    my $ttdata={
+        view             => $view,
+        stylesheet       => $stylesheet,
+        queryoptions_ref => $queryoptions->get_options,
+        sessionID        => $session->{ID},
+        targettype       => $targettype,
+        dbinfo           => $dbinfotable,
+        review           => $review_ref,
         
-        $user->add_review({
-            titid     => $titid,
-            titdb     => $titdb,
-            loginname => $loginname,
-            nickname  => $nickname,
-            title     => $title,
-            review    => $review,
-            rating    => $rating,
-        });
-        
-        $r->internal_redirect("http://$config->{servername}$config->{base_loc}/$view/$config->{handler}{search_loc}{name}?database=$titdb;searchsingletit=$titid;queryid=$queryid;no_log=1");
-        return Apache2::Const::OK;
-    }
-
-    if ($do_change){
-
-        if (!$user->{ID}){
-            OpenBib::Common::Util::print_warning("Sie müssen sich authentifizieren, um taggen zu können",$r,$msg);
-            return Apache2::Const::OK;
-        }
-
-        $logger->debug("Aufnehmen/Aendern des Reviews");
-        
-        $user->add_review({
-            titid     => $titid,
-            titdb     => $titdb,
-            loginname => $loginname,
-            nickname  => $nickname,
-            title     => $title,
-            review    => $review,
-            rating    => $rating,
-        });
-        
-        $r->internal_redirect("http://$config->{servername}$config->{userreviews_loc}?sessionID=$session->{ID};do_show=1");
-        return Apache2::Const::OK;
-    }
-
-    if ($do_vote){
-
-        if (!$user->{ID}){
-            OpenBib::Common::Util::print_warning("Sie müssen sich authentifizieren, um diese Rezension zu beurteilen",$r,$msg);
-            return Apache2::Const::OK;
-        }
-
-        my $status = $user->vote_for_review({
-            reviewid  => $reviewid,
-            rating    => $rating,
-            loginname => $loginname,
-        });
-
-        if ($status == 1){
-            OpenBib::Common::Util::print_warning("Sie haben bereits diese Rezension beurteilt",$r,$msg);
-            return Apache2::Const::OK;
-        }
-        
-        $r->internal_redirect("http://$config->{servername}$config->{base_loc}/$view/$config->{handler}{search_loc}{name}?database=$titdb;searchsingletit=$titid;queryid=$queryid;no_log=1");
-        return Apache2::Const::OK;
-
-    }
-
-        if ($do_del){
-
-        if (!$user->{ID}){
-            OpenBib::Common::Util::print_warning("Sie müssen sich authentifizieren, um taggen zu können",$r,$msg);
-            return Apache2::Const::OK;
-        }
-
-        $user->del_review_of_user({
-            id        => $reviewid,
-            loginname => $loginname,
-        });
-
-        $r->internal_redirect("http://$config->{servername}$config->{base_loc}/$view/$config->{handler}{userreviews_loc}{name}?do_show=1");
-        return Apache2::Const::OK;
-
-    }
-
-            
-    if ($do_edit){
-        
-        if (!$user->{ID}){
-            OpenBib::Common::Util::print_warning("Sie müssen sich authentifizieren, um taggen zu können",$r,$msg);
-            return Apache2::Const::OK;
-        }
-
-        my $review_ref = $user->get_review_of_user({id => $reviewid, loginname => $loginname});
-
-        {
-            my $titelidn = $review_ref->{titid};
-            my $database = $review_ref->{titdb};
-
-            $review_ref->{titnormset} = OpenBib::Record::Title->new({database=>$database})->load_brief_record({id=>$titelidn})->to_rawdata;
-        }
-        
-        # TT-Data erzeugen
-        my $ttdata={
-            view             => $view,
-            stylesheet       => $stylesheet,
-            queryoptions_ref => $queryoptions->get_options,
-            sessionID        => $session->{ID},
-            targettype       => $targettype,
-            dbinfo           => $dbinfotable,
-            review           => $review_ref,
-
-            config           => $config,
-            user             => $user,
-            msg              => $msg,
-        };
-
-        OpenBib::Common::Util::print_page($config->{tt_userreviews_edit_tname},$ttdata,$r);
-        
-        return Apache2::Const::OK;
-    }
-
+        config           => $config,
+        user             => $user,
+        msg              => $msg,
+    };
+    
+    OpenBib::Common::Util::print_page($config->{tt_resource_user_review_single_tname},$ttdata,$r);
+    
     return Apache2::Const::OK;
 }
 
+sub return_baseurl {
+    my $self = shift;
+
+    # Log4perl logger erzeugen
+    my $logger = get_logger();
+    
+    my $view           = $self->param('view')           || '';
+    my $userid         = $self->param('userid')         || '';
+
+    my $config = OpenBib::Config->instance;
+
+    my $new_location = "$config->{base_loc}/$view/$config->{handler}{resource_user_loc}{name}/$userid/review.html";
+
+    $self->query->method('GET');
+    $self->query->content_type('text/html');
+    $self->query->headers_out->add(Location => $new_location);
+    $self->query->status(Apache2::Const::REDIRECT);
+
+    return;
+}
 
 1;

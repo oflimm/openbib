20d9ce5fbacd305fd158e07249a81ec61240fe00
flimm <flimm@e7c75b2f-8ac8-f892-83e0-76010f6c4c37>
2011-02-09 22:33:24 +0000

    - Beginn zur Erweiterung als Editierbare Resource

--- branches/devel-2_4-branch/portal/perl/modules/OpenBib/Handler/Apache/Resource/Library.pm
+++ branches/devel-2_4-branch/portal/perl/modules/OpenBib/Handler/Apache/Resource/Library.pm
@@ -69,7 +69,15 @@ sub setup {
 
     $self->start_mode('show');
     $self->run_modes(
-        'show'       => 'show',
+        'show_collection_negotiate'            => 'show_collection_negotiate',
+        'show_collection_as_html'              => 'show_collection_as_html',
+        'show_collection_as_json'              => 'show_collection_as_json',
+        'show_collection_as_rdf'               => 'show_collection_as_rdf',
+        'show_record_negotiate'                => 'show_record_negotiate',
+        'show_record_form'                     => 'show_record_form',
+        'create_record'                        => 'create_record',
+        'update_record'                        => 'update_record',
+        'delete_record'                        => 'delete_record',
     );
 
     # Use current path as template path,
@@ -77,7 +85,151 @@ sub setup {
 #    $self->tmpl_path('./');
 }
 
-sub show {
+sub show_collection_negotiate {
+    my $self = shift;
+
+    # Log4perl logger erzeugen
+    my $logger = get_logger();
+    
+    my $r              = $self->param('r');
+    my $view           = $self->param('view')             || '';
+
+    my $config  = OpenBib::Config->instance;
+
+    my $negotiated_type_ref = $self->negotiate_type;
+
+    my $new_location = "$config->{base_loc}/$view/$config->{handler}{resource_library_loc}{name}.$negotiated_type_ref->{suffix}";
+
+    $self->query->method('GET');
+    $self->query->content_type($negotiated_type_ref->{content_type});
+    $self->query->headers_out->add(Location => $new_location);
+    $self->query->status(Apache2::Const::REDIRECT);
+
+    $logger->debug("Default Information Resource Type: $negotiated_type_ref->{content_type} - URI: $new_location");
+
+    return;
+}
+
+sub show_collection_as_html {
+    my $self = shift;
+
+    $self->param('representation','html');
+
+    $self->show_collection;
+
+    return;
+}
+
+sub show_collection_as_json {
+    my $self = shift;
+
+    $self->param('representation','json');
+
+    $self->show_collection;
+
+    return;
+}
+
+sub show_collection_as_rdf {
+    my $self = shift;
+
+    $self->param('representation','rdf');
+
+    $self->show_collection;
+
+    return;
+}
+
+sub show_collection {
+    my $self = shift;
+
+    # Log4perl logger erzeugen
+    my $logger = get_logger();
+
+    $logger->debug("Entered show_collection");
+    my $r              = $self->param('r');
+
+    my $view           = $self->param('view')           || '';
+
+    my $config = OpenBib::Config->instance;
+    
+    my $query  = Apache2::Request->new($r);
+
+    my $session = OpenBib::Session->instance({ apreq => $r });
+
+    my $stylesheet=OpenBib::Common::Util::get_css_by_browsertype($r);
+  
+    #####################################################################
+    # Konfigurationsoptionen bei <FORM> mit Defaulteinstellungen
+    #####################################################################
+
+    my $offset         = $query->param('offset')      || 0;
+    my $hitrange       = $query->param('hitrange')    || 50;
+    my $queryid        = $query->param('queryid')     || '';
+    my $database       = $query->param('db')    || '';
+    my $sorttype       = $query->param('srt')    || "author";
+    my $sortorder      = $query->param('srto')   || "up";
+    my $reviewid       = $query->param('reviewid')    || '';
+    my $titid          = $query->param('titid')       || '';
+    my $titdb          = $query->param('titdb')       || '';
+    my $titisbn        = $query->param('titisbn')     || '';
+    my $title          = decode_utf8($query->param('title'))    || '';
+    my $review         = decode_utf8($query->param('review'))   || '';
+    my $nickname       = decode_utf8($query->param('nickname')) || '';
+    my $rating         = $query->param('rating')      || 0;
+
+    my $do_show        = $query->param('do_show')     || '';
+    my $do_add         = $query->param('do_add')      || '';
+    my $do_change      = $query->param('do_change')   || '';
+    my $do_edit        = $query->param('do_edit')     || '';
+    my $do_del         = $query->param('do_del')      || '';
+    my $do_vote        = $query->param('do_vote')      || '';
+    
+    #####                                                          ######
+    ####### E N D E  V A R I A B L E N D E K L A R A T I O N E N ########
+    #####                                                          ######
+  
+    ###########                                               ###########
+    ############## B E G I N N  P R O G R A M M F L U S S ###############
+    ###########                                               ###########
+
+    my $queryoptions = OpenBib::QueryOptions->instance($query);
+
+    # Message Katalog laden
+    my $msg = OpenBib::L10N->get_handle($queryoptions->get_option('l')) || $logger->error("L10N-Fehler");
+    $msg->fail_with( \&OpenBib::L10N::failure_handler );
+
+    my $dbinfotable = OpenBib::Config::DatabaseInfoTable->instance;
+
+    if (!$session->is_valid()){
+        OpenBib::Common::Util::print_warning($msg->maketext("Ungültige Session"),$r,$msg);
+
+        return Apache2::Const::OK;
+    }
+    
+    my $librarylist_ref = $config->get_libraries();
+    
+    # TT-Data erzeugen
+    my $ttdata={
+        view             => $view,
+        stylesheet       => $stylesheet,
+        queryoptions_ref => $queryoptions->get_options,
+        sessionID        => $session->{ID},
+        targettype       => $targettype,
+        dbinfo           => $dbinfotable,
+        libraries        => $librarylist_ref,
+        
+        config           => $config,
+        user             => $user,
+        msg              => $msg,
+    };
+    
+    OpenBib::Common::Util::print_page($config->{tt_resource_library_collection_tname},$ttdata,$r);
+    
+    return Apache2::Const::OK;
+}
+
+sub show_record_negotiate {
     my $self = shift;
 
     # Log4perl logger erzeugen
@@ -149,7 +301,6 @@ sub show {
 
         my $libinfo_ref = $config->get_libinfo($libraryid);
 
-
         my $ttdata = {
             view          => $view,
             representation => $representation,
@@ -171,9 +322,7 @@ sub show {
         };
 
         
-        my $templatename = "tt_resource_library_tname";
-
-        OpenBib::Common::Util::print_page($config->{$templatename},$ttdata,$r);
+        OpenBib::Common::Util::print_page($config->{tt_resource_library_tname},$ttdata,$r);
 
     }
 

866fbf23033be96e88c1e840bdeb11ab79be4b3b
flimm <flimm@e7c75b2f-8ac8-f892-83e0-76010f6c4c37>
2011-02-10 21:24:40 +0000

    - Anpassung an neues Dispatching

--- branches/devel-2_4-branch/portal/perl/modules/OpenBib/Handler/Apache/DatabaseChoice.pm
+++ branches/devel-2_4-branch/portal/perl/modules/OpenBib/Handler/Apache/DatabaseChoice.pm
@@ -55,9 +55,13 @@ use base 'OpenBib::Handler::Apache';
 sub setup {
     my $self = shift;
 
-    $self->start_mode('show');
+    $self->start_mode('show_collection_negotiate');
     $self->run_modes(
-        'show'       => 'show',
+        'show_collection_negotiate'            => 'show_collection_negotiate',
+        'show_collection_as_html'              => 'show_collection_as_html',
+        'show_collection_as_json'              => 'show_collection_as_json',
+        'show_collection_as_rdf'               => 'show_collection_as_rdf',
+        'update_collection'                    => 'update_collection',
     );
 
     # Use current path as template path,
@@ -65,7 +69,147 @@ sub setup {
 #    $self->tmpl_path('./');
 }
 
-sub show {
+sub show_collection_negotiate {
+    my $self = shift;
+
+    # Log4perl logger erzeugen
+    my $logger = get_logger();
+    
+    my $r              = $self->param('r');
+    my $view           = $self->param('view')           || '';
+
+    my $config  = OpenBib::Config->instance;
+
+    my $negotiated_type_ref = $self->negotiate_type;
+
+    my $new_location = "$config->{base_loc}/$view/$config->{handler}{databasechoice_loc}{name}.$negotiated_type_ref->{suffix}";
+
+    $self->query->method('GET');
+    $self->query->content_type($negotiated_type_ref->{content_type});
+    $self->query->headers_out->add(Location => $new_location);
+    $self->query->status(Apache2::Const::REDIRECT);
+
+    $logger->debug("Default Information Resource Type: $negotiated_type_ref->{content_type} - URI: $new_location");
+
+    return;
+}
+
+sub show_collection_as_html {
+    my $self = shift;
+
+    $self->param('representation','html');
+
+    $self->show_collection;
+
+    return;
+}
+
+sub show_collection_as_json {
+    my $self = shift;
+
+    $self->param('representation','json');
+
+    $self->show_collection;
+
+    return;
+}
+
+sub show_collection_as_rdf {
+    my $self = shift;
+
+    $self->param('representation','rdf');
+
+    $self->show_collection;
+
+    return;
+}
+
+sub show_collection {
+    my $self = shift;
+
+    # Log4perl logger erzeugen
+    my $logger = get_logger();
+    
+    my $r              = $self->param('r');
+
+    my $view           = $self->param('view')           || '';
+
+    my $config  = OpenBib::Config->instance;
+    
+    my $query   = Apache2::Request->new($r);
+
+    my $session = OpenBib::Session->instance({ apreq => $r });    
+
+    my $user    = OpenBib::User->instance({sessionID => $session->{ID}});
+
+    my $stylesheet=OpenBib::Common::Util::get_css_by_browsertype($r);
+  
+    # CGI-Uebergabe
+
+    my @databases = ($query->param('db'))?$query->param('db'):();
+    my $singleidn = $query->param('singleidn') || '';
+    my $action    = ($query->param('action'))?$query->param('action'):'';
+    my $do_choose = $query->param('do_choose') || '';
+    my $verf      = $query->param('verf')      || '';
+    my $hst       = $query->param('hst')       || '';
+    my $swt       = $query->param('swt')       || '';
+    my $kor       = $query->param('kor')       || '';
+    my $sign      = $query->param('sign')      || '';
+    my $isbn      = $query->param('isbn')      || '';
+    my $issn      = $query->param('issn')      || '';
+    my $notation  = $query->param('notation')  || '';
+    my $ejahr     = $query->param('ejahr')     || '';
+    my $queryid   = $query->param('queryid')   || '';
+    my $maxcolumn = $query->param('maxcolumn') || $config->{databasechoice_maxcolumn};
+  
+    my %checkeddb;
+
+    my $queryoptions = OpenBib::QueryOptions->instance($query);
+    
+    # Message Katalog laden
+    my $msg = OpenBib::L10N->get_handle($queryoptions->get_option('l')) || $logger->error("L10N-Fehler");
+    $msg->fail_with( \&OpenBib::L10N::failure_handler );
+
+    if (!$session->is_valid()){
+        OpenBib::Common::Util::print_warning($msg->maketext("Ungültige Session"),$r,$msg);
+        return Apache2::Const::OK;
+    }
+
+    my $profile = $config->get_viewinfo($view)->profilename;
+
+    my $idnresult="";
+  
+    # Ausgewaehlte Datenbanken bestimmen
+    my $checkeddb_ref = {};
+    foreach my $dbname ($session->get_dbchoice()){
+        $checkeddb_ref->{$dbname}=1;
+    }
+
+    $logger->debug("Ausgewaehlte Datenbanken".YAML::Dump($checkeddb_ref));
+    
+    my @catdb     = $config->get_infomatrix_of_active_databases({session => $session, checkeddb_ref => $checkeddb_ref, view => $view });
+    
+    # TT-Data erzeugen
+    my $colspan=$maxcolumn*3;
+    
+    my $ttdata={
+        view       => $view,
+        profile    => $profile,
+        stylesheet => $stylesheet,
+        sessionID  => $session->{ID},
+        maxcolumn  => $maxcolumn,
+        colspan    => $colspan,
+        catdb      => \@catdb,
+        config     => $config,
+        user       => $user,
+        msg        => $msg,
+    };
+    
+    OpenBib::Common::Util::print_page($config->{tt_databasechoice_tname},$ttdata,$r);
+    return Apache2::Const::OK;
+}
+
+sub update_collection {
     my $self = shift;
 
     # Log4perl logger erzeugen
@@ -120,52 +264,25 @@ sub show {
 
     my $idnresult="";
   
-    # Wenn Kataloge ausgewaehlt wurden
-    if ($do_choose) {
-        # Zuerst die bestehende Auswahl loeschen
-        $session->clear_dbchoice();
-      
-        # Wenn es eine neue Auswahl gibt, dann wird diese eingetragen
-        foreach my $database (@databases) {
-            $session->set_dbchoice($database);
-        }
-
-        # Neue Datenbankauswahl ist voreingestellt
-        $session->set_profile('dbauswahl');
-      
-        $r->internal_redirect("http://$config->{servername}$config->{base_loc}/$view/$config->{handler}{searchmask_loc}{name}");
-    }
-    # ... sonst anzeigen
-    else {
-
-        # Ausgewaehlte Datenbanken bestimmen
-        my $checkeddb_ref = {};
-        foreach my $dbname ($session->get_dbchoice()){
-            $checkeddb_ref->{$dbname}=1;
-        }
-        
-        my @catdb     = $config->get_infomatrix_of_active_databases({session => $session, checkeddb_ref => $checkeddb_ref, view => $view });
-        
-        # TT-Data erzeugen
-        my $colspan=$maxcolumn*3;
-
-        my $ttdata={
-            view       => $view,
-            profile    => $profile,
-            stylesheet => $stylesheet,
-            sessionID  => $session->{ID},
-            maxcolumn  => $maxcolumn,
-            colspan    => $colspan,
-            catdb      => \@catdb,
-            config     => $config,
-            user       => $user,
-            msg        => $msg,
-        };
+    # Zuerst die bestehende Auswahl loeschen
+    $session->clear_dbchoice();
     
-        OpenBib::Common::Util::print_page($config->{tt_databasechoice_tname},$ttdata,$r);
-        return Apache2::Const::OK;
+    # Wenn es eine neue Auswahl gibt, dann wird diese eingetragen
+    foreach my $database (@databases) {
+        $session->set_dbchoice($database);
     }
-    return Apache2::Const::OK;
+    
+    # Neue Datenbankauswahl ist voreingestellt
+    $session->set_profile('dbauswahl');
+
+    my $new_location = "$config->{base_loc}/$view/$config->{handler}{searchform_loc}{name}.html";
+
+    $self->query->method('GET');
+    $self->query->content_type('text/html');
+    $self->query->headers_out->add(Location => $new_location);
+    $self->query->status(Apache2::Const::REDIRECT);
+
+    return;
 }
 
 1;

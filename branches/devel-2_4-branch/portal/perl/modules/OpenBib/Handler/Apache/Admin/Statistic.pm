314c7b7c60647113496ec7173ebac635242526aa
flimm <flimm@e7c75b2f-8ac8-f892-83e0-76010f6c4c37>
2011-05-10 12:19:46 +0000

    - Umbenennung Statistics.pm nach Statistic.pm

---
+++ branches/devel-2_4-branch/portal/perl/modules/OpenBib/Handler/Apache/Admin/Statistic.pm
@@ -0,0 +1,289 @@
+#####################################################################
+#
+#  OpenBib::Handler::Apache::Admin::Statistic
+#
+#  Dieses File ist (C) 2004-2011 Oliver Flimm <flimm@openbib.org>
+#
+#  Dieses Programm ist freie Software. Sie koennen es unter
+#  den Bedingungen der GNU General Public License, wie von der
+#  Free Software Foundation herausgegeben, weitergeben und/oder
+#  modifizieren, entweder unter Version 2 der Lizenz oder (wenn
+#  Sie es wuenschen) jeder spaeteren Version.
+#
+#  Die Veroeffentlichung dieses Programms erfolgt in der
+#  Hoffnung, dass es Ihnen von Nutzen sein wird, aber OHNE JEDE
+#  GEWAEHRLEISTUNG - sogar ohne die implizite Gewaehrleistung
+#  der MARKTREIFE oder der EIGNUNG FUER EINEN BESTIMMTEN ZWECK.
+#  Details finden Sie in der GNU General Public License.
+#
+#  Sie sollten eine Kopie der GNU General Public License zusammen
+#  mit diesem Programm erhalten haben. Falls nicht, schreiben Sie
+#  an die Free Software Foundation, Inc., 675 Mass Ave, Cambridge,
+#  MA 02139, USA.
+#
+#####################################################################
+
+#####################################################################
+# Einladen der benoetigten Perl-Module
+#####################################################################
+
+package OpenBib::Handler::Apache::Admin::Statistic;
+
+use strict;
+use warnings;
+no warnings 'redefine';
+use utf8;
+
+use Apache2::Const -compile => qw(:common);
+use Apache2::Log;
+use Apache2::Reload;
+use Apache2::RequestRec ();
+use Apache2::Request ();
+use Apache2::SubRequest ();
+use Date::Manip qw/ParseDate UnixDate/;
+use DBI;
+use Digest::MD5;
+use Encode qw/decode_utf8 encode_utf8/;
+use Log::Log4perl qw(get_logger :levels);
+use POSIX;
+use SOAP::Lite;
+use Template;
+
+use OpenBib::Common::Util;
+use OpenBib::Config;
+use OpenBib::Config::DatabaseInfoTable;
+use OpenBib::Database::Config;
+use OpenBib::L10N;
+use OpenBib::QueryOptions;
+use OpenBib::Session;
+use OpenBib::Statistics;
+use OpenBib::User;
+
+use base 'OpenBib::Handler::Apache';
+
+# Run at startup
+sub setup {
+    my $self = shift;
+
+    $self->start_mode('show');
+    $self->run_modes(
+        'show_overview_negotiate'   => 'show_overview_negotiate',
+        'show_overview_as_html'     => 'show_overview_as_html',
+        'show_overview_as_rdf'      => 'show_overview_as_rdf',
+        'show_overview_as_json'     => 'show_overview_as_json',
+        'show_overview_as_include'  => 'show_overview_as_include',
+        'show_statistics_negotiate' => 'show_statistics_negotiate',
+    );
+
+    # Use current path as template path,
+    # i.e. the template is in the same directory as this script
+#    $self->tmpl_path('./');
+}
+
+sub show_overview_negotiate {
+    my $self = shift;
+
+    # Log4perl logger erzeugen
+    my $logger = get_logger();
+    
+    my $r              = $self->param('r');
+
+    my $config  = OpenBib::Config->instance;
+
+    my $negotiated_type_ref = $self->negotiate_type;
+
+    my $new_location = "$config->{base_loc}/$config->{admin_statistics_loc}.$negotiated_type_ref->{suffix}";
+
+    $self->query->method('GET');
+    $self->query->content_type($negotiated_type_ref->{content_type});
+    $self->query->headers_out->add(Location => $new_location);
+    $self->query->status(Apache2::Const::REDIRECT);
+
+    $logger->debug("Default Information Resource Type: $negotiated_type_ref->{content_type} - URI: $new_location");
+
+    return;
+}
+
+sub show_overview_as_html {
+    my $self = shift;
+
+    $self->param('representation','html');
+
+    $self->show_overview;
+
+    return;
+}
+
+sub show_overview_as_rdf {
+    my $self = shift;
+
+    $self->param('representation','rdf');
+
+    $self->show_overview;
+
+    return;
+}
+
+
+sub show_overview_as_json {
+    my $self = shift;
+
+    $self->param('representation','json');
+
+    $self->show_overview;
+
+    return;
+}
+
+sub show_overview_as_include {
+    my $self = shift;
+
+    $self->param('representation','include');
+
+    $self->show_overview;
+
+    return;
+}
+
+sub show_overview {
+    my $self = shift;
+
+    # Log4perl logger erzeugen
+    my $logger = get_logger();
+    
+    my $r              = $self->param('r');
+
+    my $view           = $self->param('view')           || '';
+    my $representation = $self->param('representation') || '';
+
+    my $config  = OpenBib::Config->instance;
+    my $session = OpenBib::Session->instance({ apreq => $r });
+    my $query   = Apache2::Request->new($r);
+
+    my $stylesheet   = OpenBib::Common::Util::get_css_by_browsertype($r);
+
+    my $queryoptions = OpenBib::QueryOptions->instance($query);
+
+    # Message Katalog laden
+    my $msg = OpenBib::L10N->get_handle($queryoptions->get_option('l')) || $logger->error("L10N-Fehler");
+    $msg->fail_with( \&OpenBib::L10N::failure_handler );
+
+    my $adminuser   = $config->{adminuser};
+    my $adminpasswd = $config->{adminpasswd};
+
+    # Ist der Nutzer ein Admin?
+    my $user         = OpenBib::User->instance({sessionID => $session->{ID}});
+    
+    # Admin-SessionID ueberpruefen
+    # Entweder als Master-Adminuser eingeloggt, oder der Benutzer besitzt die Admin-Rolle
+    my $adminsession = $session->is_authenticated_as($adminuser) || $user->is_admin;
+
+    if (!$adminsession) {
+        OpenBib::Common::Util::print_warning($msg->maketext("Sie greifen auf eine nicht autorisierte Session zu"),$r,$msg);
+        return Apache2::Const::OK;
+    }
+
+    my $content_type   = $config->{'content_type_map_rev'}{$representation};
+
+    my $statistics = new OpenBib::Statistics();
+    
+    # TT-Data erzeugen
+    my $ttdata={
+        view       => $view,
+        
+        session    => $session,
+        statistics => $statistics,
+        user       => $user,
+        config     => $config,
+        msg        => $msg,
+    };
+    
+    my $templatename = "tt_admin_statistic_tname";
+    
+    OpenBib::Common::Util::print_page($config->{$templatename},$ttdata,$r);
+
+}
+
+sub show_statistics_negotiate {
+    my $self = shift;
+
+    # Log4perl logger erzeugen
+    my $logger = get_logger();
+    
+    my $r              = $self->param('r');
+
+    my $view           = $self->param('view')           || '';
+    my $statisticid    = $self->param('statisticid')    || '';
+
+    my $config  = OpenBib::Config->instance;
+    my $session = OpenBib::Session->instance({ apreq => $r });
+    my $query   = Apache2::Request->new($r);
+
+    my $stylesheet   = OpenBib::Common::Util::get_css_by_browsertype($r);
+
+    my $queryoptions = OpenBib::QueryOptions->instance($query);
+
+    # Message Katalog laden
+    my $msg = OpenBib::L10N->get_handle($queryoptions->get_option('l')) || $logger->error("L10N-Fehler");
+    $msg->fail_with( \&OpenBib::L10N::failure_handler );
+
+    my $adminuser   = $config->{adminuser};
+    my $adminpasswd = $config->{adminpasswd};
+
+    # Ist der Nutzer ein Admin?
+    my $user         = OpenBib::User->instance({sessionID => $session->{ID}});
+    
+    # Admin-SessionID ueberpruefen
+    # Entweder als Master-Adminuser eingeloggt, oder der Benutzer besitzt die Admin-Rolle
+    my $adminsession = $session->is_authenticated_as($adminuser) || $user->is_admin;
+
+    if (!$adminsession) {
+        OpenBib::Common::Util::print_warning($msg->maketext("Sie greifen auf eine nicht autorisierte Session zu"),$r,$msg);
+        return Apache2::Const::OK;
+    }
+
+    # Mit Suffix, dann keine Aushandlung des Typs
+
+    my $representation = "";
+    my $content_type   = "";
+
+    my $statistic         = "";
+    if ($statisticid=~/^(.+?)(\.html|\.json|\.rdf|\.include)$/){
+        $statistic        = $1;
+        ($representation) = $2 =~/^\.(.+?)$/;
+        $content_type   = $config->{'content_type_map_rev'}{$representation};
+    }
+    # Sonst Aushandlung
+    else {
+        $statistic = $statisticid;
+        my $negotiated_type = $self->negotiate_type;
+        $representation = $negotiated_type->{suffix};
+        $content_type   = $negotiated_type->{content_type};
+    }
+
+    # Variables
+    my $year       = $query->param('year')       || '';
+
+    my $statistics = new OpenBib::Statistics();
+    
+    # TT-Data erzeugen
+    my $ttdata={
+        view       => $view,
+        
+        year       => $year,
+        
+        session    => $session,
+        statistics => $statistics,
+        user       => $user,
+        config     => $config,
+        msg        => $msg,
+    };
+    
+    my $templatename = ($statistic)?"tt_admin_statistic_".$statistic."_tname":"tt_admin_statistic_tname";
+    
+    OpenBib::Common::Util::print_page($config->{$templatename},$ttdata,$r);
+
+}
+
+
+1;

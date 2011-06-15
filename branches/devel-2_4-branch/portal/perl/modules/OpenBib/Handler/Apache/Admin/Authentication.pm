b2f356afcd2ed560f101bc75905e590d6823ab36
flimm <flimm@e7c75b2f-8ac8-f892-83e0-76010f6c4c37>
2011-06-15 06:48:02 +0000

    - Auslagerung des Codes fuer die Authentifizierung an Fremdsystemen

---
+++ branches/devel-2_4-branch/portal/perl/modules/OpenBib/Handler/Apache/Admin/Authentication.pm
@@ -0,0 +1,524 @@
+#####################################################################
+#
+#  OpenBib::Handler::Apache::Admin::Authentication
+#
+#  Dieses File ist (C) 2004-2011 Oliver Flimm <flimm@openbib.org>
+#
+#  Dieses Programm ist freie Software. Sie koennen es unter
+#  den Bedingungen der GNU General Public License, wie von der
+#  Free Software Foundation herausgegeben, weitergeben und/oder
+#  modifizieren, entweder unter Version 2 der Lizenz oder (wenn
+#  Sie es wuenschen) jeder spaeteren Version.
+#
+#  Die Veroeffentlichung dieses Programms erfolgt in der
+#  Hoffnung, dass es Ihnen von Nutzen sein wird, aber OHNE JEDE
+#  GEWAEHRLEISTUNG - sogar ohne die implizite Gewaehrleistung
+#  der MARKTREIFE oder der EIGNUNG FUER EINEN BESTIMMTEN ZWECK.
+#  Details finden Sie in der GNU General Public License.
+#
+#  Sie sollten eine Kopie der GNU General Public License zusammen
+#  mit diesem Programm erhalten haben. Falls nicht, schreiben Sie
+#  an die Free Software Foundation, Inc., 675 Mass Ave, Cambridge,
+#  MA 02139, USA.
+#
+#####################################################################
+
+#####################################################################
+# Einladen der benoetigten Perl-Module
+#####################################################################
+
+package OpenBib::Handler::Apache::Admin::Authentication;
+
+use strict;
+use warnings;
+no warnings 'redefine';
+use utf8;
+
+use Apache2::Const -compile => qw(:common :http);
+use Apache2::Log;
+use Apache2::Reload;
+use Apache2::RequestRec ();
+use Apache2::Request ();
+use Apache2::SubRequest ();
+use Date::Manip qw/ParseDate UnixDate/;
+use DBI;
+use Digest::MD5;
+use Encode qw/decode_utf8 encode_utf8/;
+use JSON::XS;
+use List::MoreUtils qw(none any);
+use Log::Log4perl qw(get_logger :levels);
+use POSIX;
+use Template;
+
+use OpenBib::Common::Util;
+use OpenBib::Config;
+use OpenBib::Config::DatabaseInfoTable;
+use OpenBib::Database::Config;
+use OpenBib::L10N;
+use OpenBib::QueryOptions;
+use OpenBib::Session;
+use OpenBib::Statistics;
+use OpenBib::User;
+
+use CGI::Application::Plugin::Redirect;
+
+use base 'OpenBib::Handler::Apache';
+
+# Run at startup
+sub setup {
+    my $self = shift;
+
+    $self->start_mode('show_collection');
+    $self->run_modes(
+        'show_collection_negotiate' => 'show_collection_negotiate',
+        'show_collection_as_html'   => 'show_collection_as_html',
+        'show_collection_as_json'   => 'show_collection_as_json',
+        'show_collection_as_rdf'    => 'show_collection_as_rdf',
+        'show_record_form'          => 'show_record_form',
+        'create_record'             => 'create_record',
+        'update_record'             => 'update_record',
+        'delete_record'             => 'delete_record',        
+    );
+
+    # Use current path as template path,
+    # i.e. the template is in the same directory as this script
+#    $self->tmpl_path('./');
+}
+
+sub show_collection_negotiate {
+    my $self = shift;
+
+    # Log4perl logger erzeugen
+    my $logger = get_logger();
+    
+    my $r              = $self->param('r');
+    my $view           = $self->param('view')                   || '';
+
+    my $config  = OpenBib::Config->instance;
+
+    my $negotiated_type_ref = $self->negotiate_type;
+
+    my $new_location = "$config->{base_loc}/$view/$config->{admin_authentication_loc}.$negotiated_type_ref->{suffix}";
+
+    $self->query->method('GET');
+    $self->query->content_type($negotiated_type_ref->{content_type});
+    $self->query->headers_out->add(Location => $new_location);
+    $self->query->status(Apache2::Const::REDIRECT);
+
+    $logger->debug("Default Information Resource Type: $negotiated_type_ref->{content_type} - URI: $new_location");
+
+    return;
+}
+
+sub show_collection_as_html {
+    my $self = shift;
+
+    $self->param('representation','html');
+
+    $self->show_collection;
+
+    return;
+}
+
+sub show_collection_as_json {
+    my $self = shift;
+
+    $self->param('representation','json');
+
+    $self->show_collection;
+
+    return;
+}
+
+sub show_collection_as_rdf {
+    my $self = shift;
+
+    $self->param('representation','rdf');
+
+    $self->show_collection;
+
+    return;
+}
+
+sub show_collection {
+    my $self = shift;
+
+    # Log4perl logger erzeugen
+    my $logger = get_logger();
+    
+    my $r              = $self->param('r');
+
+    my $view           = $self->param('view')                   || '';
+    my $representation = $self->param('representation') || '';
+
+    my $config  = OpenBib::Config->instance;
+    my $session = OpenBib::Session->instance({ apreq => $r });
+    my $query   = Apache2::Request->new($r);
+
+    my $stylesheet   = OpenBib::Common::Util::get_css_by_browsertype($r);
+
+    my $queryoptions = OpenBib::QueryOptions->instance($query);
+
+    # Message Katalog laden
+    my $msg = OpenBib::L10N->get_handle($queryoptions->get_option('l')) || $logger->error("L10N-Fehler");
+    $msg->fail_with( \&OpenBib::L10N::failure_handler );
+
+    my $adminuser   = $config->{adminuser};
+    my $adminpasswd = $config->{adminpasswd};
+    
+    # Ist der Nutzer ein Admin?
+    my $user         = OpenBib::User->instance({sessionID => $session->{ID}});
+
+    # Admin-SessionID ueberpruefen
+    # Entweder als Master-Adminuser eingeloggt, oder der Benutzer besitzt die Admin-Rolle
+    my $adminsession = $session->is_authenticated_as($adminuser) || $user->is_admin;
+
+    if (!$adminsession) {
+        OpenBib::Common::Util::print_warning($msg->maketext("Sie greifen auf eine nicht autorisierte Session zu"),$r,$msg);
+        return Apache2::Const::OK;
+    }
+
+    $logger->debug("Server: ".$r->get_server_name."Representation: $representation");
+
+    # TT-Data erzeugen
+    my $ttdata={
+        view       => $view,
+        
+        sessionID  => $session->{ID},
+        
+        session    => $session,
+        
+        user       => $user,
+        config     => $config,
+        msg        => $msg,
+    };
+    
+    OpenBib::Common::Util::print_page($config->{tt_admin_authentication_tname},$ttdata,$r);
+}
+
+sub show_record_form {
+    my $self = shift;
+
+    # Log4perl logger erzeugen
+    my $logger = get_logger();
+    
+    my $r              = $self->param('r');
+
+    my $view             = $self->param('view')                   || '';
+    my $authenticationid = $self->param('authenticationid')       || '';
+
+    my $config  = OpenBib::Config->instance;
+    my $session = OpenBib::Session->instance({ apreq => $r });
+    my $query   = Apache2::Request->new($r);
+
+    my $stylesheet   = OpenBib::Common::Util::get_css_by_browsertype($r);
+
+    my $queryoptions = OpenBib::QueryOptions->instance($query);
+
+    # Message Katalog laden
+    my $msg = OpenBib::L10N->get_handle($queryoptions->get_option('l')) || $logger->error("L10N-Fehler");
+    $msg->fail_with( \&OpenBib::L10N::failure_handler );
+            
+    my $adminuser   = $config->{adminuser};
+    my $adminpasswd = $config->{adminpasswd};
+    
+    # Ist der Nutzer ein Admin?
+    my $user         = OpenBib::User->instance({sessionID => $session->{ID}});
+
+    if (!$user->authentication_exists($authenticationid)) {        
+        OpenBib::Common::Util::print_warning($msg->maketext("Es existiert kein Katalog unter diesem Namen"),$r,$msg);
+        
+        return Apache2::Const::OK;
+    }
+
+    # Admin-SessionID ueberpruefen
+    # Entweder als Master-Adminuser eingeloggt, oder der Benutzer besitzt die Admin-Rolle
+    my $adminsession = $session->is_authenticated_as($adminuser) || $user->is_admin;
+
+    if (!$adminsession) {
+        OpenBib::Common::Util::print_warning($msg->maketext("Sie greifen auf eine nicht autorisierte Session zu"),$r,$msg);
+        return Apache2::Const::OK;
+    }
+
+    $logger->debug("Server: ".$r->get_server_name);
+
+    my $logintarget_ref = $user->get_logintarget_by_id($authenticationid);
+    
+    my $ttdata={
+        view       => $view,
+        
+        stylesheet  => $stylesheet,
+        sessionID   => $session->{ID},
+        
+        logintarget => $logintarget_ref,
+        
+        user        => $user,
+        session    => $session,
+        config      => $config,
+        msg         => $msg,
+    };
+    
+    OpenBib::Common::Util::print_page($config->{tt_admin_authentication_record_edit_tname},$ttdata,$r);    
+}
+
+sub create_record {
+    my $self = shift;
+
+    # Log4perl logger erzeugen
+    my $logger = get_logger();
+    
+    my $r              = $self->param('r');
+
+    my $view           = $self->param('view')                   || '';
+    my $representation = $self->param('representation') || '';
+
+    my $config  = OpenBib::Config->instance;
+    my $session = OpenBib::Session->instance({ apreq => $r });
+    my $query   = Apache2::Request->new($r);
+
+    my $stylesheet   = OpenBib::Common::Util::get_css_by_browsertype($r);
+
+    my $queryoptions = OpenBib::QueryOptions->instance($query);
+
+    # Message Katalog laden
+    my $msg = OpenBib::L10N->get_handle($queryoptions->get_option('l')) || $logger->error("L10N-Fehler");
+    $msg->fail_with( \&OpenBib::L10N::failure_handler );
+
+    my $adminuser   = $config->{adminuser};
+    my $adminpasswd = $config->{adminpasswd};
+    
+    # Ist der Nutzer ein Admin?
+    my $user         = OpenBib::User->instance({sessionID => $session->{ID}});
+
+    # Admin-SessionID ueberpruefen
+    # Entweder als Master-Adminuser eingeloggt, oder der Benutzer besitzt die Admin-Rolle
+    my $adminsession = $session->is_authenticated_as($adminuser) || $user->is_admin;
+
+    if (!$adminsession) {
+        OpenBib::Common::Util::print_warning($msg->maketext("Sie greifen auf eine nicht autorisierte Session zu"),$r,$msg);
+        return Apache2::Const::OK;
+    }
+
+    $logger->debug("Server: ".$r->get_server_name);
+
+    # Variables
+    my $description     = decode_utf8($query->param('description'))     || '';
+    my $targetid        = $query->param('targetid')        || '';
+    my $hostname        = $query->param('hostname')        || '';
+    my $port            = $query->param('port')            || '';
+    my $username        = $query->param('username')        || '';
+    my $dbname          = $query->param('dbname')          || '';
+    my $type            = $query->param('type')            || '';
+
+    my $thislogintarget_ref = {
+        id          => $targetid,
+        hostname    => $hostname,
+        port        => $port,
+        username    => $username,
+        dbname      => $dbname,
+        description => $description,
+        type        => $type,
+    };
+
+    if ($description eq "") {
+        
+        OpenBib::Common::Util::print_warning($msg->maketext("Sie müssen mindestens eine Beschreibung eingeben."),$r,$msg);
+        
+        return Apache2::Const::OK;
+    }
+    
+    if ($user->logintarget_exists({description => $thislogintarget_ref->{description}})) {
+        
+        OpenBib::Common::Util::print_warning($msg->maketext("Es existiert bereits ein Anmeldeziel unter diesem Namen"),$r,$msg);
+        
+        return Apache2::Const::OK;
+    }
+    
+    $config->new_logintarget($thislogintarget_ref);
+    
+    $r->internal_redirect("http://$config->{servername}$config->{base_loc}/$view/$config->{admin_loc}?do_showlogintarget=1");
+    return Apache2::Const::OK;
+    
+}
+
+sub update_record {
+    my $self = shift;
+
+    # Log4perl logger erzeugen
+    my $logger = get_logger();
+    
+    my $r                = $self->param('r');
+
+    my $view             = $self->param('view')                   || '';
+    my $authenticationid = $self->param('authenticationid')       || '';
+
+    my $config  = OpenBib::Config->instance;
+    my $session = OpenBib::Session->instance({ apreq => $r });
+    my $query   = Apache2::Request->new($r);
+
+    my $stylesheet   = OpenBib::Common::Util::get_css_by_browsertype($r);
+
+    my $queryoptions = OpenBib::QueryOptions->instance($query);
+
+    # Message Katalog laden
+    my $msg = OpenBib::L10N->get_handle($queryoptions->get_option('l')) || $logger->error("L10N-Fehler");
+    $msg->fail_with( \&OpenBib::L10N::failure_handler );
+            
+    my $adminuser   = $config->{adminuser};
+    my $adminpasswd = $config->{adminpasswd};
+    
+    # Ist der Nutzer ein Admin?
+    my $user         = OpenBib::User->instance({sessionID => $session->{ID}});
+
+    if (!$user->authentication_exists($authenticationid)) {        
+        OpenBib::Common::Util::print_warning($msg->maketext("Es existiert keine Authentifizierungsziel unter dieser ID"),$r,$msg);
+        
+        return Apache2::Const::OK;
+    }
+
+    # Admin-SessionID ueberpruefen
+    # Entweder als Master-Adminuser eingeloggt, oder der Benutzer besitzt die Admin-Rolle
+    my $adminsession = $session->is_authenticated_as($adminuser) || $user->is_admin;
+
+    if (!$adminsession) {
+        OpenBib::Common::Util::print_warning($msg->maketext("Sie greifen auf eine nicht autorisierte Session zu"),$r,$msg);
+        return Apache2::Const::OK;
+    }
+
+    $logger->debug("Server: ".$r->get_server_name);
+
+    if (!$user->authentication_exists($authenticationid)) {        
+        OpenBib::Common::Util::print_warning($msg->maketext("Es existiert kein Authentifizierungssystem unter dieser ID"),$r,$msg);
+        
+        return Apache2::Const::OK;
+    }
+
+    # Variables
+
+    # Method workaround fuer die Unfaehigkeit von Browsern PUT/DELETE in Forms
+    # zu verwenden
+    
+    my $method          = decode_utf8($query->param('_method')) || '';
+    my $confirm         = $query->param('confirm') || 0;
+
+    if ($method eq "DELETE"){
+        $logger->debug("About to delete $authenticationid");
+        
+        if ($confirm){
+            my $logintarget_ref = $user->get_logintarget_by_id($authenticationid);
+            
+            my $ttdata={
+                stylesheet => $stylesheet,
+                logintarget  => $logintarget_ref,
+
+                view       => $view,
+                
+                config     => $config,
+                session    => $session,
+                user       => $user,
+                msg        => $msg,
+            };
+
+            $logger->debug("Asking for confirmation");
+            OpenBib::Common::Util::print_page($config->{tt_admin_authentication_record_delete_confirm_tname},$ttdata,$r);
+
+            return Apache2::Const::OK;
+        }
+        else {
+            $logger->debug("Redirecting to delete location");
+            $self->delete_record;
+#             $self->query->method('DELETE');    
+#             $self->query->headers_out->add(Location => "$config->{base_loc}/$config->{admin_view_loc}/$viewname");
+#             $self->query->status(Apache2::Const::REDIRECT);
+            return;
+        }
+    }
+
+    # Ansonsten POST oder PUT => Aktualisieren
+
+    # Variables
+    my $description     = decode_utf8($query->param('description'))     || '';
+    my $hostname        = $query->param('hostname')        || '';
+    my $port            = $query->param('port')            || '';
+    my $username        = $query->param('username')        || '';
+    my $dbname          = $query->param('dbname')          || '';
+    my $type            = $query->param('type')            || '';
+    
+    my $thislogintarget_ref = {
+        id          => $authenticationid,
+        hostname    => $hostname,
+        port        => $port,
+        username    => $username,
+        dbname      => $dbname,
+        description => $description,
+        type        => $type,
+    };
+
+    $logger->debug("Info: ".YAML::Dump($thislogintarget_ref));
+
+    $config->update_logintarget($thislogintarget_ref);
+
+    $self->query->method('GET');
+    $self->query->headers_out->add(Location => "$config->{base_loc}/$view/$config->{admin_authentication_loc}");
+    $self->query->status(Apache2::Const::REDIRECT);
+
+    return;
+}
+
+sub delete_record {
+    my $self = shift;
+
+    # Log4perl logger erzeugen
+    my $logger = get_logger();
+    
+    my $r                = $self->param('r');
+
+    my $view             = $self->param('view')                   || '';
+    my $authenticationid = $self->param('authenticationid')             || '';
+
+    my $config  = OpenBib::Config->instance;
+    my $session = OpenBib::Session->instance({ apreq => $r });
+    my $query   = Apache2::Request->new($r);
+
+    my $stylesheet   = OpenBib::Common::Util::get_css_by_browsertype($r);
+
+    my $queryoptions = OpenBib::QueryOptions->instance($query);
+
+    # Message Katalog laden
+    my $msg = OpenBib::L10N->get_handle($queryoptions->get_option('l')) || $logger->error("L10N-Fehler");
+    $msg->fail_with( \&OpenBib::L10N::failure_handler );
+            
+    my $adminuser   = $config->{adminuser};
+    my $adminpasswd = $config->{adminpasswd};
+    
+    # Ist der Nutzer ein Admin?
+    my $user         = OpenBib::User->instance({sessionID => $session->{ID}});
+
+    if (!$user->authentication_exists($authenticationid)) {        
+        OpenBib::Common::Util::print_warning($msg->maketext("Es existiert kein Katalog unter diesem Namen"),$r,$msg);
+        
+        return Apache2::Const::OK;
+    }
+
+    # Admin-SessionID ueberpruefen
+    # Entweder als Master-Adminuser eingeloggt, oder der Benutzer besitzt die Admin-Rolle
+    my $adminsession = $session->is_authenticated_as($adminuser) || $user->is_admin;
+
+    if (!$adminsession) {
+        OpenBib::Common::Util::print_warning($msg->maketext("Sie greifen auf eine nicht autorisierte Session zu"),$r,$msg);
+        return Apache2::Const::OK;
+    }
+
+    $logger->debug("Server: ".$r->get_server_name);
+
+    $config->del_logintarget($authenticationid);
+    
+    $self->query->method('GET');
+    $self->query->headers_out->add(Location => "$config->{base_loc}/$view/$config->{admin_authentication_loc}");
+    $self->query->status(Apache2::Const::REDIRECT);
+
+    return;
+}
+
+    
+1;

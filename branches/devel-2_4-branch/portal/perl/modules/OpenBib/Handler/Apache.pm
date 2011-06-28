d1b795ea750d8aab6ee8f0fd285547eac9dbeba4
flimm <flimm@e7c75b2f-8ac8-f892-83e0-76010f6c4c37>
2011-06-28 17:22:04 +0000

    - Korrektur: Language und Content Negotiation

--- branches/devel-2_4-branch/portal/perl/modules/OpenBib/Handler/Apache.pm
+++ branches/devel-2_4-branch/portal/perl/modules/OpenBib/Handler/Apache.pm
@@ -35,6 +35,7 @@ no warnings 'redefine';
 use utf8;
 
 use CGI::Application::Plugin::Apache qw(:all);
+use CGI::Application::Plugin::Redirect;
 use Log::Log4perl qw(get_logger :levels);
 use List::MoreUtils qw(none any);
 use Apache2::URI ();
@@ -55,7 +56,99 @@ use OpenBib::User;
 
 use base 'CGI::Application';
 
-sub cgiapp_init() {       # overrides
+
+sub cgiapp_prerun {
+    my $self = shift;
+    
+    # Log4perl logger erzeugen
+    my $logger = get_logger();
+    
+    $logger->debug("Entering cgiapp_prerun");
+   
+    my $r            = $self->param('r');
+    
+    my $config       = OpenBib::Config->instance;
+    my $view         = $self->param('view') || $config->get('defaultview');
+    my $session      = OpenBib::Session->instance({ apreq => $r , view => $view });
+    my $user         = OpenBib::User->instance({sessionID => $session->{ID}});
+
+    my $useragent    = $r->headers_in->{'User-Agent'} || '';
+
+    my $stylesheet   = OpenBib::Common::Util::get_css_by_browsertype($r);
+    my $queryoptions = OpenBib::QueryOptions->instance($self->query());
+    
+    my $servername   = $r->get_server_name;
+    
+    my $path_prefix          = $config->get('base_loc');
+    my $complete_path_prefix = "$path_prefix/$view";
+    
+    if (! $config->strip_view_from_uri($view)){
+        $path_prefix = $complete_path_prefix;
+    }
+    
+    # Letztes Pfad-Element bestimmen
+    my $uri  = $r->parsed_uri;
+    my $path = $uri->path;
+    
+    my ($last_uri_element) = $path =~m/([^\/]+)$/;
+    
+    $logger->debug("Full Internal Path: $path - Last URI Element: $last_uri_element ");
+    
+    if ($r->method eq "GET" && $last_uri_element !~/(\.html|\.json|\.rdf|\.rss|\.include)$/){
+       $self->negotiate_type;
+       
+       # Pfade sind immer mit base_loc und view
+       my $baseloc    = $config->get('base_loc');
+       $path =~s{^$baseloc/[^/]+}{$path_prefix};
+
+       $logger->debug("Corrected External Path: $path");
+
+       my $args="";
+       if (!$self->query->param('l')){
+           $self->negotiate_language;
+           $args="?l=".$self->param('lang');
+           if ($self->query->args()){
+               $args="$args;".$self->query->args();
+           }
+       }
+       else {
+           $args="?".$self->query->args();
+       }
+
+#        $self->query->method('GET');
+#        $self->query->content_type($self->param('content_type'));
+#        $self->query->headers_out->add(Location => $path.$self->param('representation').$args);
+#        $self->query->status(Apache2::Const::REDIRECT);
+#        return;
+       return $self->redirect($path.".".$self->param('representation').$args);
+   }
+
+   if ($r->method eq "GET" && !$self->query->param('l')){
+
+       $self->negotiate_language;
+       
+       # Pfade sind immer mit base_loc und view
+       my $baseloc    = $config->get('base_loc');
+       $path =~s{^$baseloc/[^/]+}{$path_prefix};
+       
+       $logger->debug("Corrected External Path: $path");
+       
+       my $args = "?l=".$self->param('lang');
+
+       $args=$args.";".$self->query->args() if ($self->query->args());
+
+#        $self->query->method('GET');
+#        $self->query->content_type($self->param('content_type'));
+#        $self->query->headers_out->add(Location => $path.$self->param('representation').$args);
+#        $self->query->status(Apache2::Const::REDIRECT);
+#        return;
+       return $self->redirect($path.$args);
+   }
+
+    return;
+}
+
+sub cgiapp_init() {
    my $self = shift;
 
    # Log4perl logger erzeugen
@@ -91,42 +184,71 @@ sub cgiapp_init() {       # overrides
 
    $logger->debug("Full Internal Path: $path - Last URI Element: $last_uri_element ");
    
-
-   my $representation = "";
-   my $content_type   = "";
-   
    my $id = "";
    if ($last_uri_element=~/^(.+?)(\.html|\.json|\.rdf|\.rss|\.include)$/){
        $id               = $1;
-       ($representation) = $2 =~/^\.(.+?)$/;
-       $content_type   = $config->{'content_type_map_rev'}{$representation};
+       my ($representation) = $2 =~/^\.(.+?)$/;
+       my $content_type   = $config->{'content_type_map_rev'}{$representation};
+
+       # Korrektur des ausgehandelten Typs bei direkter Auswahl einer bestimmten Repraesentation
+       $self->param('content_type',$content_type);
+       $self->param('representation',$representation);
    }
-   # Sonst Aushandlung und Redirect
-   elsif ($r->method eq "GET"){
-       # Pfade sind immer mit base_loc und view
-       my $baseloc    = $config->get('base_loc');
-       $path =~s{^$baseloc/[^/]+}{$path_prefix};
+#    # Sonst Aushandlung und Redirect
+#    elsif ($r->method eq "GET"){
 
-       $logger->debug("Corrected External Path: $path");       
+#        $self->negotiate_type;
+       
+#        # Pfade sind immer mit base_loc und view
+#        my $baseloc    = $config->get('base_loc');
+#        $path =~s{^$baseloc/[^/]+}{$path_prefix};
 
-       my $args=$self->query->args();
-       
-       $args = "?$args" if ($args);
-       
-       my $negotiated_type_ref = $self->negotiate_type;
+#        $logger->debug("Corrected External Path: $path");
 
-       $self->query->method('GET');
-       $self->query->content_type($negotiated_type_ref->{content_type});
-       $self->query->headers_out->add(Location => "$path.$negotiated_type_ref->{suffix}$args");
-       $self->query->status(Apache2::Const::REDIRECT);
+#        my $args=$self->query->args();
        
-       return;
+#        $args = "?$args" if ($args);
+       
+#        if (!$self->query->param('l')){
+#            $args="$args;l=".$self->param('lang');
+#        }
+       
+#        $self->query->method('GET');
+#        $self->query->content_type($self->param('content_type'));
+#        $self->query->headers_out->add(Location => $path.$self->param('representation').$args);
+#        $self->query->status(Apache2::Const::REDIRECT);
+       
+#        return;
+#    }
+
+   # Korrektur der ausgehandelten Sprache bei direkter Auswahl via CGI-Parameter 'l'
+   if ($self->query->param('l')){
+       $self->param('lang',$self->query->param('l'));
    }
+#    elsif ($r->method eq "GET"){
 
-   my $lang = $queryoptions->get_option('l') || 'de';
+#        $self->negotiate_language;
+       
+#        # Pfade sind immer mit base_loc und view
+#        my $baseloc    = $config->get('base_loc');
+#        $path =~s{^$baseloc/[^/]+}{$path_prefix};
+       
+#        $logger->debug("Corrected External Path: $path");
+       
+#        my $args = "?l=".$self->param('lang');
+
+#        $args=$args.";".$self->query->args() if ($self->query->args());
+       
+#        $self->query->method('GET');
+#        $self->query->content_type($self->param('content_type'));
+#        $self->query->headers_out->add(Location => $path.$self->param('representation').$args);
+#        $self->query->status(Apache2::Const::REDIRECT);
+       
+#        return;
+#    }
    
    # Message Katalog laden
-   my $msg = OpenBib::L10N->get_handle($queryoptions->get_option('l')) || $logger->error("L10N-Fehler");
+   my $msg = OpenBib::L10N->get_handle($self->param('lang')) || $logger->error("L10N-Fehler");
    $msg->fail_with( \&OpenBib::L10N::failure_handler );
 
    $self->param('config',$config);
@@ -139,13 +261,62 @@ sub cgiapp_init() {       # overrides
    $self->param('servername',$servername);
    $self->param('path_prefix',$path_prefix);
    $self->param('id',$id);
-   $self->param('representation',$representation);
-   $self->param('content_type',$content_type);
-   $self->param('lang',$lang);
    
-   $logger->debug("Setting: id = $id , representation = $representation , content_type = $content_type , path_prefix = $path_prefix");
    $logger->debug("Exit cgiapp_init");
    #   $self->query->charset('UTF-8');  # cause CGI.pm to send a UTF-8 Content-Type header
+}
+
+sub negotiate_content {
+    my $self = shift;
+
+    # Log4perl logger erzeugen
+    my $logger = get_logger();
+    
+    my $r              = $self->param('r');
+    my $config         = $self->param('config');
+    
+    my $accept       = $r->headers_in->{'Accept'} || '';
+    my @accepted_types      = map { (split ";", $_)[0] } split /\*s,\*s/, $accept;
+
+    my $lang         = $r->headers_in->{'Accept-Language'} || '';
+    my @accepted_languages  = map { ($_)=$_=~/^(..)/} map { (split ";", $_)[0] } split /\*s,\*s/, $lang;
+    
+    $logger->debug("Accept: $accept - Types: ".YAML::Dump(\@accepted_types));
+    $logger->debug("Accept-Language: $lang - Languages: ".YAML::Dump(\@accepted_languages));
+    
+    foreach my $information_resource_type (keys %{$config->{content_type_map}}){
+        if (any { $_ eq $information_resource_type } @accepted_types) {
+            $logger->debug("Negotiated Type: $information_resource_type - Suffix: ".$config->{content_type_map}->{$information_resource_type});
+            $self->param('content_type',$information_resource_type);
+            $self->param('represenatione',$config->{content_type_map}->{$information_resource_type});
+            last;
+        }
+    }
+
+    if (!$self->param('content_type') && !$self->param('representation') ){
+        $logger->debug("Default Type: text/html - Suffix: html");
+        $self->param('content_type','text/html');
+        $self->param('representation','html');
+    }
+    
+    if (!$self->param('lang')){
+        my $language_found = 0;
+        foreach my $language (@{$config->{lang}}){
+            if (any { $_ eq $language } @accepted_languages) {
+                $logger->debug("Negotiated Language: $language");
+                $self->param('lang',$language);
+                last;
+            }
+        }
+
+        if (!$self->param('lang')){
+            # Default language ist die erste definierte Sprache unter 'lang' in portal.yml
+            $logger->debug("Default Language: ".$config->{lang}[0]);
+            $self->param('lang',$config->{lang}[0]);
+        }
+    }
+
+    return;
 }
 
 sub negotiate_type {
@@ -157,363 +328,55 @@ sub negotiate_type {
     my $r              = $self->param('r');
     my $config         = $self->param('config');
     
-    my $accept       = $r->headers_in->{Accept} || '';
-    my @accept_types = map { (split ";", $_)[0] } split /\*s,\*s/, $accept;
+    my $accept       = $r->headers_in->{'Accept'} || '';
+    my @accepted_types      = map { (split ";", $_)[0] } split /\*s,\*s/, $accept;
 
-    $logger->debug("Accept: $accept - Types: ".YAML::Dump(\@accept_types));
+    $logger->debug("Accept: $accept - Types: ".YAML::Dump(\@accepted_types));
     
-#     my $content_type_map_ref = {
-#         "application/rdf+xml" => "rdf+xml",
-#         "text/rdf+n3"         => "rdf+n3",
-#         "text/html"           => "html",
-#         "application/json"    => "json",
-#     };
-
-#     my $content_type_map_rev_ref = {
-#         "rdf+xml" => "application/rdf+xml",
-#         "rdf+n3"  => "text/rdf+n3",
-#         "html"    => "text/html",
-#         "json"    => "application/json",
-#     };
-
-    my $information_resource_found = 0;
     foreach my $information_resource_type (keys %{$config->{content_type_map}}){
-        if (any { $_ eq $information_resource_type } @accept_types) {
-
-            $logger->debug("Returning Type: $information_resource_type - Suffix: ".$config->{content_type_map}->{$information_resource_type});
-
-            return {
-                content_type => $information_resource_type,
-                suffix       => $config->{content_type_map}->{$information_resource_type},
-            };
-        }                                                
+        if (any { $_ eq $information_resource_type } @accepted_types) {
+            $logger->debug("Negotiated Type: $information_resource_type - Suffix: ".$config->{content_type_map}->{$information_resource_type});
+            $self->param('content_type',$information_resource_type);
+            $self->param('represenatione',$config->{content_type_map}->{$information_resource_type});
+            last;
+        }
     }
 
-    $logger->debug("Returning Default Type: text/html - Suffix: html");
+    if (!$self->param('content_type') && !$self->param('representation') ){
+        $logger->debug("Default Type: text/html - Suffix: html");
+        $self->param('content_type','text/html');
+        $self->param('representation','html');
+    }
 
-    return {
-        content_type   => 'text/html',
-        suffix => 'html',
-    };
+    return;
 }
 
-sub negotiate_url {
+sub negotiate_language {
     my $self = shift;
 
     # Log4perl logger erzeugen
     my $logger = get_logger();
     
     my $r              = $self->param('r');
-
-    my $uri  = $r->parsed_uri;
-    my $path = $uri->path;
-
-    # Pfade sind immer mit base_loc und view
-    my $baseloc    = $self->param('config')->get('base_loc');
-    my $pathprefix = $self->param('path_prefix');
-    $path =~s{^$baseloc/[^/]+}{$pathprefix};
-
-    my $args=$self->query->args();
-
-    $args = "?$args" if ($args);
+    my $config         = $self->param('config');
     
-    my $negotiated_type_ref = $self->negotiate_type;
-
-    $self->query->method('GET');
-    $self->query->content_type($negotiated_type_ref->{content_type});
-    $self->query->headers_out->add(Location => "$path.$negotiated_type_ref->{suffix}$args");
-    $self->query->status(Apache2::Const::REDIRECT);
-
-    $logger->debug("Default Information Resource Type: $negotiated_type_ref->{content_type} - URI: $path.$negotiated_type_ref->{suffix}");
-
-    return;
-}
-
-sub show_record_negotiate {
-    my $self = shift;
-
-    $self->show_collection_negotiate;
-
-    return;
-}
-
-sub show_collection_as_html {
-    my $self = shift;
-
-    $self->param('representation','html');
-
-    $self->show_collection;
-
-    return;
-}
-
-sub show_collection_as_json {
-    my $self = shift;
-
-    $self->param('representation','json');
-
-    $self->show_collection;
-
-    return;
-}
-
-sub show_collection_as_rdf {
-    my $self = shift;
-
-    $self->param('representation','rdf');
-
-    $self->show_collection;
-
-    return;
-}
-
-sub show_collection_as_rss {
-    my $self = shift;
-
-    $self->param('representation','rss');
-
-    $self->show_collection;
-
-    return;
-}
-
-sub show_collection_as_include {
-    my $self = shift;
-
-    $self->param('representation','include');
-
-    $self->show_collection;
-
-    return;
-}
-
-sub show_collection_recent_as_html {
-    my $self = shift;
-
-    $self->param('representation','html');
-
-    $self->show_collection_recent;
-
-    return;
-}
-
-sub show_collection_recent_as_json {
-    my $self = shift;
-
-    $self->param('representation','json');
-
-    $self->show_collection_recent;
-
-    return;
-}
-
-sub show_collection_recent_as_rdf {
-    my $self = shift;
-
-    $self->param('representation','rdf');
-
-    $self->show_collection_recent;
-
-    return;
-}
-
-sub show_collection_recent_as_rss {
-    my $self = shift;
-
-    $self->param('representation','rss');
-
-    $self->show_collection_recent;
-
-    return;
-}
-
-sub show_collection_recent_as_include {
-    my $self = shift;
-
-    $self->param('representation','include');
-
-    $self->show_collection_recent;
-
-    return;
-}
-
-sub show_record_as_html {
-    my $self = shift;
-
-    $self->param('representation','html');
-
-    $self->show_record;
-
-    return;
-}
-
-sub show_record_as_json {
-    my $self = shift;
-
-    $self->param('representation','json');
-
-    $self->show_record;
-
-    return;
-}
-
-sub show_record_as_rdf {
-    my $self = shift;
-
-    $self->param('representation','rdf');
-
-    $self->show_record;
-
-    return;
-}
-
-sub show_record_as_rss {
-    my $self = shift;
-
-    $self->param('representation','rss');
-
-    $self->show_record;
-
-    return;
-}
-
-sub show_record_as_include {
-    my $self = shift;
-
-    $self->param('representation','include');
-
-    $self->show_record;
-
-    return;
-}
-
-sub show_search_as_html {
-    my $self = shift;
-
-    $self->param('representation','html');
-
-    $self->show_search;
-
-    return;
-}
-
-sub show_search_as_json {
-    my $self = shift;
-
-    $self->param('representation','json');
-
-    $self->show_search;
-
-    return;
-}
-
-sub show_search_as_rdf {
-    my $self = shift;
-
-    $self->param('representation','rdf');
-
-    $self->show_search;
-
-    return;
-}
-
-sub show_search_as_rss {
-    my $self = shift;
-
-    $self->param('representation','rss');
-
-    $self->show_search;
-
-    return;
-}
-
-sub show_search_as_include {
-    my $self = shift;
-
-    $self->param('representation','include');
-
-    $self->show_search;
-
-    return;
-}
-
-
-sub show_index_as_html {
-    my $self = shift;
-
-    $self->param('representation','html');
-
-    $self->show_index;
-
-    return;
-}
-
-sub show_index_as_json {
-    my $self = shift;
-
-    $self->param('representation','json');
-
-    $self->show_index;
-
-    return;
-}
-
-sub show_index_as_rdf {
-    my $self = shift;
-
-    $self->param('representation','rdf');
-
-    $self->show_index;
-
-    return;
-}
-
-sub show_index_as_include {
-    my $self = shift;
-
-    $self->param('representation','include');
-
-    $self->show_index;
-
-    return;
-}
-
-sub show_popular_as_html {
-    my $self = shift;
-
-    $self->param('representation','html');
-
-    $self->show_popular;
-
-    return;
-}
-
-sub show_popular_as_json {
-    my $self = shift;
-
-    $self->param('representation','json');
-
-    $self->show_popular;
-
-    return;
-}
-
-sub show_popular_as_rdf {
-    my $self = shift;
-
-    $self->param('representation','rdf');
-
-    $self->show_popular;
-
-    return;
-}
-
-sub show_popular_as_include {
-    my $self = shift;
-
-    $self->param('representation','include');
-
-    $self->show_popular;
+    my $lang         = $r->headers_in->{'Accept-Language'} || '';
+    my @accepted_languages  = map { ($_)=$_=~/^(..)/} map { (split ";", $_)[0] } split /\*s,\*s/, $lang;
+    
+    $logger->debug("Accept-Language: $lang - Languages: ".YAML::Dump(\@accepted_languages));
+    
+    foreach my $language (@{$config->{lang}}){
+        if (any { $_ eq $language } @accepted_languages) {
+            $logger->debug("Negotiated Language: $language");
+            $self->param('lang',$language);
+            last;
+        }
+    }
+
+    if (!$self->param('lang')){
+        $logger->debug("Default Language: de");
+        $self->param('lang','de');
+    }
 
     return;
 }
@@ -616,6 +479,7 @@ sub print_page {
     my $session        = $self->param('session');
     my $user           = $self->param('user');
     my $msg            = $self->param('msg');
+    my $lang           = $self->param('lang');
     my $queryoptions   = $self->param('qopts');
     my $stylesheet     = $self->param('stylesheet');
     my $useragent      = $self->param('useragent');
@@ -658,6 +522,7 @@ sub print_page {
     $ttdata->{'config'}         = $config;
     $ttdata->{'user'}           = $user;
     $ttdata->{'msg'}            = $msg;
+    $ttdata->{'lang'}           = $lang;
     $ttdata->{'stylesheet'}     = $stylesheet;
     $ttdata->{'servername'}     = $servername;
     $ttdata->{'loginname'}      = $loginname;

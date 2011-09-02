507f15a7b0701e1d257999abf36e552f8ecc2668
flimm <flimm@e7c75b2f-8ac8-f892-83e0-76010f6c4c37>
2011-09-02 15:04:03 +0000

    - Anpassungen fuer DBIx::Class mit
    dbicdump -o dump_directory=/opt/cvs/openbib-current/portal/perl/modules -o debug=1 OpenBib::Database::Config 'dbi:mysql:dbname=openbib_config' root StrengGeheim

--- branches/devel-2_4-branch/portal/perl/modules/OpenBib/Database/Config.pm
+++ branches/devel-2_4-branch/portal/perl/modules/OpenBib/Database/Config.pm
@@ -1,10 +1,19 @@
 package OpenBib::Database::Config;
 
+# Created by DBIx::Class::Schema::Loader
+# DO NOT MODIFY THE FIRST PART OF THIS FILE
+
 use strict;
 use warnings;
 
 use base 'DBIx::Class::Schema';
 
-__PACKAGE__->load_classes;
+__PACKAGE__->load_namespaces;
 
+
+# Created by DBIx::Class::Schema::Loader v0.07002 @ 2011-08-31 13:46:21
+# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:3lc6rBrTFOimoPjWmZqibg
+
+
+# You can replace this text with custom content, and it will be preserved on regeneration
 1;
--- branches/devel-2_4-branch/portal/perl/modules/OpenBib/Database/Config/Result/Viewinfo.pm
+++ branches/devel-2_4-branch/portal/perl/modules/OpenBib/Database/Config/Result/Viewinfo.pm
@@ -39,6 +39,7 @@ __PACKAGE__->table("viewinfo");
 =head2 rssid
 
   data_type: 'bigint'
+  is_foreign_key: 1
   is_nullable: 1
 
 =head2 start_loc
@@ -54,6 +55,7 @@ __PACKAGE__->table("viewinfo");
 =head2 profileid
 
   data_type: 'bigint'
+  is_foreign_key: 1
   is_nullable: 0
 
 =head2 stripuri
@@ -81,13 +83,13 @@ __PACKAGE__->add_columns(
   "description",
   { data_type => "text", is_nullable => 1 },
   "rssid",
-  { data_type => "bigint", is_nullable => 1 },
+  { data_type => "bigint", is_foreign_key => 1, is_nullable => 1 },
   "start_loc",
   { data_type => "text", is_nullable => 1 },
   "servername",
   { data_type => "text", is_nullable => 1 },
   "profileid",
-  { data_type => "bigint", is_nullable => 0 },
+  { data_type => "bigint", is_foreign_key => 1, is_nullable => 0 },
   "stripuri",
   { data_type => "tinyint", is_nullable => 1 },
   "joinindex",
@@ -130,305 +132,44 @@ __PACKAGE__->has_many(
   { cascade_copy => 0, cascade_delete => 0 },
 );
 
+=head2 profileid
 
-# Created by DBIx::Class::Schema::Loader v0.07000 @ 2011-08-26 14:20:00
-# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:1t2CYJCBTzbhxb/iXx0L0w
-# These lines were loaded from '/usr/local/lib/site_perl/OpenBib/Database/Config/Result/Viewinfo.pm' found in @INC.
-# They are now part of the custom portion of this file
-# for you to hand-edit.  If you do not either delete
-# this section or remove that file from @INC, this section
-# will be repeated redundantly when you re-create this
-# file again via Loader!  See skip_load_external to disable
-# this feature.
+Type: belongs_to
 
-package OpenBib::Database::Config::Result::Viewinfo;
-
-# Created by DBIx::Class::Schema::Loader
-# DO NOT MODIFY THE FIRST PART OF THIS FILE
-
-use strict;
-use warnings;
-
-use base 'DBIx::Class::Core';
-
-
-=head1 NAME
-
-OpenBib::Database::Config::Result::Viewinfo
+Related object: L<OpenBib::Database::Config::Result::Profileinfo>
 
 =cut
 
-__PACKAGE__->table("viewinfo");
-
-=head1 ACCESSORS
-
-=head2 id
-
-  data_type: 'bigint'
-  is_auto_increment: 1
-  is_nullable: 0
-
-=head2 viewname
-
-  data_type: 'varchar'
-  is_nullable: 1
-  size: 20
-
-=head2 description
-
-  data_type: 'text'
-  is_nullable: 1
+__PACKAGE__->belongs_to(
+  "profileid",
+  "OpenBib::Database::Config::Result::Profileinfo",
+  { id => "profileid" },
+  { is_deferrable => 1, on_delete => "CASCADE", on_update => "CASCADE" },
+);
 
 =head2 rssid
 
-  data_type: 'bigint'
-  is_nullable: 1
+Type: belongs_to
 
-=head2 start_loc
-
-  data_type: 'text'
-  is_nullable: 1
-
-=head2 servername
-
-  data_type: 'text'
-  is_nullable: 1
-
-=head2 profileid
-
-  data_type: 'bigint'
-  is_nullable: 0
-
-=head2 stripuri
-
-  data_type: 'tinyint'
-  is_nullable: 1
-
-=head2 joinindex
-
-  data_type: 'tinyint'
-  is_nullable: 1
-
-=head2 active
-
-  data_type: 'tinyint'
-  is_nullable: 1
+Related object: L<OpenBib::Database::Config::Result::Rssinfo>
 
 =cut
 
-__PACKAGE__->add_columns(
-  "id",
-  { data_type => "bigint", is_auto_increment => 1, is_nullable => 0 },
-  "viewname",
-  { data_type => "varchar", is_nullable => 1, size => 20 },
-  "description",
-  { data_type => "text", is_nullable => 1 },
+__PACKAGE__->belongs_to(
   "rssid",
-  { data_type => "bigint", is_nullable => 1 },
-  "start_loc",
-  { data_type => "text", is_nullable => 1 },
-  "servername",
-  { data_type => "text", is_nullable => 1 },
-  "profileid",
-  { data_type => "bigint", is_nullable => 0 },
-  "stripuri",
-  { data_type => "tinyint", is_nullable => 1 },
-  "joinindex",
-  { data_type => "tinyint", is_nullable => 1 },
-  "active",
-  { data_type => "tinyint", is_nullable => 1 },
-);
-__PACKAGE__->set_primary_key("id");
-__PACKAGE__->add_unique_constraint("viewname", ["viewname"]);
-
-=head1 RELATIONS
-
-=head2 view_dbs
-
-Type: has_many
-
-Related object: L<OpenBib::Database::Config::Result::ViewDb>
-
-=cut
-
-__PACKAGE__->has_many(
-  "view_dbs",
-  "OpenBib::Database::Config::Result::ViewDb",
-  { "foreign.viewid" => "self.id" },
-  { cascade_copy => 0, cascade_delete => 0 },
-);
-
-=head2 view_rsses
-
-Type: has_many
-
-Related object: L<OpenBib::Database::Config::Result::ViewRss>
-
-=cut
-
-__PACKAGE__->has_many(
-  "view_rsses",
-  "OpenBib::Database::Config::Result::ViewRss",
-  { "foreign.viewid" => "self.id" },
-  { cascade_copy => 0, cascade_delete => 0 },
+  "OpenBib::Database::Config::Result::Rssinfo",
+  { id => "rssid" },
+  {
+    is_deferrable => 1,
+    join_type     => "LEFT",
+    on_delete     => "CASCADE",
+    on_update     => "CASCADE",
+  },
 );
 
 
-# Created by DBIx::Class::Schema::Loader v0.07000 @ 2011-08-26 09:00:20
-# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:DNmb2lu9PJuJA47zCOVDLQ
-# These lines were loaded from '/usr/local/lib/site_perl/OpenBib/Database/Config/Result/Viewinfo.pm' found in @INC.
-# They are now part of the custom portion of this file
-# for you to hand-edit.  If you do not either delete
-# this section or remove that file from @INC, this section
-# will be repeated redundantly when you re-create this
-# file again via Loader!  See skip_load_external to disable
-# this feature.
-
-package OpenBib::Database::Config::Result::Viewinfo;
-
-# Created by DBIx::Class::Schema::Loader
-# DO NOT MODIFY THE FIRST PART OF THIS FILE
-
-use strict;
-use warnings;
-
-use base 'DBIx::Class::Core';
-
-
-=head1 NAME
-
-OpenBib::Database::Config::Result::Viewinfo
-
-=cut
-
-__PACKAGE__->table("viewinfo");
-
-=head1 ACCESSORS
-
-=head2 id
-
-  data_type: 'bigint'
-  is_auto_increment: 1
-  is_nullable: 0
-
-=head2 viewname
-
-  data_type: 'varchar'
-  is_nullable: 1
-  size: 20
-
-=head2 description
-
-  data_type: 'text'
-  is_nullable: 1
-
-=head2 rssid
-
-  data_type: 'bigint'
-  is_nullable: 1
-
-=head2 start_loc
-
-  data_type: 'text'
-  is_nullable: 1
-
-=head2 servername
-
-  data_type: 'text'
-  is_nullable: 1
-
-=head2 profileid
-
-  data_type: 'bigint'
-  is_nullable: 0
-
-=head2 stripuri
-
-  data_type: 'tinyint'
-  is_nullable: 1
-
-=head2 joinindex
-
-  data_type: 'tinyint'
-  is_nullable: 1
-
-=head2 active
-
-  data_type: 'tinyint'
-  is_nullable: 1
-
-=cut
-
-__PACKAGE__->add_columns(
-  "id",
-  { data_type => "bigint", is_auto_increment => 1, is_nullable => 0 },
-  "viewname",
-  { data_type => "varchar", is_nullable => 1, size => 20 },
-  "description",
-  { data_type => "text", is_nullable => 1 },
-  "rssid",
-  { data_type => "bigint", is_nullable => 1 },
-  "start_loc",
-  { data_type => "text", is_nullable => 1 },
-  "servername",
-  { data_type => "text", is_nullable => 1 },
-  "profileid",
-  { data_type => "bigint", is_nullable => 0 },
-  "stripuri",
-  { data_type => "tinyint", is_nullable => 1 },
-  "joinindex",
-  { data_type => "tinyint", is_nullable => 1 },
-  "active",
-  { data_type => "tinyint", is_nullable => 1 },
-);
-__PACKAGE__->set_primary_key("id");
-__PACKAGE__->add_unique_constraint("viewname", ["viewname"]);
-
-=head1 RELATIONS
-
-=head2 view_dbs
-
-Type: has_many
-
-Related object: L<OpenBib::Database::Config::Result::ViewDb>
-
-=cut
-
-__PACKAGE__->has_many(
-  "view_dbs",
-  "OpenBib::Database::Config::Result::ViewDb",
-  { "foreign.viewid" => "self.id" },
-  { cascade_copy => 0, cascade_delete => 0 },
-);
-
-=head2 viewrssfeeds
-
-Type: has_many
-
-Related object: L<OpenBib::Database::Config::Result::Viewrssfeed>
-
-=cut
-
-__PACKAGE__->has_many(
-  "viewrssfeeds",
-  "OpenBib::Database::Config::Result::Viewrssfeed",
-  { "foreign.viewid" => "self.id" },
-  { cascade_copy => 0, cascade_delete => 0 },
-);
-
-
-# Created by DBIx::Class::Schema::Loader v0.07000 @ 2011-08-26 08:52:55
-# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:HRZ/JO43BkZ5AsaKs0M2Jg
-
-
-# You can replace this text with custom content, and it will be preserved on regeneration
-1;
-# End of lines loaded from '/usr/local/lib/site_perl/OpenBib/Database/Config/Result/Viewinfo.pm' 
-
-
-# You can replace this text with custom content, and it will be preserved on regeneration
-1;
-# End of lines loaded from '/usr/local/lib/site_perl/OpenBib/Database/Config/Result/Viewinfo.pm' 
+# Created by DBIx::Class::Schema::Loader v0.07002 @ 2011-08-31 13:46:21
+# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:pze+vKcG8n/LsSPpNkCQQg
 
 
 # You can replace this text with custom content, and it will be preserved on regeneration

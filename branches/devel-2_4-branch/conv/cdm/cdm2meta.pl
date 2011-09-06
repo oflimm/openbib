0e5e15d5df3752145087fe6a4552e3477c09b1ea
flimm <flimm@e7c75b2f-8ac8-f892-83e0-76010f6c4c37>
2011-09-06 22:13:44 +0000

    - Umstellung auf OpenBib::Comon::Util

--- branches/devel-2_4-branch/conv/cdm/cdm2meta.pl
+++ branches/devel-2_4-branch/conv/cdm/cdm2meta.pl
@@ -38,13 +38,10 @@ use XML::Twig;
 use YAML::Syck;
 
 use OpenBib::Config;
+use OpenBib::Conv::Common::Util;
 
-our (@autdubbuf,@kordubbuf,@swtdubbuf,@notdubbuf,$mexidn);
+our ($mexidn);
 
-@autdubbuf = ();
-@kordubbuf = ();
-@swtdubbuf = ();
-@notdubbuf = ();
 $mexidn  =  1;
 
 my ($inputfile,$configfile);
@@ -66,12 +63,12 @@ exit;
 # Ininitalisierung mit Config-Parametern
 my $convconfig = YAML::Syck::LoadFile($configfile);
 
-open (TIT,     ">:utf8","unload.TIT");
-open (AUT,     ">:utf8","unload.PER");
-open (KOR,     ">:utf8","unload.KOE");
-open (NOTATION,">:utf8","unload.SYS");
-open (SWT,     ">:utf8","unload.SWD");
-open (MEX,     ">:utf8","unload.MEX");
+open (TITLE,     ">:utf8","meta.title");
+open (PERSON,     ">:utf8","meta.person");
+open (CORPORATEBODY,     ">:utf8","meta.corporatebody");
+open (CLASSIFICATION,">:utf8","meta.classification");
+open (SUBJECT,     ">:utf8","meta.subject");
+open (HOLDING,     ">:utf8","meta.holding");
 
 my $twig= XML::Twig->new(
    TwigHandlers => {
@@ -82,28 +79,28 @@ my $twig= XML::Twig->new(
 
 $twig->parsefile($inputfile);
 
-close(TIT);
-close(AUT);
-close(KOR);
-close(NOTATION);
-close(SWT);
-close(MEX);
+close(TITLE);
+close(PERSON);
+close(CORPORATEBODY);
+close(CLASSIFICATION);
+close(SUBJECT);
+close(HOLDING);
 
 sub parse_titset {
     my($t, $titset)= @_;
     
-    print TIT "0000:".$titset->first_child($convconfig->{uniqueidfield})->text()."\n";
+    print TITLE "0000:".$titset->first_child($convconfig->{uniqueidfield})->text()."\n";
 
     # Erstellungsdatum
     if(defined $titset->first_child('cdmcreated') && $titset->first_child('cdmcreated')->text()){
         my ($year,$month,$day)=split("-",$titset->first_child('cdmcreated')->text());
-        print TIT "0002:$day.$month.$year\n";
+        print TITLE "0002:$day.$month.$year\n";
     }
     
     # Aenderungsdatum
     if(defined $titset->first_child('cdmmodified') && $titset->first_child('cdmmodified')->text()){
         my ($year,$month,$day)=split("-",$titset->first_child('cdmmodified')->text());
-        print TIT "0003:$day.$month.$year\n";
+        print TITLE "0003:$day.$month.$year\n";
     }
 
     foreach my $kateg (keys %{$convconfig->{title}}){
@@ -121,7 +118,7 @@ sub parse_titset {
                 }
 
                 foreach my $part (@parts){
-                    print TIT $convconfig->{title}{$kateg}.$part."\n";
+                    print TITLE $convconfig->{title}{$kateg}.$part."\n";
                 }
             }
         }
@@ -143,18 +140,18 @@ sub parse_titset {
                 }
                 
                 foreach my $part (@parts){
-                    my $autidn=get_autidn($part);
+                    my $autidn=OpenBib::Conv::Common::Util::get_autidn($part);
                     
                     if ($autidn > 0){
-                        print AUT "0000:$autidn\n";
-                        print AUT "0001:$part\n";
-                        print AUT "9999:\n";
+                        print PERSON "0000:$autidn\n";
+                        print PERSON "0001:$part\n";
+                        print PERSON "9999:\n";
                     }
                     else {
                         $autidn=(-1)*$autidn;
                     }
                     
-                    print TIT $convconfig->{pers}{$kateg}."IDN: $autidn\n";
+                    print TITLE $convconfig->{pers}{$kateg}."IDN: $autidn\n";
                 }
             }
             # Autoren abarbeiten Ende
@@ -177,18 +174,18 @@ sub parse_titset {
                 }
                 
                 foreach my $part (@parts){
-                    my $koridn=get_koridn($part);
+                    my $koridn=OpenBib::Conv::Common::Util::get_koridn($part);
                 
                     if ($koridn > 0){
-                        print KOR "0000:$koridn\n";
-                        print KOR "0001:$part\n";
-                        print KOR "9999:\n";
+                        print CORPORATEBODY "0000:$koridn\n";
+                        print CORPORATEBODY "0001:$part\n";
+                        print CORPORATEBODY "9999:\n";
                     }
                     else {
                         $koridn=(-1)*$koridn;
                     }
                     
-                    print TIT $convconfig->{corp}{$kateg}."IDN: $koridn\n";
+                    print TITLE $convconfig->{corp}{$kateg}."IDN: $koridn\n";
                 }
             }
         }
@@ -211,17 +208,17 @@ sub parse_titset {
                 }
                 
                 foreach my $part (@parts){
-                    my $notidn=get_notidn($part);
+                    my $notidn=OpenBib::Conv::Common::Util::get_notidn($part);
                 
                     if ($notidn > 0){
-                        print NOTATION "0000:$notidn\n";
-                        print NOTATION "0001:$content\n";
-                        print NOTATION "9999:\n";
+                        print CLASSIFICATION "0000:$notidn\n";
+                        print CLASSIFICATION "0001:$content\n";
+                        print CLASSIFICATION "9999:\n";
                     }
                     else {
                         $notidn=(-1)*$notidn;
                     }
-                    print TIT $convconfig->{sys}{$kateg}."IDN: $notidn\n";
+                    print TITLE $convconfig->{sys}{$kateg}."IDN: $notidn\n";
                 }
             }
         }
@@ -244,17 +241,17 @@ sub parse_titset {
                 }
                 
                 foreach my $part (@parts){
-                    my $swtidn=get_swtidn($part);
+                    my $swtidn=OpenBib::Conv::Common::Util::get_swtidn($part);
                     
                     if ($swtidn > 0){	  
-                        print SWT "0000:$swtidn\n";
-                        print SWT "0001:$part\n";
-                        print SWT "9999:\n";
+                        print SUBJECT "0000:$swtidn\n";
+                        print SUBJECT "0001:$part\n";
+                        print SUBJECT "9999:\n";
                     }
                     else {
                         $swtidn=(-1)*$swtidn;
                     }
-                    print TIT $convconfig->{subj}{$kateg}."IDN: $swtidn\n";
+                    print TITLE $convconfig->{subj}{$kateg}."IDN: $swtidn\n";
                 }
             }
         }
@@ -276,10 +273,10 @@ sub parse_titset {
                 }
 
                 foreach my $part (@parts){
-                    print MEX "0000:$mexidn\n";
-                    print MEX "0004:".$titset->first_child($convconfig->{uniqueidfield})->text()."\n";
-                    print MEX $convconfig->{exempl}{$kateg}.$part."\n";
-                    print MEX "9999:\n";
+                    print HOLDING "0000:$mexidn\n";
+                    print HOLDING "0004:".$titset->first_child($convconfig->{uniqueidfield})->text()."\n";
+                    print HOLDING $convconfig->{exempl}{$kateg}.$part."\n";
+                    print HOLDING "9999:\n";
                     $mexidn++;
                 }
             }
@@ -287,103 +284,13 @@ sub parse_titset {
     }
     # Exemplardaten abarbeiten Ende
 
-    print TIT "9999:\n";
+    print TITLE "9999:\n";
     
     # Release memory of processed tree
     # up to here
     $t->purge();
 }
                                    
-sub get_autidn {
-    ($autans)=@_;
-    
-    $autdubidx=1;
-    $autdubidn=0;
-                                   
-    while ($autdubidx < $autdublastidx){
-        if ($autans eq $autdubbuf[$autdubidx]){
-            $autdubidn=(-1)*$autdubidx;      
-            
-            # print STDERR "AutIDN schon vorhanden: $autdubidn\n";
-        }
-        $autdubidx++;
-    }
-    if (!$autdubidn){
-        $autdubbuf[$autdublastidx]=$autans;
-        $autdubidn=$autdublastidx;
-        #print STDERR "AutIDN noch nicht vorhanden: $autdubidn\n";
-        $autdublastidx++;
-        
-    }
-    return $autdubidn;
-}
-                                   
-sub get_swtidn {
-    ($swtans)=@_;
-    
-    $swtdubidx=1;
-    $swtdubidn=0;
-    #  print "Swtans: $swtans\n";
-    
-    while ($swtdubidx < $swtdublastidx){
-        if ($swtans eq $swtdubbuf[$swtdubidx]){
-            $swtdubidn=(-1)*$swtdubidx;      
-            
-            #            print "SwtIDN schon vorhanden: $swtdubidn, $swtdublastidx\n";
-        }
-        $swtdubidx++;
-    }
-    if (!$swtdubidn){
-        $swtdubbuf[$swtdublastidx]=$swtans;
-        $swtdubidn=$swtdublastidx;
-        #        print "SwtIDN noch nicht vorhanden: $swtdubidn, $swtdubidx, $swtdublastidx\n";
-        $swtdublastidx++;
-        
-    }
-    return $swtdubidn;
-}
-                                   
-sub get_koridn {
-    ($korans)=@_;
-    
-    $kordubidx=1;
-    $kordubidn=0;
-    #  print "Korans: $korans\n";
-    
-    while ($kordubidx < $kordublastidx){
-        if ($korans eq $kordubbuf[$kordubidx]){
-            $kordubidn=(-1)*$kordubidx;
-        }
-        $kordubidx++;
-    }
-    if (!$kordubidn){
-        $kordubbuf[$kordublastidx]=$korans;
-        $kordubidn=$kordublastidx;
-        #    print "KorIDN noch nicht vorhanden: $kordubidn\n";
-        $kordublastidx++;
-    }
-    return $kordubidn;
-}
-
-sub get_notidn {
-    my ($notans)=@_;
-    
-    my $notdubidx=1;
-    my $notdubidn=0;
-    
-    while ($notdubidx <= $#notdubbuf){
-        if ($notans eq $notdubbuf[$notdubidx]){
-            $notdubidn=(-1)*$notdubidx;      
-        }
-        $notdubidx++;
-    }
-    if (!$notdubidn){
-        $notdubbuf[$notdubidx]=$notans;
-        $notdubidn=$notdubidx;
-    }
-    return $notdubidn;
-}
-
 sub konv {
     my ($content)=@_;
 

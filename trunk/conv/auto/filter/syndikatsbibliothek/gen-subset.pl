4085f36263589167d9d63128800b14369456b8cc
flimm@sigtrap.de <flimm@sigtrap.de@e7c75b2f-8ac8-f892-83e0-76010f6c4c37>
2012-03-20 12:22:43 +0000

    - Korrektur: Case sensitive Signatursuche (SB gewuenscht, Sb nicht)

--- trunk/conv/auto/filter/syndikatsbibliothek/gen-subset.pl
+++ trunk/conv/auto/filter/syndikatsbibliothek/gen-subset.pl
@@ -67,7 +67,7 @@ my $dbh=DBI->connect("DBI:$config->{dbimodule}:dbname=$sourcepool;host=$config->
 
 print "### $pool: Bestimme Titel-ID's anhand des Signaturanfangs SB\n";
 
-my $request=$dbh->prepare("select distinct conn.sourceid as titid from conn,mex where mex.category=14 and mex.content rlike '^SB' and conn.targetid=mex.id and conn.sourcetype=1 and conn.targettype=6") or $logger->error($DBI::errstr);
+my $request=$dbh->prepare("select distinct conn.sourceid as titid from conn,mex where mex.category=14 and mex.content COLLATE utf8_bin rlike '^SB' and conn.targetid=mex.id and conn.sourcetype=1 and conn.targettype=6") or $logger->error($DBI::errstr);
 
 $request->execute() or $logger->error($DBI::errstr);;
 

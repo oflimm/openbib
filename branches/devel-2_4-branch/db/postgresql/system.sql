45f17d0b1292c3c914916174e0e9592a05cd8861
flimm@sigtrap.de <flimm@sigtrap.de@e7c75b2f-8ac8-f892-83e0-76010f6c4c37>
2012-06-22 13:29:53 +0000

    - Definitionen fuer Erzeugung der system-Datenbank unter PostgreSQL

---
+++ branches/devel-2_4-branch/db/postgresql/system.sql
@@ -0,0 +1,461 @@
+/*-------------------------------------------------*/
+/*-------------- OpenBib System tables ------------*/
+/*-------------------------------------------------*/
+
+/* General configuration                           */
+/* ----------------------------------------------- */
+
+drop table IF EXISTS databaseinfo;
+CREATE TABLE databaseinfo (
+ id          BIGSERIAL primary key,
+
+/* Generel Information */
+ description TEXT,
+ shortdesc   TEXT,
+ system      TEXT, 
+ dbname      VARCHAR(25) UNIQUE,
+ sigel       VARCHAR(20),
+ url         TEXT,
+ use_libinfo BOOL,
+ active      BOOL,
+
+/* Import Configuration */
+ protocol           VARCHAR(255),
+ host               VARCHAR(255),
+ remotepath         TEXT,
+ remoteuser         VARCHAR(255),
+ remotepassword     VARCHAR(255),
+ titlefile          VARCHAR(255),
+ personfile         VARCHAR(255),
+ corporatebodyfile  VARCHAR(255),
+ subjectfile        VARCHAR(255),
+ classificationfile VARCHAR(255),
+ holdingfile        VARCHAR(255),
+
+ autoconvert  BOOL,
+ circ         BOOL,
+ circurl      TEXT,
+ circwsurl    TEXT,
+ circdb       TEXT,
+
+/* Various Counters */
+
+ allcount      BIGINT DEFAULT 0,
+ journalcount  BIGINT DEFAULT 0,
+ articlecount  BIGINT DEFAULT 0,
+ digitalcount  BIGINT DEFAULT 0
+);
+
+drop table IF EXISTS libraryinfo;
+create table libraryinfo (
+ dbid          BIGINT NOT NULL,
+
+ category      SMALLINT  NOT NULL,
+ indicator     SMALLINT,
+ content       TEXT NOT NULL
+);
+
+drop table IF EXISTS rssinfo;
+create table rssinfo (
+ id            BIGSERIAL primary key,
+ dbid          BIGINT NOT NULL,
+ type          SMALLINT,
+ subtype       SMALLINT,
+ subtypedesc   TEXT,
+ cache_tstamp  TIMESTAMP,
+ cache_content TEXT,
+
+ active        SMALLINT
+);
+
+drop table IF EXISTS profileinfo;
+CREATE TABLE profileinfo (
+ id          BIGSERIAL primary key,
+
+ profilename VARCHAR(20) NOT NULL,
+ description TEXT
+);
+
+drop table IF EXISTS orgunitinfo;
+CREATE TABLE orgunitinfo (
+ id          BIGSERIAL primary key,
+ profileid   BIGINT NOT NULL,
+
+ orgunitname VARCHAR(20) NOT NULL,
+ description TEXT,
+ nr          INT
+);
+
+drop table IF EXISTS orgunit_db;
+CREATE TABLE orgunit_db (
+ orgunitid   BIGINT NOT NULL,
+ dbid        BIGINT NOT NULL
+);
+
+drop table IF EXISTS viewinfo;
+CREATE TABLE viewinfo (
+ id          BIGSERIAL primary key,
+
+ viewname    VARCHAR(20) NOT NULL,
+ description TEXT,
+ rssid       BIGINT,
+ start_loc   TEXT,
+ servername  TEXT,
+ profileid   BIGINT NOT NULL,
+ stripuri    SMALLINT,
+ active      BOOL
+);
+
+drop table IF EXISTS view_db;
+CREATE TABLE view_db (
+ viewid   BIGINT NOT NULL,
+ dbid     BIGINT NOT NULL
+);
+
+drop table IF EXISTS view_rss;
+CREATE TABLE view_rss (
+ viewid    BIGINT NOT NULL,
+ rssid     BIGINT NOT NULL
+);
+
+DROP TABLE IF EXISTS serverinfo;
+CREATE TABLE serverinfo (
+ id         BIGSERIAL,
+ host       TEXT,
+ active     SMALLINT
+);
+
+
+/* Session-Handling                                */
+/* ----------------------------------------------- */
+
+drop table IF EXISTS sessioninfo;
+CREATE TABLE sessioninfo (
+ id            BIGSERIAL,
+ sessionid     CHAR(33) NOT NULL,
+ createtime    TIMESTAMP,
+ lastresultset TEXT,
+ username      TEXT,
+ userpassword  TEXT,
+
+ queryoptions  TEXT,
+
+ searchform     TEXT,
+ searchprofile  TEXT,
+
+ bibsonomy_user TEXT,
+ bibsonomy_key  TEXT,
+ bibsonomy_sync TEXT
+);
+
+drop table IF EXISTS sessioncollection;
+CREATE TABLE sessioncollection (
+ id         BIGSERIAL primary key,
+ sid        BIGINT NOT NULL,
+
+ dbname     TEXT,
+ titleid    VARCHAR(255),
+ titlecache TEXT
+);
+
+drop table IF EXISTS recordhistory;
+CREATE TABLE recordhistory (
+ sid       BIGINT NOT NULL,
+
+ dbname    TEXT,
+ titleid   VARCHAR(255)
+);
+
+drop table IF EXISTS eventlog;
+CREATE TABLE eventlog (
+ sid        BIGINT NOT NULL,
+
+ tstamp     TIMESTAMP,
+ type       INT,
+ content    VARCHAR(255)
+);
+
+drop table IF EXISTS eventlogjson;
+CREATE TABLE eventlogjson (
+ sid        BIGINT NOT NULL,
+
+ tstamp     TIMESTAMP,
+ type       INT,
+ content    TEXT,
+);
+
+drop table IF EXISTS queries;
+CREATE TABLE queries (
+ sid             BIGINT NOT NULL,
+
+ tstamp          TIMESTAMP,
+
+ queryid         BIGSERIAL PRIMARY KEY,
+ query           TEXT,
+ hits            INT,
+ searchprofileid BIGINT
+);
+
+drop table IF EXISTS searchhistory;
+CREATE TABLE searchhistory (
+ sid          BIGINT NOT NULL,
+
+ tstamp       TIMESTAMP,
+
+ dbname       VARCHAR(255) NOT NULL,
+ offset       INT,
+ hitrange     INT,
+ searchresult LONGTEXT,
+ hits         INT,
+ queryid      BIGINT
+);
+
+/* Users and user generated data                   */
+/* ----------------------------------------------- */
+
+DROP TABLE IF EXISTS userinfo;
+CREATE TABLE userinfo (
+ id         BIGSERIAL primary key,
+ lastlogin  TIMESTAMP,
+
+ username  VARCHAR(255),
+ password  TEXT,
+
+ /* User informatin from library system */
+ nachname   TEXT,
+ vorname    TEXT,
+ strasse    TEXT,
+ ort        TEXT,
+ plz        INT,
+ soll       TEXT,
+ gut        TEXT,
+ avanz      TEXT,
+ branz      TEXT,
+ bsanz      TEXT,
+ vmanz      TEXT,
+ maanz      TEXT,
+ vlanz      TEXT,
+ sperre     TEXT,
+ sperrdatum TEXT,
+ gebdatum   TEXT,
+
+ email      TEXT,
+
+ /* Personalization */
+ masktype             TEXT,
+ autocompletiontype   TEXT,
+
+ spelling_as_you_type BOOL,
+ spelling_resultlist  BOOL,
+
+ /* Bibsonomy Credentials for automatic sync */
+ bibsonomy_user TEXT,
+ bibsonomy_key  TEXT,
+ bibsonomy_sync TEXT
+);
+
+DROP TABLE IF EXISTS role;
+CREATE TABLE role (
+  id        BIGSERIAL primary key,
+  name      VARCHAR(255) NOT NULL
+);
+
+DROP TABLE IF EXISTS user_role;
+CREATE TABLE user_role (
+  id        BIGSERIAL primary key,
+  userid    BIGINT NOT NULL,
+  roleid    BIGINT NOT NULL
+);
+
+DROP TABLE IF EXISTS registration;
+CREATE TABLE registration (
+  id                  VARCHAR(60) primary key,
+  tstamp              TIMESTAMP,
+ 
+  username            TEXT,
+  password            TEXT
+);
+
+DROP TABLE IF EXISTS logintarget;
+CREATE TABLE logintarget (
+ id          BIGSERIAL primary key,
+
+ hostname    TEXT,
+ port        TEXT,
+ user        TEXT,
+ db          TEXT,
+ description TEXT,
+ type        TEXT
+);
+
+DROP TABLE IF EXISTS user_session;
+CREATE TABLE user_session (
+  id        BIGSERIAL primary key,
+  sid       BIGINT NOT NULL,
+  userid    BIGINT NOT NULL,
+  targetid  BIGINT NOT NULL
+);
+
+DROP TABLE IF EXISTS searchprofile;
+CREATE TABLE searchprofile (
+ id                BIGSERIAL primary key,
+ databases_as_json TEXT, /* for quick lookup having database list and initial state */
+ own_index         SMALLINT
+);
+
+drop table IF EXISTS searchprofile_db;
+CREATE TABLE searchprofile_db (
+ searchprofileid   BIGINT NOT NULL,
+ dbid              BIGINT NOT NULL
+);
+
+drop table IF EXISTS session_searchprofile;
+CREATE TABLE session_searchprofile (
+ sid             BIGINT NOT NULL,
+ searchprofileid BIGINT NOT NULL
+);
+
+DROP TABLE IF EXISTS user_searchprofile;
+CREATE TABLE user_searchprofile (
+ id                BIGSERIAL primary key,
+ searchprofileid   BIGINT NOT NULL,
+ userid            BIGINT NOT NULL,
+
+ profilename TEXT
+);
+
+DROP TABLE IF EXISTS searchfield;
+CREATE TABLE searchfield (
+ userid      BIGINT NOT NULL,
+
+ searchfield VARCHAR(255),
+ active      BOOL
+);
+
+DROP TABLE IF EXISTS livesearch;
+CREATE TABLE livesearch (
+ userid      BIGINT NOT NULL,
+ searchfield VARCHAR(255),
+ exact       BOOL,
+ active      BOOL
+);
+
+DROP TABLE IF EXISTS collection;
+CREATE TABLE collection (
+ id         BIGSERIAL primary key,
+ userid     BIGINT  NOT NULL,
+
+ dbname     TEXT,
+ titleid    VARCHAR(255),
+ titlecache TEXT
+);
+
+DROP TABLE IF EXISTS tag;
+CREATE TABLE tag (
+ id     BIGSERIAL primary key,
+ name   VARCHAR(255) NOT NULL default ''
+);
+
+DROP TABLE IF EXISTS tit_tag;
+CREATE TABLE tit_tag (
+ id         BIGSERIAL primary key,
+ tagid      BIGINT      NOT NULL,
+ userid     BIGINT       NOT NULL,
+
+ dbname     VARCHAR(25)  NOT NULL,
+ titleid    VARCHAR(255) NOT NULL,
+ titleisbn  CHAR(14)     NOT NULL default '',
+
+ titlecache TEXT,
+ type       INT(3)       NOT NULL default '1'
+);
+
+DROP TABLE IF EXISTS review;
+CREATE TABLE review (
+ id        BIGSERIAL primary key,
+ userid    BIGINT       NOT NULL,
+ tstamp    TIMESTAMP,
+
+ nickname   VARCHAR(30)  NOT NULL default '',
+ title      VARCHAR(100) NOT NULL default '',
+ reviewtext MEDIUMTEXT   NOT NULL default '',
+ rating     INT(3)       NOT NULL default '0',
+
+ dbname    VARCHAR(25)  NOT NULL default '',
+ titleid   VARCHAR(255) NOT NULL default '0',
+ titleisbn CHAR(14)     NOT NULL default ''
+);
+
+DROP TABLE IF EXISTS reviewrating;
+CREATE TABLE reviewratings (
+ id        BIGSERIAL primary key,
+ userid    BIGINT      NOT NULL,
+ reviewid  INT(11)     NOT NULL,
+
+ tstamp    TIMESTAMP,
+ rating    INT(3)      NOT NULL default '0'
+);
+
+DROP TABLE IF EXISTS litlist;
+CREATE TABLE litlist (
+ id        BIGSERIAL primary key,
+ userid    BIGINT      NOT NULL,
+
+ tstamp    TIMESTAMP,
+
+ title     TEXT        NOT NULL,
+ type      INT(3)      NOT NULL default '1',
+ lecture   INT(3)      NOT NULL default '0'
+);
+
+DROP TABLE IF EXISTS litlistitem;
+CREATE TABLE litlistitem (
+ id        BIGSERIAL primary key,
+ litlistid INT(11)     NOT NULL,
+
+ tstamp    TIMESTAMP,
+
+ dbname    VARCHAR(25)  NOT NULL,
+ titleid   VARCHAR(255) NOT NULL,
+ titleisbn CHAR(14)     NOT NULL default '',
+
+ titlecache  TEXT
+);
+
+DROP TABLE IF EXISTS subject;
+CREATE TABLE subject (
+ id           BIGSERIAL primary key,
+ name         TEXT        NOT NULL default '',
+ description  TEXT        NOT NULL default ''
+);
+
+DROP TABLE IF EXISTS litlist_subject;
+CREATE TABLE litlist_subject (
+ id           BIGSERIAL primary key,
+ litlistid    INT(11)     NOT NULL,
+ subjectid    INT(11)     NOT NULL
+);
+
+DROP TABLE IF EXISTS subjectclassification;
+CREATE TABLE subjectclassification (
+ subjectid      INT(11)     NOT NULL,
+ classification VARCHAR(20) NOT NULL,
+ type           VARCHAR(5)  NOT NULL
+);
+
+/* Standard ist Selbstregistrierung */
+insert into logintarget values(1,NULL,NULL,NULL,NULL,'Registrierte E-Mail Adresse','self');
+
+/* Standard sind Rollen Admin und Bibliothekar */
+insert into role values (1,'admin');
+insert into role values (2,'librarian');
+
+/* Standard ist Admin-User mit ID 1 und Passwort 'StrengGeheim' */
+insert into userinfo (id,username,password) values (1,'admin','StrengGeheim');
+insert into user_role (userid,roleid) values (1,1);
+
+/* Standard-Profil ist openbib */
+insert into profileinfo (id,profilename,description) values (1,'openbib','OpenBib Beispiel-Portal');
+
+/* Standard-View ist openbib */
+insert into viewinfo (id,viewname,description,start_loc,servername,profileid,stripuri,active) values (1,'openbib','OpenBib Beispiel-Portal','','',1,0,1);
---
+++ branches/devel-2_4-branch/db/postgresql/system_create_index.sql
@@ -0,0 +1,155 @@
+ALTER TABLE databaseinfo ADD PRIMARY KEY (id);
+ALTER TABLE databaseinfo ADD CONSTRAINT uq_databaseinfo_dbname UNIQUE;
+CREATE INDEX databaseinfo_dbname ON databaseinfo (dbname);
+CREATE INDEX databaseinfo_active ON databaseinfo (active);
+CREATE INDEX databaseinfo_description ON databaseinfo (description);
+
+ALTER TABLE libraryinfo ADD CONSTRAINT fk_libraryinfo_db FOREIGN KEY (dbid) REFERENCES databaseinfo (id);
+CREATE INDEX libraryinfo_category ON libraryinfo (category);
+CREATE INDEX libraryinfo_indicator ON libraryinfo (indicator);
+CREATE INDEX libraryinfo_content ON libraryinfo (content);
+
+ALTER TABLE rssinfo ADD PRIMARY KEY (id);
+ALTER TABLE rssinfo ADD CONSTRAINT fk_rssinfo_db FOREIGN KEY (dbid) REFERENCES databaseinfo (id);
+CREATE INDEX rssinfo_type ON rssinfo (type);
+CREATE INDEX rssinfo_subtype ON rssinfo (subtype);
+CREATE INDEX rssinfo_cachetstamp ON rssinfo (cache_tstamp);
+
+ALTER TABLE profileinfo ADD PRIMARY KEY (id);
+ALTER TABLE profileinfo ADD CONSTRAINT uq_profileinfo_profilename UNIQUE;
+CREATE INDEX profileinfo_profilename ON profileinfo (profilename);
+
+ALTER TABLE orgunitinfo ADD PRIMARY KEY (id);
+ALTER TABLE orgunitinfo ADD CONSTRAINT fk_orgunitinfo_profile FOREIGN KEY (profileid) REFERENCES profileinfo (id);
+CREATE INDEX orgunitinfo_orgunitname ON orgunitinfo (orgunitname);
+CREATE INDEX orgunitinfo_nr ON orgunitinfo (nr);
+
+ALTER TABLE orgunit_db ADD CONSTRAINT fk_orgunitdb_db FOREIGN KEY (dbid) REFERENCES databaseinfo (id);
+ALTER TABLE orgunit_db ADD CONSTRAINT fk_orgunitdb_orgunit FOREIGN KEY (orgunitid) REFERENCES orgunitinfo (id);
+
+ALTER TABLE viewinfo ADD PRIMARY KEY (id);
+ALTER TABLE viewinfo ADD CONSTRAINT uq_viewinfo_viewname UNIQUE;
+ALTER TABLE viewinfo ADD CONSTRAINT fk_viewinfo_profile FOREIGN KEY (profileid) REFERENCES profileinfo (id);
+ALTER TABLE viewinfo ADD CONSTRAINT fk_viewinfo_rss FOREIGN KEY (rssid) REFERENCES rssinfoinfo (id);
+CREATE INDEX viewinfo_viewname ON viewinfo (viewname);
+
+ALTER TABLE view_db ADD CONSTRAINT fk_viewdb_db FOREIGN KEY (dbid) REFERENCES databaseinfo (id);
+ALTER TABLE view_db ADD CONSTRAINT fk_viewdb_view FOREIGN KEY (viewid) REFERENCES viewinfo (id);
+
+ALTER TABLE view_rss ADD CONSTRAINT fk_viewrss_rss FOREIGN KEY (rssid) REFERENCES rssinfo (id);
+ALTER TABLE view_rss ADD CONSTRAINT fk_viewrss_view FOREIGN KEY (viewid) REFERENCES viewinfo (id);
+
+ALTER TABLE serverinfo ADD PRIMARY KEY (id);
+CREATE INDEX serverinfo_active ON serverinfo (active);
+
+ALTER TABLE sessioninfo ADD PRIMARY KEY (id);
+CREATE INDEX sessioninfo_sessionid ON sessioninfo (sessionid);
+CREATE INDEX sessioninfo_username ON sessioninfo (username);
+
+ALTER TABLE sessioncollection ADD PRIMARY KEY (id);
+ALTER TABLE sessioncollection ADD CONSTRAINT fk_sessioncollection_session FOREIGN KEY (sid) REFERENCES sessioninfo (id);
+CREATE INDEX sessioncollection_dbname ON sessioncollection (dbname);
+CREATE INDEX sessioncollection_titleid ON sessioncollection (titleid);
+
+ALTER TABLE recordhistory ADD CONSTRAINT fk_recordhistory_session FOREIGN KEY (sid) REFERENCES sessioninfo (id);
+
+ALTER TABLE eventlog ADD CONSTRAINT fk_eventlog_session FOREIGN KEY (sid) REFERENCES sessioninfo (id);
+CREATE INDEX eventlog_tstamp ON eventlog (tstamp);
+CREATE INDEX eventlog_type ON eventlog (type);
+CREATE INDEX eventlog_content ON eventlog (content);
+
+ALTER TABLE eventlogjson ADD CONSTRAINT fk_eventlogjson_session FOREIGN KEY (sid) REFERENCES sessioninfo (id);
+CREATE INDEX eventlogjson_tstamp ON eventlogjson (tstamp);
+CREATE INDEX eventlogjson_type ON eventlogjson (type);
+
+ALTER TABLE queries ADD PRIMARY KEY (queryid);
+ALTER TABLE queries ADD CONSTRAINT fk_queries_session FOREIGN KEY (sid) REFERENCES sessioninfo (id);
+ALTER TABLE queries ADD CONSTRAINT fk_queries_searchprofile FOREIGN KEY (searchprofileid) REFERENCES searchprofile (id);
+CREATE INDEX queries_searchprofileid ON queries (searchprofileid);
+CREATE INDEX queries_query ON queries (query);
+
+ALTER TABLE searchhistory ADD CONSTRAINT fk_searchhistory_session FOREIGN KEY (sid) REFERENCES sessioninfo (id);
+CREATE INDEX searchhistory_dbname ON searchhistory (dbname);
+CREATE INDEX searchhistory_queryid ON searchhistory (queryid);
+
+ALTER TABLE userinfo ADD PRIMARY KEY (id);
+ALTER TABLE userinfo ADD CONSTRAINT uq_userinfo_username UNIQUE;
+
+ALTER TABLE role ADD PRIMARY KEY (id);
+
+ALTER TABLE userrole ADD PRIMARY KEY (id);
+ALTER TABLE userrole ADD CONSTRAINT fk_userrole_user FOREIGN KEY (userid) REFERENCES userinfo (id);
+ALTER TABLE userrole ADD CONSTRAINT fk_userrole_role FOREIGN KEY (roleid) REFERENCES role (id);
+
+ALTER TABLE registration ADD PRIMARY KEY (id);
+
+ALTER TABLE logintarget ADD PRIMARY KEY (id);
+
+ALTER TABLE user_session ADD PRIMARY KEY (id);
+ALTER TABLE user_session ADD CONSTRAINT fk_usersession_user FOREIGN KEY (userid) REFERENCES userinfo (id);
+ALTER TABLE user_session ADD CONSTRAINT fk_usersession_session FOREIGN KEY (sid) REFERENCES sessioninfo (id);
+ALTER TABLE user_session ADD CONSTRAINT fk_usersession_logintarget FOREIGN KEY (targetid) REFERENCES logintarget (id);
+
+ALTER TABLE searchprofile ADD PRIMARY KEY (id);
+CREATE INDEX searchprofile_dbases_as_json ON searchprofile (databases_as_json);
+
+ALTER TABLE searchprofile_db ADD CONSTRAINT fk_searchprofiledb_profile FOREIGN KEY (searchprofileid) REFERENCES searchprofile (id);
+ALTER TABLE searchprofile_db ADD CONSTRAINT fk_searchprofiledb_db FOREIGN KEY (dbid) REFERENCES databaseinfo (id);
+
+ALTER TABLE session_searchprofile ADD CONSTRAINT fk_sessionsearchprofile_profile FOREIGN KEY (searchprofileid) REFERENCES searchprofile (id);
+ALTER TABLE session_searchprofile ADD CONSTRAINT fk_sessionsearchprofile_session FOREIGN KEY (sid) REFERENCES sessioninfo (id);
+
+ALTER TABLE user_searchprofile ADD PRIMARY KEY (id);
+ALTER TABLE user_searchprofile ADD CONSTRAINT fk_usersearchprofile_profile FOREIGN KEY (searchprofileid) REFERENCES searchprofile (id);
+ALTER TABLE user_searchprofile ADD CONSTRAINT fk_usersearchprofile_user FOREIGN KEY (userid) REFERENCES userinfo (id);
+
+ALTER TABLE searchfield ADD CONSTRAINT fk_searchfield_user FOREIGN KEY (userid) REFERENCES userinfo (id);
+CREATE INDEX searchfield_searchfield ON searchfield (searchfield);
+
+ALTER TABLE livesearch ADD CONSTRAINT fk_livesearch_user FOREIGN KEY (userid) REFERENCES userinfo (id);
+
+ALTER TABLE collection ADD PRIMARY KEY (id);
+ALTER TABLE collection ADD CONSTRAINT fk_collection_user FOREIGN KEY (userid) REFERENCES userinfo (id);
+CREATE INDEX collection_dbname ON collection (dbname);
+CREATE INDEX collection_titleid ON collection (titleid);
+
+ALTER TABLE tag ADD PRIMARY KEY (id);
+CREATE INDEX tag_name ON tag (name);
+
+ALTER TABLE tit_tag ADD PRIMARY KEY (id);
+ALTER TABLE tit_tag ADD CONSTRAINT fk_tittag_user FOREIGN KEY (userid) REFERENCES userinfo (id);
+ALTER TABLE tit_tag ADD CONSTRAINT fk_tittag_tag FOREIGN KEY (tagid) REFERENCES tag (id);
+CREATE INDEX tittag_titleid ON tit_tag (titleid);
+CREATE INDEX tittag_dbname ON tit_tag (dbname);
+CREATE INDEX tittag_type ON tit_tag (type);
+
+ALTER TABLE review ADD PRIMARY KEY (id);
+ALTER TABLE review ADD CONSTRAINT fk_review_user FOREIGN KEY (userid) REFERENCES userinfo (id);
+CREATE INDEX review_titleid ON review (titleid);
+CREATE INDEX review_dbname ON review (dbname);
+CREATE INDEX review_titleisbn ON review (titleisbn);
+
+ALTER TABLE reviewrating ADD PRIMARY KEY (id);
+ALTER TABLE reviewrating ADD CONSTRAINT fk_reviewrating_user FOREIGN KEY (userid) REFERENCES userinfo (id);
+ALTER TABLE reviewrating ADD CONSTRAINT fk_reviewrating_review FOREIGN KEY (reviewid) REFERENCES review (id);
+
+ALTER TABLE litlist ADD PRIMARY KEY (id);
+ALTER TABLE litlist ADD CONSTRAINT fk_litlist_user FOREIGN KEY (userid) REFERENCES userinfo (id);
+CREATE INDEX litlist_title ON litlist (title);
+CREATE INDEX litlist_type ON litlist (type);
+
+ALTER TABLE litlistitem ADD PRIMARY KEY (id);
+ALTER TABLE litlistitem ADD CONSTRAINT fk_litlistitem_litlist FOREIGN KEY (litlistid) REFERENCES litlist (id);
+CREATE INDEX litlistitem_dbname ON litlistitem (dbname);
+CREATE INDEX litlistitem_titleid ON litlistitem (titleid);
+CREATE INDEX litlistitem_titleisbn ON litlistitem (titleisbn);
+
+ALTER TABLE subject ADD PRIMARY KEY (id);
+
+ALTER TABLE litlist_subject ADD PRIMARY KEY (id);
+ALTER TABLE litlist_subject ADD CONSTRAINT fk_litlistsubject_litlist FOREIGN KEY (litlistid) REFERENCES litlist (id);
+ALTER TABLE litlist_subject ADD CONSTRAINT fk_litlistsubject_subject FOREIGN KEY (subjectid) REFERENCES subject (id);
+
+ALTER TABLE subjectclassification ADD CONSTRAINT fk_subjectclassification_subject FOREIGN KEY (subjectid) REFERENCES subject (id);
+CREATE INDEX subjectclassification_type ON subjectclassification (type);
+CREATE INDEX subjectclassification_classification ON subjectclassification (classification);

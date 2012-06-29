afe9e15c836f5fb9880f9680b3f853294219eafd
flimm@sigtrap.de <flimm@sigtrap.de@e7c75b2f-8ac8-f892-83e0-76010f6c4c37>
2012-06-29 07:43:40 +0000

    - Umstellung auf Database::Subset-Modul

--- branches/devel-2_4-branch/conv/auto/filter/rheinabt/alt_remote.pl
+++ branches/devel-2_4-branch/conv/auto/filter/rheinabt/alt_remote.pl
@@ -41,4 +41,4 @@ my $pool          = $ARGV[0];
 
 print "### $pool: Extrahiere der Daten der Rheinischen Abteilung aus den USB-Daten\n";
 
-system("$rootdir/filter/$pool/gen-subset-mex-rheinischeabt.pl $pool");
+system("$rootdir/filter/$pool/gen-subset.pl $pool");

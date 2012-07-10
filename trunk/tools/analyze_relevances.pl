e29fff01595eb91a031940158fb48922a7993ee2
flimm@sigtrap.de <flimm@sigtrap.de@e7c75b2f-8ac8-f892-83e0-76010f6c4c37>
2012-07-10 11:50:43 +0000

    - Umstellung auf DBIx::Class und Enrichment/Statistics-Objekte - Erweiterung um Eingabeparameter, speziell --enrichmntdbname, --statisticsdbname, um eine andere Datenbank als den default zu verwenden - Erweiterung um Logging via log4perl

--- trunk/tools/analyze_relevances.pl
+++ trunk/tools/analyze_relevances.pl
@@ -3,7 +3,7 @@
 #
 #  analyze_relevances.pl
 #
-#  Dieses File ist (C) 2006 Oliver Flimm <flimm@openbib.org>
+#  Dieses File ist (C) 2006-2012 Oliver Flimm <flimm@openbib.org>
 #
 #  Dieses Programm ist freie Software. Sie koennen es unter
 #  den Bedingungen der GNU General Public License, wie von der
@@ -26,28 +26,81 @@
 
 use Business::ISBN;
 use DBI;
+use Getopt::Long;
+use Log::Log4perl qw(get_logger :levels);
 use YAML;
-use OpenBib::Config;
 use OpenBib::Common::Util;
+use OpenBib::Config;
+use OpenBib::Enrichment;
+use OpenBib::Statistics;
 use OpenBib::Search::Util;
 use OpenBib::Record::Title;
 
+my ($statisticsdbname,$enrichmntdbname,$help,$logfile);
+
+&GetOptions("statisticsdbname=s" => \$statisticsdbname,
+            "enrichmntdbname=s"  => \$enrichmntdbname,
+            "profile=s"          => \$profile,
+            "view=s"             => \$view,
+            "logfile=s"          => \$logfile,
+	    "help"               => \$help
+	    );
+
+if ($help){
+    print_help();
+}
+
+$logfile=($logfile)?$logfile:'/var/log/openbib/analyze_relevances.log';
+
+my $log4Perl_config = << "L4PCONF";
+log4perl.rootLogger=INFO, LOGFILE, Screen
+log4perl.appender.LOGFILE=Log::Log4perl::Appender::File
+log4perl.appender.LOGFILE.filename=$logfile
+log4perl.appender.LOGFILE.mode=append
+log4perl.appender.LOGFILE.layout=Log::Log4perl::Layout::PatternLayout
+log4perl.appender.LOGFILE.layout.ConversionPattern=%d [%c]: %m%n
+log4perl.appender.Screen=Log::Dispatch::Screen
+log4perl.appender.Screen.layout=Log::Log4perl::Layout::PatternLayout
+log4perl.appender.Screen.layout.ConversionPattern=%d [%c]: %m%n
+L4PCONF
+
+Log::Log4perl::init(\$log4Perl_config);
+
+# Log4perl logger erzeugen
+my $logger = get_logger();
+
 my $config=new OpenBib::Config();
 
-# Verbindung zur SQL-Datenbank herstellen
-my $enrichdbh
-    = DBI->connect("DBI:$config->{dbimodule}:dbname=$config->{enrichmntdbname};host=$config->{enrichmntdbhost};port=$config->{enrichmntdbport}", $config->{enrichmntdbuser}, $config->{enrichmntdbpasswd});
+unless ($statisticsdbname){
+    $statisticsdbname = $config->{statisticsdbname};
+}
 
-my $statdbh
-    = DBI->connect("DBI:$config->{dbimodule}:dbname=$config->{statisticsdbname};host=$config->{statisticsdbhost};port=$config->{statisticsdbport}", $config->{statisticsdbuser}, $config->{statisticsdbpasswd});
+unless ($enrichmntdbname){
+    $enrichmntdbname = $config->{enrichmntdbname};
+}
 
-my $request=$statdbh->prepare("select distinct isbn from relevance where isbn != ''");
+my $enrichmnt  = new OpenBib::Enrichment({enrichmntdbname => $enrichmntdbname });
+my $statistics = new OpenBib::Statistics({statisticsdbname => $statisticsdbname });
 
-$request->execute();
 
+# "select distinct isbn from relevance where isbn != ''"
+my $isbns = $statistics->{schema}->resultset('Titleusage')->search_rs(
+    {
+        isbn => { '!=' => '' }, 
+    },
+    {
+        select   => ['isbn'],
+        group_by => ['isbn'],
+    }
+       
+);
+
+$logger->info("Processing ".$isbns->count." ISBN's");
+
+my $isbncount = 1;
 # Bestimme Nutzungsinformationen fuer jede ISBN
-while (my $result=$request->fetchrow_hashref){
-    my $isbn13 = $isbn = $result->{isbn};
+foreach my $item ($isbns->all){
+    my $isbn13 = $isbn = $item->isbn;
 
     # Alternative ISBN zur Rechercheanrei
     my $isbnXX = Business::ISBN->new($isbn);
@@ -61,36 +114,55 @@ while (my $result=$request->fetchrow_hashref){
         content  => $isbn13,
     });
 
-    # Bestimme alle Nutzer, die diese ISBN ausgeliehen haben
-    my $request=$statdbh->prepare("select distinct id from relevance where isbn=?");
-    $request->execute($isbn);
+    # Bestimme alle Nutzer (=Sessions), die diese ISBN ausgeliehen/angeklickt haben
 
-    my @ids=();
-    while (my $result=$request->fetchrow_hashref){
-        my $id = $result->{id};
-        push @ids, "'$id'";
-    }
+    # select distinct id from relevance where isbn=?;
+    my $sessions = $statistics->{schema}->resultset('Sessioninfo')->search_rs(
+        {
+            'titleusages.isbn' => $isbn,
+        },
+        {
+            join     => ['titleusages'],
+        }
+    );
+    
+#    my @ids=();
+#    while (my $result=$request->fetchrow_hashref){
+#        my $id = $result->{id};
+#        push @ids, "'$id'";
+#    }
 
-    my $idstring=join(",",@ids);
+#    my $idstring=join(",",@ids);
     
     # Bestimme alle ISBNs, die diese Nutzer ausgeliehen haben und erzeuge
     # daraus ein Nutzungshistogramm
 
-    my $request=$statdbh->prepare("select isbn,dbname,katkey from relevance where isbn != ? and id in ($idstring)");
-    $request->execute($isbn);
+    # "select isbn,dbname,katkey from relevance where isbn != ? and id in ($idstring)"
+    my $titles = $sessions->search_rs(
+        {
+            isbn => { '!=' => $isbn },             
+        },
+        {
+            select => ['titleusages.isbn','titleusages.id','titleusages.dbname'],
+            as     => ['titleisbn','titleid','titledbname'],
+            join   => ['titleusages'],
+        }
+        
+    );
 
     my %isbnhist=();
   ISBNHIST:
-    while (my $result=$request->fetchrow_hashref){
-        my $isbn   = $result->{isbn};
-        my $dbname = $result->{dbname};
-        my $katkey = $result->{katkey};
+
+    foreach my $title ($titles->all){
+        my $isbn   = $title->titleisbn;
+        my $dbname = $title->titledbname;
+        my $id     = $title->titleid;
         
         if (!exists $isbnhist{$isbn}){
             $isbnhist{$isbn}={
                 count  => 0,
                 dbname => $dbname,
-                katkey => $katkey,
+                id     => $id,
             };
         }
         $isbnhist{$isbn}{count}=$isbnhist{$isbn}{count}+1;
@@ -101,7 +173,7 @@ while (my $result=$request->fetchrow_hashref){
         push @{$histo[$isbnhist{$isbn}{count}]}, {
             isbn   => $isbn,
             dbname => $isbnhist{$isbn}{dbname},
-            katkey => $isbnhist{$isbn}{katkey},
+            id     => $isbnhist{$isbn}{id},
         };
     }
 
@@ -120,31 +192,31 @@ while (my $result=$request->fetchrow_hashref){
 
         my $count=0;
         $i=0;
-        # 5 References werden bestimmt
-        my $request1=$enrichdbh->prepare('delete from normdata where isbn=? and category=?');
-        my $request2=$enrichdbh->prepare('insert into normdata values (?,?,?,?,?)');
 
-        $request1->execute($isbn,4000);
-        $request1->execute($isbn,4001);
+        # Anreicherungen fuer diese Kategorie entfernen,
+
+        # DBI: 'delete from normdata where isbn=? and category=?'
+        $enrichment->{schema}->resultset('EnrichedContentByIsbn')->search_rs(
+            {
+                -or => [
+                    'field' => '4000',
+                    'field' => '4001',
+                ],
+                isbn => $isbn,
+            }
+        )->delete;
+        
+            ;
+        # 5 References werden bestimmt
+#        my $request2=$enrichdbh->prepare('insert into normdata values (?,?,?,?,?)');
+
+        my $enriched_content_ref  = [];
       REFERENCES:
         foreach my $references_ref (@references){
 
             foreach my $item_ref (@{$references_ref->{references}}){
-                my $dbh;
+                my $record = OpenBib::Record::Title->new({database => $item_ref->{dbname}, id => $item_ref->{id}})->load_full_record({dbh => $dbh});
                 
-                eval {
-                    $dbh
-                        = DBI->connect("DBI:$config->{dbimodule}:dbname=$item_ref->{dbname};host=$config->{dbhost};port=$config->{dbport}", $config->{dbuser}, $config->{dbpasswd})
-                            ;# or $logger->error_die($DBI::errstr);
-                };
-                
-                if ($@){
-                    print STDERR "$_";
-                    next REFERENCES;
-                }
-
-                my $record = OpenBib::Record::Title->new({database => $item_ref->{dbname}, id => $item_ref->{katkey}})->load_full_record({dbh => $dbh});
-
                 # Add user count
                 $record->{user_count} = $references_ref->{count} ;
 
@@ -153,12 +225,36 @@ while (my $result=$request->fetchrow_hashref){
                 $count++;
                
                 if ($record->get_category({ category => 'T0331' }) && $item_ref->{isbn}){
-                    $request2->execute($isbn13,50,4000,$count,$item_ref->{isbn});
-                    $request2->execute($isbn13,50,4001,$count,$content);
+
+                    # ISBN
+                    push @$enriched_content_ref, {
+                        isbn    => $isbn13,
+                        origin  => 50,
+                        field   => 4000,
+                        subfield => $count,
+                        content => $item_ref->{isbn},
+                    };
+
+                    # Title als JSON
+                    push @$enriched_content_ref, {
+                        isbn    => $isbn13,
+                        origin  => 50,
+                        field   => 4001,
+                        subfield => $count,
+                        content => $content,
+                    };
                 }
                 last if ($count > 5);
             }
         }
+
+        $enrichment->{schema}->resultset('EnrichedContentByIsbn')->populatet($enriched_content_ref);
     }
+
+    if ($isbncount % 1000 == 0){
+        $logger->info("$isbncount processed");
+    }
+    
+    $isbncount++;
 }
 

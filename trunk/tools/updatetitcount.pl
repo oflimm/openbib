efe455858369af52dcb0bda8000d7e4e72bf21ab
flimm@sigtrap.de <flimm@sigtrap.de@e7c75b2f-8ac8-f892-83e0-76010f6c4c37>
2012-07-12 06:58:03 +0000

    - Korrektur: error_die in error geaendert

--- trunk/tools/updatetitcount.pl
+++ trunk/tools/updatetitcount.pl
@@ -91,7 +91,7 @@ foreach $database (@databases){
     if ($config->{dbimodule} eq "Pg"){
         eval {
             # UTF8: {'pg_enable_utf8'    => 1}
-            $schema = OpenBib::Database::Catalog->connect("DBI:$config->{dbimodule}:dbname=$database;host=$config->{dbhost};port=$config->{dbport}", $config->{dbuser}, $config->{dbpasswd},{'pg_enable_utf8'    => 1}) or $logger->error_die($DBI::errstr);
+            $schema = OpenBib::Database::Catalog->connect("DBI:$config->{dbimodule}:dbname=$database;host=$config->{dbhost};port=$config->{dbport}", $config->{dbuser}, $config->{dbpasswd},{'pg_enable_utf8'    => 1}) or $logger->error($DBI::errstr);
         };
         
         if ($@){
@@ -102,7 +102,7 @@ foreach $database (@databases){
     elsif ($config->{dbimodule} eq "mysql"){
         eval {
             # UTF8: {'mysql_enable_utf8'    => 1, on_connect_do => [ q|SET NAMES 'utf8'| ,]}
-            $schema = OpenBib::Database::Catalog->connect("DBI:$config->{dbimodule}:dbname=$database;host=$config->{dbhost};port=$config->{dbport}", $config->{dbuser}, $config->{dbpasswd},{'mysql_enable_utf8'    => 1, on_connect_do => [ q|SET NAMES 'utf8'| ,]}) or $logger->error_die($DBI::errstr);
+            $schema = OpenBib::Database::Catalog->connect("DBI:$config->{dbimodule}:dbname=$database;host=$config->{dbhost};port=$config->{dbport}", $config->{dbuser}, $config->{dbpasswd},{'mysql_enable_utf8'    => 1, on_connect_do => [ q|SET NAMES 'utf8'| ,]}) or $logger->error($DBI::errstr);
         };
         
         if ($@){

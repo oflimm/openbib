15681654fb3edb63bf761d305972e72b741c2f08
flimm@sigtrap.de <flimm@sigtrap.de@e7c75b2f-8ac8-f892-83e0-76010f6c4c37>
2012-07-17 06:29:03 +0000

    - Umbenennung

--- conv/auto/filter/inst450/alt_remote.pl
+++ conv/auto/filter/inst450/alt_remote.pl
@@ -41,7 +41,7 @@ my $pool          = $ARGV[0];
 
 print "### $pool: Umwandlung in das Meta-Format\n";
 
-system("cd $pooldir/$pool ; rm unload.* pool.dat");
+system("cd $pooldir/$pool ; rm meta.* pool.dat");
 system("gzip -dc $pooldir/$pool/pool.dat.gz > $pooldir/$pool/pool.dat");
 system("cd $pooldir/$pool ; $konvdir/lidos32meta.pl --filename=$pooldir/$pool/pool.dat");
 system("rm $pooldir/$pool/pool.dat");

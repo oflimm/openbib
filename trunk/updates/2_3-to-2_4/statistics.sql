4e69dcd2ebed5c5ae3f658666ad09f2f84a6e8d2
flimm@sigtrap.de <flimm@sigtrap.de@e7c75b2f-8ac8-f892-83e0-76010f6c4c37>
2012-08-15 07:20:50 +0000

    - Weitere Tabellenaenderungen zum Ursprungs PostgreSQL-Schema in v2.4

---
+++ trunk/updates/2_3-to-2_4/statistics.sql
@@ -0,0 +1,8 @@
+alter table datacache alter column id type text;
+alter table datacache alter column subkey type text;
+ alter table titleusage alter column isbn type text;
+alter table titleusage alter column dbname type text;
+alter table titleusage alter column id type text;
+alter table searchterms alter column viewname type text;
+ alter table searchterms alter column content type text;
+alter table searchfields alter column viewname type text;

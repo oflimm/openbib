77d258a4c55414ed205c556510a1bb75c35be382
flimm@sigtrap.de <flimm@sigtrap.de@e7c75b2f-8ac8-f892-83e0-76010f6c4c37>
2012-08-30 16:44:40 +0000

    - Anpassung fuer DBIS

--- portal/perl/modules/OpenBib/Catalog/Backend/DBIS.pm
+++ portal/perl/modules/OpenBib/Catalog/Backend/DBIS.pm
@@ -49,44 +49,166 @@ use OpenBib::Record::Title;
 use base qw(OpenBib::Catalog);
 
 sub new {
-    my ($class,$arg_ref) = @_;
+    my ($class,$database) = @_;
+
+    # Log4perl logger erzeugen
+    my $logger = get_logger();
+
+    my $self = { };
+
+    bless ($self, $class);
+
+    $self->{database} = $database;
+    $self->{client}   = LWP::UserAgent->new;            # HTTP client
+    
+    return $self;
+}
+
+sub load_full_record {
+    my ($self,$arg_ref) = @_;
 
     # Set defaults
-    my $bibid     = exists $arg_ref->{bibid}
-        ? $arg_ref->{bibid}       : undef;
-
-    my $client_ip = exists $arg_ref->{client_ip}
-        ? $arg_ref->{client_ip}   : undef;
-
-    my $colors    = exists $arg_ref->{colors}
-        ? $arg_ref->{colors}      : undef;
-
-    my $ocolors   = exists $arg_ref->{ocolors}
-        ? $arg_ref->{ocolors}     : undef;
-
-    my $lang      = exists $arg_ref->{lang}
-        ? $arg_ref->{lang}        : undef;
+    my $id = exists $arg_ref->{id}
+        ? $arg_ref->{id}     : '';
 
     # Log4perl logger erzeugen
     my $logger = get_logger();
 
     my $config = OpenBib::Config->instance;
+
+    my $url="http://rzblx10.uni-regensburg.de/dbinfo/detail.php?colors=".((defined $self->{colors})?$self->{colors}:"")."&ocolors=".((defined $self->{ocolors})?$self->{ocolors}:"")."&lett=f&titel_id=$id&bibid=".((defined $self->{bibid})?$self->{bibid}:"")."&lang=".((defined $self->{lang})?$self->{lang}:"")."&xmloutput=1";
+
+    $logger->debug("Request: $url");
+
+    my $response = $self->{client}->get($url)->decoded_content(charset => 'latin1');
+
+    $logger->debug("Response: $response");
     
-    my $self = { };
+    my $parser = XML::LibXML->new();
+    my $tree   = $parser->parse_string($response);
+    my $root   = $tree->getDocumentElement;
 
-    bless ($self, $class);
-
-    $logger->debug("Initializing with colors = ".(defined $colors || '')." and ocolors = ".(defined $ocolors | '')." and lang = ".(defined $lang || ''));
+    my $access_info_ref = {};
     
-    $self->{bibid}      = (defined $bibid)?$bibid:(defined $config->{dbis_bibid})?$config->{dbis_bibid}:undef;
-    $self->{colors}     = (defined $colors)?$colors:(defined $config->{dbis_colors})?$config->{dbis_colors}:undef;
-    $self->{ocolors}    = (defined $ocolors)?$ocolors:(defined $config->{dbis_ocolors})?$config->{dbis_ocolors}:undef;
-    $self->{client_ip}  = (defined $client_ip )?$client_ip:undef;
-    $self->{lang}       = (defined $lang )?$lang:undef;
+    $access_info_ref->{icon_url}   = $root->findvalue('/dbis_page/details/db_access_info/@access_icon');
+    $access_info_ref->{desc}       = $root->findvalue('/dbis_page/details/db_access_info/db_access');
+    $access_info_ref->{desc_short} = $root->findvalue('/dbis_page/details/db_access_info/db_access_short_text');
+    
+    my $db_type_ref = [];
+    my @db_type_nodes = $root->findnodes('/dbis_page/list_dbs/db_type_infos/db_type_info');
+    foreach my $db_type_node (@db_type_nodes){
+        my $this_db_type_ref = {};
+        $this_db_type_ref->{desc}       = $db_type_node->findvalue('db_type_long_text');
+        $this_db_type_ref->{desc_short} = $db_type_node->findvalue('db_type');
+        $this_db_type_ref->{desc}=~s/\|/<br\/>/g;
+        push @$db_type_ref, $this_db_type_ref;
+    }
 
-    $self->{client}  = LWP::UserAgent->new;            # HTTP client
+    my @title_nodes = $root->findnodes('/dbis_page/details/titles/title');
 
-    return $self;
+    my $title_ref = {};
+    $title_ref->{other} = [];
+
+    foreach my $this_node (@title_nodes){
+        $title_ref->{main}     =  decode_utf8($this_node->textContent) if ($this_node->findvalue('@main') eq "Y");
+        push @{$title_ref->{other}}, decode_utf8($this_node->textContent) if ($this_node->findvalue('@main') eq "N");
+    }
+
+    my $access_ref = {};
+    $access_ref->{other} = [];
+
+    my @access_nodes = $root->findnodes('/dbis_page/details/accesses/access');
+
+    foreach my $this_node (@access_nodes){
+        $access_ref->{main}     =  decode_utf8($this_node->findvalue('@href')) if ($this_node->findvalue('@main') eq "Y");
+        push @{$access_ref->{other}}, decode_utf8($this_node->findvalue('@href')) if ($this_node->findvalue('@main') eq "N");
+    }
+    
+    my $hints   =  decode_utf8($root->findvalue('/dbis_page/details/hints'));
+    my $content =  decode_utf8($root->findvalue('/dbis_page/details/content'));
+    my $instructions =  decode_utf8($root->findvalue('/dbis_page/details/instructions'));
+
+    my @subjects_nodes =  $root->findnodes('/dbis_page/details/subjects/subject');
+
+    my $subjects_ref = [];
+
+    foreach my $subject_node (@subjects_nodes){
+        push @{$subjects_ref}, decode_utf8($subject_node->textContent);
+    }
+
+    my @keywords_nodes =  $root->findnodes('/dbis_page/details/keywords/keyword');
+
+    my $keywords_ref = [];
+
+    foreach my $keyword_node (@keywords_nodes){
+        push @{$keywords_ref}, decode_utf8($keyword_node->textContent);
+    }
+
+    my $appearence  =  decode_utf8($root->findvalue('/ezb_page/ezb_detail_about_db/db/detail/appearence'));
+
+#     return {
+#             id             => $id,
+#         title          => $title_ref,
+#         hints          => $hints,
+#         content        => $content,
+#         instructions   => $instructions,
+#         subjects       => $subjects_ref,
+#         keywords       => $keywords_ref,
+#         appearence     => $appearence,
+#         access         => $access_ref,
+#         access_info    => $access_info_ref,
+#         db_type        => $db_type_ref,
+#     };
+
+    my $record = new OpenBib::Record::Title({ database => $self->{database}, id => $id });
+
+    $record->set_field({field => 'T0331', subfield => '', mult => 1, content => $title_ref->{main}}) if ($title_ref->{main});
+
+    my $mult=1;
+
+    foreach my $othertitle (@$title_ref->{other}){
+        $record->set_field({field => 'T0370', subfield => '', mult => $mult, content => $othertitle});
+    }
+    
+#    $record->set_field({field => 'T0662', subfield => '', mult => 1, content => $zdb_node_ref->{ZDB_number}{url}}) if ($zdb_node_ref->{ZDB_number}{url});
+#    $record->set_field({field => 'T0663', subfield => '', mult => 1, content => $zdb_node_ref->{ZDB_number}{content}}) if ($zdb_node_ref->{ZDB_number}{content});
+
+    $mult=1;
+    foreach my $subject (@$subjects_ref){
+        $record->set_field({field => 'T0710', subfield => '', mult => $mult, content => $subject});
+        $mult++;
+    }
+
+    $mult=1;
+    foreach my $keyword (@$keywords_ref){
+        $record->set_field({field => 'T0700', subfield => '', mult => $mult, content => $keyword});
+        $mult++;
+    }
+    
+    $record->set_field({field => 'T0523', subfield => '', mult => 1, content => $appearence}) if ($appearence);
+#    $record->set_field({field => 'T0511', subfield => '', mult => 1, content => $costs}) if ($costs);
+#    $record->set_field({field => 'T0501', subfield => '', mult => 1, content => $remarks}) if ($remarks);
+
+#    foreach my $homepage (@$homepages_ref){
+#        $record->set_field({field => 'T2662', subfield => '', mult => 1, content => $homepage});
+#    }
+    
+    return $record;
+    
+#     return {
+#         id             => $id,
+#         title          => $title,
+#         publisher      => $publisher,
+#         ZDB_number     => $zdb_node_ref,
+#         subjects       => $subjects_ref,
+#         keywords       => $keywords_ref,
+#         firstvolume    => $firstvolume,
+#         firstdate      => $firstdate,
+#         appearence     => $appearence,
+#         costs          => $costs,
+#         homepages      => $homepages_ref,
+#         remarks        => $remarks,
+#     };
 }
 
 sub get_subjects {

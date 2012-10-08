404ecfa6201af5ebdb13847c2a6a1b33c07f9b86
flimm@sigtrap.de <flimm@sigtrap.de@e7c75b2f-8ac8-f892-83e0-76010f6c4c37>
2012-10-08 14:08:08 +0000

    - Korrektur Signaturen

--- trunk/conv/auto/filter/usbsab/gen-subset.pl
+++ trunk/conv/auto/filter/usbsab/gen-subset.pl
@@ -76,7 +76,7 @@ Log::Log4perl::init(\$log4Perl_config);
 my $logger = get_logger();
 
 my $subset = new OpenBib::Catalog::Subset("inst001",$pool);
-$subset->identify_by_mark(["^A2[3-9]","^A[3-9][0-9]"]);
+$subset->identify_by_mark(["^2[3-9]A","^[3-9][0-9]A"]);
 $subset->write_set;
 
 sub print_help {

2e3b33cb1d9304182d30685d4ecc3c00339629a7
flimm@sigtrap.de <flimm@sigtrap.de@e7c75b2f-8ac8-f892-83e0-76010f6c4c37>
2012-10-22 17:00:47 +0000

    - Aktualisierung

--- trunk/portal/perl/modules/OpenBib/Schema/System.pm
+++ trunk/portal/perl/modules/OpenBib/Schema/System.pm
@@ -11,8 +11,8 @@ use base 'DBIx::Class::Schema';
 __PACKAGE__->load_namespaces;
 
 
-# Created by DBIx::Class::Schema::Loader v0.07010 @ 2012-08-20 09:56:54
-# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:VyXnUfyGnhfDvUAdlSGwaw
+# Created by DBIx::Class::Schema::Loader v0.07010 @ 2012-10-18 16:51:34
+# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:wexPR0gFTzzCtB3EPtxf0Q
 
 
 # You can replace this text with custom code or comments, and it will be preserved on regeneration

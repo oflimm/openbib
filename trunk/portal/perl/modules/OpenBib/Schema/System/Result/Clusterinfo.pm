ec2d6640c81ac4b730f861f9b1181a99137f9666
flimm@sigtrap.de <flimm@sigtrap.de@e7c75b2f-8ac8-f892-83e0-76010f6c4c37>
2012-10-23 06:48:13 +0000

    - Neue Tabelle clusterinfo

---
+++ trunk/portal/perl/modules/OpenBib/Schema/System/Result/Clusterinfo.pm
@@ -0,0 +1,86 @@
+package OpenBib::Schema::System::Result::Clusterinfo;
+
+# Created by DBIx::Class::Schema::Loader
+# DO NOT MODIFY THE FIRST PART OF THIS FILE
+
+use strict;
+use warnings;
+
+use base 'DBIx::Class::Core';
+
+
+=head1 NAME
+
+OpenBib::Schema::System::Result::Clusterinfo
+
+=cut
+
+__PACKAGE__->table("clusterinfo");
+
+=head1 ACCESSORS
+
+=head2 id
+
+  data_type: 'bigint'
+  is_auto_increment: 1
+  is_nullable: 0
+  sequence: 'clusterinfo_id_seq'
+
+=head2 description
+
+  data_type: 'text'
+  is_nullable: 1
+
+=head2 status
+
+  data_type: 'text'
+  is_nullable: 1
+
+=head2 active
+
+  data_type: 'boolean'
+  is_nullable: 1
+
+=cut
+
+__PACKAGE__->add_columns(
+  "id",
+  {
+    data_type         => "bigint",
+    is_auto_increment => 1,
+    is_nullable       => 0,
+    sequence          => "clusterinfo_id_seq",
+  },
+  "description",
+  { data_type => "text", is_nullable => 1 },
+  "status",
+  { data_type => "text", is_nullable => 1 },
+  "active",
+  { data_type => "boolean", is_nullable => 1 },
+);
+__PACKAGE__->set_primary_key("id");
+
+=head1 RELATIONS
+
+=head2 serverinfos
+
+Type: has_many
+
+Related object: L<OpenBib::Schema::System::Result::Serverinfo>
+
+=cut
+
+__PACKAGE__->has_many(
+  "serverinfos",
+  "OpenBib::Schema::System::Result::Serverinfo",
+  { "foreign.clusterid" => "self.id" },
+  { cascade_copy => 0, cascade_delete => 0 },
+);
+
+
+# Created by DBIx::Class::Schema::Loader v0.07010 @ 2012-10-18 16:51:34
+# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:MSGHhU+kvmTrJdbS+Y8f9A
+
+
+# You can replace this text with custom code or comments, and it will be preserved on regeneration
+1;

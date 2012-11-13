ca0b82c1b5ebed72e7722bbcbd0b4c3e7b12a939
flimm@sigtrap.de <flimm@sigtrap.de@e7c75b2f-8ac8-f892-83e0-76010f6c4c37>
2012-11-13 07:43:54 +0000

    - Umstellung auf Plural-Form

--- portal/perl/modules/OpenBib/Handler/Apache/Users/Tags.pm
+++ portal/perl/modules/OpenBib/Handler/Apache/Users/Tags.pm
@@ -1,6 +1,6 @@
 #####################################################################
 #
-#  OpenBib::Handler::Apache::User::Tag.pm
+#  OpenBib::Handler::Apache::User::Tags.pm
 #
 #  Copyright 2007-2012 Oliver Flimm <flimm@openbib.org>
 #
@@ -27,7 +27,7 @@
 # Einladen der benoetigten Perl-Module
 #####################################################################
 
-package OpenBib::Handler::Apache::User::Tag;
+package OpenBib::Handler::Apache::User::Tags;
 
 use strict;
 use warnings;

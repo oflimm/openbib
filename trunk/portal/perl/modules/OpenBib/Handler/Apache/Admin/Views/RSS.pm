29fb9788e73b46729750bb5987d9f6b22bfb3eed
flimm@sigtrap.de <flimm@sigtrap.de@e7c75b2f-8ac8-f892-83e0-76010f6c4c37>
2012-11-20 13:42:52 +0000

    - Modulname korrigiert

--- trunk/portal/perl/modules/OpenBib/Handler/Apache/Admin/Views/RSS.pm
+++ trunk/portal/perl/modules/OpenBib/Handler/Apache/Admin/Views/RSS.pm
@@ -1,8 +1,8 @@
 #####################################################################
 #
-#  OpenBib::Handler::Apache::Admin::ViewsRSS
+#  OpenBib::Handler::Apache::Admin::Views::RSS
 #
-#  Dieses File ist (C) 2004-2011 Oliver Flimm <flimm@openbib.org>
+#  Dieses File ist (C) 2004-2012 Oliver Flimm <flimm@openbib.org>
 #
 #  Dieses Programm ist freie Software. Sie koennen es unter
 #  den Bedingungen der GNU General Public License, wie von der
@@ -27,7 +27,7 @@
 # Einladen der benoetigten Perl-Module
 #####################################################################
 
-package OpenBib::Handler::Apache::Admin::ViewsRSS;
+package OpenBib::Handler::Apache::Admin::Views::RSS;
 
 use strict;
 use warnings;

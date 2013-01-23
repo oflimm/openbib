6016409ec2eddf4e978918898f8f1d8d802414fc
flimm@sigtrap.de <flimm@sigtrap.de@e7c75b2f-8ac8-f892-83e0-76010f6c4c37>
2013-01-23 09:08:26 +0000

    - Textaenderung: Unter Wortwolken kann sich niemand etwas vorstellen, daher genauer spezifiziert - Aenderung: Ueberschriften Arial und serifenlos

--- portal/perl/modules/OpenBib/Handler/Apache/Users/Circulations/Borrows.pm
+++ portal/perl/modules/OpenBib/Handler/Apache/Users/Circulations/Borrows.pm
@@ -2,7 +2,7 @@
 #
 #  OpenBib::Handler::Apache::Users::Circulations::Borrows
 #
-#  Dieses File ist (C) 2004-2012 Oliver Flimm <flimm@openbib.org>
+#  Dieses File ist (C) 2004-2013 Oliver Flimm <flimm@openbib.org>
 #
 #  Dieses Programm ist freie Software. Sie koennen es unter
 #  den Bedingungen der GNU General Public License, wie von der

b4deed0855d4cbbb989dc6f8683c9d997f85eaa7
flimm@sigtrap.de <flimm@sigtrap.de@e7c75b2f-8ac8-f892-83e0-76010f6c4c37>
2013-01-27 16:52:39 +0000

    - Testweise Umstellung auf Catalog-Singleton analog DBI-Singleton in v2.3

--- trunk/portal/perl/modules/OpenBib/Catalog/Backend/Local.pm
+++ trunk/portal/perl/modules/OpenBib/Catalog/Backend/Local.pm
@@ -43,7 +43,7 @@ use Storable ();
 use OpenBib::Config;
 use OpenBib::Config::CirculationInfoTable;
 use OpenBib::Config::DatabaseInfoTable;
-use OpenBib::Schema::DBI;
+use OpenBib::Schema::Catalog::Singleton;
 use OpenBib::Record::Title;
 use OpenBib::RecordList::Title;
 
@@ -1001,7 +1001,7 @@ sub connectDB {
 
     eval {
         # UTF8: {'pg_enable_utf8'    => 1} 
-        $self->{schema} = OpenBib::Schema::Catalog->connect("DBI:Pg:dbname=$database;host=$config->{dbhost};port=$config->{dbport}", $config->{dbuser}, $config->{dbpasswd},{'pg_enable_utf8'    => 1 }) or $logger->error_die($DBI::errstr);
+        $self->{schema} = OpenBib::Schema::Catalog::Singleton->connect("DBI:Pg:dbname=$database;host=$config->{dbhost};port=$config->{dbport}", $config->{dbuser}, $config->{dbpasswd},{'pg_enable_utf8'    => 1 }) or $logger->error_die($DBI::errstr);
     };
 
     if ($@){

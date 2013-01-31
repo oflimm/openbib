eeebce6886e59afdd4479d20204c68f9271d57f7
flimm@sigtrap.de <flimm@sigtrap.de@e7c75b2f-8ac8-f892-83e0-76010f6c4c37>
2013-01-31 12:45:50 +0000

    - Umstellung des rswk-Filters auf JSON

--- conv/auto/filter/zbmed/filter-rswk.pl
+++ conv/auto/filter/zbmed/filter-rswk.pl
@@ -1,12 +1,27 @@
 #!/usr/bin/perl
 
+use JSON::XS;
+
 while (<>){
+    my $title_ref = decode_json $_;
+
     # 'Normale' Schlagworte ignorieren
-    next if (/^09[01234][27]/);
+    foreach my $field (keys %$title_ref){
+        if ($field =~/^09[01234][27]/){
+            delete $title_ref->{$field};
+        }
+    }
 
     # 'Eigene' Schlagworte stattdessen verwenden
-    if (/^19[01234][27]/){
-        substr($_,0,1)="0";
+    foreach my $field (keys %$title_ref){
+        if ($field =~/^19[01234][27]/){
+            $new_field = '0'.substr($field,1,3);
+
+            $title_ref->{$new_field} = $title_ref->{$field};
+            
+            delete $title_ref->{$field};
+        }
     }
-    print;
+
+    print encode_json $title_ref, "\n";
 }

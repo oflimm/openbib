3aeaaa613a80ae70ac24e0dbe50ea83f7b001b4a
flimm@sigtrap.de <flimm@sigtrap.de@e7c75b2f-8ac8-f892-83e0-76010f6c4c37>
2013-03-25 16:08:29 +0000

    - Anreicherung des USB-Bestands mit Information 4400:digital entsprechend der jeweiligen Kriterien

---
+++ trunk/conv/auto/filter/inst001/add-availability.pl
@@ -0,0 +1,44 @@
+#!/usr/bin/perl
+
+use JSON::XS;
+
+while (<>){
+    my $title_ref = decode_json $_;
+
+    my $is_digital = 0;
+
+    # 1) T0807 hat Inhalt 'g'
+    if (defined $title_ref->{'0807'}){
+        foreach my $item (@{$title_ref->{'0807'}}){
+            if ($item->{content} eq "g"){
+                $is_digital = 1;
+            }        
+        }
+    }
+    
+    # 2) T2662  ist besetzt
+    if (defined $title_ref->{'2662'}){
+        $is_digital = 1;
+    } 
+    
+    # 3) T0078 hat Inhalt 'ldd'
+    if (defined $title_ref->{'0078'}){
+        foreach my $item (@{$title_ref->{'0078'}}){
+            if ($item->{content} eq "ldd"){
+                $is_digital = 1;
+            }        
+        }
+    }
+
+    if ($is_digital){
+        $title_ref->{'4400'} = [
+            {
+                mult     => 1,
+                subfield => '',
+                content  => "online",
+            },
+        ];
+    }
+   
+    print encode_json $title_ref, "\n";
+}

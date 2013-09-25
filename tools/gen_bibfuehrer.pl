b146b666e04322b3cba33e874c9f25e058c26ffe
flimm@sigtrap.de <flimm@sigtrap.de@e7c75b2f-8ac8-f892-83e0-76010f6c4c37>
2013-09-25 09:44:31 +0000

    - Korrektur: #-Zeichen muss fuer LaTeX escaped werden

--- tools/gen_bibfuehrer.pl
+++ tools/gen_bibfuehrer.pl
@@ -230,6 +230,8 @@ sub filterchars {
   $content = encode("utf8",$content);
   $content=~s/\x{c2}\x{a0}//g;
   $content=~s/\x{e2}\x{80}\x{89}//g;
+  $content=~s/#/\\#/g;
+
   $content = decode("utf8", $content);
   # Umlaute
   #$content=~s/\&uuml\;/ü/g;

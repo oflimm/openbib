92212bb9733c1b8cff9731c4e6c7304043463d3a
flimm@sigtrap.de <flimm@sigtrap.de@e7c75b2f-8ac8-f892-83e0-76010f6c4c37>
2013-11-20 13:30:52 +0000

    - Standardloglevel auf INFO erhoeht

--- trunk/tools/gen_bestof.pl
+++ trunk/tools/gen_bestof.pl
@@ -65,7 +65,7 @@ if ($help){
 $logfile=($logfile)?$logfile:'/var/log/openbib/gen_bestof.log';
 
 my $log4Perl_config = << "L4PCONF";
-log4perl.rootLogger=DEBUG, LOGFILE, Screen
+log4perl.rootLogger=INFO, LOGFILE, Screen
 log4perl.appender.LOGFILE=Log::Log4perl::Appender::File
 log4perl.appender.LOGFILE.filename=$logfile
 log4perl.appender.LOGFILE.mode=append

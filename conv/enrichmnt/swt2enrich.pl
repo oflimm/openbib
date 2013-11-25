2e403c78099c770cbb7878d6d16e244887963af0
flimm@sigtrap.de <flimm@sigtrap.de@e7c75b2f-8ac8-f892-83e0-76010f6c4c37>
2013-11-25 08:11:26 +0000

    - Aenderung des Loglevels auf INFO

--- conv/enrichmnt/swt2enrich.pl
+++ conv/enrichmnt/swt2enrich.pl
@@ -65,7 +65,7 @@ my $config = OpenBib::Config->instance;
 $logfile=($logfile)?$logfile:"/var/log/openbib/swt-enrichmnt.log";
 
 my $log4Perl_config = << "L4PCONF";
-log4perl.rootLogger=DEBUG, LOGFILE, Screen
+log4perl.rootLogger=INFO, LOGFILE, Screen
 log4perl.appender.LOGFILE=Log::Log4perl::Appender::File
 log4perl.appender.LOGFILE.filename=$logfile
 log4perl.appender.LOGFILE.mode=append

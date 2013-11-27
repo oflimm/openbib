d000f4890ae14e7d302d8c3bc200e00cb59c183d
flimm@sigtrap.de <flimm@sigtrap.de@e7c75b2f-8ac8-f892-83e0-76010f6c4c37>
2013-11-27 20:38:54 +0000

    - Andere Kataloge muessen nicht gesynct werden

--- trunk/tools/update_circulation_availability.sh
+++ trunk/tools/update_circulation_availability.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for i in inst001 inst006 inst132 inst155 inst302 inst328 inst406 inst420 inst426 inst431 inst526
+for i in inst001 
 do
  /opt/openbib/bin/update_circulation_availability.pl --database=$i --configfile=/opt/openbib/conf/acq_${i}.yml
 done

568d39e5be7760da2d805b49d6f735929b206842
flimm@sigtrap.de <flimm@sigtrap.de@e7c75b2f-8ac8-f892-83e0-76010f6c4c37>
2014-02-20 07:10:13 +0000

    - Aenderung der Selektion auf Ort Koeln

--- conv/auto/filter/hochschulschriften/gen-subset.pl
+++ conv/auto/filter/hochschulschriften/gen-subset.pl
@@ -76,7 +76,7 @@ Log::Log4perl::init(\$log4Perl_config);
 my $logger = get_logger();
 
 my $subset = new OpenBib::Catalog::Subset("inst001",$pool);
-$subset->identify_by_field_content('title',([ { field => '0519', content => '.*Köln.*Diss' } ]));
+$subset->identify_by_field_content('title',([ { field => '0519', content => '.*Köln' } ]));
 $subset->write_set;
 
 sub print_help {

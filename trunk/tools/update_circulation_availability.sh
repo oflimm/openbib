390c14fc87bf8a182db0e42b3d0691ce59affeda
flimm@sigtrap.de <flimm@sigtrap.de@e7c75b2f-8ac8-f892-83e0-76010f6c4c37>
2014-03-06 13:46:57 +0000

    - Korrektur get_isbns.pl

--- trunk/tools/update_circulation_availability.sh
+++ trunk/tools/update_circulation_availability.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-for i in inst001 
+for i in inst006 inst132 inst155 inst302 inst328 inst406 inst420 inst426 inst431 inst526
 do
+ /opt/openbib/autoconv/bin/autoconv.pl --database=$i
  /opt/openbib/bin/update_circulation_availability.pl --database=$i --configfile=/opt/openbib/conf/acq_${i}.yml
 done

1ab72a4daced8bee54881c4a4ebd86904e7d4193
flimm@sigtrap.de <flimm@sigtrap.de@e7c75b2f-8ac8-f892-83e0-76010f6c4c37>
2014-05-14 14:44:11 +0000

    - Anpassung an neuen Metadaten-Namen

--- conv/auto/filter/inst132alt/alt_remote.pl
+++ conv/auto/filter/inst132alt/alt_remote.pl
@@ -49,5 +49,5 @@ print "### $pool: Alternative Umwandlung\n";
 
 system("zcat $rootdir/pools/$pool/".$dbinfo->titlefilename." | $helperexe > $rootdir/pools/$pool/pool.tmp.dat");
 system("cd $rootdir/pools/$pool ; $simpleexe --inputfile=pool.tmp.dat --configfile=/opt/openbib/conf/inst132alt.yml");
-system("cd $rootdir/pools/$pool ; rm unload.*.gz ; gzip unload.*");
+system("cd $rootdir/pools/$pool ; rm meta.*.gz ; gzip meta.*");
 

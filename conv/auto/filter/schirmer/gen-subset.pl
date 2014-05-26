f9bbd131b2f8e52c6ec264fa2e7f9e997bfc9cbf
flimm@sigtrap.de <flimm@sigtrap.de@e7c75b2f-8ac8-f892-83e0-76010f6c4c37>
2014-05-26 08:59:59 +0000

    - Umstellung Selektionskriterium der Sammlungen auf Titelkategorie 4700

--- conv/auto/filter/schirmer/gen-subset.pl
+++ conv/auto/filter/schirmer/gen-subset.pl
@@ -76,7 +76,7 @@ Log::Log4perl::init(\$log4Perl_config);
 my $logger = get_logger();
 
 my $subset = new OpenBib::Catalog::Subset("inst001",$pool);
-$subset->identify_by_field_content('classification',([ { field => '0800', content => '^Sammlung Horst Schirmer' } ]));
+$subset->identify_by_field_content('title',([ { field => '4700', content => '^Sammlung Horst Schirmer' } ]));
 $subset->write_set;
 
 sub print_help {
--- conv/auto/filter/syndikatsbibliothek/gen-subset.pl
+++ conv/auto/filter/syndikatsbibliothek/gen-subset.pl
@@ -76,7 +76,7 @@ Log::Log4perl::init(\$log4Perl_config);
 my $logger = get_logger();
 
 my $subset = new OpenBib::Catalog::Subset("inst001",$pool);
-$subset->identify_by_field_content('classification',([ { field => '0800', content => '^Sammlung Syndikatsbibliothek' } ]));
+$subset->identify_by_field_content('title',([ { field => '4700', content => '^Sammlung Syndikatsbibliothek' } ]));
 $subset->write_set;
 
 sub print_help {

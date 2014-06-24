4b9dc8708cbed2b423ce13ce36541ecc703ea51b
flimm@sigtrap.de <flimm@sigtrap.de@e7c75b2f-8ac8-f892-83e0-76010f6c4c37>
2014-06-24 12:39:04 +0000

    - Ausfiltern von Titeln, die innerhalb der vom zust. Fachref. definierten Urheberrechtsbereichs liegen

--- trunk/conv/auto/filter/digitalis/pre_conv.pl
+++ trunk/conv/auto/filter/digitalis/pre_conv.pl
@@ -51,4 +51,4 @@ my $bcp2metaexe   = "$konvdir/bcp2meta.pl";
 
 print "### $pool: Erweiterung um Zugriffsinformation online und Typ Digital\n";
 
-system("cd $pooldir/$pool ; zcat meta.title.gz| $rootdir/filter/$pool/add-fields.pl | gzip > meta.title.gz.tmp ; mv -f meta.title.gz.tmp meta.title.gz");
+system("cd $rootdir/data/$pool ; cat meta.title | $rootdir/filter/$pool/add-fields.pl |  $rootdir/filter/$pool/remove_copyrighted.pl  > meta.title.tmp ; mv -f meta.title.tmp meta.title");

8b586e471e712f453ca4aee4279bb163dd5196b0
flimm@sigtrap.de <flimm@sigtrap.de@e7c75b2f-8ac8-f892-83e0-76010f6c4c37>
2014-09-15 06:39:23 +0000

    - Verlagerung de Preprocessing bei den Muenzdaten vor die Konvertierung

--- conv/auto/filter/muenzen/pre_conv.pl
+++ conv/auto/filter/muenzen/pre_conv.pl
@@ -51,4 +51,4 @@ my $bcp2metaexe   = "$konvdir/bcp2meta.pl";
 
 print "### $pool: Erweiterung um Zugriffsinformation online\n";
 
-system("cd $pooldir/$pool ; zcat meta.title.gz| $rootdir/filter/$pool/pre_process.pl | gzip > meta.title.gz.tmp ; mv -f meta.title.gz.tmp meta.title.gz");
+system("cd $rootdir/data/$pool ; cat meta.title | $rootdir/filter/$pool/pre_process.pl > meta.title.tmp ; mv -f meta.title.tmp meta.title");

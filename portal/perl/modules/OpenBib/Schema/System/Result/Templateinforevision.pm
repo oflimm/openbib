75d66136a20facf56f30917c3de20a4962709e8b
flimm@sigtrap.de <flimm@sigtrap.de@e7c75b2f-8ac8-f892-83e0-76010f6c4c37>
2014-09-24 10:20:36 +0000

    - Zusaetzliche Template-Schemamodule dazu

---
+++ portal/perl/modules/OpenBib/Schema/System/Result/Templateinforevision.pm
@@ -0,0 +1,102 @@
+use utf8;
+package OpenBib::Schema::System::Result::Templateinforevision;
+
+# Created by DBIx::Class::Schema::Loader
+# DO NOT MODIFY THE FIRST PART OF THIS FILE
+
+=head1 NAME
+
+OpenBib::Schema::System::Result::Templateinforevision
+
+=cut
+
+use strict;
+use warnings;
+
+use base 'DBIx::Class::Core';
+
+=head1 TABLE: C<templateinforevision>
+
+=cut
+
+__PACKAGE__->table("templateinforevision");
+
+=head1 ACCESSORS
+
+=head2 id
+
+  data_type: 'bigint'
+  is_auto_increment: 1
+  is_nullable: 0
+  sequence: 'templateinforevision_id_seq'
+
+=head2 tstamp
+
+  data_type: 'timestamp'
+  is_nullable: 1
+
+=head2 templateid
+
+  data_type: 'bigint'
+  is_foreign_key: 1
+  is_nullable: 0
+
+=head2 templatetext
+
+  data_type: 'text'
+  is_nullable: 1
+
+=cut
+
+__PACKAGE__->add_columns(
+  "id",
+  {
+    data_type         => "bigint",
+    is_auto_increment => 1,
+    is_nullable       => 0,
+    sequence          => "templateinforevision_id_seq",
+  },
+  "tstamp",
+  { data_type => "timestamp", is_nullable => 1 },
+  "templateid",
+  { data_type => "bigint", is_foreign_key => 1, is_nullable => 0 },
+  "templatetext",
+  { data_type => "text", is_nullable => 1 },
+);
+
+=head1 PRIMARY KEY
+
+=over 4
+
+=item * L</id>
+
+=back
+
+=cut
+
+__PACKAGE__->set_primary_key("id");
+
+=head1 RELATIONS
+
+=head2 templateid
+
+Type: belongs_to
+
+Related object: L<OpenBib::Schema::System::Result::Templateinfo>
+
+=cut
+
+__PACKAGE__->belongs_to(
+  "templateid",
+  "OpenBib::Schema::System::Result::Templateinfo",
+  { id => "templateid" },
+  { is_deferrable => 1, on_delete => "CASCADE", on_update => "CASCADE" },
+);
+
+
+# Created by DBIx::Class::Schema::Loader v0.07025 @ 2014-09-24 11:40:48
+# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:RQ03hCdDH28Ch5X4xTIhcA
+
+
+# You can replace this text with custom code or comments, and it will be preserved on regeneration
+1;
---
+++ portal/perl/modules/OpenBib/Schema/System/Result/UserTemplateinfo.pm
@@ -0,0 +1,111 @@
+use utf8;
+package OpenBib::Schema::System::Result::UserTemplateinfo;
+
+# Created by DBIx::Class::Schema::Loader
+# DO NOT MODIFY THE FIRST PART OF THIS FILE
+
+=head1 NAME
+
+OpenBib::Schema::System::Result::UserTemplateinfo
+
+=cut
+
+use strict;
+use warnings;
+
+use base 'DBIx::Class::Core';
+
+=head1 TABLE: C<user_templateinfo>
+
+=cut
+
+__PACKAGE__->table("user_templateinfo");
+
+=head1 ACCESSORS
+
+=head2 id
+
+  data_type: 'bigint'
+  is_auto_increment: 1
+  is_nullable: 0
+  sequence: 'user_templateinfo_id_seq'
+
+=head2 userid
+
+  data_type: 'bigint'
+  is_foreign_key: 1
+  is_nullable: 0
+
+=head2 templateid
+
+  data_type: 'bigint'
+  is_foreign_key: 1
+  is_nullable: 0
+
+=cut
+
+__PACKAGE__->add_columns(
+  "id",
+  {
+    data_type         => "bigint",
+    is_auto_increment => 1,
+    is_nullable       => 0,
+    sequence          => "user_templateinfo_id_seq",
+  },
+  "userid",
+  { data_type => "bigint", is_foreign_key => 1, is_nullable => 0 },
+  "templateid",
+  { data_type => "bigint", is_foreign_key => 1, is_nullable => 0 },
+);
+
+=head1 PRIMARY KEY
+
+=over 4
+
+=item * L</id>
+
+=back
+
+=cut
+
+__PACKAGE__->set_primary_key("id");
+
+=head1 RELATIONS
+
+=head2 templateid
+
+Type: belongs_to
+
+Related object: L<OpenBib::Schema::System::Result::Templateinfo>
+
+=cut
+
+__PACKAGE__->belongs_to(
+  "templateid",
+  "OpenBib::Schema::System::Result::Templateinfo",
+  { id => "templateid" },
+  { is_deferrable => 1, on_delete => "CASCADE", on_update => "CASCADE" },
+);
+
+=head2 userid
+
+Type: belongs_to
+
+Related object: L<OpenBib::Schema::System::Result::Userinfo>
+
+=cut
+
+__PACKAGE__->belongs_to(
+  "userid",
+  "OpenBib::Schema::System::Result::Userinfo",
+  { id => "userid" },
+  { is_deferrable => 1, on_delete => "CASCADE", on_update => "CASCADE" },
+);
+
+
+# Created by DBIx::Class::Schema::Loader v0.07025 @ 2014-09-24 11:40:48
+# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:K0ln/tRqfO1mQdvwTojFTw
+
+
+# You can replace this text with custom code or comments, and it will be preserved on regeneration
+1;

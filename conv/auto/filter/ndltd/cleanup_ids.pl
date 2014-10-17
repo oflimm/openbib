9f701fb811f1b5f67f93da3d9e892cffc8b56d8d
flimm@sigtrap.de <flimm@sigtrap.de@e7c75b2f-8ac8-f892-83e0-76010f6c4c37>
2014-10-17 07:36:58 +0000

    - Eingrenzung auf IDs, die URL enthalten

--- conv/auto/filter/ndltd/cleanup_ids.pl
+++ conv/auto/filter/ndltd/cleanup_ids.pl
@@ -7,7 +7,9 @@ use JSON::XS;
 while (<>){
     my $title_ref = decode_json $_;
 
-    $title_ref->{id} =~s/[^0-9A-Za-z.:]/_/g;
-    
+    if ($title_ref->{id}=~m/http/){
+	$title_ref->{id} =~s/[^0-9A-Za-z.:]/_/g;
+    }
+
     print encode_json $title_ref, "\n";
 }

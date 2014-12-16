60141f9ece7fe1a638493d90fbe5e995524e4f64
flimm@sigtrap.de <flimm@sigtrap.de@e7c75b2f-8ac8-f892-83e0-76010f6c4c37>
2014-12-16 12:37:30 +0000

    - Generelle Ueberarbeitung an JSON-Format

--- conv/meta/unload2meta.pl
+++ conv/meta/unload2meta.pl
@@ -58,6 +58,7 @@ my ($id);
 
 my $mult_value = {};
 my $item_ref;
+my $ignore_record = 0;
 
 foreach my $thisdata_ref (@$normdata_ref){
 
@@ -65,15 +66,27 @@ foreach my $thisdata_ref (@$normdata_ref){
     open(IN,  "zcat $thisdata_ref->{in} |");
     open(OUT, "| gzip > $thisdata_ref->{out}");
 
+    my $have_title_ref = {};
+    
     while (<IN>){
         if (/^0000:(.*)$/){
             $id = $1;
 
+            if (defined $have_title_ref->{$id}){
+                print STDERR "Record $id doppelt\n";
+                $ignore_record = 1;
+                next;
+            }
+            $have_title_ref->{$id} = 1;
+            
             $mult_value = {};
             $item_ref = {};
             $item_ref->{id} = $id;
+            $item_ref->{fields} = {};
         }
         elsif (/^(\d\d\d\d)\.(\d\d\d):IDN: (\d+) ; (.+)$/) {
+            next if ($ignore_record);
+            
             my $field = $1;
             my $mult = $2;
             my $verwid = $3;
@@ -82,7 +95,7 @@ foreach my $thisdata_ref (@$normdata_ref){
             if (defined $thisdata_ref->{mapping}{$field}){
                 $field = $thisdata_ref->{mapping}{$field};
             }
-            push @{$item_ref->{$field}}, {
+            push @{$item_ref->{fields}{$field}}, {
                 id    => $verwid,
                 mult => $mult,
                 subfield => '',
@@ -90,6 +103,7 @@ foreach my $thisdata_ref (@$normdata_ref){
             };
         }
         elsif (/^(\d\d\d\d):IDN: (\d+) ; (.+)$/) {
+            next if ($ignore_record);
             my $field = $1;
             my $verwid = $2;
             my $mult = ++$mult_value->{$field};
@@ -99,7 +113,7 @@ foreach my $thisdata_ref (@$normdata_ref){
                 $field = $thisdata_ref->{mapping}{$field};
             }
             
-            push @{$item_ref->{$field}}, {
+            push @{$item_ref->{fields}{$field}}, {
                 id    => $verwid,
                 mult => $mult,
                 subfield => '',
@@ -107,6 +121,7 @@ foreach my $thisdata_ref (@$normdata_ref){
             };
         }
         elsif (/^(\d\d\d\d)\.(\d\d\d):IDN: (\d+) (\[.+\])$/) {
+            next if ($ignore_record);
             my $field = $1;
             my $mult = $2;
             my $verwid = $3;
@@ -115,7 +130,7 @@ foreach my $thisdata_ref (@$normdata_ref){
             if (defined $thisdata_ref->{mapping}{$field}){
                 $field = $thisdata_ref->{mapping}{$field};
             }
-            push @{$item_ref->{$field}}, {
+            push @{$item_ref->{fields}{$field}}, {
                 id    => $verwid,
                 mult => $mult,
                 subfield => '',
@@ -123,6 +138,7 @@ foreach my $thisdata_ref (@$normdata_ref){
             };
         }
         elsif (/^(\d\d\d\d):IDN: (\d+) (\[.+\])$/) {
+            next if ($ignore_record);
             my $field = $1;
             my $verwid = $2;
             my $mult = ++$mult_value->{$field};
@@ -132,7 +148,7 @@ foreach my $thisdata_ref (@$normdata_ref){
                 $field = $thisdata_ref->{mapping}{$field};
             }
             
-            push @{$item_ref->{$field}}, {
+            push @{$item_ref->{fields}{$field}}, {
                 id    => $verwid,
                 mult => $mult,
                 subfield => '',
@@ -140,6 +156,7 @@ foreach my $thisdata_ref (@$normdata_ref){
             };
         }
         elsif (/^(\d\d\d\d)\.(\d\d\d):IDN: (\d+)$/) {
+            next if ($ignore_record);
             my $field = $1;
             my $mult = $2;
             my $verwid = $3;
@@ -149,7 +166,7 @@ foreach my $thisdata_ref (@$normdata_ref){
                 $field = $thisdata_ref->{mapping}{$field};
             }
             
-            push @{$item_ref->{$field}}, {
+            push @{$item_ref->{fields}{$field}}, {
                 id    => $verwid,
                 mult => $mult,
                 subfield => '',
@@ -157,6 +174,7 @@ foreach my $thisdata_ref (@$normdata_ref){
             };
         }
         elsif (/^(\d\d\d\d):IDN: (\d+)$/) {
+            next if ($ignore_record);
             my $field = $1;
             my $verwid = $2;
             my $mult = ++$mult_value->{$field};
@@ -166,7 +184,7 @@ foreach my $thisdata_ref (@$normdata_ref){
             if (defined $thisdata_ref->{mapping}{$field}){
                 $field = $thisdata_ref->{mapping}{$field};
             }
-            push @{$item_ref->{$field}}, {
+            push @{$item_ref->{fields}{$field}}, {
                 id    => $verwid,
                 mult => $mult,
                 subfield => '',
@@ -174,6 +192,7 @@ foreach my $thisdata_ref (@$normdata_ref){
             };
         }
         elsif (/^(\d\d\d\d)\.(\d\d\d):(.+)$/) {
+            next if ($ignore_record);
             my $field = $1;
             my $mult = $2;
             my $content = $3;
@@ -181,30 +200,49 @@ foreach my $thisdata_ref (@$normdata_ref){
             if (defined $thisdata_ref->{mapping}{$field}){
                 $field = $thisdata_ref->{mapping}{$field};
             }
-            push @{$item_ref->{$field}}, {
+            push @{$item_ref->{fields}{$field}}, {
                 mult => $mult,
                 subfield => '',
                 content => $content,
             };
         }
         elsif (/^(\d\d\d\d):(.+)$/) {
+            next if ($ignore_record);
             my $field = $1;
-            my $mult = ++$mult_value->{$field};
             my $content = $2;
 
             if (defined $thisdata_ref->{mapping}{$field}){
                 $field = $thisdata_ref->{mapping}{$field};
             }
-            
-            push @{$item_ref->{$field}}, {
-                mult => $mult,
-                subfield => '',
-                content => $content,
-            };
+
+            my @parts = ();
+            if ($field eq "0425"){
+                @parts = split('\s+;\s+',$content);
+
+                foreach my $part (@parts){
+                    my $mult = ++$mult_value->{$field};
+                    
+                    push @{$item_ref->{fields}{$field}}, {
+                        mult => $mult,
+                        subfield => '',
+                        content => $part,
+                    };
+                }
+            }
+            else {
+                my $mult = ++$mult_value->{$field};
+                
+                push @{$item_ref->{fields}{$field}}, {
+                    mult => $mult,
+                    subfield => '',
+                    content => $content,
+                };
+            }
 
         }
         elsif (/^9999/){
-            print OUT JSON::XS->new->utf8->encode ($item_ref), "\n";            
+            print OUT JSON::XS->new->encode ($item_ref), "\n";
+            $ignore_record = 0;
         }
 
     }

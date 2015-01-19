a4287e5d0572223fa8575a48a007fe1a96d8d5d5
flimm@sigtrap.de <flimm@sigtrap.de@e7c75b2f-8ac8-f892-83e0-76010f6c4c37>
2015-01-19 15:03:29 +0000

    - Merge der Aenderungen in trunk

--- branches/psgi2-branch/conv/auto/filter/tmpebooks/pre_conv.pl
+++ branches/psgi2-branch/conv/auto/filter/tmpebooks/pre_conv.pl
@@ -51,4 +51,4 @@ my $bcp2metaexe   = "$konvdir/bcp2meta.pl";
 
 print "### $pool: Erweiterung um Zugriffsinformation online\n";
 
-system("cd $pooldir/$pool ; zcat meta.title.gz| $rootdir/filter/$pool/exclude_from_wikipage.pl | $rootdir/filter/$pool/add-fields.pl | gzip > meta.title.gz.tmp ; mv -f meta.title.gz.tmp meta.title.gz");
+system("cd $rootdir/data/$pool ; cat meta.title | $rootdir/filter/$pool/exclude_from_wikipage.pl | $rootdir/filter/$pool/add-fields.pl > meta.title.tmp ; mv -f meta.title.tmp meta.title");

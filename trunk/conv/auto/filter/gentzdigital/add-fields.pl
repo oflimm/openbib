468e1149dfd97d35c9806fadb62b6e4f7d7cb9c0
flimm@sigtrap.de <flimm@sigtrap.de@e7c75b2f-8ac8-f892-83e0-76010f6c4c37>
2015-01-23 08:26:35 +0000

    - Anreicherung mit Registerkategorien: Jahre fuer 'Briefe an/von Gentz' und 'Briefe Dritter'

---
+++ trunk/conv/auto/filter/gentzdigital/add-fields.pl
@@ -0,0 +1,46 @@
+#!/usr/bin/perl
+
+use JSON::XS;
+
+while (<>){
+    my $title_ref = decode_json $_;
+
+    if (defined $title_ref->{fields}{'0800'}[0]{content}){
+        if ($title_ref->{fields}{'0800'}[0]{content} eq "Briefe von Gentz"){
+            if (defined $title_ref->{fields}{'0425'}[0]{content}){
+                $title_ref->{fields}{'0426'} = [
+                    {
+                        mult     => 1,
+                        subfield => '',
+                        content  => $title_ref->{fields}{'0425'}[0]{content},
+                    },
+                ];
+            }
+        }
+        elsif ($title_ref->{fields}{'0800'}[0]{content} eq "Briefe an Gentz"){
+            if (defined $title_ref->{fields}{'0425'}[0]{content}){
+                $title_ref->{fields}{'0427'} = [
+                    {
+                        mult     => 1,
+                        subfield => '',
+                        content  => $title_ref->{fields}{'0425'}[0]{content},
+                    },
+                ];
+            }
+        }
+        elsif ($title_ref->{fields}{'0800'}[0]{content} eq "Briefe Dritter"){
+            if (defined $title_ref->{fields}{'0425'}[0]{content}){
+                $title_ref->{fields}{'0428'} = [
+                    {
+                        mult     => 1,
+                        subfield => '',
+                        content  => $title_ref->{fields}{'0425'}[0]{content},
+                    },
+                ];
+            }
+        }
+        
+    }
+
+    print encode_json $title_ref, "\n";
+}

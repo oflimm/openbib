ca1a3b541d6cc5dc261ac493ce87c4cb0b3794db
flimm@sigtrap.de <flimm@sigtrap.de@e7c75b2f-8ac8-f892-83e0-76010f6c4c37>
2015-02-09 11:33:07 +0000

    - Uebertragung der Aenderung aus Trunk

--- branches/psgi2-branch/conv/auto/filter/inst001/add-locationid.pl
+++ branches/psgi2-branch/conv/auto/filter/inst001/add-locationid.pl
@@ -55,6 +55,10 @@ while (<HOLDING>){
         elsif ($location_ref->{content} =~m/^Fachbibliothek Asien \/ China/){
             push @{$title_locationid_ref->{$titleid}}, "DE-38-450";
         }
+        elsif ($location_ref->{content} =~m/^Fachbibliothek Arch.*?ologien \/ Arch.*?ologisches Institut/){
+            push @{$title_locationid_ref->{$titleid}}, "DE-38-427";
+            push @{$title_locationid_ref->{$titleid}}, "DE-38-ARCH";
+        }
 
         if ($location_ref->{content} =~m/^Fachbibliothek Asien/){
             push @{$title_locationid_ref->{$titleid}}, "DE-38-ASIEN";
@@ -105,6 +109,7 @@ while (<>){
                 push @{$title_ref->{'locations'}}, "DE-38-HWA";
                 push @{$title_ref->{'locations'}}, "DE-38-ASIEN";
                 push @{$title_ref->{'locations'}}, "DE-38-MEKUTH";
+                push @{$title_ref->{'locations'}}, "DE-38-ARCH";
             }
         }
     }

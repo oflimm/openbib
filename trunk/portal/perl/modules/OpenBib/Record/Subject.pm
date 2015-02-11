b3740a6e5fb032b41e76feff1a3c6d605822cdaf
flimm@sigtrap.de <flimm@sigtrap.de@e7c75b2f-8ac8-f892-83e0-76010f6c4c37>
2015-02-11 13:28:17 +0000

    - Erweiterung um Record-Methoden save_record und delete_record

--- trunk/portal/perl/modules/OpenBib/Record/Subject.pm
+++ trunk/portal/perl/modules/OpenBib/Record/Subject.pm
@@ -221,6 +221,165 @@ sub load_name {
     return $self;
 }
 
+sub save_record {
+    my ($self,$arg_ref) = @_;
+
+    # Set defaults
+    my $id                = exists $arg_ref->{id}
+        ? $arg_ref->{id}                :
+            (exists $self->{id})?$self->{id}:undef;
+
+    # Log4perl logger erzeugen
+    
+    my $logger = get_logger();
+
+    my $config = OpenBib::Config->instance;
+
+    my ($atime,$btime,$timeall);
+
+    if ($config->{benchmark}) {
+	$atime=new Benchmark;
+    }
+
+    if ($id){
+        my $record_exists = $self->{schema}->resultset('Subject')->search(
+            {
+                'me.id' => $id,
+            },
+        )->count;
+
+        # Wenn noch nicht da, dann eintragen,
+        if (!$record_exists){
+
+            $logger->debug("Record doesn't exist. Creating.");
+            
+            my $fields_ref = $self->{_fields};
+            
+            # Primaeren Normdatensatz erstellen und schreiben
+
+            my $create_ref = {
+                id => $id,
+            };
+            my $create_tstamp = "1970-01-01 12:00:00";
+            
+            if (defined $fields_ref->{'0002'} && defined $fields_ref->{'0002'}[0]) {
+                $create_tstamp = $fields_ref->{'0002'}[0]{content};
+                if ($create_tstamp=~/^(\d\d)\.(\d\d)\.(\d\d\d\d)/) {
+                    $create_tstamp=$3."-".$2."-".$1." 12:00:00";
+                }
+                $create_ref->{tstamp_create} = $create_tstamp;
+            }
+        
+            my $update_tstamp = "1970-01-01 12:00:00";
+        
+            if (exists $fields_ref->{'0003'} && exists $fields_ref->{'0003'}[0]) {
+                $update_tstamp = $fields_ref->{'0003'}[0]{content};
+                if ($update_tstamp=~/^(\d\d)\.(\d\d)\.(\d\d\d\d)/) {
+                    $update_tstamp=$3."-".$2."-".$1." 12:00:00";
+                }
+                $create_ref->{tstamp_update} = $update_tstamp;
+            }
+
+            $self->{schema}->resultset('Subject')->create($create_ref);
+        }
+
+        my $record = $self->{schema}->resultset('Subject')->single(
+            {
+                'me.id' => $id,
+            },
+        );
+
+        $record->subject_fields->delete;
+        
+        # Ausgabe der Anzahl verkuepfter Titel
+        my $titcount = $self->get_number_of_titles;
+        
+        push @{$self->{fields}{S5000}}, {
+            content => $titcount,
+        };
+
+        $logger->debug("Populating new fields.");
+
+        my $fields_ref = $self->get_fields;
+
+        my $subject_fields_ref = [];
+
+        foreach my $field (keys %$fields_ref){
+            foreach my $content_ref (@{$fields_ref->{$field}}){
+                $content_ref->{subjectid}    = $id;
+                $content_ref->{field} = $field;
+                push @$subject_fields_ref, $content_ref;
+            }
+        }
+        
+        $record->subject_fields->populate($subject_fields_ref);
+        
+        if ($config->{benchmark}) {
+            $btime=new Benchmark;
+            $timeall=timediff($btime,$atime);
+            $logger->info("Benoetigte Zeit ist ".timestr($timeall));
+            undef $atime;
+            undef $btime;
+            undef $timeall;
+        }
+
+    }
+    # sonst komplett neu anlegen inkl. ID
+    else {
+        # Derzeit koennen keine Titel ohne bereits festgelegte ID aufgenommen werden.
+        # Hierzu wird zukuenftig eine Kombination aus Trigger und Sequenztabelle noetig, mit der zusaetzlichen Problematik,
+        # dass die Titel-ID als Textfeld definiert ist. Hier koennen nur Numerische IDs darin verwendet werden, da sonst kein
+        # Hochzaehlen moeglich ist.
+    }
+
+    $logger->debug("Record with ID $id saved to database $self->{database}");
+    return $self;
+}
+
+sub delete_record {
+    my ($self,$arg_ref) = @_;
+
+    # Set defaults
+    my $id                = exists $arg_ref->{id}
+        ? $arg_ref->{id}                :
+            (exists $self->{id})?$self->{id}:undef;
+
+    # Log4perl logger erzeugen
+    
+    my $logger = get_logger();
+
+    my $config = OpenBib::Config->instance;
+
+    my ($atime,$btime,$timeall);
+
+    if ($config->{benchmark}) {
+	$atime=new Benchmark;
+    }
+
+    # DBI "select category,content,indicator from subject where id = ?";
+    my $subject = $self->{schema}->resultset('Subject')->search(
+        {
+            'me.id' => $id,
+        },
+    );
+
+    $subject->subject_fields->delete;
+    $subject->title_people->delete;
+
+    $logger->debug("Deleted title $self->{id} in database $self->{database}");
+    
+    if ($config->{benchmark}) {
+	$btime=new Benchmark;
+	$timeall=timediff($btime,$atime);
+	$logger->info("Benoetigte Zeit fuer Subjectbestimmung ist ".timestr($timeall));
+	undef $atime;
+	undef $btime;
+	undef $timeall;
+    }
+
+    return $self;
+}
+
 sub get_number_of_titles {
     my ($self,$arg_ref) = @_;
 

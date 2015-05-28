7503948198782b00367e61bd20757bbed5339fda
Oliver Flimm <flimm@sigtrap.de>
2015-05-28 14:00:20 +0200

    - Aenderung Gentz-Digital: Fuer Register und Facettierung wird automatisch 425 mit ohne Jahr belegt, wenn diese Kategorie nicht belegt ist. Ebenso 426, 427 und 428.

--- conv/auto/filter/gentzdigital/add-fields.pl
+++ conv/auto/filter/gentzdigital/add-fields.pl
@@ -16,6 +16,22 @@ while (<>){
                     },
                 ];
             }
+            else {
+                $title_ref->{fields}{'0425'} = [
+                    {
+                        mult     => 1,
+                        subfield => '',
+                        content  => 'ohne Jahr',
+                    },
+                ];
+                $title_ref->{fields}{'0426'} = [
+                    {
+                        mult     => 1,
+                        subfield => '',
+                        content  => 'ohne Jahr',
+                    },
+                ];
+            }
         }
         elsif ($title_ref->{fields}{'0800'}[0]{content} eq "Briefe an Gentz"){
             if (defined $title_ref->{fields}{'0425'}[0]{content}){
@@ -27,6 +43,22 @@ while (<>){
                     },
                 ];
             }
+            else {
+                $title_ref->{fields}{'0425'} = [
+                    {
+                        mult     => 1,
+                        subfield => '',
+                        content  => 'ohne Jahr',
+                    },
+                ];
+                $title_ref->{fields}{'0427'} = [
+                    {
+                        mult     => 1,
+                        subfield => '',
+                        content  => 'ohne Jahr',
+                    },
+                ];
+            }
         }
         elsif ($title_ref->{fields}{'0800'}[0]{content} eq "Briefe Dritter"){
             if (defined $title_ref->{fields}{'0425'}[0]{content}){
@@ -35,6 +67,22 @@ while (<>){
                         mult     => 1,
                         subfield => '',
                         content  => $title_ref->{fields}{'0425'}[0]{content},
+                    },
+                ];
+            }
+            else {
+                $title_ref->{fields}{'0425'} = [
+                    {
+                        mult     => 1,
+                        subfield => '',
+                        content  => 'ohne Jahr',
+                    },
+                ];
+                $title_ref->{fields}{'0428'} = [
+                    {
+                        mult     => 1,
+                        subfield => '',
+                        content  => 'ohne Jahr',
                     },
                 ];
             }

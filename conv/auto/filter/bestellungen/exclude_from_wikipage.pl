0c020ca29a2036a854b8da5dfd05e992c6fb01ed
Oliver Flimm <flimm@sigtrap.de>
2015-09-24 11:34:14 +0200

    - Ausschluss von Titeln durch eine Wikiseite fuer die Bestellungen

---
+++ conv/auto/filter/bestellungen/exclude_from_wikipage.pl
@@ -0,0 +1,33 @@
+#!/usr/bin/perl
+
+use warnings;
+use strict;
+use utf8;
+
+use MediaWiki::API;
+use JSON::XS qw/decode_json encode_json/;
+
+my $mw = MediaWiki::API->new( {api_url => 'http://usbwiki.ub.uni-koeln.de/usbwiki/api.php' } );
+
+my $page = $mw->get_page( { title => 'Bestellungen - Exkludierte Titel' } );
+
+my $page_content = $page->{'*'};
+
+my %excluded_ids = ();
+
+foreach my $line (split /\n/, $page_content){
+    $line=~s/^\s*//g;
+    $line=~s/\s*$//g;
+    $excluded_ids{$line} = 1;
+}
+
+while (<>){
+    my $title_ref = decode_json $_;
+
+    if (defined $excluded_ids{$title_ref->{id}}){
+        print STDERR "Titel-ID $title_ref->{id} excluded\n";
+        next;
+    }
+
+    print;
+}

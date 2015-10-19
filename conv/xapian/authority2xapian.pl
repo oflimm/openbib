86ffa24e781dae1f41f629e4fea77fc680b842c7
Oliver Flimm <flimm@sigtrap.de>
2015-10-19 14:52:17 +0200

    - Erweiterung um Info zum indexierten Katalog mit Xapian

--- conv/xapian/authority2xapian.pl
+++ conv/xapian/authority2xapian.pl
@@ -192,7 +192,7 @@ foreach my $authority_file_ref (@authority_files){
                         my $resulttime = timestr($timeall,"nop");
                         $resulttime    =~s/(\d+\.\d+) .*/$1/;
                         $atime         = new Benchmark;
-                        $logger->info("$count Saetze indexiert in $resulttime Sekunden");
+                        $logger->info("$database: $count Saetze indexiert in $resulttime Sekunden");
                     }
                     
                     $count++;

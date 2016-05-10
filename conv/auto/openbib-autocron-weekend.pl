545994b129598c345b14ab1ac43b055a8d1d9a27
Oliver Flimm <flimm@sigtrap.de>
2016-05-10 16:19:12 +0200

    - Integration von inst409master und inst409

--- conv/auto/openbib-autocron-weekend.pl
+++ conv/auto/openbib-autocron-weekend.pl
@@ -119,6 +119,8 @@ my $blacklist_ref = {
     'inst323' => 1,
     'inst324' => 1,
     'inst325' => 1,
+    'inst409' => 1,
+    'inst409master' => 1,
     'inst418' => 1,
     'inst418master' => 1,
     'inst420' => 1,
@@ -402,13 +404,13 @@ sub threadC {
 
     $logger->info("### Sonstige Master-Institutskataloge");
     
-    autoconvert({ updatemaster => $updatemaster, sync => 1, databases => ['inst622master','inst401master'] });
+    autoconvert({ updatemaster => $updatemaster, sync => 1, databases => ['inst622master','inst401master','inst409master'] });
 
     ##############################
     
     $logger->info("### Aufgesplittete sonstige Master-Institutskataloge");
     
-    autoconvert({ updatemaster => $updatemaster, sync => 1, databases => ['inst622','inst401'] });
+    autoconvert({ updatemaster => $updatemaster, sync => 1, databases => ['inst622','inst401','inst409'] });
 
     ##############################
 

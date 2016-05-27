ed359b9b003833bf86cdfd1bb8604305b50e9828
Oliver Flimm <flimm@sigtrap.de>
2016-05-27 11:09:22 +0200

    - Neues Einlade-Programm fuer die aus rvk2topics.pl generierten JSON-Topic-Dateien

---
+++ conv/enrichmnt/load_topics.pl
@@ -0,0 +1,185 @@
+#!/usr/bin/perl
+
+#####################################################################
+#
+#  load_topics.pl
+#
+#  Einladen der JSON-Topics-Daten von rvk2topics.pl
+#
+#  Dieses File ist (C) 2016 Oliver Flimm <flimm@openbib.org>
+#
+#  Dieses Programm ist freie Software. Sie koennen es unter
+#  den Bedingungen der GNU General Public License, wie von der
+#  Free Software Foundation herausgegeben, weitergeben und/oder
+#  modifizieren, entweder unter Version 2 der Lizenz oder (wenn
+#  Sie es wuenschen) jeder spaeteren Version.
+#
+#  Die Veroeffentlichung dieses Programms erfolgt in der
+#  Hoffnung, dass es Ihnen von Nutzen sein wird, aber OHNE JEDE
+#  GEWAEHRLEISTUNG - sogar ohne die implizite Gewaehrleistung
+#  der MARKTREIFE oder der EIGNUNG FUER EINEN BESTIMMTEN ZWECK.
+#  Details finden Sie in der GNU General Public License.
+#
+#  Sie sollten eine Kopie der GNU General Public License zusammen
+#  mit diesem Programm erhalten haben. Falls nicht, schreiben Sie
+#  an die Free Software Foundation, Inc., 675 Mass Ave, Cambridge,
+#  MA 02139, USA.
+#
+#####################################################################   
+
+use warnings;
+use strict;
+use utf8;
+
+use YAML;
+
+
+use Business::ISBN;
+use Encode 'decode_utf8';
+use Getopt::Long;
+use Log::Log4perl qw(get_logger :levels);
+use MARC::Batch;
+use MARC::Charset 'marc8_to_utf8';
+use MARC::File::XML;
+use JSON::XS;
+
+use OpenBib::Common::Util;
+use OpenBib::Config;
+use OpenBib::Enrichment;
+use OpenBib::Catalog::Factory;
+
+# Autoflush
+$|=1;
+
+my ($help,$importjson,$init,$jsonfile,$logfile,$loglevel);
+
+&GetOptions("help"         => \$help,
+            "init"         => \$init,
+            "jsonfile=s"   => \$jsonfile,
+            "import-json"  => \$importjson,
+            "logfile=s"    => \$logfile,
+            "loglevel=s"   => \$loglevel,
+	    );
+
+if ($help){
+   print_help();
+}
+
+my $config = OpenBib::Config->new;
+
+exit unless ($jsonfile);
+
+$logfile=($logfile)?$logfile:"/var/log/openbib/load_topics.log";
+$loglevel=($loglevel)?$loglevel:"INFO";
+
+my $log4Perl_config = << "L4PCONF";
+log4perl.rootLogger=$loglevel, LOGFILE, Screen
+log4perl.appender.LOGFILE=Log::Log4perl::Appender::File
+log4perl.appender.LOGFILE.filename=$logfile
+log4perl.appender.LOGFILE.mode=append
+log4perl.appender.LOGFILE.layout=Log::Log4perl::Layout::PatternLayout
+log4perl.appender.LOGFILE.layout.ConversionPattern=%d [%c]: %m%n
+log4perl.appender.Screen=Log::Dispatch::Screen
+log4perl.appender.Screen.layout=Log::Log4perl::Layout::PatternLayout
+log4perl.appender.Screen.layout.ConversionPattern=%d [%c]: %m%n
+L4PCONF
+
+Log::Log4perl::init(\$log4Perl_config);
+
+# Log4perl logger erzeugen
+my $logger = get_logger();
+
+my $enrichment = new OpenBib::Enrichment;
+
+my $origin = 24;
+
+$logger->debug("Origin: $origin");
+
+if ($init){
+    $logger->info("Loeschen der bisherigen Daten");
+    
+    $enrichment->init_enriched_content({ field => '4102', origin => $origin });
+}
+
+$logger->info("Bestimmung der TOC-URLs");
+
+if ($importjson){
+    if (! -e $jsonfile){
+        $logger->error("JSON-Datei $jsonfile existiert nicht");
+        exit;
+    }
+    open(JSON,$jsonfile);
+
+    my $count=1;
+    
+    my $subject_tuple_count = 1;
+    
+    my $enrich_data_by_isbn_ref   = [];
+    my $enrich_data_by_bibkey_ref = [];
+    
+    $logger->info("Einlesen und -laden der neuen Daten");
+
+    while (<JSON>){
+        my $item_ref = decode_json($_);
+
+        push @{$enrich_data_by_isbn_ref},   $item_ref if (defined $item_ref->{isbn});
+        push @{$enrich_data_by_bibkey_ref}, $item_ref if (defined $item_ref->{bibkey});
+
+        $subject_tuple_count++;
+        
+        if ($count % 1000 == 0){
+            $enrichment->add_enriched_content({ matchkey => 'isbn',   content => $enrich_data_by_isbn_ref }) if (@$enrich_data_by_isbn_ref);
+            $enrichment->add_enriched_content({ matchkey => 'bibkey', content => $enrich_data_by_bibkey_ref })  if (@$enrich_data_by_bibkey_ref);
+            $enrich_data_by_isbn_ref   = [];
+            $enrich_data_by_bibkey_ref = [];
+        }
+        $count++;
+    }
+
+    $enrichment->add_enriched_content({ matchkey => 'isbn',   content => $enrich_data_by_isbn_ref }) if (@$enrich_data_by_isbn_ref);
+    $enrichment->add_enriched_content({ matchkey => 'bibkey', content => $enrich_data_by_bibkey_ref }) if (@$enrich_data_by_bibkey_ref);
+    
+    $logger->info("$subject_tuple_count Topic-Tupel eingefuegt");
+
+    if ($jsonfile){
+        close(JSON);
+    }
+    
+}
+
+sub print_help {
+    print << "ENDHELP";
+load_topics.pl - Einladen der Topics aus rvk2topics.pl
+
+   Optionen:
+   -help                 : Diese Informationsseite
+
+   -init                 : Zuerst Eintraege fuer dieses Feld und Origin aus Anreicherungsdatenbank loeschen
+   --jsonfile=...        : Name der JSON-Einlade-/ausgabe-Datei
+
+     -import-json        : Einladen der Daten aus der JSON-Einlade-Datei
+
+   --logfile=...         : Name der Log-Datei
+   --loglevel=...        : Loglevel (default: INFO)
+
+ENDHELP
+    exit;
+}
+
+sub konv {
+    my $content = shift;
+
+    $content=~s/\s*[.,:]\s*$//g;
+    $content=~s/&/&amp;/g;
+    $content=~s/</&lt;/g;
+    $content=~s/>/&gt;/g;
+    # Buchstabenersetzungen Grundbuchstabe plus Diaeresis
+    $content=~s/u\x{0308}/ü/g;
+    $content=~s/a\x{0308}/ä/g;
+    $content=~s/o\x{0308}/ö/g;
+    $content=~s/U\x{0308}/Ü/g;
+    $content=~s/A\x{0308}/Ä/g;
+    $content=~s/O\x{0308}/Ö/g;
+
+    return $content;
+}

34e6289d2c9b5db864e4ca7a718a49ce4a3abbf5
Oliver Flimm <flimm@sigtrap.de>
2016-06-07 10:51:48 +0200

    - Neues Tool: Umwandlung eines JSON-Strings in einen mit deterministischer Serialisierung (Sortierung der Hashkeys)

---
+++ tools/canonify-json.pl
@@ -0,0 +1,48 @@
+#!/usr/bin/perl
+
+#####################################################################
+#
+#  canonify-json.pl
+#
+#  Umwandlung eines JSON-Strings in einen deterministisch serialisierten 
+#  JSON-String
+#
+#  Dieses File ist (C) 2016 Oliver Flimm <flimm@openbib.org>
+#
+#  Dieses Programm ist freie Software. Sie koennen es unter
+#  den Bedingungen der GNU General Public License, wie von der
+#  Free Software Foundation herausgegeben, weitergeben und/oder
+#  modifizieren, entweder unter Version 2 der Lizenz oder (wenn
+#  Sie es wuenschen) jeder spaeteren Version.
+#
+#  Die Veroeffentlichung dieses Programms erfolgt in der
+#  Hoffnung, dass es Ihnen von Nutzen sein wird, aber OHNE JEDE
+#  GEWAEHRLEISTUNG - sogar ohne die implizite Gewaehrleistung
+#  der MARKTREIFE oder der EIGNUNG FUER EINEN BESTIMMTEN ZWECK.
+#  Details finden Sie in der GNU General Public License.
+#
+#  Sie sollten eine Kopie der GNU General Public License zusammen
+#  mit diesem Programm erhalten haben. Falls nicht, schreiben Sie
+#  an die Free Software Foundation, Inc., 675 Mass Ave, Cambridge,
+#  MA 02139, USA.
+#
+#####################################################################
+
+#####################################################################
+# Einladen der benoetigten Perl-Module
+#####################################################################
+
+use strict;
+use warnings;
+
+use JSON::XS;
+
+my $json = JSON::XS->new->utf8;
+
+$json->canonical(1);
+
+while (<>){
+    my $item_ref = decode_json $_;
+
+    print $json->encode($item_ref),"\n";
+}

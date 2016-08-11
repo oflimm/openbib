cf9543c5b21966bc49424296b66d90f13bbfbb11
Oliver Flimm <flimm@sigtrap.de>
2016-08-11 08:30:43 +0200

    - Korrektur/Neusetzen von Standortinformationen in 3330 fuer Oppenheim-Stiftung

---
+++ conv/auto/filter/inst001/fix-holding.pl
@@ -0,0 +1,25 @@
+#!/usr/bin/perl
+
+use warnings;
+use strict;
+
+use JSON::XS;
+use List::MoreUtils qw{uniq};
+
+print STDERR "### inst001 Korrektur Exemplardaten\n";
+
+while (<>){
+    my $holding_ref = decode_json $_;
+
+    foreach my $location_ref (@{$holding_ref->{fields}{'0016'}}){
+        if ($location_ref->{content} =~m/Oppenheim-Stiftung/){
+            if (defined $holding_ref->{fields}{'3330'}){
+                push @{$holding_ref->{fields}{'3330'}},{
+                    content => '435',
+                }
+            }
+        }
+    }
+
+    print encode_json $holding_ref, "\n";
+}

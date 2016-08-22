21fe8cd31efd5be5c8d7f3e65478556bf7100852
Oliver Flimm <flimm@sigtrap.de>
2016-08-22 17:06:14 +0200

    - Korrektur: Falsches Encoding bei Signaturen gefixt

--- portal/perl/modules/OpenBib/Importer/JSON/Title.pm
+++ portal/perl/modules/OpenBib/Importer/JSON/Title.pm
@@ -1284,7 +1284,7 @@ sub process {
         if (exists $self->{storage}{listitemdata_holding}{$id}){
             my $thisholdings = $self->{storage}{listitemdata_holding}{$id};
             foreach my $content (@{$thisholdings}) {
-                $content = decode_utf8($content);
+                # $content = decode_utf8($content);
 
                 $index_doc->add_data('X0014', {
                     content => $content,

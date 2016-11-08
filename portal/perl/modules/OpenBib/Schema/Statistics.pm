06ea9afe6876124683b94eddb4b927f7f5ffce16
Oliver Flimm <flimm@sigtrap.de>
2016-11-08 10:57:18 +0100

    - Erweiterung um Tabelle loans

--- portal/perl/modules/OpenBib/Schema/Statistics.pm
+++ portal/perl/modules/OpenBib/Schema/Statistics.pm
@@ -12,8 +12,8 @@ use base 'DBIx::Class::Schema';
 __PACKAGE__->load_namespaces;
 
 
-# Created by DBIx::Class::Schema::Loader v0.07025 @ 2013-01-07 17:04:57
-# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:9ra9mPV4AVKNTQGIvgFp7Q
+# Created by DBIx::Class::Schema::Loader v0.07042 @ 2016-11-08 10:53:36
+# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:sx378nXYAIY6a55AOX8Z/g
 
 
 # You can replace this text with custom code or comments, and it will be preserved on regeneration

ba5ad16860d34da5e535b0982fb4a3204965b17d
Oliver Flimm <flimm@sigtrap.de>
2016-11-17 10:35:15 +0100

    - Erweiterung um Tabelle enriched_content_by_title

---
+++ portal/perl/modules/OpenBib/Schema/Enrichment/Result/EnrichedContentByTitle.pm
@@ -0,0 +1,80 @@
+use utf8;
+package OpenBib::Schema::Enrichment::Result::EnrichedContentByTitle;
+
+# Created by DBIx::Class::Schema::Loader
+# DO NOT MODIFY THE FIRST PART OF THIS FILE
+
+=head1 NAME
+
+OpenBib::Schema::Enrichment::Result::EnrichedContentByTitle
+
+=cut
+
+use strict;
+use warnings;
+
+use base 'DBIx::Class::Core';
+
+=head1 TABLE: C<enriched_content_by_title>
+
+=cut
+
+__PACKAGE__->table("enriched_content_by_title");
+
+=head1 ACCESSORS
+
+=head2 titleid
+
+  data_type: 'text'
+  is_nullable: 0
+
+=head2 dbname
+
+  data_type: 'text'
+  is_nullable: 0
+
+=head2 origin
+
+  data_type: 'smallint'
+  is_nullable: 1
+
+=head2 field
+
+  data_type: 'smallint'
+  is_nullable: 0
+
+=head2 subfield
+
+  data_type: 'varchar'
+  is_nullable: 1
+  size: 3
+
+=head2 content
+
+  data_type: 'text'
+  is_nullable: 0
+
+=cut
+
+__PACKAGE__->add_columns(
+  "titleid",
+  { data_type => "text", is_nullable => 0 },
+  "dbname",
+  { data_type => "text", is_nullable => 0 },
+  "origin",
+  { data_type => "smallint", is_nullable => 1 },
+  "field",
+  { data_type => "smallint", is_nullable => 0 },
+  "subfield",
+  { data_type => "varchar", is_nullable => 1, size => 3 },
+  "content",
+  { data_type => "text", is_nullable => 0 },
+);
+
+
+# Created by DBIx::Class::Schema::Loader v0.07042 @ 2016-11-17 10:34:54
+# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:lE1AiGUtjxEcvkV4QpL1Pw
+
+
+# You can replace this text with custom code or comments, and it will be preserved on regeneration
+1;

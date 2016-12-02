61ea216c873734d953fa13590bf746e4bf644595
Oliver Flimm <flimm@sigtrap.de>
2016-12-02 09:16:45 +0100

    - Erweiterung um eigenes Steuerscript zum Aufbau des Zeitschriftenkatalogs und anschliessende Erzeugung von Zeitschriftenlisten im PDF-Format

---
+++ conv/auto/openbib-autocron-instzs.pl
@@ -0,0 +1,220 @@
+#!/usr/bin/perl
+
+#####################################################################
+#
+#  openbib-autocron.pl
+#
+#  CRON-Job zum automatischen aktualisieren der instzs-Datenbank
+#
+#  Dieses File ist (C) 1997-2016 Oliver Flimm <flimm@openbib.org>
+#
+#  Dieses Programm ist freie Software. Sie koennen es unter
+#  den Bedingungen der GNU General Public License, wie von der
+#  Free Software Foundation herausgegeben, weitergeben und/oder
+#  modifizieren, entweder unter Version 2 der Lizenz oder (wenn
+#  Sie es wuenschen) jeder spaeteren Version.
+#
+#  Die Veroeffentlichung dieses Programms erfolgt in der
+#  Hoffnung, dass es Ihnen von Nutzen sein wird, aber OHNE JEDE
+#  GEWAEHRLEISTUNG - sogar ohne die implizite Gewaehrleistung
+#  der MARKTREIFE oder der EIGNUNG FUER EINEN BESTIMMTEN ZWECK.
+#  Details finden Sie in der GNU General Public License.
+#
+#  Sie sollten eine Kopie der GNU General Public License zusammen
+#  mit diesem Programm erhalten haben. Falls nicht, schreiben Sie
+#  an die Free Software Foundation, Inc., 675 Mass Ave, Cambridge,
+#  MA 02139, USA.
+#
+#####################################################################
+
+use 5.008001;
+use utf8;
+use strict;
+use warnings;
+use threads;
+use threads::shared;
+
+use Getopt::Long;
+use Log::Log4perl qw(get_logger :levels);
+use OpenBib::Config;
+
+our ($logfile,$loglevel,$test,$cluster,$maintenance,$updatemaster,$incremental);
+
+&GetOptions(
+    "cluster"       => \$cluster,
+    "test"          => \$test,
+    "maintenance"   => \$maintenance,
+    "incremental"   => \$incremental,
+    "logfile=s"     => \$logfile,
+    "loglevel=s"    => \$loglevel,
+    "update-master" => \$updatemaster,
+    );
+
+my $config = OpenBib::Config->new;
+
+$logfile=($logfile)?$logfile:"/var/log/openbib/openbib-autocron-instzs.log";
+$loglevel=($loglevel)?$loglevel:"INFO";
+
+my $log4Perl_config = << "L4PCONF";
+log4perl.rootLogger=$loglevel, LOGFILE, Screen
+log4perl.appender.LOGFILE=Log::Log4perl::Appender::File
+log4perl.appender.LOGFILE.filename=$logfile
+log4perl.appender.LOGFILE.mode=append
+log4perl.appender.LOGFILE.layout=Log::Log4perl::Layout::PatternLayout
+log4perl.appender.LOGFILE.layout.ConversionPattern=%d [%c]: %m%n
+log4perl.appender.Screen=Log::Dispatch::Screen
+log4perl.appender.Screen.layout=Log::Log4perl::Layout::PatternLayout
+log4perl.appender.Screen.layout.ConversionPattern=%d [%c]: %m%n
+L4PCONF
+
+Log::Log4perl::init(\$log4Perl_config);
+
+# Log4perl logger erzeugen
+my $logger = get_logger();
+
+$logger->info("###### Starting automatic update");
+
+$logger->info("### Restarting starman");
+
+if ($cluster){
+    if ($config->local_server_belongs_to_updatable_cluster()){
+        $logger->info("### Updating in cluster mode");
+        $logger->info("### Changing server-status to updating");
+        $config->update_local_serverstatus("updating");
+        if ($config->all_servers_of_local_cluster_have_status('updating')){
+            $logger->info("### Changing cluster-status to updating");
+            $config->update_local_clusterstatus("updating");
+        }
+    }
+    else {
+        $logger->info("### Local server is not updatable. Exiting.");
+        exit;
+    }
+}
+
+my @threads;
+
+if ($test){
+    push @threads, threads->new(\&threadTest,'Testkatalog');
+}
+else {
+    push @threads, threads->new(\&threadA,'Thread 1');
+}
+
+foreach my $thread (@threads) {
+    my $thread_description = $thread->join;
+    $logger->info("### -> done with $thread_description");
+}
+
+$logger->info("### Listen erstellen");
+
+system("/opt/openbib/bin/gen_zsstlist-all.pl > /tmp/gen_zsstlist-all.log 2>&1");
+
+$logger->info("###### Updating done");
+
+if ($cluster){
+    $logger->info("### Changing cluster/server-status to updated");
+    $config->update_local_serverstatus("updated");
+}
+
+sub threadA {
+    my $thread_description = shift;
+
+    $logger->info("### -> $thread_description");    
+
+    $logger->info("### instzs");
+    
+    autoconvert({ incremental => $incremental, updatemaster => $updatemaster, sync => 1, databases => ['instzs'] });
+
+    return $thread_description;
+}
+
+sub threadTest {
+    my $thread_description = shift;
+
+    $logger->info("### -> Testkatalog");
+
+    $logger->info("### Openbib");
+
+    autoconvert({ incremental => $incremental, updatemaster => $updatemaster, databases => ['openbib'] });
+
+    return $thread_description;
+}
+
+sub autoconvert {
+    my ($arg_ref) = @_;
+
+    my @ac_cmd = ();
+    
+    # Set defaults
+    my $blacklist_ref   = exists $arg_ref->{blacklist}
+        ? $arg_ref->{blacklist}             : {};
+
+    my $databases_ref   = exists $arg_ref->{databases}
+        ? $arg_ref->{databases}             : [];
+
+    my $sync            = exists $arg_ref->{sync}
+        ? $arg_ref->{sync}                  : 0;
+
+    my $genmex          = exists $arg_ref->{genmex}
+        ? $arg_ref->{genmex}                : 0;
+
+    my $incremental     = exists $arg_ref->{incremental}
+        ? $arg_ref->{incremental}           : 0;
+    
+    my $autoconv        = exists $arg_ref->{autoconv}
+        ? $arg_ref->{autoconv}              : 0;
+
+    my $updatemaster    = exists $arg_ref->{updatemaster}
+        ? $arg_ref->{updatemaster}          : 0;
+
+    # Log4perl logger erzeugen
+    my $logger = get_logger();
+
+
+    push @ac_cmd, "/opt/openbib/autoconv/bin/autoconv.pl";
+    push @ac_cmd, "-sync"    if ($sync); 
+    push @ac_cmd, "-gen-mex" if ($genmex);
+    push @ac_cmd, "-incremental" if ($incremental);
+    push @ac_cmd, "-update-master" if ($updatemaster);
+
+    my $ac_cmd_base = join(' ',@ac_cmd);
+
+    my @databases = ();
+
+    if (@$databases_ref){
+        push @databases, @$databases_ref;
+    }
+
+    if ($autoconv){
+        my $dbinfo = $config->get_databaseinfo->search(
+            {
+                'autoconvert' => 1,
+            },
+            {
+                order_by => 'dbname',
+            }
+        );
+        foreach my $item ($dbinfo->all){
+            push @databases, $item->dbname;
+        }
+    }
+  
+    foreach my $database (@databases){
+        if (exists $blacklist_ref->{$database}){
+            $logger->info("Katalog $database auf Blacklist");
+            next;
+        }
+        
+        my $this_cmd = "$ac_cmd_base --database=$database";
+        $logger->info("Konvertierung von $database");
+        $logger->info("Ausfuehrung von $this_cmd");
+        system($this_cmd);
+
+        if ($maintenance){
+            $logger->info("### Enriching subject headings for all institutes");
+            
+            system("$config->{'base_dir'}/conv/swt2enrich.pl --database=$database");
+        }
+    }
+}

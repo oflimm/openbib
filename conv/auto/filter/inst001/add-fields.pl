2e983bcd7904cf2f396476780428fa84b0f6e7f1
Oliver Flimm <flimm@sigtrap.de>
2017-05-29 11:01:18 +0200

    - Aenderung: Bestehende Inhalte von 4400 und 4410 werden nicht mehr ueberschrieben (bisher wurden sie aber auch nicht erfasst...). Damit koennen sie jetzt auch individuell fuer die Erfassung verwendet werden.

--- conv/auto/filter/inst001/add-fields.pl
+++ conv/auto/filter/inst001/add-fields.pl
@@ -31,21 +31,40 @@ while (<>){
     }
 
     if ($is_digital){
-        $title_ref->{fields}{'4400'} = [
-            {
+	if (@{$title_ref->{fields}{'4400'}}){
+	    push @{$title_ref->{fields}{'4400'}}, {
                 mult     => 1,
                 subfield => '',
                 content  => "online",
-            },
-        ];
-        
-        $title_ref->{fields}{'4410'} = [
-            {
+            };
+	}
+	else {
+	    $title_ref->{fields}{'4400'} = [
+		{
+		    mult     => 1,
+		    subfield => '',
+		    content  => "online",
+		},
+		];
+        }
+
+	
+	if (@{$title_ref->{fields}{'4400'}}){
+	    push @{$title_ref->{fields}{'4400'}}, {
                 mult     => 1,
                 subfield => '',
                 content  => "Digital",
-            },
-        ];
+            };
+	}
+	else {
+	    $title_ref->{fields}{'4410'} = [
+		{
+		    mult     => 1,
+		    subfield => '',
+		    content  => "Digital",
+		},
+		];
+	}
     }
    
     print encode_json $title_ref, "\n";

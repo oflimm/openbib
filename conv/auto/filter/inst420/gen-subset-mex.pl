f2cf2ffa0a558e928e8ab2a8b3732849592b4f4f
Oliver Flimm <flimm@sigtrap.de>
2017-10-16 08:57:23 +0200

    - Aenderung: Katalog wird nur noch aus den mit Retro markierten Titeln aus inst420retro gespeist

--- conv/auto/filter/inst420/gen-subset-mex.pl
+++ conv/auto/filter/inst420/gen-subset-mex.pl
@@ -75,7 +75,7 @@ Log::Log4perl::init(\$log4Perl_config);
 # Log4perl logger erzeugen
 my $logger = get_logger();
 
-my $subset = new OpenBib::Catalog::Subset("inst420master",$pool);
+my $subset = new OpenBib::Catalog::Subset("inst420retro",$pool);
 $subset->identify_by_mark('^420 *\\$');
 $subset->write_set;
 

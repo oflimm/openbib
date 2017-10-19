1dce899de197cbf71ec3ef051bae55c70c499c2b
Oliver Flimm <flimm@sigtrap.de>
2017-10-19 15:01:59 +0200

    - uzkzeitschriften soll zukuenftig Basis von instzs sein

---
+++ conv/auto/filter/uzkzeitschriften/add-locationid.pl
@@ -0,0 +1,116 @@
+#!/usr/bin/perl
+
+use warnings;
+use strict;
+
+use JSON::XS;
+
+my $title_locationid_ref = {};
+
+print STDERR "### instzs: Analysiere Exemplardaten\n";
+
+open(HOLDING,"meta.holding");
+
+while (<HOLDING>){
+    my $holding_ref = decode_json $_;
+
+    my $titleid = $holding_ref->{fields}{'0004'}[0]{content};
+    
+    next unless ($titleid);
+
+    foreach my $location_ref (@{$holding_ref->{fields}{'3330'}}){
+        if ($location_ref->{content} eq "587"){
+            push @{$title_locationid_ref->{$titleid}}, "DE-587";
+        }
+        elsif ($location_ref->{content} eq "101" || $location_ref->{content} eq "103" || $location_ref->{content} eq "105" || $location_ref->{content} eq "106" || $location_ref->{content} eq "120" || $location_ref->{content} eq "121" || $location_ref->{content} eq "128" || $location_ref->{content} eq "146" || $location_ref->{content} eq "157"){
+            push @{$title_locationid_ref->{$titleid}}, "DE-38-101";
+            push @{$title_locationid_ref->{$titleid}}, "DE-38-USBFB";
+        }
+        elsif ($location_ref->{content} eq "123"){
+            push @{$title_locationid_ref->{$titleid}}, "DE-38-123";
+            push @{$title_locationid_ref->{$titleid}}, "DE-38-VERS";
+            push @{$title_locationid_ref->{$titleid}}, "DE-38-USBFB";
+        }
+        elsif ($location_ref->{content} eq "132"){
+            push @{$title_locationid_ref->{$titleid}}, "DE-38-132";
+            push @{$title_locationid_ref->{$titleid}}, "DE-38-USBFB";
+        }
+        elsif ($location_ref->{content} eq "418"){
+            push @{$title_locationid_ref->{$titleid}}, "DE-38-418";
+            push @{$title_locationid_ref->{$titleid}}, "DE-38-USBFB";
+        }
+        elsif ($location_ref->{content} eq "426"){
+            push @{$title_locationid_ref->{$titleid}}, "DE-38-426";
+            push @{$title_locationid_ref->{$titleid}}, "DE-38-ARCH";
+            push @{$title_locationid_ref->{$titleid}}, "DE-38-USBFB";
+        }        
+        elsif ($location_ref->{content} eq "427"){
+            push @{$title_locationid_ref->{$titleid}}, "DE-38-427";
+            push @{$title_locationid_ref->{$titleid}}, "DE-38-ARCH";
+            push @{$title_locationid_ref->{$titleid}}, "DE-38-USBFB";
+        }        
+        elsif ($location_ref->{content} eq "429"){
+            push @{$title_locationid_ref->{$titleid}}, "DE-38-429";
+            push @{$title_locationid_ref->{$titleid}}, "DE-38-MEKUTH";
+            push @{$title_locationid_ref->{$titleid}}, "DE-38-USBFB";
+        }
+        elsif ($location_ref->{content} eq "438"){
+            push @{$title_locationid_ref->{$titleid}}, "DE-38-438";
+            push @{$title_locationid_ref->{$titleid}}, "DE-38-ARCH";
+            push @{$title_locationid_ref->{$titleid}}, "DE-38-USBFB";
+        }        
+        elsif ($location_ref->{content} eq "448"){
+            push @{$title_locationid_ref->{$titleid}}, "DE-38-448";
+            push @{$title_locationid_ref->{$titleid}}, "DE-38-MEKUTH";
+            push @{$title_locationid_ref->{$titleid}}, "DE-38-USBFB";
+        }
+        elsif ($location_ref->{content} eq "450"){
+            push @{$title_locationid_ref->{$titleid}}, "DE-38-450";
+            push @{$title_locationid_ref->{$titleid}}, "DE-38-ASIEN";
+            push @{$title_locationid_ref->{$titleid}}, "DE-38-USBFB";
+        }                
+        elsif ($location_ref->{content} eq "459"){
+            push @{$title_locationid_ref->{$titleid}}, "DE-38-459";
+            push @{$title_locationid_ref->{$titleid}}, "DE-38-ASIEN";
+            push @{$title_locationid_ref->{$titleid}}, "DE-38-USBFB";
+        }                
+        elsif ($location_ref->{content} eq "38"){
+            push @{$title_locationid_ref->{$titleid}}, "DE-38";
+            push @{$title_locationid_ref->{$titleid}}, "DE-38-USBFB";
+	    foreach my $signatur_ref (@{$holding_ref->{fields}{'0014'}}){
+		if ($signatur_ref->{content} =~m/^EWA-LS\s*:?\s*Z/ || $signatur_ref->{content} =~m/^EWA Z/ || $signatur_ref->{content} =~m/^EWA-LS-Theke Z/ || $signatur_ref->{content} =~m/^HP-LS B/){
+		    push @{$title_locationid_ref->{$titleid}}, "DE-38-HWA";
+		}
+		
+	    }
+        }
+        elsif ($location_ref->{content} =~/^\d\d\d$/){
+            push @{$title_locationid_ref->{$titleid}}, "DE-38-".$location_ref->{content};
+        }
+        else {
+            push @{$title_locationid_ref->{$titleid}}, $location_ref->{content};
+        }
+
+    }
+}
+
+close(HOLDING);
+
+print STDERR "### instzs: Erweitere Titeldaten\n";
+
+while (<>){
+    my $title_ref = decode_json $_;
+
+    my $titleid = $title_ref->{id};
+    
+    my %have_locationid = ();
+
+    foreach my $locationid (@{$title_locationid_ref->{$titleid}}){
+        next if (defined $have_locationid{$locationid});
+        push @{$title_ref->{'locations'}}, $locationid;
+
+        $have_locationid{$locationid} = 1;
+    }
+    
+    print encode_json $title_ref, "\n";
+}
---
+++ conv/auto/filter/uzkzeitschriften/alt_remote.pl
@@ -0,0 +1,89 @@
+#!/usr/bin/perl
+
+#####################################################################
+#
+#  alt_remote.pl
+#
+#  Holen via http und konvertieren in das Meta-Format
+#
+#  Dieses File ist (C) 2003-2006 Oliver Flimm <flimm@openbib.org>
+#
+#  Dieses Programm ist freie Software. Sie koennen es unter
+#  den Bedingungen der GNU General Public License, wie von der
+#  Free Software Foundation herausgegeben, weitergeben und/oder
+#  modifizieren, entweder unter Version 2 der Lizenz oder (wenn
+#  Sie es wuenschen) jeder spaeteren Version.
+#
+#  Die Veroeffentlichung dieses Programms erfolgt in der
+#  Hoffnung, dass es Ihnen von Nutzen sein wird, aber OHNE JEDE
+#  GEWAEHRLEISTUNG - sogar ohne die implizite Gewaehrleistung
+#  der MARKTREIFE oder der EIGNUNG FUER EINEN BESTIMMTEN ZWECK.
+#  Details finden Sie in der GNU General Public License.
+#
+#  Sie sollten eine Kopie der GNU General Public License zusammen
+#  mit diesem Programm erhalten haben. Falls nicht, schreiben Sie
+#  an die Free Software Foundation, Inc., 675 Mass Ave, Cambridge,
+#  MA 02139, USA.
+#
+#####################################################################   
+
+#####################################################################
+# Einladen der benoetigten Perl-Module 
+#####################################################################
+
+use DBI;
+use OpenBib::Config;
+
+my $config = new OpenBib::Config();
+
+my $rootdir       = $config->{'autoconv_dir'};
+my $pooldir       = $rootdir."/pools";
+my $konvdir       = $config->{'conv_dir'};
+my $confdir       = $config->{'base_dir'}."/conf";
+my $wgetexe       = "/usr/bin/wget -v ";
+my $mab2metaexe   = "$konvdir/mab2meta.pl";
+
+my $pool          = $ARGV[0];
+
+my $dbinfo        = $config->get_databaseinfo->search_rs({ dbname => $pool })->single;
+
+my $url        = $dbinfo->protocol."://".$dbinfo->host."/".$dbinfo->titlefile;
+
+my $ftpauthstring="";
+if ($dbinfo->protocol eq "ftp" && $dbinfo->remoteuser ne "" && $dbinfo->remotepassword ne ""){
+    $ftpauthstring=" --ftp-user=".$dbinfo->remoteuser." --ftp-password=".$dbinfo->remotepassword;
+}
+
+
+print "### $pool: Datenabzug via http von $url\n";
+system("cd $pooldir/$pool ; rm meta.* ; rm tmp.* ; rm export_*");
+system("$wgetexe $ftpauthstring --no-passive-ftp -N -P $pooldir/$pool/ $url ");
+
+opendir(DIR, "$pooldir/$pool/");
+@FILES= readdir(DIR); 
+
+my $lastdate = 0;
+foreach my $file(@FILES){
+    print "Processing $file\n";
+    if ($file=~m/export_mab_HBZ\d\d.K0.K1.F.(\d\d\d\d\d\d\d\d).\d+\.zip/){
+        my $thisdate = $1;
+        if ($thisdate > $lastdate){
+            $lastdate = $thisdate;
+        }
+    }
+}
+
+print "Letztes Datum: $lastdate\n";
+
+foreach my $file(@FILES){
+    if ($file=~m/export_mab_HBZ01.K0.K1.F.$lastdate.\d+\.zip/){
+        system("unzip -v -p $pooldir/$pool/$file | perl -n -e '/\x1E025z/ and print' > $pooldir/$pool/tmp.TIT");
+    }
+    if ($file=~m/export_mab_HBZ60.K0.K1.F.$lastdate.\d+\.zip/){
+        system("unzip -v -p $pooldir/$pool/$file > $pooldir/$pool/tmp.MEX");
+    }
+}
+
+system("cd $pooldir/$pool; $mab2metaexe --titlefile=tmp.TIT --holdingfile=tmp.MEX --configfile=/opt/openbib/conf/$pool.yml");
+system("cd $pooldir/$pool; gzip meta.* ; rm tmp.*");
+

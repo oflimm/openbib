352ff5c9415095716d242e4de553da4e2951b497
Oliver Flimm <flimm@sigtrap.de>
2017-10-23 10:47:02 +0200

    - Umstellung auf uzkzeitschriften mit Teilbestand instzs

--- conv/auto/openbib-autocron-instzs.pl
+++ conv/auto/openbib-autocron-instzs.pl
@@ -124,7 +124,7 @@ sub threadA {
 
     $logger->info("### instzs");
     
-    autoconvert({ incremental => $incremental, updatemaster => $updatemaster, sync => 1, databases => ['instzs'] });
+    autoconvert({ incremental => $incremental, updatemaster => $updatemaster, sync => 1, databases => ['uzkzeitschriften','instzs'] });
 
     return $thread_description;
 }

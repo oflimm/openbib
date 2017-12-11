f003293db879068887b86b43d53905803f0e8cf1
Oliver Flimm <flimm@sigtrap.de>
2017-12-11 10:33:35 +0100

    - Korrektur

--- conv/auto/filter/ebookpda/only_bought.pl
+++ conv/auto/filter/ebookpda/only_bought.pl
@@ -25,13 +25,22 @@ while (<>){
 
     if (defined $title_ref->{fields}{'0662'}){
 	foreach my $field_ref (@{$title_ref->{fields}{'0662'}}){
-	    my ($mil_id)=$field_ref->{content}=~m/id=(\d+)$/;
+	    my ($mil_id)=$field_ref->{content}=~m/id=(\d+)$/i;
 	    next unless (defined $mil_id);
 	    if (defined $positive_ids{$mil_id} && $positive_ids{$mil_id}){
 		$exclude=0;
+		$positive_ids{$mil_id}++;
 	    }
 	}
     }
     
     print if (!$exclude);
 }
+
+my @rest_ids = ();
+
+foreach my $key (keys %positive_ids){
+    push @rest_ids, $key if ($positive_ids{$key} < 2);
+}
+
+print STDERR join(';',@rest_ids),"\n";

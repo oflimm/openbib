3cc9e1c4dab5acdd82012042903786938700bfc8
Oliver Flimm <flimm@sigtrap.de>
2018-04-11 10:57:09 +0200

    - Erweiterung um Feldinhalt Digital und online

--- conv/auto/filter/nationallizenzen/pre_unpack.pl
+++ conv/auto/filter/nationallizenzen/pre_unpack.pl
@@ -51,4 +51,4 @@ my $bcp2metaexe   = "$konvdir/bcp2meta.pl";
 
 print "### $pool: Erweiterung um Zugriffsinformation online\n";
 
-system("cd $pooldir/$pool ; zcat meta.title.gz| $rootdir/filter/$pool/remove-titles-without-urls.pl | $rootdir/filter/$pool/add-availability.pl | gzip > meta.title.gz.tmp ; mv -f meta.title.gz.tmp meta.title.gz");
+system("cd $pooldir/$pool ; zcat meta.title.gz| $rootdir/filter/$pool/remove-titles-without-urls.pl | $rootdir/filter/$pool/add-fields.pl | $rootdir/filter/$pool/add-availability.pl | gzip > meta.title.gz.tmp ; mv -f meta.title.gz.tmp meta.title.gz");

f3a39044e67285b4634d3a33c0b3269ec670c655
Oliver Flimm <flimm@sigtrap.de>
2018-10-31 09:35:45 +0100

    - Korrektur: Aufsaetze entfernt

--- conv/auto/filter/inst113/gen-subset.pl
+++ conv/auto/filter/inst113/gen-subset.pl
@@ -77,7 +77,6 @@ my $logger = get_logger();
 
 my $subset = new OpenBib::Catalog::Subset("${pool}master",$pool);
 $subset->identify_by_field_content('title',[ { field => '0960', content => 'Retro' } ]);
-$subset->identify_by_field_content('title',[ { field => '0960', content => 'Aufsatz' } ]);
 $subset->write_set;
 
 sub print_help {

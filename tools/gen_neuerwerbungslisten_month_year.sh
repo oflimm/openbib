4b42431632c45e4a76faee2abfe9dd7a03693c0b
Oliver Flimm <flimm@sigtrap.de>
2018-11-06 15:12:20 +0100

    - Korrektur

--- tools/gen_neuerwerbungslisten_month_year.sh
+++ tools/gen_neuerwerbungslisten_month_year.sh
@@ -2,6 +2,6 @@
 
 export MONTH=$1
 export YEAR=$2
-for i in inst006 inst103 inst123 inst128 inst136 inst301 inst406 inst420 inst428 inst431 inst434 inst445 inst514 inst526; do
+for i in inst006 inst103 inst123 inst128 inst136 inst301 inst302 inst406 inst420 inst425 inst428 inst431 inst434 inst445 inst514 inst526; do
 /opt/openbib/bin/gen_neuerwerbungslisten.pl --configfile=/opt/openbib/conf/acq_${i}.yml --month=$MONTH --year=$YEAR
 done

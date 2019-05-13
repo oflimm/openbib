7f2836ad471d3bc60459958401e7ce757e8327c2
Oliver Flimm <flimm@sigtrap.de>
2019-05-13 09:39:40 +0200

    - Integration der Teilkatalogbildung fuer inst136/master

--- conv/auto/openbib-autocron-weekend.pl
+++ conv/auto/openbib-autocron-weekend.pl
@@ -102,6 +102,8 @@ my $blacklist_ref = {
     'inst132master' => 1,
     'inst134' => 1,
     'inst134master' => 1,
+    'inst136' => 1,
+    'inst136master' => 1,
     'inst146' => 1,
     'inst146master' => 1,
     'inst157' => 1,
@@ -482,13 +484,13 @@ sub threadC {
 
     $logger->info("### Sonstige Master-Institutskataloge");
     
-    autoconvert({ updatemaster => $updatemaster, sync => 1, databases => ['inst108master','inst125master','inst134master','inst323master','inst401master','inst404master','inst405master','inst406master','inst407master','inst409master','inst410master','inst411master','inst412master','inst413master','inst414master','inst416master','inst418master','inst419master','inst426master','inst427master','inst428master','inst430master','inst431master','inst432master','inst434master','inst437master','inst438master','inst444master','inst445master','inst460master','inst461master','inst464master','inst468master','inst622master','inst623master'] });
+    autoconvert({ updatemaster => $updatemaster, sync => 1, databases => ['inst108master','inst125master','inst134master','inst136master','inst323master','inst401master','inst404master','inst405master','inst406master','inst407master','inst409master','inst410master','inst411master','inst412master','inst413master','inst414master','inst416master','inst418master','inst419master','inst426master','inst427master','inst428master','inst430master','inst431master','inst432master','inst434master','inst437master','inst438master','inst444master','inst445master','inst460master','inst461master','inst464master','inst468master','inst622master','inst623master'] });
 
     ##############################
     
     $logger->info("### Aufgesplittete sonstige Master-Institutskataloge");
     
-    autoconvert({ updatemaster => $updatemaster, sync => 1, databases => ['inst108','inst125','inst134','inst323','inst401','inst404','inst405','inst406','inst407','inst409','inst410','inst411','inst412','inst413','inst414','inst416','inst418','inst419','inst426','inst427','inst428','inst430','inst431','inst432','inst434','inst437','inst438','inst444','inst445','inst460','inst461','inst464','inst468','inst622','inst623'] });
+    autoconvert({ updatemaster => $updatemaster, sync => 1, databases => ['inst108','inst125','inst134','inst136','inst323','inst401','inst404','inst405','inst406','inst407','inst409','inst410','inst411','inst412','inst413','inst414','inst416','inst418','inst419','inst426','inst427','inst428','inst430','inst431','inst432','inst434','inst437','inst438','inst444','inst445','inst460','inst461','inst464','inst468','inst622','inst623'] });
 
     ##############################
 

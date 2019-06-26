070436956c1c3b53670b384d88dd3bcf47e194c0
Oliver Flimm <flimm@sigtrap.de>
2019-06-26 08:29:02 +0200

    - Erweiterung um Exclude from Wikipage 'Nationallizenzen - Excludierte ISBNS'

---
+++ conv/auto/filter/gbvnationallizenzen/exclude_from_wikipage.pl
@@ -0,0 +1,58 @@
+#!/usr/bin/perl
+
+use warnings;
+use strict;
+use utf8;
+
+use MediaWiki::API;
+use JSON::XS qw/decode_json encode_json/;
+
+my $mw = MediaWiki::API->new( {api_url => 'https://usbwiki.ub.uni-koeln.de/usbwiki/api.php' } );
+
+my $page = $mw->get_page( { title => 'Nationallizenzen - Exkludierte ISBNS' } );
+
+my $page_content = $page->{'*'};
+
+my %excluded_isbns = ();
+
+foreach my $line (split /\n/, $page_content){
+    $line=~s/^\s*//g;
+    $line=~s/\s*$//g;
+    $excluded_isbns{$line} = 1;
+}
+
+while (<>){
+    my $title_ref;
+
+    eval {
+       $title_ref = decode_json $_;
+    };
+
+    if ($@){
+        print STDERR $@,"\n";
+        next;
+    }
+
+    my $exclude_title = 0;
+    foreach my $isbn_ref (@{$title_ref->{fields}{'0540'}}){
+	if (defined $excluded_isbns{$isbn_ref->{content}}){
+	    $exclude_title = $isbn_ref->{content}; 
+	    last;
+	} 
+    }
+
+
+    foreach my $isbn_ref (@{$title_ref->{fields}{'0553'}}){
+	if (defined $excluded_isbns{$isbn_ref->{content}}){
+	    $exclude_title = $isbn_ref->{content}; 
+	    last;
+	} 
+    }
+
+    if ($exclude_title){
+        print STDERR "Titel mit ISBN $exclude_title excluded\n";
+        next;
+    }
+
+    print;
+}

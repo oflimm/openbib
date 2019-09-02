790ee4eb7336ef7cf33a8ab7aca3780ff0bd180c
Oliver Flimm <flimm@sigtrap.de>
2019-09-02 16:11:59 +0200

    - Korrektur: Bearbeitung Erscheinungsverlauf

--- conv/auto/filter/uzkzeitschriften/join-journalholdings.pl
+++ conv/auto/filter/uzkzeitschriften/join-journalholdings.pl
@@ -9,21 +9,29 @@ while (<>){
     my $record_ref = decode_json $_;
 
     my $holdings_ref = [];
+
+    # Zuerst der Erscheinungsverlauf
+    if (defined $record_ref->{fields}{'1204'}){
+	push @$holdings_ref, $record_ref->{fields}{'1204'}[0]{content};
+
+    }
+    else {
+	if (defined $record_ref->{fields}{'1200'}){
+	    push @$holdings_ref, $record_ref->{fields}{'1200'}[0]{content};
+	    delete $record_ref->{fields}{'1200'};
+	}
+	
+	if (defined $record_ref->{fields}{'1201'}){
+	    push @$holdings_ref, $record_ref->{fields}{'1201'}[0]{content};
+	    delete $record_ref->{fields}{'1201'};
+	}
+    }
+
+    # Dann ggf. die Bemerkung in Klammern dahinter
     if (defined $record_ref->{fields}{'1202'}){
-        push @$holdings_ref, $record_ref->{fields}{'1202'}[0]{content};
+        push @$holdings_ref, "(".$record_ref->{fields}{'1202'}[0]{content}.")";
         delete $record_ref->{fields}{'1202'};
     }
-
-    if (defined $record_ref->{fields}{'1200'}){
-        push @$holdings_ref, $record_ref->{fields}{'1200'}[0]{content};
-        delete $record_ref->{fields}{'1200'};
-    }
-
-    if (defined $record_ref->{fields}{'1201'}){
-        push @$holdings_ref, $record_ref->{fields}{'1201'}[0]{content};
-        delete $record_ref->{fields}{'1201'};
-    }
-
 
     if (@$holdings_ref){
         $record_ref->{fields}{'1204'} = [ {

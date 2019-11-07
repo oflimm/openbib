4f2d9640027bc50a6f3a4f332d846f0a6cb6e3e4
Oliver Flimm <flimm@sigtrap.de>
2019-11-07 14:30:35 +0100

    - Erweiterung um Teilkatalogbildung von inst118/master

--- conv/auto/openbib-autocron-weekend.pl
+++ conv/auto/openbib-autocron-weekend.pl
@@ -97,6 +97,8 @@ my $blacklist_ref = {
     'inst110master' => 1,
     'inst113' => 1,
     'inst113master' => 1,
+    'inst118' => 1,
+    'inst118master' => 1,
     'inst123' => 1,
     'inst125' => 1,
     'inst125master' => 1,
@@ -497,13 +499,13 @@ sub threadC {
 
     $logger->info("### Sonstige Master-Institutskataloge");
     
-    autoconvert({ updatemaster => $updatemaster, sync => 1, databases => ['inst006master','inst108master','inst110master','inst125master','inst134master','inst136master','inst156master','inst323master','inst401master','inst404master','inst405master','inst406master','inst407master','inst409master','inst410master','inst411master','inst412master','inst413master','inst414master','inst416master','inst418master','inst419master','inst426master','inst427master','inst428master','inst430master','inst431master','inst432master','inst434master','inst437master','inst438master','inst444master','inst445master','inst460master','inst461master','inst464master','inst468master','inst501master','inst526master','inst622master','inst623master'] });
+    autoconvert({ updatemaster => $updatemaster, sync => 1, databases => ['inst006master','inst108master','inst110master','inst118master','inst125master','inst134master','inst136master','inst156master','inst323master','inst401master','inst404master','inst405master','inst406master','inst407master','inst409master','inst410master','inst411master','inst412master','inst413master','inst414master','inst416master','inst418master','inst419master','inst426master','inst427master','inst428master','inst430master','inst431master','inst432master','inst434master','inst437master','inst438master','inst444master','inst445master','inst460master','inst461master','inst464master','inst468master','inst501master','inst526master','inst622master','inst623master'] });
 
     ##############################
     
     $logger->info("### Aufgesplittete sonstige Master-Institutskataloge");
     
-    autoconvert({ updatemaster => $updatemaster, sync => 1, databases => ['inst006','inst108','inst110','inst125','inst134','inst136','inst156','inst323','inst401','inst404','inst405','inst406','inst407','inst409','inst410','inst411','inst412','inst413','inst414','inst416','inst418','inst419','inst426','inst427','inst428','inst430','inst431','inst432','inst434','inst437','inst438','inst444','inst445','inst460','inst461','inst464','inst468','inst501','inst526','inst622','inst623'] });
+    autoconvert({ updatemaster => $updatemaster, sync => 1, databases => ['inst006','inst108','inst110','inst118','inst125','inst134','inst136','inst156','inst323','inst401','inst404','inst405','inst406','inst407','inst409','inst410','inst411','inst412','inst413','inst414','inst416','inst418','inst419','inst426','inst427','inst428','inst430','inst431','inst432','inst434','inst437','inst438','inst444','inst445','inst460','inst461','inst464','inst468','inst501','inst526','inst622','inst623'] });
 
     ##############################
 

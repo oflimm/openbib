6f75421a0bbcf4a3682bc5ea36f683a012fb73da
BartChris <christoph.bartmann@gmx.de>
2020-11-04 17:50:58 +0100

    query param geaendert

--- portal/perl/modules/OpenBib/Handler/PSGI/Extensions/FidPhil/Login.pm
+++ portal/perl/modules/OpenBib/Handler/PSGI/Extensions/FidPhil/Login.pm
@@ -317,7 +317,7 @@ sub authenticate {
 
     # Wenn Return_url existiert, dann wird dorthin gesprungen
     if ($redirect_to) {
-        $redirecturl = $redirect_to;
+        $redirecturl = $redirect_to ."/user/$userid";
         #$redirecturl = $redirect_to."%3Fuserid%3D$userid";
     }
     

713d0e502bc802964b936ff7a01c64be579d5430
Oliver Flimm <flimm@sigtrap.de>
2020-11-19 15:43:03 +0100

    - Automatische Befuellung von 0800 mit Briefe an Gentz, Briefe von Gentz und Briefe Dritter

--- conv/auto/filter/gentzbriefe/add-fields.pl
+++ conv/auto/filter/gentzbriefe/add-fields.pl
@@ -5,6 +5,38 @@ use JSON::XS;
 while (<>){
     my $title_ref = decode_json $_;
 
+    my $mediatype = "";
+    
+    if (defined $title_ref->{fields}{'0100'}[0]{content}){
+	my $briefaussteller = $title_ref->{fields}{'0100'}[0]{content};
+
+	if ($briefaussteller eq "Gentz, Friedrich"){
+	    $mediatype = "Briefe von Gentz";
+	}
+    }
+
+    if (defined $title_ref->{fields}{'0200'}[0]{content}){
+	my $briefempfaenger = $title_ref->{fields}{'0200'}[0]{content};
+
+	if ($briefempfaenger eq "Gentz, Friedrich"){
+	    $mediatype = "Briefe an Gentz";
+	}
+    }
+
+    if (!$mediatype){
+	$mediatype = "Briefe Dritter";
+    }
+
+    if ($mediatype){
+	$title_ref->{fields}{'0800'} = [
+	    {
+		mult     => 1,
+		subfield => '',
+		content  => $mediatype,
+	    }
+	    ];
+    }
+    
     if (defined $title_ref->{fields}{'0800'}[0]{content}){
         if ($title_ref->{fields}{'0800'}[0]{content} eq "Briefe von Gentz"){
             if (defined $title_ref->{fields}{'0425'}[0]{content}){

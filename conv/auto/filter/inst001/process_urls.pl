7a74d56aba43b8ae059f5207f99201f7a71b8afa
Oliver Flimm <flimm@sigtrap.de>
2021-03-24 11:24:33 +0100

    - Verarbeitung und Einordnung von URLs in 0662 in Links fuer Inhaltsverzeichnisse sowie zum Volltext

---
+++ conv/auto/filter/inst001/process_urls.pl
@@ -0,0 +1,56 @@
+#!/usr/bin/perl
+
+use JSON::XS;
+use YAML;
+use utf8;
+
+use warnings;
+use strict;
+
+while (<>){
+    my $record_ref = decode_json $_;
+    my $fields_ref = $record_ref->{fields};
+    
+    # Uebertragung der URLs in andere Katagorie
+    #
+    # Link zum Volltext:
+    #
+    # Uebertragen in Titelfeld fuer Inhaltsverzeichnisse T4120
+    # Analog zu angereicherten Link zu Inhaltsverzeichnissen in E4120
+    #
+    # Markierung von URLs nach Typ in Subfield
+    #
+    #  : Unbekannt
+    # a: Freier Zugriff    
+    # b: Eingeschraenkter Zugriff
+    # c: Kein Zugriff
+
+    # Uebertragung der URLs in andere Katagorie
+    #
+    # Link zum Inhaltsverzeichnis:
+    #
+    # Uebertragen in Titelfeld fuer Inhaltsverzeichnisse T4110
+    # Analog zu angereicherten Link zu Inhaltsverzeichnissen in E4110
+    
+    if (defined $fields_ref->{'0662'}) {
+	my $description_ref = {};
+
+	foreach my $item_ref (@{$fields_ref->{'0663'}}){
+	    $description_ref->{$item_ref->{mult}} = $item_ref->{content};
+	}
+
+	foreach my $item_ref (@{$fields_ref->{'0662'}}){
+	    if ($description_ref->{$item_ref->{mult}} =~m/Inhaltsverzeichnis/){
+	    $record_ref->{fields}{'4110'} = [
+		{
+		    mult     => 1,
+		    subfield => '',
+		    content  => $item_ref->{content},
+		},
+		];
+	    }
+	}
+    }
+
+    print encode_json $record_ref, "\n";
+}

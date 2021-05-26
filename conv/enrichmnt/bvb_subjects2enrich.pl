b26fbd9422351b74fed19b397d785b719ec7b99a
Oliver Flimm <flimm@sigtrap.de>
2021-05-26 09:27:43 +0200

    - Umstellung Subjects aus BVB auf MARC

--- conv/enrichmnt/bvb_subjects2enrich.pl
+++ conv/enrichmnt/bvb_subjects2enrich.pl
@@ -39,10 +39,9 @@ use Business::ISBN;
 use Encode 'decode_utf8';
 use Getopt::Long;
 use Log::Log4perl qw(get_logger :levels);
-use XML::Twig::XPath;
-use XML::Simple;
-use Storable ();
-use Data::Dumper;
+use MARC::Batch;
+use MARC::Charset 'marc8_to_utf8';
+use MARC::File::XML;
 use JSON::XS;
 
 use OpenBib::Common::Util;
@@ -53,14 +52,15 @@ use OpenBib::Catalog::Factory;
 # Autoflush
 $|=1;
 
-
-my ($help,$importjson,$import,$init,$jsonfile,$inputfile,$logfile,$loglevel);
+my ($help,$format,$use_xml,$importjson,$import,$init,$jsonfile,$inputfile,$logfile,$loglevel);
 
 &GetOptions("help"         => \$help,
             "init"         => \$init,
             "import"       => \$import,
             "inputfile=s"  => \$inputfile,
             "jsonfile=s"   => \$jsonfile,
+            "use-xml"      => \$use_xml,
+            "format=s"     => \$format,
             "import-json"  => \$importjson,
             "logfile=s"    => \$logfile,
             "loglevel=s"   => \$loglevel,
@@ -74,7 +74,7 @@ my $config = OpenBib::Config->new;
 
 $jsonfile=($jsonfile)?$jsonfile:"$inputfile.json";
 
-$logfile=($logfile)?$logfile:"/var/log/openbib/bvb_subject-enrichmnt.log";
+$logfile=($logfile)?$logfile:"/var/log/openbib/bvb_rvk-enrichmnt.log";
 $loglevel=($loglevel)?$loglevel:"INFO";
 
 my $log4Perl_config = << "L4PCONF";
@@ -100,55 +100,80 @@ my $origin = 24;
 
 $logger->debug("Origin: $origin");
 
-my $twig= XML::Twig::XPath->new(
-   TwigHandlers => {
-     "marc:collection/marc:record" => \&parse_record
-#     "/collection/record" => \&parse_record
-   }
- );
-
-#$twig->set_namespace('marc',
-#       'http://www.loc.gov/MARC21/slim');
-
 if ($init){
     $logger->info("Loeschen der bisherigen Daten");
     
-    $enrichment->init_enriched_content({ field => '4300', origin => $origin });
+    $enrichment->init_enriched_content({ field => '4101', origin => $origin });
 }
 
-#$logger->info("Bestimmung der Schlagworte");
+$logger->info("Bestimmung der SUBJECTs");
 
-our $count=1;
+$format=($format)?$format:'USMARC';
 
-our $subject_tuple_count = 1;
+$logger->debug("Using format $format");
 
-our $enrich_data_by_isbn_ref = [];
+my $batch;
+
+if ($use_xml){
+    $logger->debug("Using MARC-XML");
+    
+    MARC::File::XML->default_record_format($format);
+    
+    $batch = MARC::Batch->new('XML', $inputfile);    
+}
+else {
+    $logger->debug("Using native MARC");
+    $batch = MARC::Batch->new($format, $inputfile);
+}
+
+# Recover from errors
+$batch->strict_off();
+$batch->warnings_off();
+
+# Fallback to UTF8
+MARC::Charset->assume_unicode(1);
+
+# Ignore Encoding Errors
+MARC::Charset->ignore_errors(1);
 
 if ($importjson){
     if (! -e $jsonfile){
         $logger->error("JSON-Datei $jsonfile existiert nicht");
         exit;
     }
+
     open(JSON,$jsonfile);
 
+    my $count=1;
+    
+    my $subject_tuple_count = 1;
+    
+    my $enrich_data_by_isbn_ref   = [];
+    my $enrich_data_by_bibkey_ref = [];
+    
     $logger->info("Einlesen und -laden der neuen Daten");
 
     while (<JSON>){
         my $item_ref = decode_json($_);
 
         push @{$enrich_data_by_isbn_ref},   $item_ref if (defined $item_ref->{isbn});
+        push @{$enrich_data_by_bibkey_ref}, $item_ref if (defined $item_ref->{bibkey});
+
         $subject_tuple_count++;
         
         if ($count % 1000 == 0){
             $enrichment->add_enriched_content({ matchkey => 'isbn',   content => $enrich_data_by_isbn_ref }) if (@$enrich_data_by_isbn_ref);
+            $enrichment->add_enriched_content({ matchkey => 'bibkey', content => $enrich_data_by_bibkey_ref })  if (@$enrich_data_by_bibkey_ref);
             $enrich_data_by_isbn_ref   = [];
+            $enrich_data_by_bibkey_ref = [];
         }
         $count++;
     }
 
     $enrichment->add_enriched_content({ matchkey => 'isbn',   content => $enrich_data_by_isbn_ref }) if (@$enrich_data_by_isbn_ref);
+    $enrichment->add_enriched_content({ matchkey => 'bibkey', content => $enrich_data_by_bibkey_ref }) if (@$enrich_data_by_bibkey_ref);
     
-    $logger->info("$subject_tuple_count Schlagwort-Tupel eingefuegt");
+    $logger->info("$subject_tuple_count SUBJECT-Tupel eingefuegt");
 
     if ($jsonfile){
         close(JSON);
@@ -160,14 +185,152 @@ else {
         open(JSON,">$jsonfile");
     }
     
+    my $count=1;
+
+    my $subject_tuple_count = 1;
+
+    # Logik: Bibkeys werden nur verwendet, wenn es keine ISBNs gibt. Also nicht entweder ISBN oder Bibkey, sondern sowohl ISBN wie auch Bibkey!
+    
+    my $enrich_data_by_isbn_ref = [];
+    my $enrich_data_by_bibkey_ref = [];
     
     $logger->info("Einlesen und -laden der neuen Daten");
+    
+    while (my $record = $batch->next()){
+        
+        my $encoding = $record->encoding();
+        
+        $logger->debug("Encoding:$encoding:");
 
-    $twig->safe_parsefile($inputfile);
+        my $bibkey = OpenBib::Common::Util::gen_bibkey_from_marc($record,$encoding);
+
+        my @isbns = ();
+        {
+            # ISBN
+            foreach my $field ($record->field('020')){
+                my $content_a = ($encoding eq "MARC-8")?marc8_to_utf8($field->as_string('a')):decode_utf8($field->as_string('a'));
+                my $content_z = ($encoding eq "MARC-8")?marc8_to_utf8($field->as_string('z')):decode_utf8($field->as_string('z'));
+                
+                $content_a=~s/\s+\(.+?\)\s*$//;
+                $content_z=~s/\s+\(.+?\)\s*$//;
+                
+                if ($content_a){
+                    my $isbn = $content_a;
+                    my $isbnXX = Business::ISBN->new($content_a);
+                    
+                    if (defined $isbnXX && $isbnXX->is_valid){
+                        $isbn = $isbnXX->as_isbn13->as_string;
+                    }
+                    else {
+                        next;
+                    }
+                    
+                    $isbn = OpenBib::Common::Util::normalize({
+                        field    => 'T0540',
+                        content  => $isbn,
+                    });
+                    
+                    push @isbns, $isbn;
+                }
+            }
+        }
+        
+        my @subjects = ();
+        {        
+            # SUBJECTs
+            foreach my $fieldno ('650','651'){
+                foreach my $field ($record->field($fieldno)){
+                    my $content_a = ($encoding eq "MARC-8")?marc8_to_utf8($field->as_string('a')):decode_utf8($field->as_string('a'));
+                    my $content_x = ($encoding eq "MARC-8")?marc8_to_utf8($field->as_string('x')):decode_utf8($field->as_string('x'));
+                    my $content_y = ($encoding eq "MARC-8")?marc8_to_utf8($field->as_string('y')):decode_utf8($field->as_string('y'));
+                    my $content_z = ($encoding eq "MARC-8")?marc8_to_utf8($field->as_string('z')):decode_utf8($field->as_string('z'));
+
+                    push @subjects, {
+                        content  => $content_a,
+                        subfield => 'a'
+                    } if ($content_a);
+
+                    push @subjects, {
+                        content  => $content_x,
+                        subfield => 'x'
+                    } if ($content_x);
+
+		    push @subjects, {
+                        content  => $content_y,
+                        subfield => 'y'
+                    } if ($content_y);
+
+		    push @subjects, {
+                        content  => $content_z,
+                        subfield => 'z'
+                    } if ($content_z);
+
+                }
+            }
+        }
+
+        if (@isbns){
+            # Dublette SUBJECT's entfernen
+            my %seen_terms  = ();
+            my @unique_isbns    = grep { ! $seen_terms{$_} ++ } @isbns;
+            
+            foreach my $isbn (@unique_isbns){
+                foreach my $subject (@subjects){
+                    $logger->debug("Found $isbn -> $subject");
+                    my $subject_ref = {
+                        isbn     => $isbn,
+                        origin   => $origin,
+                        field    => '4300',
+                        subfield => $subject->{subfield},
+                        content  => $subject->{content},
+                    };
+                    
+                    print JSON encode_json($subject_ref),"\n" if ($jsonfile);
+                    
+                    push @{$enrich_data_by_isbn_ref}, $subject_ref;
+                    $subject_tuple_count++;
+                }
+            }
+        }
+        elsif ($bibkey){
+            foreach my $subject (@subjects){
+                $logger->debug("Found Bibkey $bibkey -> $subject");
+                my $subject_ref = {
+                    bibkey   => $bibkey,
+                    origin   => $origin,
+                    field    => '4300',
+                    subfield => $subject->{subfield},
+                    content  => $subject->{content},
+                };
+                
+                print JSON encode_json($subject_ref),"\n" if ($jsonfile);
+                
+                push @{$enrich_data_by_bibkey_ref}, $subject_ref;
+                $subject_tuple_count++;
+            }            
+        }
+        
+        if ($import && $count % 1000 == 0){
+            $enrichment->add_enriched_content({ matchkey => 'isbn',   content => $enrich_data_by_isbn_ref }) if (@$enrich_data_by_isbn_ref);
+            $enrichment->add_enriched_content({ matchkey => 'bibkey', content => $enrich_data_by_bibkey_ref })  if (@$enrich_data_by_bibkey_ref);
+            $enrich_data_by_isbn_ref   = [];
+            $enrich_data_by_bibkey_ref = [];
+        }
+
+	if ($count % 10000 == 0){
+	    $logger->info("$count Datensaetze bearbeitet");
+	}
+	
+        $count++;
+        
+    }
     
-    $logger->info("$count done");
+    if ($import && (@$enrich_data_by_isbn_ref || @$enrich_data_by_bibkey_ref) ){
+        $enrichment->add_enriched_content({ matchkey => 'isbn',   content => $enrich_data_by_isbn_ref }) if (@$enrich_data_by_isbn_ref);
+        $enrichment->add_enriched_content({ matchkey => 'bibkey', content => $enrich_data_by_bibkey_ref })  if (@$enrich_data_by_bibkey_ref);
+    }
     
-    $logger->info("$subject_tuple_count ISBN-Schlagwort-Tupel eingefuegt");
+    $logger->info("$subject_tuple_count SUBJECT-Tupel eingefuegt");
 
     if ($jsonfile){
         close(JSON);
@@ -175,123 +338,17 @@ else {
 
 }
 
-sub parse_record {
-    my($t, $titset)= @_;
-
-    my $logger = get_logger();
-
-#    $logger->info("Bestimmung der ISBNs");
-
-    my @isbns = ();
-    
-    {
-	my @elements = $titset->findnodes('//marc:datafield[@tag="020"]/marc:subfield[@code="a"]');
-#	my @elements = $titset->findnodes('//datafield[@tag="020"]/subfield[@code="a"]');
-
-#	$logger->info(Data::Dumper::Dumper(\@elements));
-	foreach my $element (@elements){
-	    
-	    my $isbn = $element->text();
-
-#	    $logger->info("ISBN: $isbn");
-	    $isbn=~s/\s+\(.+?\)\s*$//;
-
-
-	    
-	    if ($isbn){
-		my $isbn = $isbn;
-		my $isbnXX = Business::ISBN->new($isbn);
-		
-		if (defined $isbnXX && $isbnXX->is_valid){
-		    $isbn = $isbnXX->as_isbn13->as_string;
-		}
-		else {
-		    $logger->error("$isbn NOT valid!");
-		    next;
-		}
-		
-		$isbn = OpenBib::Common::Util::normalize({
-		    field    => 'T0540',
-		    content  => $isbn,
-							 });
-		
-		push @isbns, $isbn;
-	    }
-	}
-    }       
-
-#    $logger->info("Found ISBN: ".join(';',@isbns)) if (@isbns);
-
-#    $logger->info("Bestimmung der Schlagworte");    
-
-    my @subjects = ();
-    {        
-	# Schlagwort
-	my @elements = $titset->findnodes('//marc:datafield[@tag="650" or @tag="651"]/marc:subfield[@code="a" or @code="x" or @code="y" or @code="z"]');
-	
-	foreach my $element (@elements){
-	    
-	    my $subject = $element->text();
-	    my $code    = $element->att("code");
-
-	    $logger->debug("SW: $subject - Code: $code");
-	    push @subjects, {
-		content  => konv($subject),
-		subfield => $code
-	    } if ($subject);
-	}
-    }
-    
-    # Dublette Schlagworte's entfernen
-    my %seen_terms  = ();
-    my @unique_subjects = grep { ! $seen_terms{$_->{content}} ++ } @subjects; 
-    
-    if (@isbns){
-	my @unique_isbns    = grep { ! $seen_terms{$_} ++ } @isbns;
-	
-	foreach my $isbn (@unique_isbns){
-	    foreach my $subject (@unique_subjects){
-		$logger->debug("Found $isbn -> $subject");
-		my $item_ref = {
-		    isbn     => $isbn,
-		    origin   => $origin,
-		    field    => '4300',
-		    subfield => $subject->{subfield},
-		    content  => $subject->{content},
-		};
-		
-		print JSON encode_json($item_ref),"\n" if ($jsonfile);
-		
-		push @{$enrich_data_by_isbn_ref}, $item_ref;
-		$subject_tuple_count++;
-	    }
-	}
-    }
-    
-    if ($count % 1000 == 0){
-	$logger->info("$count done");
-	if ($import){
-	    $enrichment->add_enriched_content({ matchkey => 'isbn',   content => $enrich_data_by_isbn_ref }) if (@$enrich_data_by_isbn_ref);
-	    $enrich_data_by_isbn_ref   = [];
-	}
-    }
-    $count++;
-    
-    # Release memory of processed tree
-    # up to here
-    $t->purge();
-
-}
-
 sub print_help {
     print << "ENDHELP";
-bvb_subjects2enrich.pl - Anreicherung mit Schlagwort-Informationen aus den offenen Daten des BVB
+bvb_subjects2enrich.pl - Anreicherung mit SUBJECT-Informationen aus den offenen Daten des BVB
 
    Optionen:
    -help                 : Diese Informationsseite
 
    -init                 : Zuerst Eintraege fuer dieses Feld und Origin aus Anreicherungsdatenbank loeschen
    -import               : Einladen der verarbeiteten Daten
+   -use-xml              : MARCXML-Format verwenden
+   -format=...           : Format z.B. UNIMARC (default: USMARC)
 
    --inputfile=...       : Name der Einladedatei im MARC-Format
    --jsonfile=...        : Name der JSON-Einlade-/ausgabe-Datei

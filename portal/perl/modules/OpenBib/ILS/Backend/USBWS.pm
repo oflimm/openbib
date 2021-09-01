1bd51c2b134614be12b632f62fd707c3a6e0ca75
Oliver Flimm <flimm@sigtrap.de>
2021-09-01 10:46:01 +0200

    - Erweiterung um Methode get_mediastatus analog DAIA

--- portal/perl/modules/OpenBib/ILS/Backend/USBWS.pm
+++ portal/perl/modules/OpenBib/ILS/Backend/USBWS.pm
@@ -46,6 +46,8 @@ use YAML::Syck;
 use OpenBib::Config;
 use OpenBib::Config::File;
 use OpenBib::Config::CirculationInfoTable;
+use OpenBib::Config::DatabaseInfoTable;
+use OpenBib::Config::LocationInfoTable;
 
 ######################################################################
 # Authentication
@@ -67,7 +69,7 @@ sub authenticate {
     my $userid = 0;
 
     my $config  = $self->get_config;
-    my $dbname  = $self->get('name');
+    my $dbname  = $self->get_database;
 
     $logger->debug("Authenticate info via USB Authentication-Service");
 
@@ -286,13 +288,227 @@ sub renew_loans {
 ######################################################################
 
 sub get_mediastatus {
-    my ($self,$arg_ref) = @_;
+    my ($self,$titleid) = @_;
+    
+    # Log4perl logger erzeugen
+    my $logger = get_logger();
 
+    my $config    = $self->get_config;
+    my $database  = $self->get_database;
+    
     my $result_ref = {};
+    
+    unless ($database && $titleid){
+	$result_ref = {
+	    timestamp   => $self->get_timestamp,
+	    circulation => [],
+	    error       => "missing parameters",	    
+	};
+	
+	return $result_ref;
+    }
+    
+    my $dbinfotable   = OpenBib::Config::DatabaseInfoTable->new;
+    my $circinfotable = OpenBib::Config::CirculationInfoTable->new;
+    my $locinfotable  = OpenBib::Config::LocationInfoTable->new;
 
-    # todo
+    my $items_ref = [];
 
+    # Ausleihinformationen der Exemplare
+    {
+	my $circexlist=undef;
+	
+	if ($circinfotable->has_circinfo($database) && defined $circinfotable->get($database)->{circ}) {
+	    
+	    $logger->debug("Getting Circulation info via USB-SOAP");
+	    
+	    my @args = ($titleid);
+	    
+	    my $uri = "urn:/Loan";
+	    
+	    if ($circinfotable->get($database)->{circdb} ne "sisis"){
+		$uri = "urn:/Loan_inst";
+		push @args, $database;
+	    }
+	    
+	    $logger->debug("Trying connection to uri $uri at ".$config->get('usbws_url'));
+	    
+	    eval {
+		my $soap = SOAP::Lite
+		    -> uri($uri)
+		    -> proxy($config->get('usbws_url'));
+		my $result = $soap->show_all_items(@args);
+		
+		unless ($result->fault) {
+		    $circexlist = $result->result;
+		    if ($logger->is_debug){
+			$logger->debug("SOAP Result: ".YAML::Dump($circexlist));
+		    }
+		}
+		else {
+		    $logger->error("SOAP MediaStatus Error", join ', ', $result->faultcode, $result->faultstring, $result->faultdetail);
+		}
+	    };
+	    
+	    if ($@){
+		$logger->error("SOAP-Target ".$circinfotable->get($database)->{circcheckurl}." konnte nicht erreicht werden :".$@);
+	    }
+	    
+	}
+	
+	# Bei einer Ausleihbibliothek haben - falls Exemplarinformationen
+	# in den Ausleihdaten vorhanden sind -- diese Vorrange ueber die
+	# titelbasierten Exemplardaten
+	
+	if (defined($circexlist)) {
+	    my $itemstring = (defined $circexlist->{Exemplardaten})?'Exemplardaten':'PresentExemplardaten';
+	    
+	    foreach my $nr (keys %{$circexlist->{$itemstring}}){
+		my $circ_ref = $circexlist->{$itemstring}{$nr};
+		
+		$logger->debug(YAML::Dump($circ_ref));
+		
+		my $zweigabteil = $circ_ref->{'ZweigAbteil'};
+		
+		$logger->debug("Matching $zweigabteil");
+		if ($zweigabteil=~m/^\$msg\{USB-KUG-Locations,(.+?)\}/){
+		    my $isil=$1;
+		    
+		    my $zweigname = $locinfotable->get('identifier')->{$isil}{description};
+		    $logger->debug("Found $zweigname");
+		    
+		    $circ_ref->{'ZweigAbteil'} =~s/^\$msg\{USB-KUG-Locations,.+?\}/$zweigname/;		    		    		    
+		}
+		else {
+		    $logger->debug("No match");
+		}
+
+		# Umwandeln
+		my $item_ref = {};
+
+		$item_ref->{'label'} = $circ_ref->{'Signatur'};
+		$item_ref->{'id'}    = $circ_ref->{'MedienNr'};
+
+		if ($circ_ref->{'ZweigAbteil'} =~/^(.+?)\s+\/\s+(.+?)$/){
+		    $circ_ref->{'ZweigName'} = $1;
+		    $circ_ref->{'AbteilName'} = $2;
+		}
+		
+		if ($circ_ref->{'ZweigName'} && $circ_ref->{'AbteilName'}){
+		    $item_ref->{'department'} = {
+			content => $circ_ref->{ZweigName},
+			id      => $circ_ref->{Zweigstelle},
+		    };
+		    $item_ref->{'storage'} = {
+			content => $circ_ref->{AbteilName},
+		    };
+		}
+		else {
+		    $item_ref->{'department'} = {
+			content => $circ_ref->{ZweigAbteil},
+			id      => $circ_ref->{Zweigstelle},
+		    };
+		}
+
+		my $available_ref   = [];
+		my $unavailable_ref = [];
+
+		if ($circ_ref->{LeihstatusText} eq "Präsenzbestand"){
+		    push @$available_ref, {
+			service => 'presence',
+		    };
+		}
+		elsif ($circ_ref->{LeihstatusText} eq "bestellbar (Nutzung nur im Lesesaal)"){
+		    push @$available_ref, {
+			service => 'loan',
+			limitation => "bestellbar (Nutzung nur im Lesesaal)",
+			type => 'Stationary',
+		    };
+		}
+		elsif ($circ_ref->{LeihstatusText} =~m/bestellbar/ || $circ_ref->{LeihstatusText} =~m/verfügbar/){
+		    push @$available_ref, {
+			service => 'order',
+		    };
+		}
+		elsif ($circ_ref->{LeihstatusText} = "ausleihbar"){
+		    push @$available_ref, {
+			service => 'loan',
+		    };
+		}
+		elsif ($circ_ref->{LeihstatusText} = "nicht entleihbar"){
+		    push @$available_ref, {
+			service => 'presence',
+		    };
+		}
+		elsif ($circ_ref->{LeihstatusText} = "nur in bes. Lesesaal bestellbar"){
+		    push @$available_ref, {
+			service => 'order',
+			limitation => $circ_ref->{LeihstatusText},
+			type => 'Stationary',
+		    };
+		}
+		elsif ($circ_ref->{LeihstatusText} = "nur Wochenende"){
+		    push @$available_ref, {
+			service => 'loan',
+			limitation => $circ_ref->{LeihstatusText},
+			type => 'ShortLoan',
+		    };
+		}
+		elsif ($circ_ref->{LeihstatusText} =~m/entliehen/){
+		    my $this_unavailable_ref = {
+			service => 'loan',
+			expected => $circ_ref->{RueckgabeDatum},
+		    };
+
+		    if ($circ_ref->{VormerkAnzahl}){
+			$this_unavailable_ref->{queue} = $circ_ref->{VormerkAnzahl} ;
+		    }
+
+		    push @$unavailable_ref, $this_unavailable_ref;
+		    
+		}
+		elsif ($circ_ref->{LeihstatusText} =~m/vermisst/){
+		    my $this_unavailable_ref = {
+			service => 'loan',
+		    };
+
+		    push @$unavailable_ref, $this_unavailable_ref;
+		    
+		}
+
+		if (@$available_ref){
+		    $item_ref->{available} = $available_ref;
+		}
+		
+		if (@$unavailable_ref){
+		    $item_ref->{unavailable} = $unavailable_ref;
+		}
+		
+		push @$items_ref, $item_ref;
+	    }
+	    
+	}	    
+    }
+    
+    $result_ref = {
+	id          => $titleid,
+	database    => $database,
+	items       => $items_ref,
+	timestamp   => $self->get_timestamp,
+    };
+    
+    $logger->debug("Circ: ".YAML::Dump($result_ref));
+            
     return $result_ref;
+}
+
+sub get_timestamp {
+    my $self = shift;
+    
+    my ($sec,$min,$hour,$mday,$mon,$year,$wday,$yday,$isdst)=localtime(time);
+    my $timestamp = sprintf ( "%04d-%02d-%02dT%02d:%02d:%02dZ",
+                                   $year+1900,$mon+1,$mday,$hour,$min,$sec);
+    return $timestamp;
 }
 
 1;

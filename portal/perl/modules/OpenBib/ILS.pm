df5e1cbc378264e9b96ff11f90a3d0a055b14057
Oliver Flimm <flimm@sigtrap.de>
2021-09-06 13:58:50 +0200

    - ILS: Erweiterung um perldoc Manpage mit Erklaerungen zu den bereitgestellten Methoden

--- portal/perl/modules/OpenBib/ILS.pm
+++ portal/perl/modules/OpenBib/ILS.pm
@@ -104,7 +104,7 @@ __END__
 
 =head1 NAME
 
-OpenBib::ILS - Objekt zur Anbindung eines Integrierten Bibliothekssystems (Authentifizierung, Ausleihe)
+OpenBib::ILS - Objekt zur Anbindung eines Integrierten Bibliothekssystems (Authentifizierung, Ausleihe, Medienstatus)
 
 =head1 DESCRIPTION
 
@@ -116,22 +116,85 @@ Dieses Objekt stellt Funktionen eines ILS bereit
 
  my $ils = OpenBib::ILS::Factory->create_ils({ database => $database });
 
-=head1 Returncodes
+=head1 GENERELLES
 
 =over 4
 
-=item -1 
+=item Instanziierung
 
-Es existiert keine ILS-Anbindung fuer die Datenbank $database
+Die Instanziierung erfolgt immer ueber die Methode create_ils inder Klasse
+OpenBib::ILS::Factory
 
+=item Bezug zum Bibliothekssystem
+
+Das Objekt weiss anhand des Uebergebenen Parameters $database in create_ils, 
+wie es sich mit dem jeweiligen Bibliothekssystem verbindung muss (via Backends) 
+und speichert $database intern im Objekt ab
+
+=back
 
 =head1 METHODS
 
 =over 4
 
-=item new
+=item authenticate({username => $username, password => $password})
 
-Erzeugung des Objektes
+Authentifizierung des Nutzers $username mit dem Passwort $password an ILS zu $database (via create_ils einmalig uebergeben)
+
+=item get_mediastatus($titleid)
+
+Liefere alle Exemplare mit ihren Ausleihstatus-Informationen zum Titel mit der $titleid zurueck
+
+=item get_loans($username)
+
+Liefere alle getaetigten Ausleihen des Nutzers mit dem Namen $username zurueck
+
+=item get_orders($username)
+
+Liefere alle getaetigten Bestellungen des Nutzers mit dem Namen $username zurueck
+
+=item get_reservations($username)
+
+Liefere alle getaetigten Reservierungen von Medien des Nutzers mit dem Namen $username zurueck
+
+=item get_fees($username)
+
+Liefere alle Gebuehren des Nutzers mit dem Namen $username zurueck
+
+=item get_items($username)
+
+Liefere in einem Request alle Informationen von get_loans, get_orders und get_reservations in einem Request 
+zum nutzer mit dem Namen $username zurueck
+
+=item check_order({ username => $username, holdingid => $mediennr, unit => $zweigstelle })
+
+Ueberpruefe, ob eine Bestellung auf die Mediennummer $mediennr in der (Teil)Bibliothek $zweigstelle durchgefuehrt werden kann 
+und liefere eine Auswahl der Ausgabeorte (pickup_locations) fuer den Aufruf der Methode make_order zurueck.
+
+=item make_order({ username => $username, holdingid => $mediennr, unit => $zweigstelle, pickup_location => $ausgabeort })
+
+Fuehre eine Bestellung eines Mediums mit der Mediennummer $mediennr in der (Teil)Bibliothek $zweigstelle und dem Ausgabeort $ausgabeort durch
+
+=item check_reservation({ username => $username, holdingid => $mediennr, unit => $zweigstelle })
+
+Ueberpruefe, ob eine Vormerkung auf das Medium $mediennr in der (Teil)Bibliothek $zweigstelle  durchgefuehrt werden kann 
+und liefere eine Auswahl der Ausgabeorte (pickup_locations) fuer den Aufruf der Methode make_reservation zurueck.
+
+=item make_reservation({ username => $username, holdingid => $mediennr, unit => $zweigstelle, $pickup_location => $ausgabeort, titleid => $katkey, type => $type })
+
+Fuehre entweder 
+a) eine vollqualifizierte Vormerkung auf das Medium $mediennr in der (Teil)Bibliothek $zweigstelle mit dem Ausgabeort $ausgabeort oder
+b) eine teilqualifizierte Vormerkung auf ein beliebiges Exemplar des Titels mit der ID $katkey in der Zweigstelle $zweigstelle mit dem Ausgabeort $ausgabeort und dem Type $type="unqualified" durch
+
+=item cancel_reservation({ username => $username, holdingid => $mediennr, unit => $zweigstelle, titleid => $katkey })
+
+Storniere 
+a) eine vollqualifizierte Vormerkung auf das Medium $mediennr in der (Teil)Bibliothek $zweigstelle oder
+b) eine teilqualifizierte Vormerkung auf ein beliebiges Exemplar des Titels mit der ID $katkey in der Zweigstelle $zweigstelle
+
+=item renew_loans($username)
+
+Fuehre eine Gesamtkontoverlaengerung fuer den Account des Nutzers $username durch
 
 =back
 

2f35859b18b6889dff65be6eae5351e988013fa9
Oliver Flimm <flimm@sigtrap.de>
2022-01-18 10:41:39 +0100

    - Anreicherung mit Bemerkung

---
+++ conv/auto/filter/inst413karten/add-comments.pl
@@ -0,0 +1,42 @@
+#!/usr/bin/perl
+
+use JSON::XS;
+use utf8;
+
+my $commentfile = "/opt/openbib/autoconv/pools/inst413karten/comments.dat";
+
+open(COMMENTIN,'<:encoding(UTF-8)',$commentfile);
+
+my $comments_ref = {};
+
+while (<COMMENTIN>){
+    my ($id,$comment)=split("\t",$_);
+
+    chomp($comment);
+
+    # HTML entfernen
+    $comment=~s/<.+?>//g;
+    
+    $comments_ref->{$id} = $comment;
+}
+
+close(COMMENTIN);
+
+while (<>){
+    my $title_ref = decode_json $_;
+
+    my $titleid = $title_ref->{id};
+
+    if (defined $comments_ref->{$titleid}){
+	$title_ref->{fields}{'0403'} = [
+	    {
+		mult     => 1,
+		subfield => '',
+		content  => $comments_ref->{$titleid},
+	    },
+	];
+    }
+   
+    print encode_json $title_ref, "\n";
+}
+

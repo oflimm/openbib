6619be5cfa2342fa15f2eb0cb2700362d7fa8de4
Oliver Flimm <flimm@sigtrap.de>
2022-01-21 15:47:29 +0100

    - Entladen aller relevanten System-Tabellen, die der aktuellen Konfiguration (Kataloge, Views, ...) entsprechend. Auslassen von Tabellen fuer Sessions und Eventlogs

---
+++ tools/dump_systemtables.pl
@@ -0,0 +1,105 @@
+#!/usr/bin/perl
+#####################################################################
+#
+#  dump_systemtables.pl
+#
+#  Export der Systemtabellen aus der System-DB
+#
+#  Dieses File ist (C) 2022 Oliver Flimm <flimm@openbib.org>
+#
+#  Dieses Programm ist freie Software. Sie koennen es unter
+#  den Bedingungen der GNU General Public License, wie von der
+#  Free Software Foundation herausgegeben, weitergeben und/oder
+#  modifizieren, entweder unter Version 2 der Lizenz oder (wenn
+#  Sie es wuenschen) jeder spaeteren Version.
+#
+#  Die Veroeffentlichung dieses Programms erfolgt in der
+#  Hoffnung, dass es Ihnen von Nutzen sein wird, aber OHNE JEDE
+#  GEWAEHRLEISTUNG - sogar ohne die implizite Gewaehrleistung
+#  der MARKTREIFE oder der EIGNUNG FUER EINEN BESTIMMTEN ZWECK.
+#  Details finden Sie in der GNU General Public License.
+#
+#  Sie sollten eine Kopie der GNU General Public License zusammen
+#  mit diesem Programm erhalten haben. Falls nicht, schreiben Sie
+#  an die Free Software Foundation, Inc., 675 Mass Ave, Cambridge,
+#  MA 02139, USA.
+#
+#####################################################################
+
+use strict;
+use warnings;
+no warnings 'redefine';
+use utf8;
+
+use Encode 'decode_utf8';
+use Log::Log4perl qw(get_logger :levels);
+use Benchmark ':hireswallclock';
+use DBI;
+use Getopt::Long;
+use Unicode::Collate;
+use YAML;
+
+use OpenBib::Config;
+use OpenBib::Catalog;
+use OpenBib::Schema::Catalog;
+use OpenBib::Schema::System;
+use OpenBib::Statistics;
+use OpenBib::Record::Title;
+use OpenBib::Search::Util;
+use OpenBib::User;
+
+my ($database,$help,$loglevel,$logfile);
+
+&GetOptions(
+    "logfile=s"       => \$logfile,
+    "loglevel=s"      => \$loglevel,
+    "help"            => \$help
+    );
+
+if ($help){
+    print_help();    
+}
+
+$loglevel = ($loglevel)?$loglevel:"INFO";
+$logfile  = ($logfile)?$logfile:'/var/log/openbib/dump_systemtables.log';
+
+my $log4Perl_config = << "L4PCONF";
+log4perl.rootLogger=$loglevel, LOGFILE, Screen
+log4perl.appender.LOGFILE=Log::Log4perl::Appender::File
+log4perl.appender.LOGFILE.filename=$logfile
+log4perl.appender.LOGFILE.mode=append
+log4perl.appender.LOGFILE.layout=Log::Log4perl::Layout::PatternLayout
+log4perl.appender.LOGFILE.layout.ConversionPattern=%d [%c]: %m%n
+log4perl.appender.Screen=Log::Dispatch::Screen
+log4perl.appender.Screen.layout=Log::Log4perl::Layout::PatternLayout
+log4perl.appender.Screen.layout.ConversionPattern=%d [%c]: %m%n
+L4PCONF
+
+Log::Log4perl::init(\$log4Perl_config);
+
+# Log4perl logger erzeugen
+my $logger = get_logger();
+
+my $config     = OpenBib::Config->new;
+
+my $pg_dump    = "/usr/bin/pg_dump";
+my $gzip       = "/bin/gzip";
+my $tar        = "/bin/tar";
+
+
+system("echo \"*:*:*:$config->{'systemdbuser'}:$config->{'systemdbpasswd'}\" > ~/.pgpass ; chmod 0600 ~/.pgpass");
+
+system("$pg_dump -U $config->{'systemdbuser'} -c -T 'updatelog' -T 'session*' -T 'recordhistory' -T 'event*' -T 'queries' -T 'searchhistory' openbib_system | $gzip > system_tables.sql.gz");
+
+sub print_help {
+    print << "ENDHELP";
+dump_systemtables.pl - Export der Systemtabellen aus der System-DB
+
+   Optionen:
+   -help                 : Diese Informationsseite
+   --logfile=...         : Alternatives Logfile
+   --loglevel=...        : Alternatives Loglevel
+ENDHELP
+    exit;
+}
+

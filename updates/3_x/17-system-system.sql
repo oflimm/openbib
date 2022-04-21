1552e553fe93120275d8c0ad8d8bec7e243b2b7c
Oliver Flimm <flimm@sigtrap.de>
2022-04-21 08:28:56 +0200

    - Umstellung System-Identifier der Kataloge

---
+++ updates/3_x/17-system-system.sql
@@ -0,0 +1,17 @@
+UPDATE databaseinfo SET system = 'system_sisis' where system = 'Sisis';
+UPDATE databaseinfo SET system = 'system_aleph' where system = 'Aleph';
+UPDATE databaseinfo SET system = 'system_allegro' where system = 'Allegro';
+UPDATE databaseinfo SET system = 'system_lars' where system = 'Lars';
+UPDATE databaseinfo SET system = 'oai_dc' where system = 'OAI';
+UPDATE databaseinfo SET system = 'format_mab2' where system = 'MAB';
+UPDATE databaseinfo SET system = 'format_marc21' where system = 'MARC';
+UPDATE databaseinfo SET system = 'api_z3950' where system = 'Backend: Z3950';
+UPDATE databaseinfo SET system = 'api_ezb' where system = 'Backend: EZB';
+UPDATE databaseinfo SET system = 'api_dbis' where system = 'Backend: DBIS';
+UPDATE databaseinfo SET system = 'api_bibsonomy' where system = 'Backend: BibSonomy';
+UPDATE databaseinfo SET system = 'api_eds' where system = 'Backend: EDS';
+UPDATE databaseinfo SET system = 'api_gvi' where system = 'Backend: GVI';
+UPDATE databaseinfo SET system = 'other' where system = 'Anderes';
+
+
+

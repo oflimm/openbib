73df0ca292ce94c3745084ae0f17cf62c6851e5e
Oliver Flimm <flimm@sigtrap.de>
2022-05-06 13:03:22 +0200

    - Markierung von Volltext-URLs in brillphil

--- conv/auto/filter/brillphil/pre_conv.pl
+++ conv/auto/filter/brillphil/pre_conv.pl
@@ -51,4 +51,4 @@ my $bcp2metaexe   = "$konvdir/bcp2meta.pl";
 
 print "### $pool: Erweiterung um Zugriffsinformation online\n";
 
-system("cd $rootdir/data/$pool ; cat meta.title | $rootdir/filter/$pool/add-fields.pl > meta.title.tmp ; mv -f meta.title.tmp meta.title");
+system("cd $rootdir/data/$pool ; cat meta.title | $rootdir/filter/$pool/add-fields.pl | $rootdir/filter/$pool/process_urls.pl > meta.title.tmp ; mv -f meta.title.tmp meta.title");

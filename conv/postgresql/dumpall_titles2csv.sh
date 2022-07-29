defcce6184c9731ca0d2ef258230c20aae30aa5b
Oliver Flimm <flimm@sigtrap.de>
2022-07-29 08:14:42 +0200

    - Erweiterung um Dump des USB-Katalogs fuer opendata.ub.uni-koeln.de

--- conv/postgresql/dumpall_titles2csv.sh
+++ conv/postgresql/dumpall_titles2csv.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 export exportbasepath="/exports/aperol/www/documents/export"
+export localdatabasepath="/opt/openbib/autoconv/pools"
 
 for i in `/opt/openbib/bin/show_active_pools.pl|sort`
 do
@@ -9,3 +10,8 @@ do
      /opt/openbib/conv/dump_titles2csv.pl --database=$i --outputfile="$exportbasepath/$i/$i.csv" --logfile=/tmp/titles2csv.log
   fi
 done
+
+cd $localdatabasepath/$i/
+rm inst001.csv.gz
+/opt/openbib/conv/dump_titles2csv.pl --database=inst001 --outputfile="$localdatabasepath/inst001/inst001.csv" --logfile=/tmp/titles2csv.log
+gzip inst001.csv

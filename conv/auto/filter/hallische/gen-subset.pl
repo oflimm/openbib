f0896ffe11b90de1f29d9f1b7762503dd63a79e2
Oliver Flimm <flimm@sigtrap.de>
2022-07-29 08:48:02 +0200

    - Korrektur

--- conv/auto/filter/hallische/gen-subset.pl
+++ conv/auto/filter/hallische/gen-subset.pl
@@ -76,7 +76,7 @@ Log::Log4perl::init(\$log4Perl_config);
 my $logger = get_logger();
 
 my $subset = new OpenBib::Catalog::Subset("inst001",$pool);
-$subset->identify_by_field_content('title',([ { field => '4700', content => '^Hallische Jahrbücher' } ]));
+$subset->identify_by_field_content('title',([ { field => '4700', content => 'Sammlung Hallische Jahrbücher' } ]));
 $subset->write_set;
 
 sub print_help {

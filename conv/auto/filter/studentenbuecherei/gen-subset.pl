2fa01140e5767cc925f84715961b29b2efce54c2
Oliver Flimm <flimm@sigtrap.de>
2022-08-23 15:10:29 +0200

    - Korrektur

--- conv/auto/filter/studentenbuecherei/gen-subset.pl
+++ conv/auto/filter/studentenbuecherei/gen-subset.pl
@@ -76,7 +76,7 @@ Log::Log4perl::init(\$log4Perl_config);
 my $logger = get_logger();
 
 my $subset = new OpenBib::Catalog::Subset("inst001",$pool);
-$subset->identify_by_field_content('title',([ { field => '4700', content => 'Sammlung Studentenbücherei' } ]));
+$subset->identify_by_field_content('title',([ { field => '4700', content => 'Sammlung Studentenb.cherei' } ]));
 $subset->write_set;
 
 sub print_help {

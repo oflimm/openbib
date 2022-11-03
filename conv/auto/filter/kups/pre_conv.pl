1dee188f2981cabc26734055b93c376163f33534
Oliver Flimm <flimm@sigtrap.de>
2022-11-03 15:12:02 +0100

    - Umstellung auf eigenen Importer fuer EPrints. Damit URL-Processing unnoetig

--- conv/auto/filter/kups/pre_conv.pl
+++ conv/auto/filter/kups/pre_conv.pl
@@ -51,4 +51,4 @@ my $bcp2metaexe   = "$konvdir/bcp2meta.pl";
 
 print "### $pool: Erweiterung um Zugriffsinformation online\n";
 
-system("cd $rootdir/data/$pool ; cat meta.title | $rootdir/filter/$pool/add-availability.pl | $rootdir/filter/$pool/add-locationid.pl | $rootdir/filter/$pool/process_urls.pl > meta.title.tmp ; mv -f meta.title.tmp meta.title");
+system("cd $rootdir/data/$pool ; cat meta.title | $rootdir/filter/$pool/add-availability.pl | $rootdir/filter/$pool/add-locationid.pl > meta.title.tmp ; mv -f meta.title.tmp meta.title");

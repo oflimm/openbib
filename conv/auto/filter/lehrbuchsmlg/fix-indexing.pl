23c9c8c9fab009e42e8266fb1619d15638ec6e39
Oliver Flimm <flimm@sigtrap.de>
2022-11-08 14:02:31 +0100

    - LBS: Signaturen auf Basis-Signatur reduzieren und numerischen Anteil vornullen

---
+++ conv/auto/filter/lehrbuchsmlg/fix-indexing.pl
@@ -0,0 +1,41 @@
+#!/usr/bin/perl
+
+use warnings;
+use strict;
+
+use JSON::XS;
+use List::MoreUtils qw{uniq};
+
+print STDERR "### lehrbuchsmlg: Korrektur Indexierung\n";
+
+while (<>){
+    my $index_ref = decode_json $_;
+
+    # Signaturen fuer die Anzeige auf Grundsignatur reduzieren und Ziffern vornullen
+
+    if (defined $index_ref->{'record'}{'X0014'}){
+	my @signaturen = ();
+
+	foreach my $item_ref (@{$index_ref->{'record'}{'X0014'}}){
+	    my $signatur = $item_ref->{content};
+
+	    if ($signatur =~m/^([A-Z][A-Z])(\d+?)\#\d+$/){ 
+		$signatur = sprintf "%s%06d",$1,$2;
+		push @signaturen, $signatur;
+	    }
+	}
+	
+	if (@signaturen){
+	    $index_ref->{'record'}{'X0014'} = [];
+	    foreach my $signatur (uniq @signaturen){
+		push @{$index_ref->{'record'}{'X0014'}}, {
+		    content => $signatur,
+		};
+	    }
+	}
+    }
+
+    
+    print encode_json $index_ref, "\n";
+}
+

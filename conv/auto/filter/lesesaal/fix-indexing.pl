8ce54406d344d56489e0a65693b04482bc81bb08
Oliver Flimm <flimm@sigtrap.de>
2022-11-09 11:19:56 +0100

    - Import-Skripte zur Anreicherung mit Lesesaal-Systematikgruppen in 0351 sowie vornullen des numerischen Signaturteils zur Sortierung

---
+++ conv/auto/filter/lesesaal/fix-indexing.pl
@@ -0,0 +1,41 @@
+#!/usr/bin/perl
+
+use warnings;
+use strict;
+
+use JSON::XS;
+use List::MoreUtils qw{uniq};
+
+print STDERR "### lesesaal: Korrektur Indexierung\n";
+
+while (<>){
+    my $index_ref = decode_json $_;
+
+    # Signaturen fuer die Anzeige auf Grundsignatur reduzieren und Ziffern vornullen
+
+    if (defined $index_ref->{'record'}{'X0014'}){
+	my @signaturen = ();
+
+	foreach my $item_ref (@{$index_ref->{'record'}{'X0014'}}){
+	    my $signatur = $item_ref->{content};
+
+	    if ($signatur =~m{^LS/([A-Z]+)(\d+?)(.*)$}){ 
+		$signatur = sprintf "LS/%s%06d%s",$1,$2,$3;
+		push @signaturen, $signatur;
+	    }
+	}
+	
+	if (@signaturen){
+	    $index_ref->{'record'}{'X0014'} = [];
+	    foreach my $signatur (uniq @signaturen){
+		push @{$index_ref->{'record'}{'X0014'}}, {
+		    content => $signatur,
+		};
+	    }
+	}
+    }
+
+    
+    print encode_json $index_ref, "\n";
+}
+

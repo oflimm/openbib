c9c77aa1e827e2929632f8575d9a6932f03d644f
Oliver Flimm <flimm@sigtrap.de>
2022-12-05 13:48:13 +0100

    - Entfernung obsoleter Nutzerdaten

---
+++ updates/3_x/25-system-privacy.sql
@@ -0,0 +1,11 @@
+alter table userinfo drop column if exists strasse;
+alter table userinfo drop column if exists ort;
+alter table userinfo drop column if exists plz;
+alter table userinfo drop column if exists soll;
+alter table userinfo drop column if exists gut;
+alter table userinfo drop column if exists avanz;
+alter table userinfo drop column if exists branz;
+alter table userinfo drop column if exists vmanz;
+alter table userinfo drop column if exists gebdatum;
+
+

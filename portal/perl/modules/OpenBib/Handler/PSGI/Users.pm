e9e2e4d67bd0899560e301b836b7917dc4a3ba3f
Oliver Flimm <flimm@sigtrap.de>
2022-12-05 13:47:56 +0100

    - Entfernung obsoleter Nutzerdaten

--- portal/perl/modules/OpenBib/Handler/PSGI/Users.pm
+++ portal/perl/modules/OpenBib/Handler/PSGI/Users.pm
@@ -331,26 +331,6 @@ sub get_input_definition {
             encoding => 'none',
             type     => 'scalar',
         },
-	strasse => {
-            default  => '',
-            encoding => 'none',
-            type     => 'scalar',
-        },
-	ort => {
-            default  => '',
-            encoding => 'none',
-            type     => 'scalar',
-        },
-	plz => {
-            default  => '',
-            encoding => 'none',
-            type     => 'scalar',
-        },
-	gebdatum => {
-            default  => '',
-            encoding => 'none',
-            type     => 'scalar',
-        },
 	email => {
             default  => '',
             encoding => 'none',

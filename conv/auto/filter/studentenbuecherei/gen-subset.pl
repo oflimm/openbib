dbb6f012125262b6b667ef2014ec0ccdc4aba390
Oliver Flimm <flimm@sigtrap.de>
2022-12-06 11:29:25 +0100

    - Anpassung Teilkatalogbildung Studentenbuecherei

--- conv/auto/filter/studentenbuecherei/gen-subset.pl
+++ conv/auto/filter/studentenbuecherei/gen-subset.pl
@@ -35,6 +35,8 @@
 use strict;
 use warnings;
 
+use utf8;
+
 use Getopt::Long;
 use OpenBib::Catalog::Subset;
 
@@ -76,7 +78,7 @@ Log::Log4perl::init(\$log4Perl_config);
 my $logger = get_logger();
 
 my $subset = new OpenBib::Catalog::Subset("inst001",$pool);
-$subset->identify_by_field_content('title',([ { field => '4700', content => 'Sammlung Studentenb.cherei' } ]));
+$subset->identify_by_field_content('title',([ { field => '4700', content => 'Sammlung Studentenbücherei' } ]));
 $subset->write_set;
 
 sub print_help {

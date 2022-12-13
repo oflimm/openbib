8f88e6f9ce42663c87e54c621685e55d75e0d893
Oliver Flimm <flimm@sigtrap.de>
2022-12-13 14:13:54 +0100

    - CLI helper *_doccount.pl

---
+++ tools/xapian_doccount.pl
@@ -0,0 +1,11 @@
+#!/usr/bin/perl
+
+use OpenBib::Index::Factory;
+use YAML;
+
+my $database = $ARGV[0];
+
+my $index = OpenBib::Index::Factory->create_indexer({ sb => 'xapian', database => $database});
+my $count = $index->get_doccount;
+
+print $count,"\n";

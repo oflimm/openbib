8a0d7081bcf36aab8c15e09a4f4ba300b14bb25b
Oliver Flimm <flimm@sigtrap.de>
2023-01-24 15:38:15 +0100

    - Steuer-Skript fuer generischen Filter boost_relevance.pl

---
+++ conv/auto/filter/emedienkauf/pre_searchengine.pl
@@ -0,0 +1,46 @@
+#!/usr/bin/perl
+
+#####################################################################
+#
+#  post_unpack.pl
+#
+#  Bearbeitung der Suchmaschinendaten
+#
+#  Dieses File ist (C) 2023 Oliver Flimm <flimm@openbib.org>
+#
+#  Dieses Programm ist freie Software. Sie k"onnen es unter
+#  den Bedingungen der GNU General Public License, wie von der
+#  Free Software Foundation herausgegeben, weitergeben und/oder
+#  modifizieren, entweder unter Version 2 der Lizenz oder (wenn
+#  Sie es w"unschen) jeder sp"ateren Version.
+#
+#  Die Ver"offentlichung dieses Programms erfolgt in der
+#  Hoffnung, da"s es Ihnen von Nutzen sein wird, aber OHNE JEDE
+#  GEW"AHRLEISTUNG - sogar ohne die implizite Gew"ahrleistung
+#  der MARKTREIFE oder der EIGNUNG F"UR EINEN BESTIMMTEN ZWECK.
+#  Details finden Sie in der GNU General Public License.
+#
+#  Sie sollten eine Kopie der GNU General Public License zusammen
+#  mit diesem Programm erhalten haben. Falls nicht, schreiben Sie
+#  an die Free Software Foundation, Inc., 675 Mass Ave, Cambridge,
+#  MA 02139, USA.
+#
+#####################################################################   
+
+#####################################################################
+# Einladen der benoetigten Perl-Module 
+#####################################################################
+
+use OpenBib::Config;
+
+my $pool          = $ARGV[0];
+
+my $config        = new OpenBib::Config;
+
+my $rootdir       = $config->{'autoconv_dir'};
+my $datadir       = $rootdir."/data";
+
+print "### $pool: Boosting von Suchbegriffen fuer Relevance-Ranking \n";
+
+system("cd $datadir/$pool ; cat searchengine.json | $rootdir/filter/_common/boost_relevance.pl > searchengine.json.tmp ; mv -f searchengine.json.tmp searchengine.json");
+

a0733abd9ca7d5912be94ce3bdaea15413970d2f
Oliver Flimm <flimm@sigtrap.de>
2023-03-14 11:15:29 +0100

    - Korrektur Indexierung Lehrbuchsammlung: Nur Signaturen fuer die Indexierung im Feld markstring verwenden, die die Form einer LBS-Signatur haben (AB123#2), um Kollisionen mit LBS-fremden Signaturen der gleichen Signaturgruppe beim Lookup mit Signaturanfang zu vermeiden

--- conv/auto/filter/lehrbuchsmlg/fix-indexing.pl
+++ conv/auto/filter/lehrbuchsmlg/fix-indexing.pl
@@ -35,6 +35,37 @@ while (<>){
 	}
     }
 
+    # Nur Signaturen fuer die Indexierung im Feld markstring verwenden, die die Form einer LBS-Signatur haben (AB123#2), um Kollisionen mit LBS-fremden Signaturen der gleichen Signaturgruppe zu vermeiden
+
+    my $new_markstring = {};
+    
+    if (defined $index_ref->{'index'}{'markstring'}){
+	foreach my $weight (keys %{$index_ref->{'index'}{'markstring'}}){
+
+	    my @signaturen = ();
+
+	    foreach my $item_ref (@{$index_ref->{'index'}{'markstring'}{$weight}}){
+		my $signatur = $item_ref->[1] if ($item_ref->[0] eq "X0014");
+
+		if ($signatur =~m/^[A-Z][A-Z]\d+?\#\d+$/){ 
+		    
+		    push @signaturen, $item_ref;
+		}
+	    }
+
+	    if (@signaturen){
+		$new_markstring->{$weight} = \@signaturen;
+	    }
+	}
+    }
+    
+    if  (keys %$new_markstring){
+	$index_ref->{'index'}{'markstring'} = $new_markstring;
+    }
+    else {
+	$index_ref->{'index'}{'markstring'} = {};
+	delete $index_ref->{'index'}{'markstring'};
+    }
     
     print encode_json $index_ref, "\n";
 }

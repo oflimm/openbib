777ca7550d13c575e521d148e2b5f829d29df96c
Oliver Flimm <flimm@sigtrap.de>
2023-03-15 12:51:01 +0100

    - Erweiterung um Metrikerstellung fuer LS Systematik

--- conv/auto/filter/lesesaal/post_cleanup.pl
+++ conv/auto/filter/lesesaal/post_cleanup.pl
@@ -2,4 +2,9 @@
 
 my $pool=$ARGV[0];
 
+print "### $pool: Metriken erstellen und cachen\n";
+system("/opt/openbib/bin/gen_metrics.pl --database=$pool --type=15");
+
+print "### $pool: Daten als CSV exportieren\n";
 system("/opt/openbib/conv/dump_titles2csv.pl --database=$pool --outputfile=/opt/openbib/autoconv/pools/$pool/${pool}.csv");
+

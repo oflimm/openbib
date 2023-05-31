870dd2e978ee1d8c258b321bb1a883aa2e7ea725
Oliver Flimm <flimm@sigtrap.de>
2023-05-31 11:09:17 +0200

    - Erweiterung: URL-Processing sowie Medientyp-Analyse

--- conv/auto/filter/uni/pre_conv.pl
+++ conv/auto/filter/uni/pre_conv.pl
@@ -50,9 +50,13 @@ my $wgetexe       = "/usr/bin/wget -nH --cut-dirs=3";
 my $bcp2metaexe   = "$konvdir/bcp2meta.pl";
 
 
-print "### $pool: Erweiterung um Standortinformationen\n";
+print "### $pool: Erweiterung um Zugriffsinformation online, Typ Digital und Themengebiet \n";
 
-system("cd $datadir/$pool ; cat meta.title | $rootdir/filter/$pool/fix-linkage.pl | $rootdir/filter/$pool/add-locationid.pl > meta.title.tmp ; mv -f meta.title.tmp meta.title");
+system("cd $datadir/$pool ; cat meta.title | $rootdir/filter/$pool/add-fields.pl > meta.title.tmp ; mv -f meta.title.tmp meta.title");
+
+print "### $pool: Erweiterung um Standortinformationen, weiteres Processing\n";
+
+system("cd $datadir/$pool ; cat meta.title | $rootdir/filter/$pool/fix-linkage.pl | $rootdir/filter/$pool/add-locationid.pl | $rootdir/filter/$pool/process_urls.pl > meta.title.tmp ; mv -f meta.title.tmp meta.title");
 
 #print "### $pool: Entfernen aller nicht GND-Fremdnummern sowie des (DE-588) GND-Prefixes \n";
 

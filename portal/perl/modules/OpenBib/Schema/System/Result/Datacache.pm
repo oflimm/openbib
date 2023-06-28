da61a1a41939594855e08239099d1cfcb46b3f27
Oliver Flimm <flimm@sigtrap.de>
2023-06-28 15:27:01 +0200

    - Datacache-Tabelle soll perspektivisch von der Statistik-DB in die System-DB migriert werden

---
+++ portal/perl/modules/OpenBib/Schema/System/Result/Datacache.pm
@@ -0,0 +1,72 @@
+use utf8;
+package OpenBib::Schema::System::Result::Datacache;
+
+# Created by DBIx::Class::Schema::Loader
+# DO NOT MODIFY THE FIRST PART OF THIS FILE
+
+=head1 NAME
+
+OpenBib::Schema::System::Result::Datacache
+
+=cut
+
+use strict;
+use warnings;
+
+use base 'DBIx::Class::Core';
+
+=head1 TABLE: C<datacache>
+
+=cut
+
+__PACKAGE__->table("datacache");
+
+=head1 ACCESSORS
+
+=head2 id
+
+  data_type: 'text'
+  is_nullable: 1
+
+=head2 tstamp
+
+  data_type: 'timestamp'
+  is_nullable: 1
+
+=head2 type
+
+  data_type: 'integer'
+  is_nullable: 1
+
+=head2 subkey
+
+  data_type: 'text'
+  is_nullable: 1
+
+=head2 data
+
+  data_type: 'text'
+  is_nullable: 1
+
+=cut
+
+__PACKAGE__->add_columns(
+  "id",
+  { data_type => "text", is_nullable => 1 },
+  "tstamp",
+  { data_type => "timestamp", is_nullable => 1 },
+  "type",
+  { data_type => "integer", is_nullable => 1 },
+  "subkey",
+  { data_type => "text", is_nullable => 1 },
+  "data",
+  { data_type => "text", is_nullable => 1 },
+);
+
+
+# Created by DBIx::Class::Schema::Loader v0.07049 @ 2023-06-28 15:25:29
+# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:xu7hkDSrEb3B9bpVkRj2Qw
+
+
+# You can replace this text with custom code or comments, and it will be preserved on regeneration
+1;

2c8f7f6e3919b9d0fb5a4f91f0ee56b735378c70
Oliver Flimm <flimm@sigtrap.de>
2023-08-31 08:50:20 +0200

    - Anpassung usbls

--- conv/auto/filter/usbls/post_cleanup.pl
+++ conv/auto/filter/usbls/post_cleanup.pl
@@ -3,5 +3,5 @@
 my $pool=$ARGV[0];
 
 print "### $pool: Metriken erstellen und cachen\n";
-system("/opt/openbib/bin/gen_metrics.pl --database=$pool --type=16 --scheme=marc21");
+system("/opt/openbib/bin/gen_metrics.pl --database=$pool --type=15 --scheme=marc21");
 

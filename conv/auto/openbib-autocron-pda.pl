74d3c729b5e76fc1d472b65d798bb25a7cdd7a5a
Oliver Flimm <flimm@sigtrap.de>
2023-09-25 13:16:57 +0200

    - Cronjob-Skript zur Aktualisierung aller fuer PDA relevanten Kataloge

---
+++ conv/auto/openbib-autocron-pda.pl
@@ -0,0 +1,151 @@
+#!/usr/bin/perl
+
+#####################################################################
+#
+#  openbib-autocron.pl
+#
+#  CRON-Job zum automatischen aktualisieren aller OpenBib-Datenbanken
+#
+#  Dieses File ist (C) 1997-2021 Oliver Flimm <flimm@openbib.org>
+#
+#  Dieses Programm ist freie Software. Sie koennen es unter
+#  den Bedingungen der GNU General Public License, wie von der
+#  Free Software Foundation herausgegeben, weitergeben und/oder
+#  modifizieren, entweder unter Version 2 der Lizenz oder (wenn
+#  Sie es wuenschen) jeder spaeteren Version.
+#
+#  Die Veroeffentlichung dieses Programms erfolgt in der
+#  Hoffnung, dass es Ihnen von Nutzen sein wird, aber OHNE JEDE
+#  GEWAEHRLEISTUNG - sogar ohne die implizite Gewaehrleistung
+#  der MARKTREIFE oder der EIGNUNG FUER EINEN BESTIMMTEN ZWECK.
+#  Details finden Sie in der GNU General Public License.
+#
+#  Sie sollten eine Kopie der GNU General Public License zusammen
+#  mit diesem Programm erhalten haben. Falls nicht, schreiben Sie
+#  an die Free Software Foundation, Inc., 675 Mass Ave, Cambridge,
+#  MA 02139, USA.
+#
+#####################################################################
+
+use 5.008001;
+use utf8;
+use strict;
+use warnings;
+use threads;
+use threads::shared;
+
+use Getopt::Long;
+use Log::Log4perl qw(get_logger :levels);
+use OpenBib::Config;
+
+our ($logfile,$loglevel,$test,$cluster,$maintenance,$updatemaster,$incremental);
+
+&GetOptions(
+    "cluster"       => \$cluster,
+    "test"          => \$test,
+    "maintenance"   => \$maintenance,
+    "incremental"   => \$incremental,
+    "logfile=s"     => \$logfile,
+    "loglevel=s"    => \$loglevel,
+    "update-master" => \$updatemaster,
+    );
+
+my $config = OpenBib::Config->new;
+
+$logfile=($logfile)?$logfile:"/var/log/openbib/openbib-autocron-pda.log";
+$loglevel=($loglevel)?$loglevel:"INFO";
+
+my $log4Perl_config = << "L4PCONF";
+log4perl.rootLogger=$loglevel, LOGFILE, Screen
+log4perl.appender.LOGFILE=Log::Log4perl::Appender::File
+log4perl.appender.LOGFILE.filename=$logfile
+log4perl.appender.LOGFILE.mode=append
+log4perl.appender.LOGFILE.layout=Log::Log4perl::Layout::PatternLayout
+log4perl.appender.LOGFILE.layout.ConversionPattern=%d [%c]: %m%n
+log4perl.appender.Screen=Log::Dispatch::Screen
+log4perl.appender.Screen.layout=Log::Log4perl::Layout::PatternLayout
+log4perl.appender.Screen.layout.ConversionPattern=%d [%c]: %m%n
+L4PCONF
+
+Log::Log4perl::init(\$log4Perl_config);
+
+# Log4perl logger erzeugen
+my $logger = get_logger();
+
+$logger->info("###### Starting PDA update");
+
+autoconvert({ updatemaster => $updatemaster, sync => 1, databases => ['bestellungen','dreierpda','vubpda','schweitzerpda','roemkepda'] });
+
+$logger->info("###### Updating done");
+
+sub autoconvert {
+    my ($arg_ref) = @_;
+
+    my @ac_cmd = ();
+    
+    # Set defaults
+    my $denylist_ref   = exists $arg_ref->{denylist}
+        ? $arg_ref->{denylist}             : {};
+
+    my $databases_ref   = exists $arg_ref->{databases}
+        ? $arg_ref->{databases}             : [];
+
+    my $sync            = exists $arg_ref->{sync}
+        ? $arg_ref->{sync}                  : 0;
+
+    my $incremental     = exists $arg_ref->{incremental}
+        ? $arg_ref->{incremental}           : 0;
+
+    my $genmex          = exists $arg_ref->{genmex}
+        ? $arg_ref->{genmex}                : 0;
+
+    my $autoconv        = exists $arg_ref->{autoconv}
+        ? $arg_ref->{autoconv}              : 0;
+
+    my $updatemaster    = exists $arg_ref->{updatemaster}
+        ? $arg_ref->{updatemaster}          : 0;
+
+    my $nosearchengine  = exists $arg_ref->{nosearchengine}
+        ? $arg_ref->{nosearchengine}        : 0;
+    
+    # Log4perl logger erzeugen
+    my $logger = get_logger();
+
+
+    push @ac_cmd, "/opt/openbib/autoconv/bin/autoconv.pl";
+    push @ac_cmd, "-sync"    if ($sync); 
+    push @ac_cmd, "-gen-mex" if ($genmex);
+    push @ac_cmd, "-incremental" if ($incremental);
+    push @ac_cmd, "-update-master" if ($updatemaster);
+    push @ac_cmd, "-no-searchengine" if ($nosearchengine);
+
+    my $ac_cmd_base = join(' ',@ac_cmd);
+
+    my @databases = ();
+
+    if (@$databases_ref){
+        push @databases, @$databases_ref;
+    }
+
+    if ($autoconv){
+        my $dbinfo = $config->get_databaseinfo->search(
+            {
+                'autoconvert' => 1,
+            },
+            {
+                order_by => 'dbname',
+            }
+        );
+        foreach my $item ($dbinfo->all){
+            push @databases, $item->dbname;
+        }
+    }
+  
+    foreach my $database (@databases){
+        my $this_cmd = "$ac_cmd_base --database=$database";
+        $logger->info("Konvertierung von $database");
+        $logger->info("Ausfuehrung von $this_cmd");
+        system($this_cmd);
+
+    }
+}

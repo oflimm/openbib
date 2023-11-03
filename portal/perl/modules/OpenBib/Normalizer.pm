cebe08055ef3cc8405da7c5e7cd7b00bc08c01bf
Oliver Flimm <flimm@sigtrap.de>
2023-11-03 10:13:29 +0100

    - Indexierung: Punctuation am Feld-Ende entfernen wg. MARC21 Absonderlichkeiten z.B Punkt in 245$a ('Der Staat.') bei String-Indexierung

--- portal/perl/modules/OpenBib/Normalizer.pm
+++ portal/perl/modules/OpenBib/Normalizer.pm
@@ -1245,6 +1245,9 @@ sub normalize {
     else {
         if ($type eq 'string'){
             $logger->debug("Processing Type $type");
+	    # Punctuation am Feld-Ende entfernen (MARC21 Absonderlichkeit z.B Punkt in 245$a ('Der Staat.')
+	    
+	    $content=~s/\p{XPosixPunct}*$//;
             # Ausfiltern nicht akzeptierter Zeichen (Positivliste)
             # * wird fuer die Indexierung auf _ normiert
             $content=~s/[^\p{Alphabetic}0-9]/_/g;

072be07c282ae2dacdede21edd0ce7cba3c5774f
Oliver Flimm <flimm@sigtrap.de>
2024-01-09 13:36:58 +0100

    - Aenderung: Merklistenversand in User-Bereich verlagert

--- portal/perl/modules/OpenBib/Handler/PSGI/Users/CartItems.pm
+++ portal/perl/modules/OpenBib/Handler/PSGI/Users/CartItems.pm
@@ -34,8 +34,16 @@ use warnings;
 no warnings 'redefine';
 use utf8;
 
+use URI::Escape;
+
 use DBI;
-use Encode 'decode_utf8';
+use Data::Pageset;
+use Email::Valid;
+use Encode qw/decode_utf8 encode_utf8/;
+use Email::Stuffer;
+use File::Slurper qw/read_binary read_text/;
+use HTML::Entities qw/decode_entities/;
+use HTML::Escape qw/escape_html/;
 use Log::Log4perl qw(get_logger :levels);
 use POSIX;
 
@@ -142,6 +150,263 @@ sub get_items_in_collection {
     my $queryoptions = $self->param('qopts');
     
     return $user->get_items_in_collection({view => $view, queryoptions => $queryoptions });
+}
+
+sub mail_collection {
+    my $self = shift;
+
+    # Log4perl logger erzeugen
+    my $logger = get_logger();
+
+    # Dispatched Args
+    my $view           = $self->param('view');
+    my $database       = $self->param('database');
+    my $id             = $self->strip_suffix($self->decode_id($self->param('titleid')));
+
+    # Shared Args
+    my $query          = $self->query();
+    my $r              = $self->param('r');
+    my $config         = $self->param('config');    
+    my $session        = $self->param('session');
+    my $user           = $self->param('user');
+    my $msg            = $self->param('msg');
+    my $queryoptions   = $self->param('qopts');
+    my $stylesheet     = $self->param('stylesheet');    
+    my $useragent      = $self->param('useragent');
+    my $path_prefix    = $self->param('path_prefix');
+
+    # CGI Args
+    my $format                  = $query->param('format')                || '';
+
+    # Ab hier ist in $user->{ID} entweder die gueltige Userid oder nichts, wenn
+    # die Session nicht authentifiziert ist
+
+    $logger->info("SessionID: $session->{ID}");
+
+    my $username=$user->get_username();
+    
+    my $recordlist = new OpenBib::RecordList::Title();
+    
+    if ($id && $database) {
+        $recordlist->add(new OpenBib::Record::Title({ database => $database , id => $id}));
+	$recordlist->load_full_records;
+    }
+    else {
+        $recordlist = $self->get_items_in_collection()
+    }
+
+    my $total_count = $self->get_number_of_items_in_collection();
+    
+    my $nav = Data::Pageset->new({
+        'total_entries'    => $total_count,
+        'entries_per_page' => $queryoptions->get_option('num'),
+        'current_page'     => $queryoptions->get_option('page'),
+        'mode'             => 'slide',
+    });
+    
+    # TT-Data erzeugen
+    my $ttdata={
+        qopts       => $queryoptions->get_options,				
+        format      => $format,
+
+        username    => $username,
+        titleid     => $id,
+        database    => $database,
+
+	hits        => $total_count,
+	nav         => $nav,	
+        recordlist  => $recordlist,
+
+	highlightquery    => \&highlightquery,
+	sort_circulation => \&sort_circulation,
+	
+    };
+    
+    return $self->print_page($config->{tt_cartitems_mail_tname},$ttdata);
+}
+
+sub mail_collection_send {
+    my $self = shift;
+
+    # Log4perl logger erzeugen
+    my $logger = get_logger();
+
+    # Dispatched Args
+    my $view           = $self->param('view')           || '';
+    my $database       = $self->param('database');
+    my $id             = $self->strip_suffix($self->decode_id($self->param('titleid')));
+
+    # Shared Args
+    my $query          = $self->query();
+    my $r              = $self->param('r');
+    my $config         = $self->param('config');    
+    my $session        = $self->param('session');
+    my $user           = $self->param('user');
+    my $msg            = $self->param('msg');
+    my $queryoptions   = $self->param('qopts');
+    my $stylesheet     = $self->param('stylesheet');    
+    my $useragent      = $self->param('useragent');
+    my $path_prefix    = $self->param('path_prefix');
+   
+    # CGI Args
+    my $email     = ($query->param('email'))?$query->param('email'):'';
+    my $subject   = ($query->param('subject'))?$query->param('subject'):'Ihre Merkliste';
+    $id           = $query->param('id');
+    my $mail      = $query->param('mail');
+    $database     = $query->param('db');
+    my $format    = $query->param('format')||'full';
+
+    # Ab hier ist in $user->{ID} entweder die gueltige Userid oder nichts, wenn
+    # die Session nicht authentifiziert ist
+    if ($email eq "") {
+        return $self->print_warning($msg->maketext("Sie haben keine Mailadresse eingegeben."));
+    }
+
+    unless (Email::Valid->address($email)) {
+        return $self->print_warning($msg->maketext("Sie haben eine ungültige Mailadresse eingegeben."));
+    }	
+
+    my $sysprofile= $config->get_profilename_of_view($view);
+
+    my $recordlist = new OpenBib::RecordList::Title();
+    
+    if ($id && $database) {
+        $recordlist->add(new OpenBib::Record::Title({ database => $database , id => $id}));
+	$recordlist->load_full_records;
+    }
+    else {
+        $recordlist = $self->get_items_in_collection();
+    }
+
+    my $total_count = $self->get_number_of_items_in_collection();
+    
+    my $nav = Data::Pageset->new({
+        'total_entries'    => $total_count,
+        'entries_per_page' => $queryoptions->get_option('num'),
+        'current_page'     => $queryoptions->get_option('page'),
+        'mode'             => 'slide',
+    });
+    
+    # TT-Data erzeugen
+    
+    my $ttdata={
+        view        => $view,
+        sysprofile  => $sysprofile,
+        stylesheet  => $stylesheet,
+        sessionID   => $session->{ID},
+	qopts       => $queryoptions->get_options,
+        format      => $format,
+
+	hits        => $total_count,
+	nav         => $nav,		
+        recordlist  => $recordlist,
+        
+        config      => $config,
+        user        => $user,
+        msg         => $msg,
+
+	highlightquery    => \&highlightquery,
+	sort_circulation => \&sort_circulation,
+	
+    };
+
+    my $maildata="";
+    my $ofile="merkliste-" . $$ .".txt";
+
+    my $datatemplate = Template->new({
+        LOAD_TEMPLATES => [ OpenBib::Template::Provider->new({
+            INCLUDE_PATH   => $config->{tt_include_path},
+            ABSOLUTE       => 1,
+  	    ENCODING     => 'utf8',	    
+        }) ],
+        #        ABSOLUTE      => 1,
+        #        INCLUDE_PATH  => $config->{tt_include_path},
+        # Es ist wesentlich, dass OUTPUT* hier und nicht im
+        # Template::Provider definiert wird
+        RECURSION      => 1,
+        OUTPUT_PATH   => '/tmp',
+        OUTPUT        => $ofile,
+    });
+  
+
+    my $mimetype="text/html";
+    my $filename="merkliste";
+    my $datatemplatename=$config->{tt_cartitems_mail_html_tname};
+
+    $logger->debug("Using view $view in profile $sysprofile");
+    
+    if ($format eq "short" || $format eq "full") {
+        $filename.=".html";
+    }
+    else {
+        $mimetype="text/plain";
+        $filename.=".txt";
+        $datatemplatename=$config->{tt_cartitems_mail_plain_tname};
+    }
+
+    $datatemplatename = OpenBib::Common::Util::get_cascaded_templatepath({
+        view         => $ttdata->{view},
+        profile      => $ttdata->{sysprofile},
+        templatename => $datatemplatename,
+    });
+
+    $logger->debug("Using database/view specific Template $datatemplatename");
+    
+    $datatemplate->process($datatemplatename, $ttdata, undef, {binmode => ':utf8'}) || do {
+        $logger->error($datatemplate->error());
+        $self->header_add('Status',400); # server error
+        return;
+    };
+  
+    my $afile = "an." . $$;
+
+    my $mainttdata = {
+	msg => $msg,
+	
+    };
+
+    my $maintemplate = Template->new({
+        LOAD_TEMPLATES => [ OpenBib::Template::Provider->new({
+            INCLUDE_PATH   => $config->{tt_include_path},
+            ABSOLUTE       => 1,
+        }) ],
+        #        ABSOLUTE      => 1,
+        #        INCLUDE_PATH  => $config->{tt_include_path},
+        # Es ist wesentlich, dass OUTPUT* hier und nicht im
+        # Template::Provider definiert wird
+        RECURSION      => 1,
+	ENCODING     => 'utf8',
+        OUTPUT_PATH   => '/tmp',
+        OUTPUT        => $afile,
+    });
+
+    my $messagetemplatename = $config->{tt_cartitems_mail_message_tname};
+    
+    $messagetemplatename = OpenBib::Common::Util::get_cascaded_templatepath({
+        view         => $ttdata->{view},
+        profile      => $ttdata->{sysprofile},
+        templatename => $messagetemplatename,
+    });
+
+    $logger->debug("Using database/view specific Template $messagetemplatename");
+
+    $maintemplate->process($messagetemplatename, $mainttdata ) || do { 
+    };
+    
+    my $anschfile="/tmp/" . $afile;
+    my $mailfile ="/tmp/" . $ofile;
+    
+    Email::Stuffer->to($email)
+	->from($config->{contact_email})
+	->subject($subject)
+	->text_body(read_binary($anschfile))
+	->attach_file($mailfile)
+	->send;
+
+    unlink $anschfile;
+    unlink $mailfile;
+
+    return $self->print_page($config->{tt_cartitems_mail_success_tname},$ttdata);
 }
 
 sub return_baseurl {

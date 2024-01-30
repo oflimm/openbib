4c052970cdf11f2e21419480016e395ee64ab8ea
Oliver Flimm <flimm@sigtrap.de>
2024-01-30 14:05:03 +0100

    - uni: Aktivierung Anreicherung mit navid beim Import

--- conv/auto/filter/uni/pre_conv.pl
+++ conv/auto/filter/uni/pre_conv.pl
@@ -54,13 +54,6 @@ print "### $pool: Erweiterung um Standortinformationen, weiteres Processing\n";
 
 system("cd $datadir/$pool ; cat meta.title | $rootdir/filter/$pool/remove_duplicates_in_nz.pl | $rootdir/filter/$pool/remove_ill.pl | $rootdir/filter/$pool/fix-linkage.pl | $rootdir/filter/$pool/add-locationid.pl | $rootdir/filter/$pool/gen_local_topic.pl | $rootdir/filter/$pool/process_urls.pl | $rootdir/filter/$pool/process_ids.pl | $rootdir/filter/$pool/volume2year.pl | $rootdir/filter/$pool/process_provenances.pl  > meta.title.tmp ; mv -f meta.title.tmp meta.title");
 
-#print "### $pool: Entfernen aller nicht GND-Fremdnummern sowie des (DE-588) GND-Prefixes \n";
+print "### $pool: Anreicherung der Exemplarinformationen\n";
 
-#system("cd $datadir/$pool ; cat meta.person | $rootdir/filter/$pool/fix-gnd.pl > meta.person.tmp ; mv -f meta.person.tmp meta.person");
-#system("cd $datadir/$pool ; cat meta.corporatebody | $rootdir/filter/$pool/fix-gnd.pl > meta.corporatebody.tmp ; mv -f meta.corporatebody.tmp meta.corporatebody");
-#system("cd $datadir/$pool ; cat meta.subject | $rootdir/filter/$pool/flag_discriminatory_subjects.pl | $rootdir/filter/$pool/fix-gnd.pl > meta.subject.tmp ; mv -f meta.subject.tmp meta.subject");
-#system("cd $datadir/$pool ; cat meta.classification | $rootdir/filter/$pool/fix-gnd.pl > meta.classification.tmp ; mv -f meta.classification.tmp meta.classification");
-
-#print "### $pool: Korrektur der Exemplarinformationen\n";
-
-#system("cd $datadir/$pool ; cat meta.holding| $rootdir/filter/$pool/fix-holding.pl > meta.holding.tmp ; mv -f meta.holding.tmp meta.holding");
+system("cd $datadir/$pool ; cat meta.holding| $rootdir/filter/$pool/add-navid.pl > meta.holding.tmp ; mv -f meta.holding.tmp meta.holding");

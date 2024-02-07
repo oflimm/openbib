2ae0fe5fb62f71c12c27b02bfa8f92ea55949cda
Oliver Flimm <flimm@sigtrap.de>
2024-02-07 09:57:06 +0100

    - Korrektur

--- conv/auto/filter/tmpebooks/add-fields.pl
+++ conv/auto/filter/tmpebooks/add-fields.pl
@@ -5,38 +5,7 @@ use List::MoreUtils qw/ uniq /;
 
 while (<>){
     my $title_ref = decode_json $_;
-
-    my @rvks = ();
     
-    # RVK aus 084$a in 4101 vereinheitlichen
-    
-    # Auswertung von 084$
-    if (defined $title_ref->{fields}{'0084'}){
-	my $cln_ref = {};
-        foreach my $item (@{$title_ref->{fields}{'0084'}}){
-	    $cln_ref->{$item->{mult}}{$item->{subfield}} = $item->{content};
-	}
-	
-	foreach my $mult (keys %{$cln_ref}){
-	    next unless (defined $cln_ref->{$mult}{'2'} && defined $cln_ref->{$mult}{'a'});
-	    if ($cln_ref->{$mult}{'2'} eq "rvk"){
-		push @rvks, $cln_ref->{$mult}{'a'};
-	    }
-	}
-    }
-        
-    if (@rvks){
-	my $rvk_mult = 1;
-
-	foreach my $rvk (uniq @rvks){
-	    push @{$title_ref->{fields}{'4101'}}, {
-		mult     => $rvk_mult++,
-		subfield => '',
-		content  => $rvk,
-	    };
-	}
-    }
-
     ### Medientyp Digital/online zusaetzlich vergeben        
     $title_ref->{fields}{'4400'} = [
         {

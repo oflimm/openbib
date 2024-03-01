55a4150c36dd79f4c3111d44ab9ce9d4e5b65b62
Oliver Flimm <flimm@sigtrap.de>
2024-03-01 08:30:26 +0100

    - Korrektur

--- conv/auto/filter/tmpebooks/exclude_from_wikipage.pl
+++ conv/auto/filter/tmpebooks/exclude_from_wikipage.pl
@@ -61,7 +61,7 @@ while (<>){
 
     my $exclude_title = 0;
     foreach my $isbn_ref (@{$title_ref->{fields}{'0020'}}){
-	if (isbn_ref->{subfield} eq "a" && defined $excluded_isbns{$isbn_ref->{content}}){
+	if ($isbn_ref->{subfield} eq "a" && defined $excluded_isbns{$isbn_ref->{content}}){
 	    $exclude_title = $isbn_ref->{content}; 
 	    last;
 	} 

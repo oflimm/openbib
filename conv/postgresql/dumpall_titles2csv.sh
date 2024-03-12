ea6e08785acc05cc8ef06ec7f43ab11ad2346a08
Oliver Flimm <flimm@sigtrap.de>
2024-03-12 09:36:59 +0100

    - Erweiterung: CSV-Export inaktiver Kataloge

--- conv/postgresql/dumpall_titles2csv.sh
+++ conv/postgresql/dumpall_titles2csv.sh
@@ -10,3 +10,11 @@ do
      /opt/git/openbib-current/conv/postgresql/fulldump_titles2csv.pl --database=$i --configfile=/opt/git/openbib-current/conv/postgresql/conf/default.yml --outputfile="$exportbasepath/$i/$i.csv" --logfile=/tmp/titles2csv.log
   fi
 done
+
+for i in inst104 inst120 inst155 inst158 inst159 inst136 inst170 inst171 inst202 inst220 inst229 inst307 inst327 inst402 inst509 inst519
+do
+  if [ -d "$exportbasepath/$i" ]
+  then
+     /opt/git/openbib-current/conv/postgresql/fulldump_titles2csv.pl --database=$i --configfile=/opt/git/openbib-current/conv/postgresql/conf/default.yml --outputfile="$exportbasepath/$i/$i.csv" --logfile=/tmp/titles2csv.log
+  fi
+done

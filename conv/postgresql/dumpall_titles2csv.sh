9f7a04e6a2def2ee8d0a3118e9703e97b8501e11
Oliver Flimm <flimm@sigtrap.de>
2024-03-12 09:17:18 +0100

    - Umstellung CSV-Dumps auf Full-Dump

--- conv/postgresql/dumpall_titles2csv.sh
+++ conv/postgresql/dumpall_titles2csv.sh
@@ -3,15 +3,10 @@
 export exportbasepath="/exports/aperol/www/documents/export"
 export localdatabasepath="/opt/openbib/autoconv/pools"
 
-for i in `/opt/openbib/bin/show_active_pools.pl|sort`
+for i in `/opt/openbib/bin/show_active_pools.pl|egrep '^inst[0-9][0-9][0-9]$'|egrep -v '(inst001|inst900)'|sort`
 do
   if [ -d "$exportbasepath/$i" ]
   then
-     /opt/openbib/conv/dump_titles2csv.pl --database=$i --outputfile="$exportbasepath/$i/$i.csv" --logfile=/tmp/titles2csv.log
+     /opt/openbib/conv/fulldump_titles2csv.pl --database=$i --configfile=/opt/git/openbib-current/conv/postgresql/conf/default.yml --outputfile="$exportbasepath/$i/$i.csv" --logfile=/tmp/titles2csv.log
   fi
 done
-
-cd $localdatabasepath/$i/
-rm inst001.csv.gz
-/opt/openbib/conv/dump_titles2csv.pl --database=inst001 --outputfile="$localdatabasepath/inst001/inst001.csv" --logfile=/tmp/titles2csv.log
-gzip inst001.csv

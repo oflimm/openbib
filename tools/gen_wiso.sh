4ff6661895414edd6a268d630cada396c094360e
Oliver Flimm <flimm@sigtrap.de>
2024-05-06 16:05:32 +0200

    - Erweiterung: Entfernung der NZ-Titel

--- tools/gen_wiso.sh
+++ tools/gen_wiso.sh
@@ -3,3 +3,8 @@
 cd /var/www.opendata/dumps/misc/
 /opt/openbib/bin/get_titleid_by_subjectarea.pl --area=wiwi --excludelabel=wi
 /opt/openbib/bin/get_titleid_by_subjectarea.pl --area=sowi --excludelabel=so
+
+cat wiwi.txt | egrep -v '6441$' > wiwi.txt.tmp
+cat sowi.txt | egrep -v '6441$' > sowi.txt.tmp
+mv -f wiwi.txt.tmp wiwi.txt
+mv -f sowi.txt.tmp sowi.txt

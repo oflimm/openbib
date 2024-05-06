985fc99c2599209fb47288aaee361773d7c7203a
Oliver Flimm <flimm@sigtrap.de>
2024-05-06 14:40:53 +0200

    - Korrektur

--- tools/gen_wiso.sh
+++ tools/gen_wiso.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 cd /var/www.opendata/dumps/misc/
-/opt/openbib/bin/get_titleid_by_subjectarea.pl --area=wiwi --excludelabel=so
-/opt/openbib/bin/get_titleid_by_subjectarea.pl --area=sowi --excludelabel=wi
+/opt/openbib/bin/get_titleid_by_subjectarea.pl --area=wiwi --excludelabel=wi
+/opt/openbib/bin/get_titleid_by_subjectarea.pl --area=sowi --excludelabel=so

1de3b22cdca87a3ec70ab44cb655ba74e43a9e00
Oliver Flimm <flimm@sigtrap.de>
2024-06-12 10:22:30 +0200

    - Teilkatalogbildung Klosterbibliotheken

---
+++ conv/auto/filter/kloster/alt_remote.pl
@@ -0,0 +1,44 @@
+#!/usr/bin/perl
+
+#####################################################################
+#
+#  alt_remote.pl
+#
+#  Dieses File ist (C) 2005-2006 Oliver Flimm <flimm@openbib.org>
+#
+#  Dieses Programm ist freie Software. Sie koennen es unter
+#  den Bedingungen der GNU General Public License, wie von der
+#  Free Software Foundation herausgegeben, weitergeben und/oder
+#  modifizieren, entweder unter Version 2 der Lizenz oder (wenn
+#  Sie es wuenschen) jeder spaeteren Version.
+#
+#  Die Veroeffentlichung dieses Programms erfolgt in der
+#  Hoffnung, dass es Ihnen von Nutzen sein wird, aber OHNE JEDE
+#  GEWAEHRLEISTUNG - sogar ohne die implizite Gewaehrleistung
+#  der MARKTREIFE oder der EIGNUNG FUER EINEN BESTIMMTEN ZWECK.
+#  Details finden Sie in der GNU General Public License.
+#
+#  Sie sollten eine Kopie der GNU General Public License zusammen
+#  mit diesem Programm erhalten haben. Falls nicht, schreiben Sie
+#  an die Free Software Foundation, Inc., 675 Mass Ave, Cambridge,
+#  MA 02139, USA.
+#
+#####################################################################
+
+#####################################################################
+# Einladen der benoetigten Perl-Module
+#####################################################################
+
+use OpenBib::Config;
+
+my $config = OpenBib::Config->new;
+
+my $rootdir       = $config->{'autoconv_dir'};
+my $pooldir       = $rootdir."/pools";
+my $konvdir       = $config->{'conv_dir'};
+
+my $pool          = $ARGV[0];
+
+print "### $pool: Extrahiere die Daten aus uni\n";
+
+system("$rootdir/filter/$pool/gen-subset.pl $pool");

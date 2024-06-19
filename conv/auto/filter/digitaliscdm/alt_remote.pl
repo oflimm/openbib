3c1e7eaa42f58931f40466c71d3a5875dba55e84
Oliver Flimm <flimm@sigtrap.de>
2024-06-19 09:45:19 +0200

    - Erweiterung

---
+++ conv/auto/filter/digitaliscdm/alt_remote.pl
@@ -0,0 +1,48 @@
+#!/usr/bin/perl
+
+#####################################################################
+#
+#  alt_remote.pl
+#
+#  Konvertieren in das Meta-Format
+#
+#  Dieses File ist (C) 2003-2011 Oliver Flimm <flimm@openbib.org>
+#
+#  Dieses Programm ist freie Software. Sie koennen es unter
+#  den Bedingungen der GNU General Public License, wie von der
+#  Free Software Foundation herausgegeben, weitergeben und/oder
+#  modifizieren, entweder unter Version 2 der Lizenz oder (wenn
+#  Sie es wuenschen) jeder spaeteren Version.
+#
+#  Die Veroeffentlichung dieses Programms erfolgt in der
+#  Hoffnung, dass es Ihnen von Nutzen sein wird, aber OHNE JEDE
+#  GEWAEHRLEISTUNG - sogar ohne die implizite Gewaehrleistung
+#  der MARKTREIFE oder der EIGNUNG FUER EINEN BESTIMMTEN ZWECK.
+#  Details finden Sie in der GNU General Public License.
+#
+#  Sie sollten eine Kopie der GNU General Public License zusammen
+#  mit diesem Programm erhalten haben. Falls nicht, schreiben Sie
+#  an die Free Software Foundation, Inc., 675 Mass Ave, Cambridge,
+#  MA 02139, USA.
+#
+#####################################################################   
+
+#####################################################################
+# Einladen der benoetigten Perl-Module 
+#####################################################################
+
+use DBI;
+use OpenBib::Config;
+
+my $config = new OpenBib::Config();
+
+my $rootdir       = $config->{'autoconv_dir'};
+my $pooldir       = $rootdir."/pools";
+my $konvdir       = $config->{'conv_dir'};
+
+my $simplexml2metaexe = "$config->{'conv_dir'}/simplexml2meta.pl";
+
+my $pool          = $ARGV[0];
+
+system("cd $pooldir/$pool; rm meta.*");
+system("cd $pooldir/$pool; $simplexml2metaexe --inputfile=digitalis.xml --configfile=/opt/openbib/conf/${pool}.yml; gzip meta.*");

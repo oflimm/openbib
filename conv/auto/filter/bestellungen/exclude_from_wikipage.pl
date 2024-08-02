0c1678401acf096167509cd8fc2756cbdae769d3
Oliver Flimm <flimm@sigtrap.de>
2024-08-02 12:01:45 +0200

    - Erweiterung: Aufiltern von Titeln anhand ISNBS aus Wikipage fuer Bestellungen

--- conv/auto/filter/bestellungen/exclude_from_wikipage.pl
+++ conv/auto/filter/bestellungen/exclude_from_wikipage.pl
@@ -7,18 +7,35 @@ use utf8;
 use MediaWiki::API;
 use JSON::XS qw/decode_json encode_json/;
 
-my $mw = MediaWiki::API->new( {api_url => 'https://intern.ub.uni-koeln.de/usbwiki/api.php' } );
-
-my $page = $mw->get_page( { title => 'Bestellungen - Exkludierte Titel' } );
-
-my $page_content = $page->{'*'};
-
 my %excluded_ids = ();
+my %excluded_isbns = ();
 
-foreach my $line (split /\n/, $page_content){
-    $line=~s/^\s*//g;
-    $line=~s/\s*$//g;
-    $excluded_ids{$line} = 1;
+eval {
+    my $mw = MediaWiki::API->new( {api_url => 'https://intern.ub.uni-koeln.de/usbwiki/api.php' } );
+    
+    my $page = $mw->get_page( { title => 'Bestellungen - Exkludierte Titel' } );
+    
+    my $page_content = $page->{'*'};
+
+    foreach my $line (split /\n/, $page_content){
+	$line=~s/^\s*//g;
+	$line=~s/\s*$//g;
+	$excluded_ids{$line} = 1;
+    }
+
+    $page = $mw->get_page( { title => 'Bestellungen - Exkludierte ISBNS' } );
+    
+    $page_content = $page->{'*'};
+
+    foreach my $line (split /\n/, $page_content){
+	$line=~s/^\s*//g;
+	$line=~s/\s*$//g;
+	$excluded_isbns{$line} = 1;
+    }
+};
+
+if ($@){
+    print STDERR $@,"\n";
 }
 
 while (<>){
@@ -38,5 +55,25 @@ while (<>){
         next;
     }
 
+    my $exclude_title = 0;
+    foreach my $isbn_ref (@{$title_ref->{fields}{'0020'}}){
+	if ($isbn_ref->{subfield} eq "a"){	    
+	    my $isbn = $isbn_ref->{content};
+	    $isbn =~s/^\s+//;	    
+	    $isbn =~s/-//g;
+	    $isbn =~s/^([0-9Xx]+)\s+.+$/$1/;
+
+	    if (defined $excluded_isbns{$isbn} && $excluded_isbns{$isbn}){
+		$exclude_title = $isbn;
+		last;
+	    }
+	} 
+    }
+
+    if ($exclude_title){
+        print STDERR "Titel mit ISBN $exclude_title excluded\n";
+        next;
+    }
+    
     print;
 }

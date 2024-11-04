17ebf996fed37333370cd86a8d77f9b128df77d0
Oliver Flimm <flimm@sigtrap.de>
2024-11-04 10:12:53 +0100

    - jki: Umstellung Import auf MARC21-Internformat

---
+++ conv/auto/filter/jki/add-fields.pl
@@ -0,0 +1,53 @@
+#!/usr/bin/perl
+
+use JSON::XS;
+use List::MoreUtils qw/ uniq /;
+
+while (<>){
+    my $title_ref = decode_json $_;
+
+    # Anreichern von 1008 $a mit vierstelliger Jahreszahl aus 260/264 $c, wenn 1008 $a nicht mit Jahreszahl besetzt.
+
+    my $year_from_26x = "";
+
+    if (defined $title_ref->{fields}{'0260'}){
+	foreach my $item_ref (@{$title_ref->{fields}{'0260'}}){
+	    if ($item_ref->{subfield} eq 'c' && $item_ref->{content} =~m/(\d\d\d\d)/){
+		$year_from_26x = $1;
+		last;
+	    }
+	}
+    }
+
+    if (!$year_from_26x && defined $title_ref->{fields}{'0264'}){
+	foreach my $item_ref (@{$title_ref->{fields}{'0264'}}){
+	    if ($item_ref->{subfield} eq 'c' && $item_ref->{content} =~m/(\d\d\d\d)/){
+		$year_from_26x = $1;
+		last;
+	    }
+	}
+    }
+
+    my $field_1008_has_a = 0;
+    
+    if ($year_from_26x && defined $title_ref->{fields}{'1008'}){
+	foreach my $item_ref (@{$title_ref->{fields}{'1008'}}){
+	    if ($item_ref->{subfield} eq 'a'){
+		$field_1008_has_a = 1;
+		if ($item_ref->{content} !~m/\d\d\d\d/){
+		    $item_ref->{content} = $year_from_26x;
+		}
+	    }
+	}
+    }
+
+    if ($year_from_26x && (!$field_1008_has_a || !defined $title_ref->{fields}{'1008'})){
+	push @{$title_ref->{fields}{'1008'}}, {
+	    mult => 1,
+	    subfield => 'a',
+	    content => $year_from_26x,	    
+	};
+    }
+
+    print encode_json $title_ref, "\n";
+}

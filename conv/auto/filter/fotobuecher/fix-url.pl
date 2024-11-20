7ea31eda0d989acaa8dcf00198d0363c19b1adc0
Oliver Flimm <flimm@sigtrap.de>
2024-11-20 09:57:06 +0100

    - Korrektur

---
+++ conv/auto/filter/fotobuecher/fix-url.pl
@@ -0,0 +1,18 @@
+#!/usr/bin/perl
+
+use JSON::XS;
+
+while (<>){
+    my $title_ref = decode_json $_;
+
+    my $id = $title_ref->{id};
+
+    if (defined $title_ref->{fields}{'4120'}){
+        foreach my $item_ref (@{$title_ref->{fields}{'4120'}}){
+            $item_ref->{content}=~s{http://www.ub.uni-koeln.de}{https://services.ub.uni-koeln.de};
+        }
+    }
+    
+    print encode_json $title_ref, "\n";
+}
+

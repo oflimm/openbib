78b5449a755145c24f2754d3fca92882a5821943
Oliver Flimm <flimm@sigtrap.de>
2024-11-21 07:47:24 +0100

    - fotobuecher: Anpassung thumbnail URL

--- conv/auto/filter/fotobuecher/fix-url.pl
+++ conv/auto/filter/fotobuecher/fix-url.pl
@@ -12,7 +12,13 @@ while (<>){
             $item_ref->{content}=~s{http://www.ub.uni-koeln.de}{https://services.ub.uni-koeln.de};
         }
     }
-    
+
+    if (defined $title_ref->{fields}{'2662'}){
+        foreach my $item_ref (@{$title_ref->{fields}{'2662'}}){
+            $item_ref->{content}=~s{http://www.ub.uni-koeln.de}{https://services.ub.uni-koeln.de};
+        }
+    }
+
     print encode_json $title_ref, "\n";
 }
 

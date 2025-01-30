7760f635f2e3cdd1e834bbb76312b8aaf492f410
Oliver Flimm <flimm@sigtrap.de>
2025-01-30 10:43:20 +0100

    - Anpassung

--- conv/auto/filter/oersi/add-locationid.pl
+++ conv/auto/filter/oersi/add-locationid.pl
@@ -5,14 +5,14 @@ use strict;
 
 use JSON::XS;
 
-print STDERR "### emedienkauf Erweitere Titeldaten\n";
+print STDERR "### oersi Erweitere Titeldaten\n";
 
 while (<>){
     my $title_ref = decode_json $_;
 
+    push @{$title_ref->{'locations'}}, "freemedia";    
     push @{$title_ref->{'locations'}}, "emedien";
     push @{$title_ref->{'locations'}}, "usbebooks";
-#    push @{$title_ref->{'locations'}}, "DE-38-USBFB";
     
     print encode_json $title_ref, "\n";
 }

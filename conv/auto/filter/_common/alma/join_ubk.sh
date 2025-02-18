2494dd79af0ac9d9fc099075127eb26caf71f8a7
Oliver Flimm <flimm@sigtrap.de>
2025-02-18 15:08:48 +0100

    - Korrektur

--- conv/auto/filter/_common/alma/join_ubk.sh
+++ conv/auto/filter/_common/alma/join_ubk.sh
@@ -6,7 +6,7 @@ mmv "ubkfull_*_new_?.mrc" "ubkfull_#1_new_0#2.mrc"
 
 cat /dev/null > /opt/openbib/autoconv/pools/uni/pool.mrc
 
-for thisdate in `ls -1 ubkfull_*.mrc ubkelectronicfull_*.mrc|cut -d_ -f 2|sort -ru`;do
+for thisdate in `ls -1 ubkfull_*.mrc ubkelectronicfull_*.mrc|cut -d_ -f 2|sort -ru|xargs`;do
  echo "Joining ${thisdate}"
  cat `ls -1 ubkfull_${thisdate}_*delete*.mrc|xargs` >> /opt/openbib/autoconv/pools/uni/pool.mrc
  cat `ls -1 ubkfull_${thisdate}_*new*.mrc|xargs` >> /opt/openbib/autoconv/pools/uni/pool.mrc

124f40fe20db9db565ed515f35b1b60836483368
Oliver Flimm <flimm@sigtrap.de>
2025-03-28 13:41:21 +0100

    - Erweiterung: Angereicherte IIIF-DOIs sind immer online/Digital

--- conv/auto/filter/_common/alma/add-iiifdoi.pl
+++ conv/auto/filter/_common/alma/add-iiifdoi.pl
@@ -85,7 +85,25 @@ while (<>){
 	    mult     => 1,
 	    subfield => 'g',
 	    content  => $url,
-	}];	
+					}];
+
+	# Medientyp und Verfuegbarkeit setzen
+
+	$title_ref->{fields}{'4400'} = [
+	    {
+		mult     => 1,
+		subfield => '',
+		content  => "online",
+	    },
+	    ];
+	
+	$title_ref->{fields}{'4410'} = [
+	    {
+		mult     => 1,
+		subfield => '',
+		content  => "Digital",
+	    },
+	    ];
     }
    
     print encode_json $title_ref, "\n";

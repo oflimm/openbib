47cacb70fa0ca4cd9c2a80d4a22c3c5ff3733c72
Oliver Flimm <flimm@sigtrap.de>
2025-04-08 07:58:53 +0200

    - Korrektur

--- conv/auto/filter/jesuiten/process_collection.pl
+++ conv/auto/filter/jesuiten/process_collection.pl
@@ -14,28 +14,26 @@ while (<>){
     my $titleid = $title_ref->{id};
 
     # Signaturlose Historische Systematik in 4314$b vereinigen
-    foreach my $field ('4314'){
-	if (defined $title_ref->{fields}{$field}){
-	    foreach my $item_ref (@{$title_ref->{fields}{$field}}){
-		my $content = $item_ref->{'content'};
-		
-		if ($content =~m/^(.+?)\s*\;.+?$/ || $content =~m/^(.+?)\s*[A-Z][A-Z]*?\d+$/){
-		    push @{$title_ref->{fields}{'4314'}}, {
-			content  => $1,
-			subfield => 'b',
-			mult     => $item_ref->{'mult'},
-		    };
-		}
-		else {
-		    push @{$title_ref->{fields}{'4314'}}, {
-			content  => $content,
-			subfield => 'b',
-			mult     => $item_ref->{'mult'},
-		    };
-		}
+    if (defined $title_ref->{fields}{'4314'}){
+	foreach my $item_ref (@{$title_ref->{fields}{'4314'}}){
+	    my $content = $item_ref->{'content'};
+	    
+	    if ($content =~m/^(.+?)\s*\;.+?$/ || $content =~m/^(.+?)\s*[A-Z][A-Z]*?\d+$/){
+		push @{$title_ref->{fields}{'4314'}}, {
+		    content  => $1,
+		    subfield => 'b',
+		    mult     => $item_ref->{'mult'},
+		};
+	    }
+	    else {
+		push @{$title_ref->{fields}{'4314'}}, {
+		    content  => $content,
+		    subfield => 'b',
+		    mult     => $item_ref->{'mult'},
+		};
 	    }
 	}
-    }    
+    }
 
     print encode_json $title_ref, "\n";
 }

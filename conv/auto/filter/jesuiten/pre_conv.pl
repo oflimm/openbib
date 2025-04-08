d2f9ad1afbda9a7ad4ffaeacc73beeefb9b64dfd
Oliver Flimm <flimm@sigtrap.de>
2025-04-08 08:15:22 +0200

    - Korrektur

--- conv/auto/filter/jesuiten/pre_conv.pl
+++ conv/auto/filter/jesuiten/pre_conv.pl
@@ -60,7 +60,11 @@ system("cd $datadir/$pool ; cat meta.title | $rootdir/filter/_common/alma/add-lo
 
 print "### $pool: Weiteres Processing - Stage 3\n";
 
-system("cd $datadir/$pool ; cat meta.title | $rootdir/filter/_common/alma/gen_local_topic.pl | $rootdir/filter/_common/alma/process_urls.pl | $rootdir/filter/_common/alma/process_ids.pl | $rootdir/filter/_common/alma/volume2year.pl | $rootdir/filter/_common/alma/process_provenances.pl | $rootdir/filter/$pool/process_collection.pl | $rootdir/filter/$pool/restrict_jesuiten.pl > meta.title.tmp ; mv -f meta.title.tmp meta.title");
+system("cd $datadir/$pool ; cat meta.title | $rootdir/filter/_common/alma/gen_local_topic.pl | $rootdir/filter/_common/alma/process_urls.pl | $rootdir/filter/_common/alma/process_ids.pl | $rootdir/filter/_common/alma/volume2year.pl | $rootdir/filter/_common/alma/process_provenances.pl > meta.title.tmp ; mv -f meta.title.tmp meta.title");
+
+print "### $pool: Sammlungsspezifisches Processing - Stage 4\n";
+
+system("cd $datadir/$pool ; cat meta.title | $rootdir/filter/$pool/process_collection.pl | $rootdir/filter/$pool/restrict_jesuiten.pl > meta.title.tmp ; mv -f meta.title.tmp meta.title");
 
 print "### $pool: Anreicherung der Exemplarinformationen\n";
 

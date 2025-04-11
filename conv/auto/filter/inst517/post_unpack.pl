1c8cff5714d15e4a2b7c5621d98754d5b90139fe
Oliver Flimm <flimm@sigtrap.de>
2025-04-11 12:26:41 +0200

    - Erweiterung Reduzierung via Sciebo fuer inst517

---
+++ conv/auto/filter/inst517/post_unpack.pl
@@ -0,0 +1,47 @@
+#!/usr/bin/perl
+
+#####################################################################
+#
+#  post_unpack.pl
+#
+#  Bearbeitung der Titeldaten
+#
+#  Dieses File ist (C) 2005-2011 Oliver Flimm <flimm@openbib.org>
+#
+#  Dieses Programm ist freie Software. Sie k"onnen es unter
+#  den Bedingungen der GNU General Public License, wie von der
+#  Free Software Foundation herausgegeben, weitergeben und/oder
+#  modifizieren, entweder unter Version 2 der Lizenz oder (wenn
+#  Sie es w"unschen) jeder sp"ateren Version.
+#
+#  Die Ver"offentlichung dieses Programms erfolgt in der
+#  Hoffnung, da"s es Ihnen von Nutzen sein wird, aber OHNE JEDE
+#  GEW"AHRLEISTUNG - sogar ohne die implizite Gew"ahrleistung
+#  der MARKTREIFE oder der EIGNUNG F"UR EINEN BESTIMMTEN ZWECK.
+#  Details finden Sie in der GNU General Public License.
+#
+#  Sie sollten eine Kopie der GNU General Public License zusammen
+#  mit diesem Programm erhalten haben. Falls nicht, schreiben Sie
+#  an die Free Software Foundation, Inc., 675 Mass Ave, Cambridge,
+#  MA 02139, USA.
+#
+#####################################################################   
+
+#####################################################################
+# Einladen der benoetigten Perl-Module 
+#####################################################################
+
+use OpenBib::Config;
+
+my $pool          = $ARGV[0];
+
+my $config        = new OpenBib::Config;
+
+my $rootdir       = $config->{'autoconv_dir'};
+my $pooldir       = $rootdir."/pools";
+my $datadir       = $rootdir."/data";
+
+print "### $pool: Entfernung retro katalogisierter Titel \n";
+
+system("cd $datadir/$pool ; $rootdir/filter/$pool/exclude_from_sciebo.pl $pool");
+

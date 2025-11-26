723a6230d6bd3bdfdb7b3e0497600a6d2e3c1425
Oliver Flimm <flimm@sigtrap.de>
2025-11-26 10:03:32 +0100

    - Importparametrisierung fuer Transformation Working Paper Series der NEC aus OJS

---
+++ conv/auto/filter/ojs_nec/add-fields.pl
@@ -0,0 +1,27 @@
+#!/usr/bin/perl
+
+use JSON::XS;
+use List::MoreUtils qw/ uniq /;
+
+while (<>){
+    my $title_ref = decode_json $_;
+
+    ### Medientyp Digital/online zusaetzlich vergeben    
+    $title_ref->{fields}{'4400'} = [
+        {
+            mult     => 1,
+            subfield => '',
+            content  => "online",
+        },
+    ];
+
+    $title_ref->{fields}{'4410'} = [
+        {
+            mult     => 1,
+            subfield => '',
+            content  => "Digital",
+        },
+    ];
+
+    print encode_json $title_ref, "\n";
+}

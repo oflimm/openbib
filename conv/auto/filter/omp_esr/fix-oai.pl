16c68499589e3c60e6e914e85b930d0340fcf964
Oliver Flimm <flimm@sigtrap.de>
2025-11-26 08:39:13 +0100

    - Korrektur OAI-Fix

--- conv/auto/filter/omp_esr/fix-oai.pl
+++ conv/auto/filter/omp_esr/fix-oai.pl
@@ -8,6 +8,7 @@ while (<>){
 
     # Doppelte Feldinhalte entfernen
     my $have_content_ref = {};
+    my $mult_ref = {};
     foreach my $field (keys %{$title_ref->{fields}}){
 	my $new_field = [];
 	foreach my $item_ref (@{$title_ref->{fields}{$field}}){
@@ -24,45 +25,68 @@ while (<>){
 	my $new_field = [];
 	foreach my $item_ref (@{$title_ref->{fields}{'0540'}}){
 	    if ($item_ref->{content} =~m/^http/){
+		my $mult = ++$multcount_ref->{'0662'};
 		push @{$title_ref->{fields}{'0662'}}, {
 		    content => $item_ref->{content},
-		    mult => 1,
-		    subfield => '',
+		    mult => $mult,
+		    subfield => 'g',
 		};
 		push @{$title_ref->{fields}{'0663'}}, {
-		    content => $item_ref->{content},
-		    mult => 1,
+		    content => 'Volltext',
+		    mult => $mult,
 		    subfield => '',
 		};		
 		push @{$title_ref->{fields}{'4120'}}, {
 		    content => $item_ref->{content},
-		    mult => 1,
-		    subfield => '',
+		    mult => $mult,
+		    subfield => 'g',
 		};
 	    }
-	    elsif ($item_ref->{content} =~m/10.18716/){ # DOI
+	    elsif ($item_ref->{content} =~m/10\.18716/){ # DOI
+		my $mult = ++$multcount_ref->{'0552'};		
 		push @{$title_ref->{fields}{'0552'}}, {
 		    content => $item_ref->{content},
-		    mult => 1,
+		    mult => $mult,
 		    subfield => '',
 		};
 	    }
 	    elsif ($item_ref->{content} =~m/^urn/){ # URN
+		my $mult = ++$multcount_ref->{'0552'};		
 		push @{$title_ref->{fields}{'0552'}}, {
 		    content => $item_ref->{content},
 		    mult => 1,
 		    subfield => '',
 		};
 	    }
-	    else {
+	    # 13-Stellige ISBN
+	    elsif (/\d-*\d-*\d-*\d-*\d-*\d-*\d-*\d-*\d-*\d-*\d-*\d-*[0-9xX]/){
 		push @$new_field, $item_ref;
 	    }
+	    # 10-Stellige ISBN	    
+	    elsif (/\d-*\d-*\d-*\d-*\d-*\d-*\d-*\d-*\d-*[0-9xX]/){
+		push @$new_field, $item_ref;
+	    }
+	    # ISSN
+	    elsif (/\d\d\d\d-*\d\d\d[0-9xX]/){
+		my $mult = ++$multcount_ref->{'0543'};
+		push @{$title_ref->{fields}{'0543'}}, {
+		    content => $item_ref->{content},
+		    mult => $mult,
+		    subfield => '',
+		};
+	    }
 	}
+
+	# Wenn ISBN, dann diese setzen
 	if (@$new_field){
 	    $title_ref->{fields}{'0540'} = $new_field;
 	}
+	# Sonst ISBN-Feld entfernen
+	else {
+	    $title_ref->{fields}{'0540'} = [];
+	    delete $title_ref->{fields}{'0540'};
+	}
     }
-
     
     print encode_json $title_ref, "\n";
 }
